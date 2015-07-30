719870a9a2725db5051272c67862669caf80f914
qianyizh <qianyizh@qianyizh-DESK.amr.corp.intel.com>
2015-07-30 09:48:38 -0700

    Minor edits

---
+++ src/IO/Config.h
@@ -0,0 +1,3 @@
+#pragma once
+
+#define DEFAULT_IO_BUFFER_SIZE 1024
--- src/IO/FileXYZ.cpp
+++ src/IO/FileXYZ.cpp
@@ -1,3 +1,4 @@
+#include "Config.h"
 #include "PointCloudIO.h"
 
 namespace three{
@@ -6,14 +7,42 @@ bool ReadPointCloudFromXYZ(
 		const std::string &filename,
 		PointCloud &pointcloud)
 {
-	return false;
+	FILE *file = fopen(filename.c_str(), "r");
+	if (file != NULL) {
+		char line_buffer[DEFAULT_IO_BUFFER_SIZE];
+		double x, y, z;
+
+		while (fgets(line_buffer, DEFAULT_IO_BUFFER_SIZE, file)) {
+			if (sscanf(line_buffer, "%ld %ld %ld", &x, &y, &z) == 3) {
+				pointcloud.points_.push_back(Eigen::Vector3d(x, y, z));
+			}
+		}
+
+		fclose(file);
+		return true;
+	} else { // if (file != NULL)
+		return false;
+	}
 }
 
 bool WritePointCloudToXYZ(
 		const std::string &filename,
 		const PointCloud &pointcloud)
 {
-	return false;
+	FILE *file = fopen(filename.c_str(), "w");
+	if (file != NULL) {
+		for (size_t i = 0; i < pointcloud.points_.size(); i++) {
+			const Eigen::Vector3d &point = pointcloud.points_[i];
+			if (fprintf(file, "%.10f %.10f %.10f\n", point(0), point(1), point(2)) < 0) {
+				return false;	// error happens during writting.
+			}
+		}
+
+		fclose(file);
+		return true;
+	} else {	// if (file != NULL)
+		return false;
+	}
 }
 
 }	// namespace three
