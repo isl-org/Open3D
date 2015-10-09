d889dc3a5bab937dde0fb64e52ef9ebe408d5d59
qianyizh <qianyizh@qianyizh-DESK.amr.corp.intel.com>
2015-10-09 16:06:24 -0700

    Add ViewTrajectory for better view control.

---
+++ src/Visualization/ViewTrajectory.h
@@ -0,0 +1,52 @@
+// ----------------------------------------------------------------------------
+// -                       Open3DV: www.open3dv.org                           -
+// ----------------------------------------------------------------------------
+// The MIT License (MIT)
+//
+// Copyright (c) 2015 Qianyi Zhou <Qianyi.Zhou@gmail.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+// ----------------------------------------------------------------------------
+
+#pragma once
+
+#include <vector>
+#include <Eigen/Core>
+
+namespace three {
+
+class ViewTrajectory {
+public:
+	struct ViewStatus {
+	public:
+		double field_of_view;
+		double zoom;
+		Eigen::Vector3d lookat;
+		Eigen::Vector3d up;
+		Eigen::Vector3d front;
+	};
+
+public:
+	void InterpolateViewStatus(int interval = 30, bool close_loop = false);
+
+public:
+	std::vector<ViewStatus> view_status_;
+};
+
+}	// namespace three
