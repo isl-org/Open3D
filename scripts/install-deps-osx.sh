626fbaa4014f660425c0ea1d3279e70957772fff
Qianyi Zhou <Qianyi.Zhou@gmail.com>
2016-04-06 23:05:30 -0700

    Update script for OS X

---
+++ scripts/install-deps-osx.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+which -s brew
+if [[ $? != 0 ]] ; then
+    echo "Install Homebrew."
+    echo "Please follow the instructions on http://brew.sh/"
+    echo "After installation, run this script again."
+else
+    echo "Homebrew Detected."
+    echo "Performing update ..."
+    sudo brew update
+fi
+
+for pkg in cmake libusb; do
+    if brew list -1 | grep -q "^${pkg}\$"; then
+        echo "Package '$pkg' has already been installed."
+    else
+        echo "Package '$pkg' is being installed ..."
+        sudo brew install $pkg
+    fi
+done
