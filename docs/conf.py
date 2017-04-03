ea608f809cdafcb1893f85ac6419c8a97c661812
Qianyi Zhou <Qianyi.Zhou@gmail.com>
2017-04-03 14:41:58 -0700

    :lipstick:

--- docs/conf.py
+++ docs/conf.py
@@ -20,6 +20,11 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+import sphinx_rtd_theme
+
+html_theme = "sphinx_rtd_theme"
+
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # -- General configuration ------------------------------------------------
 
@@ -82,7 +87,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+#html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -151,6 +156,3 @@ texinfo_documents = [
      author, 'Open3D', 'One line description of project.',
      'Miscellaneous'),
 ]
-
-
-
