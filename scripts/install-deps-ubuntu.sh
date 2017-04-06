535623cd7ed4c786f85f8a990184a5aa2354d98a
Qianyi Zhou <Qianyi.Zhou@gmail.com>
2017-04-06 13:51:41 -0700

    Native eigen

--- scripts/install-deps-ubuntu.sh
+++ scripts/install-deps-ubuntu.sh
@@ -3,4 +3,4 @@
 set -ev
 
 # install OpenGL, for GLFW
-sudo apt-get install xorg-dev libglu1-mesa-dev libgl1-mesa-glx libglew-dev libglfw3-dev libjsoncpp-dev
+sudo apt-get install xorg-dev libglu1-mesa-dev libgl1-mesa-glx libglew-dev libglfw3-dev libjsoncpp-dev libeigen3-dev
