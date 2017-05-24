42b4e5d890d936e9ec7341153307252f5749162b
Jaesik Park <syncle@gmail.com>
2017-05-23 17:05:34 -0700

    helper functions for odometry

---
+++ src/Experimental/OdometryRGBD/OdometryRGBD.cpp
@@ -0,0 +1,64 @@
+// ----------------------------------------------------------------------------
+// -                        Open3D: www.open3d.org                            -
+// ----------------------------------------------------------------------------
+// The MIT License (MIT)
+//
+// Copyright (c) 2015 Qianyi Zhou <Qianyi.Zhou@gmail.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+// ----------------------------------------------------------------------------
+
+#include <iostream>
+#include <memory>
+
+#include <Core/Core.h>
+#include <IO/IO.h>
+
+#include "Odometry.h"
+
+void PrintHelp()
+{
+	printf("Usage:\n");
+	printf("    > RGBDOdometry [options]\n");
+	printf("      From RGBD stream estimate 6D odometry.\n");
+	printf("\n");
+	// will be added
+}
+
+int main(int argc, char *argv[])
+{
+	using namespace three;
+
+	if (argc <= 1 || ProgramOptionExists(argc, argv, "--help") ||
+			ProgramOptionExists(argc, argv, "-h")) {
+		PrintHelp();
+		return 0;
+	}
+
+	//////////////////////
+	// will be added
+	// read color and depth image
+	Image depth, depth_f, color;
+	std::string dir_name, st;
+	ReadImage(dir_name + st[0], depth);
+	ReadImage(dir_name + st[1], color);
+	//////////////////////
+
+	return 1;
+}
