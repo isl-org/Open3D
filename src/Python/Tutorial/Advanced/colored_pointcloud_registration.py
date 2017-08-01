b8f58570c5154402dff28d7cdf8d8067f362a383
Jaesik Park <syncle@gmail.com>
2017-08-01 16:23:47 -0700

    python example

--- src/Python/Tutorial/Advanced/colored_pointcloud_registration.py
+++ src/Python/Tutorial/Advanced/colored_pointcloud_registration.py
@@ -20,42 +20,38 @@ def DrawRegistrationResultOriginalColor(source, target, transformation):
 if __name__ == "__main__":
 
 	print("1. Load two point clouds.")
-	source = ReadPointCloud("../../TestData/ICP/cloud_bin_0.pcd")
-	target = ReadPointCloud("../../TestData/ICP/cloud_bin_1.pcd")
+	source = ReadPointCloud("../../TestData/frag_115.ply")
+	target = ReadPointCloud("../../TestData/frag_116.ply")
 
-	print("2. Downsample with a voxel size 0.05.")
-	source_down = VoxelDownSample(source, 0.05)
-	target_down = VoxelDownSample(target, 0.05)
+	voxel_radius = [ 0.04, 0.02, 0.01 ];
+	max_iter = [ 50, 30, 14 ];
+	current = []
+	current_transformation = np.identity(4);
 
-	print("3. Estimate normal with search radius 0.1.")
-	EstimateNormals(source_down, KDTreeSearchParamHybrid(
-			radius = 0.1, max_nn = 30))
-	EstimateNormals(target_down, KDTreeSearchParamHybrid(
-			radius = 0.1, max_nn = 30))
+	for scale in range(3):
+		iter = max_iter[scale]
+		radius = voxel_radius[scale]
+		scale = reg_num_scale[scale]
+		print([iter,radius,scale])
 
-	print("4. Compute FPFH feature with search radius 0.25")
-	source_fpfh = ComputeFPFHFeature(source_down,
-			KDTreeSearchParamHybrid(radius = 0.25, max_nn = 100))
-	target_fpfh = ComputeFPFHFeature(target_down,
-			KDTreeSearchParamHybrid(radius = 0.25, max_nn = 100))
+		print("2. Downsample with a voxel size %.2f" % radius)
+		source_down = VoxelDownSample(source, scale)
+		target_down = VoxelDownSample(target, scale)
 
-	print("5. RANSAC registration on downsampled point clouds.")
-	print("   Since the downsampling voxel size is 0.05, we use a liberal")
-	print("   distance threshold 0.075.")
-	result_ransac = RegistrationRANSACBasedOnFeatureMatching(
-			source_down, target_down, source_fpfh, target_fpfh, 0.075,
-			TransformationEstimationPointToPoint(False), 4,
-			[CorrespondenceCheckerBasedOnEdgeLength(0.9),
-			CorrespondenceCheckerBasedOnDistance(0.075)],
-			RANSACConvergenceCriteria(40000000, 500))
-	print(result_ransac)
-	#DrawRegistrationResultUniformColor(source_down, target_down,
-	#		result_ransac.transformation)
+		print("3. Estimate normal with search radius 0.1.")
+		EstimateNormals(source_down, KDTreeSearchParamHybrid(
+				radius = scale, max_nn = 30))
+		EstimateNormals(target_down, KDTreeSearchParamHybrid(
+				radius = scale, max_nn = 30))
 
-	print("6. Colored point cloud registration is applied on original point")
-	print("   clouds to refine the alignment. This time we use a strict")
-	print("   distance threshold 0.02.")
-	result_icp = RegistrationColoredICP(source, target, 0.02,
-			result_ransac.transformation)
-	print(result_icp)
+		print("4. Colored point cloud registration is applied on original point")
+		print("   clouds to refine the alignment. This time we use a strict")
+		print("   distance threshold %.2f" % radius)
+		result_icp = RegistrationColoredICP(source, target,
+				radius, current_transformation,
+				ICPConvergenceCriteria(relative_fitness = 1e-6,
+				relative_rmse = 1e-6, max_iteration = iter))
+		current_transformation = result_icp.transformation
+		print(result_icp)
+
 	DrawRegistrationResultOriginalColor(source, target, result_icp.transformation)
