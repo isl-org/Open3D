24397496ff1a3abe12e9f61f15411b9b050b37ea
Jaesik Park <syncle@gmail.com>
2017-08-02 12:35:42 -0700

    Fix based on comments from Qian-Yi

--- src/Python/Tutorial/Advanced/colored_pointcloud_registration.py
+++ src/Python/Tutorial/Advanced/colored_pointcloud_registration.py
@@ -7,12 +7,6 @@ sys.path.append("../..")
 from py3d import *
 import numpy as np
 
-def DrawRegistrationResultUniformColor(source, target, transformation):
-	source.PaintUniformColor([1, 0.706, 0])
-	target.PaintUniformColor([0, 0.651, 0.929])
-	source.Transform(transformation)
-	DrawGeometries([source, target])
-
 def DrawRegistrationResultOriginalColor(source, target, transformation):
 	source.Transform(transformation)
 	DrawGeometries([source, target])
@@ -20,27 +14,26 @@ def DrawRegistrationResultOriginalColor(source, target, transformation):
 if __name__ == "__main__":
 
 	print("1. Load two point clouds.")
-	source = ReadPointCloud("../../TestData/frag_115.ply")
-	target = ReadPointCloud("../../TestData/frag_116.ply")
+	source = ReadPointCloud("../../TestData/ColoredICP/frag_115.ply")
+	target = ReadPointCloud("../../TestData/ColoredICP/frag_116.ply")
 
-
+	# colored pointcloud registration
+ 	# This is implementation of following paper
+ 	# J. Park, Q.-Y. Zhou, V. Koltun,
+ 	# Colored Point Cloud Registration Revisited, ICCV 2017
 	voxel_radius = [ 0.04, 0.02, 0.01 ];
-	reg_num_scale = [ 400, 3000, 20000 ];
 	max_iter = [ 50, 30, 14 ];
-	current = []
 	current_transformation = np.identity(4);
-
 	for scale in range(3):
 		iter = max_iter[scale]
 		radius = voxel_radius[scale]
-		scale = reg_num_scale[scale]
 		print([iter,radius,scale])
 
 		print("2. Downsample with a voxel size %.2f" % radius)
 		source_down = VoxelDownSample(source, scale)
 		target_down = VoxelDownSample(target, scale)
 
-		print("3. Estimate normal with search radius 0.1.")
+		print("3. Estimate normal.")
 		EstimateNormals(source_down, KDTreeSearchParamHybrid(
 				radius = scale, max_nn = 30))
 		EstimateNormals(target_down, KDTreeSearchParamHybrid(
@@ -55,4 +48,13 @@ if __name__ == "__main__":
 				relative_rmse = 1e-6, max_iteration = iter))
 		current_transformation = result_icp.transformation
 		print(result_icp)
+	DrawRegistrationResultOriginalColor(source, target, result_icp.transformation)
+
+	# point to plane ICP
+	current_transformation = np.identity(4);
+	print("5. Point-to-plane ICP registration is applied on original point")
+	print("   clouds to refine the alignment. Distance threshold 0.02.")
+	result_icp = RegistrationICP(source, target, 0.02,
+			current_transformation, TransformationEstimationPointToPlane())
+	print(result_icp)
 	DrawRegistrationResultOriginalColor(source, target, result_icp.transformation)
