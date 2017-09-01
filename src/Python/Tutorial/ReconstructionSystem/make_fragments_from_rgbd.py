9dec7ab887f0d1b6b7cf6f81e317e4fbd4942a9b
Jaesik Park <syncle@gmail.com>
2017-09-01 14:37:03 -0700

    separate python functions

--- src/Python/Tutorial/ReconstructionSystem/make_fragments_from_rgbd.py
+++ src/Python/Tutorial/ReconstructionSystem/make_fragments_from_rgbd.py
@@ -1,32 +1,15 @@
 import numpy as np
-from os import listdir, makedirs
-from os.path import isfile, join, exists
+from os import makedirs
+from os.path import exists
 import math
 import sys
 sys.path.append("../..")
 from py3d import *
 #from joblib import Parallel, delayed
+from utility import *
 
-# some global parameters
-path_dataset = '/Users/jaesikpa/Dropbox/Intel/fragment/011/'
-path_fragment = path_dataset + 'fragments/'
-n_frames_per_fragment = 100
-n_keyframes_per_n_frame = 5
-
-def initialize_opencv():
-	global opencv_installed
-	opencv_installed = True
-	try:
-		import cv2
-	except ImportError, e:
-		pass
-		print('OpenCV is not detected. Using Identity as an initial')
-		opencv_installed = False
-	if opencv_installed:
-		print('OpenCV is detected. Using ORB + 5pt algorithm')
-
-
-def process_one_rgbd_pair(s, t, color_files, depth_files):
+def process_one_rgbd_pair(s, t, color_files, depth_files,
+		intrinsic, with_opencv):
 	# read images
 	color_s = ReadImage(color_files[s])
 	depth_s = ReadImage(depth_files[s])
@@ -36,32 +19,29 @@ def process_one_rgbd_pair(s, t, color_files, depth_files):
 	target_rgbd_image = CreateRGBDImageFromColorAndDepth(color_t, depth_t)
 
 	# initialize_camera_pose
-	if abs(s-t) is not 1 and opencv_installed:
+	if abs(s-t) is not 1 and with_opencv:
 		odo_init = pose_estimation(source_rgbd_image, target_rgbd_image,
-				pinhole_camera_intrinsic, False)
+				intrinsic, False)
 	else:
 		odo_init = np.identity(4)
 
 	# perform RGB-D odometry
 	[success, trans, info] = ComputeRGBDOdometry(
-			source_rgbd_image, target_rgbd_image,
-			pinhole_camera_intrinsic, odo_init,
+			source_rgbd_image, target_rgbd_image, intrinsic, odo_init,
 			RGBDOdometryJacobianFromHybridTerm(), OdometryOption())
 	return [trans, info]
 
 
-def get_flie_lists(path_dataset):
+def get_file_lists(path_dataset):
 	# get list of color and depth images
 	path_color = path_dataset + 'image/'
 	path_depth = path_dataset + 'depth/'
-	color_files = [path_color + f for f in listdir(path_color)
-			if isfile(join(path_color, f))]
-	depth_files = [path_depth + f for f in listdir(path_depth)
-			if isfile(join(path_depth, f))]
+	color_files = get_file_list(path_color)
+	depth_files = get_file_list(path_depth)
 	return color_files, depth_files
 
 
-def make_one_fragment(fragment_id):
+def make_one_fragment(fragment_id, intrinsic, with_opencv):
 
 	SetVerbosityLevel(VerbosityLevel.Error)
 	sid = fragment_id * n_frames_per_fragment
@@ -75,27 +55,22 @@ def make_one_fragment(fragment_id):
 		for t in range(s+1, eid):
 			# odometry
 			if t is s + 1:
-				#print([s,t])
 				[trans, info] = process_one_rgbd_pair(
-						s, t, color_files, depth_files)
+						s, t, color_files, depth_files, intrinsic, with_opencv)
 				trans_odometry = np.dot(trans, trans_odometry)
 				trans_odometry_inv = np.linalg.inv(trans_odometry)
 				pose_graph.nodes.append(PoseGraphNode(trans_odometry_inv))
 				pose_graph.edges.append(
 						PoseGraphEdge(s, t, trans, info, False))
-				#print(trans)
 				print(pose_graph)
 
 			# keyframe loop closure
 			if s % n_keyframes_per_n_frame is 0 \
 					and t % n_keyframes_per_n_frame is 0:
-				#print([s,t])
 				[trans, info] = process_one_rgbd_pair(
-						s, t, color_files, depth_files)
+						s, t, color_files, depth_files, intrinsic, with_opencv)
 				pose_graph.edges.append(
 						PoseGraphEdge(s, t, trans, info, True))
-				#print(trans)
-				#print(info)
 				print(pose_graph)
 	return pose_graph
 
@@ -139,31 +114,37 @@ def integrate_rgb_frames(pose_graph_name):
 
 if __name__ == "__main__":
 
-	print(len(sys.argv))
-	if len(sys.argv) is 0:
-		print('usage: ')
-	else:
-		if sys.argv is in "test_mode":
-			print("testmode")
+	path_dataset = parse_argument(sys.argv, "--path_dataset")
+	path_intrinsic = parse_argument(sys.argv, "--path_intrinsic")
+
+	if path_dataset:
+
+		# some global parameters
+		n_frames_per_fragment = 100
+		n_keyframes_per_n_frame = 5
 
 		# check opencv python package
-		initialize_opencv()
-		if opencv_installed:
+		with_opencv = initialize_opencv()
+		if with_opencv:
 			from opencv_pose_estimation import pose_estimation
 
+		path_fragment = path_dataset + 'fragments/'
 		if not exists(path_fragment):
 			makedirs(path_fragment)
 
-		[color_files, depth_files] = get_flie_lists(path_dataset) 
+		[color_files, depth_files] = get_file_lists(path_dataset)
 		n_files = len(color_files)
 		n_fragments = int(math.ceil(n_files / n_frames_per_fragment))
 
-		pinhole_camera_intrinsic = PinholeCameraIntrinsic.PrimeSenseDefault
-
+		if path_intrinsic:
+			intrinsic = ReadPinholeCameraIntrinsic(path_intrinsic)
+		else:
+			intrinsic = PinholeCameraIntrinsic.PrimeSenseDefault
+		
 		#for fragment_id in range(n_fragments):
 		for fragment_id in range(1):
 			pose_graph_name = path_fragment + "fragments_%03d.json" % fragment_id
-			pose_graph = make_one_fragment(fragment_id)
+			pose_graph = make_one_fragment(fragment_id, intrinsic, with_opencv)
 			WritePoseGraph(pose_graph_name, pose_graph)
 			pose_graph_optmized_name = path_fragment + \
 					"fragments_opt_%03d.json" % fragment_id
--- src/Python/Tutorial/ReconstructionSystem/test_pairwise_alignment.py
+++ src/Python/Tutorial/ReconstructionSystem/test_pairwise_alignment.py
@@ -5,9 +5,12 @@ import math
 import sys
 sys.path.append("../..")
 from py3d import *
+from utility import *
+from make_fragments_from_rgbd import \
+		process_one_rgbd_pair, initialize_opencv, get_file_lists
 
 
-def test_single_frame_integrate(i):
+def test_single_frame_integrate(i, intrinsic):
 	min_depth = 0.3
 	cubic_length = 4.0
 	volume = UniformTSDFVolume(length = cubic_length, resolution = 512,
@@ -23,7 +26,7 @@ def test_single_frame_integrate(i):
 	print(depth_files[i])
 	rgbd = CreateRGBDImageFromColorAndDepth(color, depth, depth_trunc = 4.0,
 			convert_rgb_to_intensity = False)
-	volume.Integrate(rgbd, pinhole_camera_intrinsic, trans_offset)
+	volume.Integrate(rgbd, intrinsic, trans_offset)
 
 	mesh = volume.ExtractTriangleMesh()
 	mesh.ComputeVertexNormals()
@@ -32,18 +35,19 @@ def test_single_frame_integrate(i):
 	return mesh
 
 
-def test_single_pair(s, t):
+def test_single_pair(s, t, intrinsic, with_opencv):
 
 	SetVerbosityLevel(VerbosityLevel.Debug)
 
 	pose_graph = PoseGraph()
-	[trans, info] = process_one_rgbd_pair(s, t, color_files, depth_files)
+	[trans, info] = process_one_rgbd_pair(s, t,
+			color_files, depth_files, intrinsic, with_opencv)
 	pose_graph.nodes.append(PoseGraphNode(trans))
 	pose_graph.nodes.append(PoseGraphNode(np.identity(4)))
 
 	# integration
-	mesh_s = test_single_frame_integrate(s)
-	mesh_t = test_single_frame_integrate(t)
+	mesh_s = test_single_frame_integrate(s, intrinsic)
+	mesh_t = test_single_frame_integrate(t, intrinsic)
 	mesh_s.Transform(trans) # for 5pt
 	DrawGeometries([mesh_s, mesh_t])
 
@@ -51,13 +55,20 @@ def test_single_pair(s, t):
 # test wide baseline matching
 if __name__ == "__main__":
 
-	initialize_opencv()
-	if opencv_installed:
-		from opencv_pose_estimation import pose_estimation
+	path_dataset = parse_argument(sys.argv, "--path_dataset")
+	path_intrinsic = parse_argument(sys.argv, "--path_intrinsic")
+	source_id = parse_argument_int(sys.argv, "--source_id")
+	target_id = parse_argument_int(sys.argv, "--target_id")
 
-	[color_files, depth_files] = get_flie_lists(path_dataset) 
-	pinhole_camera_intrinsic = PinholeCameraIntrinsic.PrimeSenseDefault
+	if path_dataset:
+		with_opencv = initialize_opencv()
+		if with_opencv:
+			from opencv_pose_estimation import pose_estimation
 
-	s = 20
-	t =	130
-	test_single_pair(s, t)
+		[color_files, depth_files] = get_file_lists(path_dataset)
+		if path_intrinsic:
+			intrinsic = ReadPinholeCameraIntrinsic(path_intrinsic)
+		else:
+			intrinsic = PinholeCameraIntrinsic.PrimeSenseDefault
+
+		test_single_pair(source_id, target_id, intrinsic, with_opencv)
