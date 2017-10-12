aa99c97f2bd3a0704822a7cb432c4f2ad14a0543
Qianyi Zhou <Qianyi.Zhou@gmail.com>
2017-10-12 03:59:51 -0700

    fix semantic meaning

--- src/Python/Tutorial/Advanced/rgbd_image_integration.py
+++ src/Python/Tutorial/Advanced/rgbd_image_integration.py
@@ -6,19 +6,19 @@ import numpy as np
 
 if __name__ == "__main__":
     intrinsic = PinholeCameraIntrinsic.PrimeSenseDefault
-    extrinsic = read_trajectory("../../TestData/RGBD/odometry.log")
+    camera_poses = read_trajectory("../../TestData/RGBD/odometry.log")
     #volume = UniformTSDFVolume(length = 4.0, resolution = 512, sdf_trunc = 0.04,
     #        with_color = True)
     volume = ScalableTSDFVolume(voxel_length = 4.0 / 512.0, sdf_trunc = 0.04,\
             with_color = True)
 
-    for i in range(len(extrinsic)):
+    for i in range(len(camera_poses)):
         print("Integrate {:d}-th image into the volume.".format(i))
         color = ReadImage("../../TestData/RGBD/color/{:05d}.jpg".format(i))
         depth = ReadImage("../../TestData/RGBD/depth/{:05d}.png".format(i))
         rgbd = CreateRGBDImageFromColorAndDepth(color, depth, depth_trunc = 4.0,
                 convert_rgb_to_intensity = False)
-        volume.Integrate(rgbd, intrinsic, np.linalg.inv(extrinsic[i].pose))
+        volume.Integrate(rgbd, intrinsic, np.linalg.inv(camera_poses[i].pose))
 
     print("Extract a triangle mesh from the volume and visualize it.")
     mesh = volume.ExtractTriangleMesh()
