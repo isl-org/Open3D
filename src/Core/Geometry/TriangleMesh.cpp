e6c77dd80c4d9e4aa8391d92c5b5079bfcab34f0
Fangjun Kuang <csukuangfj@gmail.com>
2018-02-14 11:16:35 +0800

    Resolves #167.

--- src/Core/Geometry/TriangleMesh.cpp
+++ src/Core/Geometry/TriangleMesh.cpp
@@ -52,11 +52,11 @@ Eigen::Vector3d TriangleMesh::GetMinBound() const
 		return Eigen::Vector3d(0.0, 0.0, 0.0);
 	}
 	auto itr_x = std::min_element(vertices_.begin(), vertices_.end(),
-		[](Eigen::Vector3d a, Eigen::Vector3d b) { return a(0) < b(0); });
+		[](const Eigen::Vector3d &a, const Eigen::Vector3d &b) { return a(0) < b(0); });
 	auto itr_y = std::min_element(vertices_.begin(), vertices_.end(),
-		[](Eigen::Vector3d a, Eigen::Vector3d b) { return a(1) < b(1); });
+		[](const Eigen::Vector3d &a, const Eigen::Vector3d &b) { return a(1) < b(1); });
 	auto itr_z = std::min_element(vertices_.begin(), vertices_.end(),
-		[](Eigen::Vector3d a, Eigen::Vector3d b) { return a(2) < b(2); });
+		[](const Eigen::Vector3d &a, const Eigen::Vector3d &b) { return a(2) < b(2); });
 	return Eigen::Vector3d((*itr_x)(0), (*itr_y)(1), (*itr_z)(2));
 }
 
@@ -66,11 +66,11 @@ Eigen::Vector3d TriangleMesh::GetMaxBound() const
 		return Eigen::Vector3d(0.0, 0.0, 0.0);
 	}
 	auto itr_x = std::max_element(vertices_.begin(), vertices_.end(),
-		[](Eigen::Vector3d a, Eigen::Vector3d b) { return a(0) < b(0); });
+		[](const Eigen::Vector3d &a, const Eigen::Vector3d &b) { return a(0) < b(0); });
 	auto itr_y = std::max_element(vertices_.begin(), vertices_.end(),
-		[](Eigen::Vector3d a, Eigen::Vector3d b) { return a(1) < b(1); });
+		[](const Eigen::Vector3d &a, const Eigen::Vector3d &b) { return a(1) < b(1); });
 	auto itr_z = std::max_element(vertices_.begin(), vertices_.end(),
-		[](Eigen::Vector3d a, Eigen::Vector3d b) { return a(2) < b(2); });
+		[](const Eigen::Vector3d &a, const Eigen::Vector3d &b) { return a(2) < b(2); });
 	return Eigen::Vector3d((*itr_x)(0), (*itr_y)(1), (*itr_z)(2));
 }
 
