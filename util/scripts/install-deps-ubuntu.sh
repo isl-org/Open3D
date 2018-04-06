82b04d8f53122e98726d36c1662e16f02c771bfb
Jaesik Park <syncle@gmail.com>
2018-04-06 13:21:44 -0700

    minor fix

--- util/scripts/install-deps-ubuntu.sh
+++ util/scripts/install-deps-ubuntu.sh
@@ -3,4 +3,17 @@
 set -ev
 
 # install OpenGL, for GLFW
-sudo apt-get install xorg-dev libglu1-mesa-dev libgl1-mesa-glx libglew-dev libglfw3-dev libjsoncpp-dev libeigen3-dev libpng-dev libjpeg-dev python-dev python3-dev python-tk python3-tk
+sudo apt-get install \
+		xorg-dev \
+		libglu1-mesa-dev \
+		libgl1-mesa-glx \
+		libglew-dev \
+		libglfw3-dev \
+		libjsoncpp-dev \
+		libeigen3-dev \
+		libpng-dev \
+		libjpeg-dev \
+		python-dev \
+		python3-dev \
+		python-tk \
+		python3-tk
