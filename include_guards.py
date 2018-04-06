902f50a1bd75883dd57bbf1a8f00344f748c7e3d
takanokage <danut.petre@gmail.com>
2018-04-06 12:31:50 -0700

    treat a few corner cases where the '_' is not added.

--- include_guards.py
+++ include_guards.py
@@ -7,11 +7,6 @@ import glob
 def files(path, ext):
     output = glob.glob(path + '/**/*.' + ext, recursive=True)
     return output
-
-path = "/home/dpetre/Open3D/issue_278/src/Core"
-
-headers = files(path, 'h')
-
 def label(header):
     basename = os.path.basename(header)
     fileName = os.path.splitext(basename)[0]
@@ -19,8 +14,9 @@ def label(header):
     label = list()
     label.extend(list("OPEN3D"))
 
-    prvUpper = False
     upper = list()
+    prvUpper = False
+    first = True
     for c in list(fileName):
         if c.isupper():
             upper.append(c)
@@ -28,18 +24,27 @@ def label(header):
         else:
             if 1 == len(upper):
                 label.append('_')
+                first = False
                 label.extend(upper)
                 upper = list()
                 label.append(c.upper())
             elif 1 < len(upper):
                 label.append('_')
+                first = False
                 label.extend(upper[:-1])
                 label.append('_')
                 label.append(upper[-1])
                 upper = list()
                 label.append(c.upper())
             else:
+                if first:
+                    label.append('_')
+                    first = False
                 label.append(c.upper())
+
+    if 0 < len(upper):
+        label.append('_')
+        label.extend(upper)
 
     label.append('_H')
 
@@ -47,7 +52,20 @@ def label(header):
 
     return output
 
-for header in headers:
-    basename = os.path.basename(header)
-    guard = label(basename)
-    print("%-50s%s" % (basename, guard))
+paths = [
+    "/home/dpetre/Open3D/issue_278/src/Core",
+    "/home/dpetre/Open3D/issue_278/src/Experimental",
+    "/home/dpetre/Open3D/issue_278/src/IO",
+    "/home/dpetre/Open3D/issue_278/src/Python",
+    "/home/dpetre/Open3D/issue_278/src/Test",
+    "/home/dpetre/Open3D/issue_278/src/Tools",
+    "/home/dpetre/Open3D/issue_278/src/Visualization"
+]
+
+for path in paths:
+    headers = files(path, 'h')
+
+    for header in headers:
+        basename = os.path.basename(header)
+        guard = label(basename)
+        print("%-50s%s" % (guard, basename))
