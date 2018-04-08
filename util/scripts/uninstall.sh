0f4ec68abd6838cc61f4f5be7b18bd35ce4ec4d8
takanokage <danut.petre@gmail.com>
2018-04-07 20:16:26 -0700

    removed unnecessary comments.

--- util/scripts/uninstall.sh
+++ util/scripts/uninstall.sh
@@ -2,7 +2,6 @@ echo
 echo uninstalling...
 
 # remove the Open3D Python module
-# rm -rf ~/.local/lib/python3.5/site-packages/py3d.cpython-35m-x86_64-linux-gnu.so
 find $(python  -m site --user-site) -iname py3d*.so -delete
 find $(python3 -m site --user-site) -iname py3d*.so -delete
 
