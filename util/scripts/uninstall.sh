e6b5e24a70d242dd51d792fda0a05dae0fd4dfc6
takanokage <danut.petre@gmail.com>
2018-04-09 13:37:42 -0700

    echo the uninstalled path.

--- util/scripts/uninstall.sh
+++ util/scripts/uninstall.sh
@@ -1,5 +1,7 @@
 echo
 echo uninstalling...
+echo $(find $(python  -m site --user-site) -iname py3d*.so)
+echo $(find $(python3 -m site --user-site) -iname py3d*.so)
 
 # remove the Open3D Python module
 find $(python  -m site --user-site) -iname py3d*.so -delete
