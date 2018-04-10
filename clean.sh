93db67adca2c59dfd0c667ffbb7bf73a66defc52
takanokage <danut.petre@gmail.com>
2018-04-10 14:24:26 -0700

    note that in the current form the include folder structure is not preserved.

--- clean.sh
+++ clean.sh
@@ -10,13 +10,25 @@ find . -name cmake_install.cmake -exec rm -rf {} +
 
 # Install the project...
 # -- Install configuration: "Release"
-# -- Installing: /home/dpetre/.local/lib/libCore.a
-# -- Installing: /home/dpetre/.local/include/Core.h
-# -- Installing: /home/dpetre/.local/lib/libIO.a
-# -- Installing: /home/dpetre/.local/include/IO.h
-# -- Installing: /home/dpetre/.local/lib/libVisualization.a
-# -- Installing: /home/dpetre/.local/include/Visualization.h
-# -- Installing: /home/dpetre/.local/lib/python3.5/site-packages/py3d.cpython-35m-x86_64-linux-gnu.so
+# -- Installing: /home/dpetre/.local/lib/Open3D/libCore.a
+# -- Installing: /home/dpetre/.local/include/Open3D/Eigen.h
+# -- Installing: /home/dpetre/.local/include/Open3D/Helper.h
+# -- Installing: /home/dpetre/.local/include/Open3D/Timer.h
+# -- Installing: /home/dpetre/.local/include/Open3D/FileSystem.h
+# -- Installing: /home/dpetre/.local/include/Open3D/Console.h
+# -- Installing: /home/dpetre/.local/lib/Open3D/libIO.a
+# -- Installing: /home/dpetre/.local/lib/Open3D/libVisualization.a
+# -- Installing: /home/dpetre/.local/include/Open3D/RenderOptionWithEditing.h
+# -- Installing: /home/dpetre/.local/include/Open3D/RenderOption.h
+# -- Installing: /home/dpetre/.local/include/Open3D/Visualizer.h
+# -- Installing: /home/dpetre/.local/include/Open3D/ViewControlWithCustomAnimation.h
+# -- Installing: /home/dpetre/.local/include/Open3D/VisualizerWithKeyCallback.h
+# -- Installing: /home/dpetre/.local/include/Open3D/VisualizerWithEditing.h
+# -- Installing: /home/dpetre/.local/include/Open3D/ViewControl.h
+# -- Installing: /home/dpetre/.local/include/Open3D/ViewControlWithEditing.h
+# -- Installing: /home/dpetre/.local/include/Open3D/ViewParameters.h
+# -- Installing: /home/dpetre/.local/include/Open3D/ViewTrajectory.h
+# -- Installing: /home/dpetre/.local/include/Open3D/VisualizerWithCustomAnimation.h
 
 # remove the Open3D runtime binaries
 rm -rf ~/.local/bin/Open3D
