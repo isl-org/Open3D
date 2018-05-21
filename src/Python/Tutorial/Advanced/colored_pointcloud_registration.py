4788b35f127267aceeebc32167114fa0940c3870
Jaesik Park <syncle@gmail.com>
2018-05-21 15:30:55 -0700

    working

--- src/Python/Tutorial/Advanced/colored_pointcloud_registration.py
+++ src/Python/Tutorial/Advanced/colored_pointcloud_registration.py
@@ -15,22 +15,36 @@ def draw_registration_result_original_color(source, target, transformation):
 
 if __name__ == "__main__":
 
+    set_verbosity_level(VerbosityLevel.Debug)
+
     print("1. Load two point clouds and show initial pose")
-    source = read_point_cloud("../../TestData/ColoredICP/frag_115.ply")
-    target = read_point_cloud("../../TestData/ColoredICP/frag_116.ply")
+    source = read_point_cloud("/Users/jaesikpa/Downloads/testClouds/testclouda.ply")
+    target = read_point_cloud("/Users/jaesikpa/Downloads/testClouds/testcloudb.ply")
+    for i in range(len(source.points)):
+        source.points[i] /= 30.0
+    for i in range(len(target.points)):
+        target.points[i] /= 30.0
+    radius = 0.02
+    estimate_normals(source, KDTreeSearchParamHybrid(
+            radius = radius * 2, max_nn = 30))
+    estimate_normals(target, KDTreeSearchParamHybrid(
+            radius = radius * 2, max_nn = 30))
+    # orient_normals_towards_camera_location(source)
+    # orient_normals_towards_camera_location(target)
 
     # draw initial alignment
-    current_transformation = np.identity(4)
     draw_registration_result_original_color(
-            source, target, current_transformation)
+            source, target, np.identity(4))
 
     # point to plane ICP
-    current_transformation = np.identity(4);
     print("2. Point-to-plane ICP registration is applied on original point")
     print("   clouds to refine the alignment. Distance threshold 0.02.")
-    result_icp = registration_icp(source, target, 0.02,
-            current_transformation, TransformationEstimationPointToPlane())
-    print(result_icp)
+    result_icp = registration_icp(source, target, radius * 5,
+            np.identity(4), TransformationEstimationPointToPoint())
+    draw_registration_result_original_color(
+            source, target, result_icp.transformation)
+    result_icp = registration_icp(source, target, radius * 5,
+            result_icp.transformation, TransformationEstimationPointToPlane())
     draw_registration_result_original_color(
             source, target, result_icp.transformation)
 
@@ -40,7 +54,7 @@ if __name__ == "__main__":
      # Colored Point Cloud Registration Revisited, ICCV 2017
     voxel_radius = [ 0.04, 0.02, 0.01 ];
     max_iter = [ 50, 30, 14 ];
-    current_transformation = np.identity(4)
+    current_transformation = result_icp.transformation
     print("3. Colored point cloud registration")
     for scale in range(3):
         iter = max_iter[scale]
@@ -64,5 +78,7 @@ if __name__ == "__main__":
                 relative_rmse = 1e-6, max_iteration = iter))
         current_transformation = result_icp.transformation
         print(result_icp)
+    draw_registration_result_original_color(
+            source_down, target_down, result_icp.transformation)
     draw_registration_result_original_color(
             source, target, result_icp.transformation)
