d0362332514fb2652f2f06d8a6135cc0ff2c8524
takanokage <danut.petre@gmail.com>
2018-05-23 13:24:29 -0700

    another solution for 'from open3d import read_point_cloud'

--- util/pip_package/open3d/__init__.py
+++ util/pip_package/open3d/__init__.py
@@ -4,14 +4,10 @@
 
 import importlib
 from sys import platform
-from sys import path
 
 if platform == "linux" or platform == "linux2":
-    from open3d.linux import open3d
-    globals().update(importlib.import_module('open3d.linux.open3d').__dict__)
+    from open3d.linux import *
 elif platform == "darwin":
-    from open3d.linmacosux import open3d
-    globals().update(importlib.import_module('open3d.macos.open3d').__dict__)
+    from open3d.macos import *
 elif platform == "win32":
-    from open3d.win32 import open3d
-    globals().update(importlib.import_module('open3d.win32.open3d').__dict__)
+    from open3d.win32 import *
---
+++ util/pip_package/open3d/macos/__init__.py
@@ -0,0 +1,7 @@
+# Open3D: www.open3d.org
+# The MIT License (MIT)
+# See license file or visit www.open3d.org for details
+
+import importlib
+
+globals().update(importlib.import_module('open3d.macos.open3d').__dict__)
