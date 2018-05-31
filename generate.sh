91d9fd152e3aec5a71fd747b941949d5a0fadbb9
takanokage <danut.petre@gmail.com>
2018-05-31 10:53:22 -0700

    have generate.sh and build.sh use the same test install destination.

--- generate.sh
+++ generate.sh
@@ -4,9 +4,6 @@ echo generating...
 mkdir -p build
 cd build
 
-cmake ../src
-
-# you can specify a custom install location
-# cmake ../src/ -DCMAKE_INSTALL_PREFIX=~/Open3D_install
+cmake ../src/ -DCMAKE_INSTALL_PREFIX=~/Open3D_install
 
 echo
