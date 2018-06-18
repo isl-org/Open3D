62d84943c18be3b54be535b89260da2d52aa0f7c
takanokage <danut.petre@gmail.com>
2018-06-18 14:48:03 -0700

    no need for a separate build.sh - use the one added to the root of the project.

--- util/docker/open3d-xvfb/tools/run.sh
+++ util/docker/open3d-xvfb/tools/run.sh
@@ -6,18 +6,6 @@
 # create shared folder
 mkdir -p ~/open3d_docker
 
-# create folder dependencies
-mkdir -p ~/open3d_docker/build/lib/Tutorial/Advanced
-# cd ~/open3d_docker/build/lib/Tutorial/Advanced
-
-# copy the script for running the docker sample inside a container terminal
-cp -f ../setup/docker_sample.sh ~/open3d_docker/build/lib/Tutorial/Advanced
-chmod a+x ~/open3d_docker/build/lib/Tutorial/Advanced/docker_sample.sh
-
-# this is the Open3D build script
-cp -f ../setup/build.sh ~/open3d_docker
-chmod a+x ~/open3d_docker/build.sh
-
 # clone into existing non-empty directory
 # clone Open3D from the host side, build later inside the container
 cd ~/open3d_docker
