03d03657843b76b77e7aeaaacfa38bc7b0d080c8
Qianyi Zhou <Qianyi.Zhou@gmail.com>
2018-07-07 23:59:07 -0700

    fix travis

--- util/scripts/install-deps-ubuntu.sh
+++ util/scripts/install-deps-ubuntu.sh
@@ -4,19 +4,13 @@ set -ev
 
 sudo apt-get update
 
-sudo apt-get --yes install \
-		xorg-dev \
-		libglu1-mesa-dev \
-		libgl1-mesa-glx \
-		libglew-dev \
-		libglfw3-dev \
-		libjsoncpp-dev \
-		libeigen3-dev \
-		libpng-dev \
-		libjpeg-dev \
-		python-dev \
-		python3-dev \
-		python-tk \
-		python3-tk
-
+sudo apt-get --yes install libglu1-mesa-dev libgl1-mesa-glx
+sudo apt-get --yes install libglew-dev
+sudo apt-get --yes install libglfw3-dev
+sudo apt-get --yes install libjsoncpp-dev
+sudo apt-get --yes install libeigen3-dev
+sudo apt-get --yes install libpng-dev
 sudo apt-get --yes install libpng16-dev
+sudo apt-get --yes install libjpeg-dev
+sudo apt-get --yes install python-dev python-tk
+sudo apt-get --yes install python3-dev python3-tk
