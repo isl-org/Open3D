1ad1e03614f69649ec813c5d777258f2995dab71
Qianyi Zhou <Qianyi.Zhou@gmail.com>
2018-07-08 00:51:39 -0700

    install script fails silently

--- util/scripts/install-deps-ubuntu.sh
+++ util/scripts/install-deps-ubuntu.sh
@@ -4,13 +4,13 @@ set -ev
 
 sudo apt-get update
 
-sudo apt-get --yes install xorg-dev libglu1-mesa-dev libgl1-mesa-glx; true
-sudo apt-get --yes install libglew-dev; true
-sudo apt-get --yes install libglfw3-dev; true
-sudo apt-get --yes install libjsoncpp-dev; true
-sudo apt-get --yes install libeigen3-dev; true
-sudo apt-get --yes install libpng-dev; true
-sudo apt-get --yes install libpng16-dev; true
-sudo apt-get --yes install libjpeg-dev; true
-sudo apt-get --yes install python-dev python-tk; true
-sudo apt-get --yes install python3-dev python3-tk; true
+sudo apt-get --yes install xorg-dev libglu1-mesa-dev libgl1-mesa-glx || true
+sudo apt-get --yes install libglew-dev || true
+sudo apt-get --yes install libglfw3-dev || true
+sudo apt-get --yes install libjsoncpp-dev || true
+sudo apt-get --yes install libeigen3-dev || true
+sudo apt-get --yes install libpng-dev || true
+sudo apt-get --yes install libpng16-dev || true
+sudo apt-get --yes install libjpeg-dev || true
+sudo apt-get --yes install python-dev python-tk || true
+sudo apt-get --yes install python3-dev python3-tk || true
