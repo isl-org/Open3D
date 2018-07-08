4b36423dd41255993d5abaab68c2934fa4300d1b
Qianyi Zhou <Qianyi.Zhou@gmail.com>
2018-07-07 18:48:01 -0700

    fix

--- util/scripts/install-deps-ubuntu.sh
+++ util/scripts/install-deps-ubuntu.sh
@@ -2,6 +2,8 @@
 
 set -ev
 
+sudo apt-get update
+
 sudo apt-get install \
 		xorg-dev \
 		libglu1-mesa-dev \
@@ -11,10 +13,10 @@ sudo apt-get install \
 		libjsoncpp-dev \
 		libeigen3-dev \
 		libpng-dev \
-		libpng16-dev \
 		libjpeg-dev \
-		pybind11-dev \
 		python-dev \
 		python3-dev \
 		python-tk \
 		python3-tk
+
+sudo apt-get install libpng16-dev
