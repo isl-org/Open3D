9f86a2d72272fcace72b471d186e47252cf146a3
Jaesik Park <syncle@gmail.com>
2018-09-17 01:11:07 -0700

    adding plant example for redwood objects

---
+++ examples/Python/ReconstructionSystem/scripts/download_redwood_objects.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+unzip_redwood_scene()
+{
+    DATA_NUM=$1
+    mkdir ../dataset/redwood_objects/$DATA_NUM
+    unzip --qq $DATA_NUM.zip -d ../dataset/redwood_objects/$DATA_NUM
+}
+
+./gdrive_download.sh 1iMxjIZMFcoL3s9FzzqM0K-tM2ehO74D0
+./gdrive_download.sh 1_WZK0AZTt7N3QBh9JnDypM1sPKnE0oIY
+./gdrive_download.sh 1HCRVzlZ0huAsTyQsjOvI2OsZ_oU3zpt9
+./gdrive_download.sh 1jZLTNrOIP2sFgzF9sHb027iI2JhOOqpu
+./gdrive_download.sh 1nWilJfkAA7D3a8JEc_Tx9pLD4a25u8xG
+./gdrive_download.sh 1TqkWcdzQZG50ZV9nXdZYZmR_aLH-WYkr
+
+mkdir ../dataset/redwood_objects
+unzip_redwood_scene "00021"
+unzip_redwood_scene "00577"
+unzip_redwood_scene "01833"
+unzip_redwood_scene "05984"
+unzip_redwood_scene "06127"
+unzip_redwood_scene "06822"
+
+rm *.zip
