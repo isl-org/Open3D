3c4e5dad7f7515a5a92526f34f5ec4245a84ec36
Jaesik Park <syncle@gmail.com>
2018-09-19 15:25:05 -0700

    refine scripts

--- examples/Python/ReconstructionSystem/scripts/download_indoor_lidar_rgbd.sh
+++ examples/Python/ReconstructionSystem/scripts/download_indoor_lidar_rgbd.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
-cd .. && mkdir dataset/indoor_lidar_rgbd && cd dataset/indoor_lidar_rgbd
-../../scripts/gdrive_download.sh 1TqoIE1Q1M30q8FBQ_dMcZj9JR6G0ztI5
-../../scripts/gdrive_download.sh 1SN318pHOQn7ioSABJLY6SQ1O7gVMDvB2
-../../scripts/gdrive_download.sh 1gRDVGgPR--cKKHkrlaXVzc1Zj9VhC3Dr
-../../scripts/gdrive_download.sh 1MhjCJuznp3pG6zxHrIbmcjPjXhvlBStu
-../../scripts/gdrive_download.sh 1OOmymidV5nhmGSdk1Y7yI_9fXxLHNPjX
-unzip --qq rgbd_apartment.zip 
-unzip --qq rgbd_boardroom.zip
-unzip --qq rgbd_lobby.zip
-unzip --qq rgbd_loft.zip
-unzip --qq rgbd_bedroom.zip
-rm *.zip
+process_scene()
+{
+    DATA_NAME=$1
+    GDRIVE_ID=$2
+    ./gdrive_download.sh $GDRIVE_ID
+    unzip --qq $DATA_NAME.zip -d ../dataset/indoor_lidar_rgbd
+    rm $DATA_NAME.zip
+}
+
+mkdir ../dataset/indoor_lidar_rgbd
+process_scene "rgbd_apartment" "1TqoIE1Q1M30q8FBQ_dMcZj9JR6G0ztI5"
+process_scene "rgbd_boardroom" "1SN318pHOQn7ioSABJLY6SQ1O7gVMDvB2"
+process_scene "rgbd_lobby" "1gRDVGgPR--cKKHkrlaXVzc1Zj9VhC3Dr"
+process_scene "rgbd_loft" "1MhjCJuznp3pG6zxHrIbmcjPjXhvlBStu"
+process_scene "rgbd_bedroom" "1OOmymidV5nhmGSdk1Y7yI_9fXxLHNPjX"
