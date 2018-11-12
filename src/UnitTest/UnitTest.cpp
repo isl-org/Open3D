7744bdd68a801d1b1628469f1a17d33b7442133d
takanokage <danut.petre@gmail.com>
2018-11-12 10:10:39 -0800

    + using namespace directive in order to reduce code line length. removed global variables. shortened local variables names. simplified comparisons using unit_test::ExpectEQ(...).

--- src/UnitTest/UnitTest.cpp
+++ src/UnitTest/UnitTest.cpp
@@ -155,3 +155,23 @@ void unit_test::ExpectGE(const double& v00, const double& v01, const double& v02
     EXPECT_GE(v01, v1(1, 0));
     EXPECT_GE(v02, v1(2, 0));
 }
+
+// ----------------------------------------------------------------------------
+// Test equality of two arrays of uint8_t.
+// ----------------------------------------------------------------------------
+void unit_test::ExpectEQ(const uint8_t* const v0,
+              const uint8_t* const v1,
+              const size_t& size)
+{
+    for (int i = 0; i < size; i++)
+        EXPECT_EQ(v0[i], v1[i]);
+}
+
+// ----------------------------------------------------------------------------
+// Test equality of two vectors of uint8_t.
+// ----------------------------------------------------------------------------
+void unit_test::ExpectEQ(const vector<uint8_t>& v0, const vector<uint8_t>& v1)
+{
+    EXPECT_EQ(v0.size(), v1.size());
+    ExpectEQ(&v0[0], &v1[0], v0.size());
+}
--- src/UnitTest/UnitTest.h
+++ src/UnitTest/UnitTest.h
@@ -39,6 +39,12 @@ namespace unit_test
     // thresholds for comparing floating point values
     const double THRESHOLD_1E_6 = 1e-6;
 
+    // Eigen Zero()
+    const Eigen::Vector2d Zero2d = Eigen::Vector2d::Zero();
+    const Eigen::Vector3d Zero3d = Eigen::Vector3d::Zero();
+    const Eigen::Matrix<double, 6, 1> Zero6d = Eigen::Matrix<double, 6, 1>::Zero();
+    const Eigen::Vector2i Zero2i = Eigen::Vector2i::Zero();
+
     // Mechanism for reporting unit tests for which there is no implementation yet.
     void NotImplemented();
 
@@ -68,4 +74,20 @@ namespace unit_test
     // Greater than or Equal test over Eigen::Vector3d components.
     void ExpectGE(const Eigen::Vector3d& v0, const Eigen::Vector3d& v1);
     void ExpectGE(const double& v00, const double& v01, const double& v02, const Eigen::Vector3d& v1);
+
+    // Test equality of two arrays of uint8_t.
+    void ExpectEQ(const uint8_t* const v0,
+                  const uint8_t* const v1,
+                  const size_t& size);
+
+    // Test equality of two vectors of uint8_t.
+    void ExpectEQ(const std::vector<uint8_t>& v0,
+                  const std::vector<uint8_t>& v1);
+
+    // Reinterpret cast from uint8_t* to float*.
+    template<class T>
+    T* const Cast(uint8_t* data)
+    {
+        return reinterpret_cast<T* const>(data);
+    }
 }
