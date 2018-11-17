c0e06f158a4be7ff04d27a5cee131ddb175b352e
Dan Petre <danut.petre@gmail.com>
2018-11-17 13:45:01 -0800

    PinholeCameraTrajectory (#688)
    * added template for the PinholeCameraParameters test case.
    * implemented PinholeCameraTrajectory test case.
    fixed bug in PinholeCameraTrajectory ConvertToJsonValue(...) and ConvertFromJsonValue(...).
    * removed unused lines.
    * updated camera_trajectory.json to the new format of PinholeCameraTrajectory which encapsulates an array of PinholeCameraParameters.
    * fixed bug where convert_from_pinhole_camera_parameters was called with incorrect arguments.
    * fixed bug where camera trajectory parameters where accessed incorrectly.

--- examples/Python/Advanced/camera_trajectory.py
+++ examples/Python/Advanced/camera_trajectory.py
@@ -27,14 +27,14 @@ if __name__ == "__main__":
     trajectory = read_pinhole_camera_trajectory("../../TestData/RGBD/trajectory.log")
     write_pinhole_camera_trajectory("test.json", trajectory)
     print(trajectory)
-    print(trajectory.extrinsic)
-    print(np.asarray(trajectory.extrinsic))
+    print(trajectory.parameters[0].extrinsic)
+    print(np.asarray(trajectory.parameters[0].extrinsic))
     for i in range(5):
         im1 = read_image("../../TestData/RGBD/depth/{:05d}.png".format(i))
         im2 = read_image("../../TestData/RGBD/color/{:05d}.jpg".format(i))
         im = create_rgbd_image_from_color_and_depth(im2, im1, 1000.0, 5.0, False)
         pcd = create_point_cloud_from_rgbd_image(im,
-                trajectory.intrinsic, trajectory.extrinsic[i])
+                trajectory.parameters[i].intrinsic, trajectory.parameters[i].extrinsic)
         pcds.append(pcd)
     draw_geometries(pcds)
     print("")
