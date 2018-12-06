b94608931642baaed29dd48dbca34c461c587051
takanokage <danut.petre@gmail.com>
2018-12-06 15:28:34 -0800

    copied the build & test code to the base image as test.sh. simplified /tools/test.sh.

---
+++ util/docker/open3d-test/setup/test.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+echo
+
+git clone https://github.com/IntelVCL/Open3D.git open3d
+cd open3d
+echo
+
+echo building...
+mkdir -p build
+cd build
+cmake .. -DPYTHON_EXECUTABLE=/usr/bin/${PYTHON} \
+         -DCMAKE_BUILD_TYPE=Release \
+         -DBUILD_UNIT_TESTS=ON
+echo
+make -j
+echo
+
+echo running the unit tests...
+./bin/unitTests
--- util/docker/open3d-test/tools/test.sh
+++ util/docker/open3d-test/tools/test.sh
@@ -24,22 +24,7 @@ docker container run \
 
 # attach to the running container, clone & build Open3d
 echo "testing $NAME:$TAG..."
-docker container exec -it -w /root $CONTAINER_NAME bash -c '\
-    echo && \
-    git clone https://github.com/IntelVCL/Open3D.git open3d && \
-    cd open3d && \
-    echo && \
-    echo building... && \
-    mkdir -p build && \
-    cd build && \
-    cmake .. -DPYTHON_EXECUTABLE=/usr/bin/${PYTHON} \
-             -DCMAKE_BUILD_TYPE=Release \
-             -DBUILD_UNIT_TESTS=ON && \
-    echo && \
-    make -j && \
-    echo && \
-    echo running the unit tests... && \
-    ./bin/unitTests'
+docker container exec -it -w /root $CONTAINER_NAME bash -c './test.sh'
 
 # stop the container
 docker container stop -t 0 $CONTAINER_NAME
