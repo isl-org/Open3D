45c909d0df3a9c486bcacf89ae51a0e52d450dd7
takanokage <danut.petre@gmail.com>
2018-12-06 16:14:18 -0800

    build dependencies before a Docker image gets built.

--- util/docker/open3d-test/tools/build.sh
+++ util/docker/open3d-test/tools/build.sh
@@ -2,6 +2,14 @@
 
 . ./set_variables.sh
 
+if [ "$3" = "with_deps" ]; then
+    ./build.sh ${1} ${2} no_deps
+fi
+
+if [ "$3" = "no_deps" ]; then
+    ./build.sh ${1} base
+fi
+
 # build the image
 echo "building $NAME:$TAG..."
 docker image build -t $NAME:$TAG -f ../Dockerfiles/${1}/$DOCKERFILE ..
