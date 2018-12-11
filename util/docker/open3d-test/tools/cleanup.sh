8e051f033bd7b7a4fb8d97c1c1feee053c2caec3
takanokage <danut.petre@gmail.com>
2018-12-11 15:18:17 -0800

    removed comments and unused code.

--- util/docker/open3d-test/tools/cleanup.sh
+++ util/docker/open3d-test/tools/cleanup.sh
@@ -1,29 +1,9 @@
 #!/bin/bash
 
-. name.sh
-
 . arguments.sh
 
 echo "cleaning up images..."
 echo
-
-# for ubuntu in ${ubuntu_version[@]}; do
-#     for deps in ${bundle_type[@]}; do
-#         # build the tag of the image
-#         TAG=${ubuntu}-${deps}
-#         echo "removing $NAME:${TAG}..."
-#         docker image rm $NAME:${TAG}
-#         echo
-
-#         for python in ${python_version[@]}; do
-#             # build the tag of the image
-#             TAG=${ubuntu}-${deps}-${python}
-#             echo "removing $NAME:${TAG}..."
-#             docker image rm $NAME:${TAG}
-#             echo
-#         done
-#     done
-# done
 
 for ubuntu in ${ubuntu_version[@]}; do
     for deps in ${bundle_type[@]}; do
--- util/docker/open3d-test/tools/stop.sh
+++ util/docker/open3d-test/tools/stop.sh
@@ -1,35 +1,9 @@
 #!/bin/bash
 
-. name.sh
-
 . arguments.sh
 
 echo "stopping containers..."
 echo
-
-# for ubuntu in ${ubuntu_version[@]}; do
-#     for deps in ${bundle_type[@]}; do
-#         # build the tag of the image
-#         TAG=${ubuntu}-${deps}
-#         # build the container name
-#         CONTAINER_NAME=${NAME}-${TAG}
-
-#         echo "stopping $CONTAINER_NAME..."
-#         docker container stop -t 0 $CONTAINER_NAME
-#         echo
-
-#         for python in ${python_version[@]}; do
-#             # build the tag of the image
-#             TAG=${ubuntu}-${deps}-${python}
-#             # build the container name
-#             CONTAINER_NAME=${NAME}-${TAG}
-
-#             echo "stopping $CONTAINER_NAME..."
-#             docker container stop -t 0 $CONTAINER_NAME
-#             echo
-#         done
-#     done
-# done
 
 for ubuntu in ${ubuntu_version[@]}; do
     for deps in ${bundle_type[@]}; do
