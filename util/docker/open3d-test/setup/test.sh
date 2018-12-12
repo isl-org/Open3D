a3fd1d9d8ec2e17fcab5fcfbcca47a6de20b4214
takanokage <danut.petre@gmail.com>
2018-12-12 11:31:44 -0800

    WIP: - updated miniconda dockerfiles to install required conda dependencies - setup/test.sh updated with command line arguments in order to allow reuse for mc2/3. - tools/test.sh updated with (untested) example on how to build using conda environment.

--- util/docker/open3d-test/setup/test.sh
+++ util/docker/open3d-test/setup/test.sh
@@ -1,65 +1,104 @@
-#!/bin/sh
+#!/bin/bash
+
+declare -a build_type=(Debug Release)
+declare -a link_type=(STATIC SHARED)
+declare -a env_type=(py2 py3 mc2 mc3)
+
+if [ $# -eq 0 ] || [ "$1" = "--help" ]; then
+    echo "./build.sh <build_type> <link_type> <env_type>"
+    echo
+    echo "Required:"
+    echo "    Build type:       ${build_type[*]}"
+    echo "    Link type:        ${link_type[*]}"
+    echo "    Environment type: ${env_type[*]}"
+    echo
+    exit 1
+fi
+
+if [[ ! " ${build_type[@]} " =~ " $1 " ]]; then
+    echo "    the first argument must be the build type: ${build_type[*]}"
+    echo "    argument provided: '$1'"
+    echo
+    exit 1
+fi
+
+if [[ ! " ${link_type[@]} " =~ " $2 " ]]; then
+    echo "    the second argument must be the library link type: ${link_type[*]}"
+    echo "    argument provided: '$2'"
+    echo
+    exit 1
+fi
+
+if [[ ! " ${env_type[@]} " =~ " $3 " ]]; then
+    echo "    the third argument must be the environment type: ${env_type[*]}"
+    echo "    argument provided: '$3'"
+    echo
+    exit 1
+fi
 
 echo
 
-git clone --recurse-submodules https://github.com/IntelVCL/Open3D.git open3d
+# git clone --recurse-submodules https://github.com/IntelVCL/Open3D.git open3d
 cd open3d
 echo
 
-echo "building STATIC..."
+echo "building $2..."
 date
+
+# set the library link mode to OFF (STATIC) or ON (SHARED)
+SHARED=OFF
+if [ "$3" = "STATIC" ]; then
+    SHARED="OFF"
+elif [ "$3" = "SHARED" ]; then
+    SHARED="ON"
+fi
+
+# set the python executable
+PYTHON=$3
+if [ "$3" = "py2" ]; then
+    PYTHON="/usr/bin/python2"
+elif [ "$3" = "py3" ]; then
+    PYTHON="/usr/bin/python3"
+elif [ "$3" = "mc2" ]; then
+    PYTHON="/root/miniconda2/bin/python2"
+elif [ "$3" = "mc3" ]; then
+    PYTHON="/root/miniconda3/bin/python3"
+fi
 
 mkdir -p build
 cd build
-cmake .. -DPYTHON_EXECUTABLE=/usr/bin/${PYTHON} \
-         -DCMAKE_BUILD_TYPE=Release \
+cmake .. -DCMAKE_BUILD_TYPE=${1} \
+         -DBUILD_SHARED_LIBS=${SHARED} \
+         -DPYTHON_EXECUTABLE=${PYTHON} \
          -DBUILD_UNIT_TESTS=ON
 echo
 make -j
 date
 echo
 
-echo building pip package...
+echo "building pip package..."
 date
-make pip-package
-#make install-pip-package
+if [ "$3" = "py2" ]; then
+    make pip-package
+    #make install-pip-package
+elif [ "$3" = "py3" ]; then
+    make pip-package
+    #make install-pip-package
+elif [ "$3" = "mc2" ]; then
+    make conda-package
+elif [ "$3" = "mc3" ]; then
+    make conda-package
+fi
 date
 echo
 
-echo running the unit tests...
+echo "running the unit tests..."
 ./bin/unitTests
 
 date
 echo
 
-echo cleaning...
+echo "cleaning..."
 cd ..
 rm -rf build
-echo
-
-echo building SHARED...
-date
-
-mkdir -p build
-cd build
-cmake .. -DBUILD_SHARED_LIBS=ON \
-         -DPYTHON_EXECUTABLE=/usr/bin/${PYTHON} \
-         -DCMAKE_BUILD_TYPE=Release \
-         -DBUILD_UNIT_TESTS=ON
-echo
-make -j
-date
-echo
-
-echo building pip package...
-date
-make pip-package
-# make install-pip-package
-date
-echo
-
-echo running the unit tests...
-./bin/unitTests
-
-date
 echo
--- util/docker/open3d-test/tools/test.sh
+++ util/docker/open3d-test/tools/test.sh
@@ -20,7 +20,12 @@ docker container run \
 
 # attach to the running container, clone & build Open3d
 echo "testing $IMAGE_NAME..."
-docker container exec -it -w /root $CONTAINER_NAME bash -c './test.sh'
+docker container exec -it -w /root $CONTAINER_NAME bash -c '\
+    git clone --recurse-submodules https://github.com/IntelVCL/Open3D.git open3d && \
+    source /root/miniconda2/bin/activate && \
+    ./test.sh Release STATIC ${3} && \
+    ./test.sh Release SHARED ${3} && \
+    bash'
 
 # stop the container
 docker container stop -t 0 $CONTAINER_NAME
