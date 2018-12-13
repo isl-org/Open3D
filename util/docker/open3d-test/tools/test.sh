b7ef22c64ee0841b935fe4f0847bbb16f9b2fb88
takanokage <danut.petre@gmail.com>
2018-12-13 11:25:18 -0800

    timestamp each test.

--- util/docker/open3d-test/tools/test.sh
+++ util/docker/open3d-test/tools/test.sh
@@ -30,6 +30,7 @@ docker container run \
 
 # attach to the running container, clone once & build Open3D twice
 echo "testing $IMAGE_NAME..."
+date
 docker container exec -it -w /root $CONTAINER_NAME bash -c '\
     git clone --recursive https://github.com/IntelVCL/Open3D.git open3d && \
     $ACTIVATE_CONDA && \
