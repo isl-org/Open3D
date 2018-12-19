471381ce558242a7aeda9fd030e9a8b9684085ae
takanokage <danut.petre@gmail.com>
2018-12-19 05:52:58 -0800

    the activation of the conda environment is now embedded in the dockerfiles. removed unnecessary code.

--- util/docker/open3d-test/tools/test.sh
+++ util/docker/open3d-test/tools/test.sh
@@ -5,14 +5,6 @@
 # build the image
 ./build.sh ${1} ${2} ${3}
 
-# miniconda needs to be activated before building Open3D
-ACTIVATE_CONDA=""
-if [ "$3" = "mc2" ]; then
-    ACTIVATE_CONDA="source /root/miniconda2/bin/activate"
-elif [ "$3" = "mc3" ]; then
-    ACTIVATE_CONDA="source /root/miniconda3/bin/activate"
-fi
-
 # helps sync the container clock with the host clock
 TIMEZONE=$(cat /etc/timezone)
 
@@ -33,7 +25,6 @@ echo "testing $IMAGE_NAME..."
 date
 docker container exec -it -w /root $CONTAINER_NAME bash -c '\
     git clone --recursive https://github.com/IntelVCL/Open3D.git open3d && \
-    $ACTIVATE_CONDA && \
     ./test.sh Release STATIC $ENV_TYPE && \
     ./test.sh Release SHARED $ENV_TYPE'
 
