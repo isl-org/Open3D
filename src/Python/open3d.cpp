2d50f2ffee741287e7c2ffae924f57fa28edab31
Jaesik Park <syncle@gmail.com>
2018-12-19 02:24:56 -0800

    minor polish

--- src/Python/open3d.cpp
+++ src/Python/open3d.cpp
@@ -32,11 +32,6 @@ PYBIND11_MODULE(open3d, m) {
     pybind_eigen(m);
 
     pybind_core(m);
-    // pybind_core_classes(m);
-    pybind_io_classes(m);
+    pybind_io(m);
     pybind_visualization(m);
-
-    // pybind_core_methods(m);
-    pybind_io_methods(m);
-    // pybind_visualization_methods(m);
 }
