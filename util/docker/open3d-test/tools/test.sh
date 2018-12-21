d5164e9963b8069716d2145dd46ad484207d4a7f
takanokage <danut.petre@gmail.com>
2018-12-21 11:09:56 -0800

    the conda settings in .bashrc only work with interactive shells, for this reason we need to explicitly activate conda for the mc2/mc3 path.

--- util/docker/open3d-test/tools/test.sh
+++ util/docker/open3d-test/tools/test.sh
@@ -22,10 +22,21 @@ docker container run \
 # attach to the running container, clone once & build Open3D twice
 echo "testing ${IMAGE_NAME}..."
 date
-docker container exec -it -w /root ${CONTAINER_NAME} bash -c '\
-    git clone --recursive https://github.com/IntelVCL/Open3D.git open3d && \
-    ./test.sh Release STATIC $ENV_TYPE && \
-    ./test.sh Release SHARED $ENV_TYPE'
+
+if [ "${3}" = "py2" ] || [ "${3}" = "py3" ]; then
+    docker container exec -it -w /root ${CONTAINER_NAME} /bin/bash -c '\
+        git clone --recursive https://github.com/IntelVCL/Open3D.git open3d && \
+        ./test.sh Release STATIC $ENV_TYPE && \
+        ./test.sh Release SHARED $ENV_TYPE'
+elif [ "${3}" = "mc2" ] || [ "${3}" = "mc3" ]; then
+    # the conda settings in .bashrc only work with interactive shells
+    # for this reason we need to explicitly activate conda here
+    docker container exec -e CONDA_DIR="${CONDA_DIR}" -it -w /root ${CONTAINER_NAME} /bin/bash -c '\
+        git clone --recursive https://github.com/IntelVCL/Open3D.git open3d && \
+        source ${CONDA_DIR}/bin/activate && \
+        ./test.sh Release STATIC $ENV_TYPE && \
+        ./test.sh Release SHARED $ENV_TYPE'
+fi
 
 # stop the container
 docker container stop -t 0 ${CONTAINER_NAME}
