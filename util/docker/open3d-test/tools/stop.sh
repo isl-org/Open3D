eabccb0935120504f5424352328a32e703375867
takanokage <danut.petre@gmail.com>
2019-01-07 10:39:38 -0800

    stop.sh -> stop-all.sh. added stop.sh to stop just one container.

--- util/docker/open3d-test/tools/stop.sh
+++ util/docker/open3d-test/tools/stop.sh
@@ -1,24 +1,7 @@
 #!/bin/bash
 
-. arguments.sh
+. set_variables.sh
 
-echo "stopping containers..."
-echo
-
-for ubuntu in ${ubuntu_version[@]}; do
-    for bundle in ${bundle_type[@]}; do
-        . set_variables.sh ${ubuntu} ${bundle}
-
-        echo "stopping ${CONTAINER_NAME}..."
-        docker container stop -t 0 ${CONTAINER_NAME} >/dev/null 2>&1
-
-        for env in ${env_type[@]}; do
-            . set_variables.sh ${ubuntu} ${bundle} ${env}
-
-            echo "stopping ${CONTAINER_NAME}..."
-            docker container stop -t 0 ${CONTAINER_NAME} >/dev/null 2>&1
-        done
-    done
-done
-
+echo "stopping ${CONTAINER_NAME}..."
+docker container stop -t 0 ${CONTAINER_NAME} >/dev/null 2>&1
 echo
