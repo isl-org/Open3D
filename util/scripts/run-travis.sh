0e9dd054a1a0ec65764dcb005544cfd6a054792d
takanokage <danut.petre@gmail.com>
2019-01-11 00:33:20 -0800

    cleanup the travis script. no need to install twice. cleanup at the end. the script can be tested on the host side if moved to the root of the Open3D folder.

--- util/scripts/run-travis.sh
+++ util/scripts/run-travis.sh
@@ -2,54 +2,73 @@
 
 python --version
 cmake --version
+echo
 
-# Build and install to CMake's default install path
+INSTALL_DIR=~/open3d_install
+
+# configure the project
+# install to a specified CMAKE_INSTALL_PREFIX
 mkdir build
 cd build
 if [ "$BUILD_DEPENDENCY_FROM_SOURCE" == "OFF" ]; then
-    cmake -DBUILD_SHARED_LIBS=$SHARED -DBUILD_UNIT_TESTS=ON ..
+    cmake -DBUILD_SHARED_LIBS=$SHARED \
+          -DBUILD_UNIT_TESTS=ON
+          -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} \
+          ..
 else
-    cmake -DBUILD_SHARED_LIBS=$SHARED -DBUILD_UNIT_TESTS=ON -DBUILD_EIGEN3=ON -DBUILD_GLEW=ON -DBUILD_GLFW=ON -DBUILD_JPEG=ON -DBUILD_JSONCPP=ON -DBUILD_PNG=ON ..
+    cmake -DBUILD_SHARED_LIBS=$SHARED \
+          -DBUILD_UNIT_TESTS=ON \
+          -DBUILD_EIGEN3=ON \
+          -DBUILD_GLEW=ON \
+          -DBUILD_GLFW=ON \
+          -DBUILD_JPEG=ON \
+          -DBUILD_JSONCPP=ON \
+          -DBUILD_PNG=ON \
+          -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} \
+          ..
 fi
+echo
 
-# make -j brings 'virtual memory exhausted: Cannot allocate memory' message
-# this is presumably due to limited memory space of travis-ci
-# also set the time limit to 30 mins, this is a fix to issue #557
-sudo make install -j$(nproc)
+# build Open3D
+make install -j$(nproc)
+echo
 
+# run Open3D unit tests
 ./bin/unitTests
 
-test=`cmake --find-package -DNAME=Open3D -DCOMPILER_ID=GNU -DLANGUAGE=C -DMODE=EXIST`
-if [ "$test" == "Open3D found." ]; then
-    echo "PASSED find_package(Open3D) in default install path.";
-else
-    echo "FAILED find_package(Open3D) in default install path.";
-    exit 1;
-fi
-
-sudo make uninstall
-
-# Reconfig and install in a specified CMAKE_INSTALL_PREFIX
-if [ "$BUILD_DEPENDENCY_FROM_SOURCE" == "OFF" ]; then
-    cmake -DBUILD_SHARED_LIBS=$SHARED -DBUILD_UNIT_TESTS=ON -DCMAKE_INSTALL_PREFIX=~/open3d_install ..;
-else
-    cmake -DBUILD_SHARED_LIBS=$SHARED -DBUILD_UNIT_TESTS=ON -DBUILD_EIGEN3=ON -DBUILD_GLEW=ON -DBUILD_GLFW=ON -DBUILD_JPEG=ON -DBUILD_JSONCPP=ON -DBUILD_PNG=ON -DCMAKE_INSTALL_PREFIX=~/open3d_install ..;
-fi
-
-sudo make install -j$(nproc)
-
-test=`cmake --find-package -DNAME=Open3D -DCOMPILER_ID=GNU -DLANGUAGE=C -DMODE=EXIST -DCMAKE_PREFIX_PATH="~/open3d_install/lib/cmake"`
+test=`cmake --find-package \
+            -DNAME=Open3D \
+            -DCOMPILER_ID=GNU \
+            -DLANGUAGE=C \
+            -DMODE=EXIST \
+            -DCMAKE_PREFIX_PATH="${INSTALL_DIR}/lib/cmake"`
 if [ "$test" == "Open3D found." ]; then
     echo "PASSED find_package(Open3D) in specified install path.";
 else
     echo "FAILED find_package(Open3D) in specified install path.";
     exit 1;
 fi
+echo
 
-# Test building example with installed Open3D
+# Test building a C++ example with installed Open3D
 cd ../docs/_static/C++
 mkdir build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=~/open3d_install ..
+cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} ..
 make
 ./TestVisualizer
+echo
+
+# cleanup the C++ example
+cd ../
+rm -rf build
+
+# uninstall Open3D
+cd ../../../build
+make uninstall
+
+# cleanup Open3D
+cd ../
+rm -rf build
+rm -rf ${INSTALL_DIR}
+echo
