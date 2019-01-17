f78a6e2038a612d8874dae98a60ed50357080cc0
takanokage <danut.petre@gmail.com>
2019-01-17 11:35:44 -0800

    set -e breaks the installation of the dependencies.

--- util/scripts/setup-linux.sh
+++ util/scripts/setup-linux.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -e
-
 ./util/scripts/install-deps-ubuntu.sh
 ./util/scripts/make-documentation.sh
 ./util/scripts/install-gtest.sh
--- util/scripts/setup-osx.sh
+++ util/scripts/setup-osx.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-set -e
-
 ./util/scripts/install-deps-osx.sh
 ./util/scripts/install-gtest.sh
