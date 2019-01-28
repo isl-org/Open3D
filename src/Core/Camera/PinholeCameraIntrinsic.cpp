a298fbdc111780176d3a83205fe9dfe4d328c81a
Sergei Chicherin <chroneus@chroneus.com>
2019-01-28 22:26:31 +0300

    fixed parameters for depth in Kinect2
    cx and cy was replaced with fx, fy

--- src/Core/Camera/PinholeCameraIntrinsic.cpp
+++ src/Core/Camera/PinholeCameraIntrinsic.cpp
@@ -46,7 +46,7 @@ PinholeCameraIntrinsic::PinholeCameraIntrinsic(
         SetIntrinsics(640, 480, 525.0, 525.0, 319.5, 239.5);
     else if (param ==
              PinholeCameraIntrinsicParameters::Kinect2DepthCameraDefault)
-        SetIntrinsics(512, 424, 254.878, 205.395, 365.456, 365.456);
+        SetIntrinsics(512, 424, 365.456, 365.456, 254.878, 205.395);
     else if (param ==
              PinholeCameraIntrinsicParameters::Kinect2ColorCameraDefault)
         SetIntrinsics(1920, 1080, 1059.9718, 1059.9718, 975.7193, 545.9533);
