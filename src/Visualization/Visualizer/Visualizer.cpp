849cd71ed9a18412c79da5b479cd3b50ef613d42
Jaesik Park <syncle@gmail.com>
2019-02-07 22:51:38 -0800

    Change OpenGL 2.1 -> 3.2

--- src/Visualization/Visualizer/Visualizer.cpp
+++ src/Visualization/Visualizer/Visualizer.cpp
@@ -99,8 +99,10 @@ bool Visualizer::CreateVisualizerWindow(
     }
 
     glfwWindowHint(GLFW_SAMPLES, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_VISIBLE, visible ? 1 : 0);
 
     window_ = glfwCreateWindow(width, height, window_name_.c_str(), NULL, NULL);
