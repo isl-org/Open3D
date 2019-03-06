c32fd21c2113a2e8ceb3192a1e09692e9f01d5b3
Yixing Lao <yixing.lao@gmail.com>
2019-03-06 15:24:49 -0800

    split by ->

--- src/UnitTest/PyBind/Docstring.cpp
+++ src/UnitTest/PyBind/Docstring.cpp
@@ -27,18 +27,39 @@
 #include <string>
 #include <unordered_map>
 #include <tuple>
+#include <unordered_set>
 
 #include "TestUtility/UnitTest.h"
+
+// Count the length of current word starting from start_pos
+size_t word_length(const std::string& docs,
+                   size_t start_pos,
+                   const std::unordered_set<char>& valid_chars = {'_'}) {
+    auto is_word_char = [&valid_chars](const char& c) {
+        return std::isalnum(c) || valid_chars.find(c) != valid_chars.end();
+    };
+    size_t length = 0;
+    for (size_t pos = start_pos; pos < docs.size(); ++pos) {
+        if (!is_word_char(docs[pos])) {
+            break;
+        }
+        length++;
+    }
+    return length;
+}
 
 // Splits
 // "create_mesh_arrow(cylinder_radius: float = 1.0) -> geometry.TriangleMesh"
 // to
 // ("create_mesh_arrow(cylinder_radius: float = 1.0)",
 //  "geometry.TriangleMesh")
-// std::pair<std::string, std::string> split_arrow(const std::string& docs) {
-//     std::size_t arrow_pos = docs.rfind(" -> ");
-//     std::string func_name_and_param = docs.substr(0, arrow_pos);
-// }
+std::pair<std::string, std::string> split_arrow(const std::string& docs) {
+    std::size_t arrow_pos = docs.rfind(" -> ");
+    std::string func_name_and_params = docs.substr(0, arrow_pos);
+    std::string return_type = docs.substr(
+            arrow_pos + 4, word_length(docs, arrow_pos + 4, {'_', '.'}));
+    return std::make_pair(func_name_and_params, return_type);
+}
 
 // Currently copied this function for testing
 // TODO: link unit test with python module to enable direct testing
@@ -49,6 +70,12 @@ parse_pybind_function_doc(const std::string& pybind_docs) {
     std::vector<std::string> ordered_parameters;
 
     // Split by "->"
+    std::string func_name_and_params;
+    std::string return_type;
+    std::tie(func_name_and_params, return_type) = split_arrow(pybind_docs);
+
+    std::cout << "func_name_and_params " << func_name_and_params << std::endl;
+    std::cout << "return_type " << return_type << std::endl;
 
     return std::make_pair(map_parameter_type_docs, ordered_parameters);
 }
