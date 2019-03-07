c71426d011b636e7b88fdcb47d8a2aad8c647e77
Yixing Lao <yixing.lao@gmail.com>
2019-03-07 14:23:29 -0800

    parse pybind11 and generate google-style docstring

---
+++ src/Python/docstring.cpp
@@ -0,0 +1,273 @@
+// ----------------------------------------------------------------------------
+// -                        Open3D: www.open3d.org                            -
+// ----------------------------------------------------------------------------
+// The MIT License (MIT)
+//
+// Copyright (c) 2018 www.open3d.org
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+// ----------------------------------------------------------------------------
+
+#include <string>
+#include <unordered_map>
+#include <tuple>
+#include <unordered_set>
+#include <sstream>
+#include <regex>
+
+#include "Python/docstring.h"
+#include "Open3D/Utility/Helper.h"
+
+namespace open3d {
+namespace docstring {
+
+void FunctionDocInject(py::module& pybind_module,
+                       const std::string& function_name,
+                       const std::unordered_map<std::string, std::string>
+                               map_parameter_body_docs) {
+    // Get function
+    PyObject* module = pybind_module.ptr();
+    PyObject* f_obj = PyObject_GetAttrString(module, function_name.c_str());
+    if (Py_TYPE(f_obj) != &PyCFunction_Type) {
+        return;
+    }
+    PyCFunctionObject* f = (PyCFunctionObject*)f_obj;
+
+    FunctionDoc fd(f->m_ml->ml_doc);
+    for (const auto& it : map_parameter_body_docs) {
+        fd.InjectArgumentDocBody(it.first, it.second);
+    }
+    f->m_ml->ml_doc = strdup(fd.ToGoogleDocString().c_str());
+}
+
+FunctionDoc::FunctionDoc(const std::string& pybind_doc)
+    : pybind_doc_(pybind_doc) {
+    ParseFunctionName();
+    ParseSummary();
+    ParseArguments();
+    ParseReturn();
+}
+
+void FunctionDoc::InjectArgumentDocBody(const std::string& argument_name,
+                                        const std::string& argument_doc_body) {
+    for (ArgumentDoc& argument_doc : argument_docs_) {
+        if (argument_doc.name_ == argument_name) {
+            argument_doc.body_ = argument_doc_body;
+            break;
+        }
+    }
+}
+
+void FunctionDoc::ParseFunctionName() {
+    size_t parenthesis_pos = pybind_doc_.find("(");
+    if (parenthesis_pos == std::string::npos) {
+        return;
+    } else {
+        std::string name = pybind_doc_.substr(0, parenthesis_pos);
+        name_ = name;
+    }
+}
+
+void FunctionDoc::ParseSummary() {
+    size_t arrow_pos = pybind_doc_.rfind(" -> ");
+    if (arrow_pos != std::string::npos) {
+        size_t result_type_pos = arrow_pos + 4;
+        size_t summary_start_pos =
+                result_type_pos +
+                utility::WordLength(pybind_doc_, result_type_pos, "._:");
+        size_t summary_len = pybind_doc_.size() - summary_start_pos;
+        if (summary_len > 0) {
+            std::string summary =
+                    pybind_doc_.substr(summary_start_pos, summary_len);
+            summary_ = StringCleanAll(summary);
+        }
+    }
+}
+
+void FunctionDoc::ParseArguments() {
+    // Parse docstrings of arguments
+    // Input: "foo(arg0: float, arg1: float = 1.0, arg2: int = 1) -> open3d.bar"
+    // Goal: split to {"arg0: float", "arg1: float = 1.0", "arg2: int = 1"} and
+    //       call function to parse each argument respectively
+    std::vector<std::string> argument_tokens = GetArgumentTokens(pybind_doc_);
+    argument_docs_.clear();
+    for (const std::string& argument_token : argument_tokens) {
+        argument_docs_.push_back(ParseArgumentToken(argument_token));
+    }
+}
+
+void FunctionDoc::ParseReturn() {
+    size_t arrow_pos = pybind_doc_.rfind(" -> ");
+    if (arrow_pos != std::string::npos) {
+        size_t result_type_pos = arrow_pos + 4;
+        std::string return_type = pybind_doc_.substr(
+                result_type_pos,
+                utility::WordLength(pybind_doc_, result_type_pos, "._:"));
+        return_doc_.type_ = StringCleanAll(return_type);
+    }
+}
+
+std::string FunctionDoc::ToGoogleDocString() const {
+    // Example Gooele style:
+    // http://www.sphinx-doc.org/en/1.5/ext/example_google.html
+
+    std::ostringstream rc;
+    std::string indent = "    ";
+
+    // Function signature to be parsed by Sphinx
+    rc << name_ << "(";
+    for (size_t i = 0; i < argument_docs_.size(); ++i) {
+        const ArgumentDoc& argument_doc = argument_docs_[i];
+        rc << argument_doc.name_;
+        if (argument_doc.default_ != "") {
+            rc << "=" << argument_doc.default_;
+        }
+        if (i != argument_docs_.size() - 1) {
+            rc << ", ";
+        }
+    }
+    rc << ")" << std::endl;
+
+    // Summary line, strictly speaking this shall be at the very front. However
+    // from a compiled Python module we need the function signature hints in
+    // front for Sphinx parsing and PyCharm autocomplete
+    if (summary_ != "") {
+        rc << std::endl;
+        rc << summary_ << std::endl;
+    }
+
+    // Arguments
+    if (argument_docs_.size() != 0) {
+        rc << std::endl;
+        rc << "Args:" << std::endl;
+        for (const ArgumentDoc& argument_doc : argument_docs_) {
+            rc << indent << argument_doc.name_ << " (" << argument_doc.type_;
+            if (argument_doc.default_ != "") {
+                rc << ", optional";
+            }
+            rc << ")";
+            if (argument_doc.body_ != "") {
+                rc << ": " << argument_doc.body_;
+            }
+            rc << std::endl;
+        }
+    }
+
+    // Return
+    rc << std::endl;
+    rc << "Returns:" << std::endl;
+    rc << indent << return_doc_.type_;
+    if (return_doc_.body_ != "") {
+        rc << ": " << return_doc_.body_;
+    }
+    rc << std::endl;
+
+    return rc.str();
+}
+
+std::string FunctionDoc::NamespaceFix(const std::string& s) {
+    std::string rc = std::regex_replace(s, std::regex("::"), ".");
+    rc = std::regex_replace(rc, std::regex("open3d\\.open3d\\."), "open3d.");
+    return rc;
+}
+
+std::string FunctionDoc::StringCleanAll(const std::string& s,
+                                        const std::string& white_space) {
+    std::string rc = utility::StripString(s, white_space);
+    rc = NamespaceFix(rc);
+    return rc;
+}
+
+ArgumentDoc FunctionDoc::ParseArgumentToken(const std::string& argument_token) {
+    ArgumentDoc argument_doc;
+
+    // Argument with default value
+    std::regex rgx_with_default(
+            "([A-Za-z_][A-Za-z\\d_]*): ([A-Za-z_][A-Za-z\\d_:\\.]*) = (.*)");
+    std::smatch matches;
+    if (std::regex_search(argument_token, matches, rgx_with_default)) {
+        argument_doc.name_ = matches[1].str();
+        argument_doc.type_ = NamespaceFix(matches[2].str());
+        argument_doc.default_ = matches[3].str();
+    } else {
+        // Argument without default value
+        std::regex rgx_without_default(
+                "([A-Za-z_][A-Za-z\\d_]*): ([A-Za-z_][A-Za-z\\d_:\\.]*)");
+        if (std::regex_search(argument_token, matches, rgx_without_default)) {
+            argument_doc.name_ = matches[1].str();
+            argument_doc.type_ = NamespaceFix(matches[2].str());
+        }
+    }
+
+    return argument_doc;
+}
+
+std::vector<std::string> FunctionDoc::GetArgumentTokens(
+        const std::string& pybind_doc) {
+    // First insert commas to make things easy
+    // From:
+    // "foo(arg0: float, arg1: float = 1.0, arg2: int = 1) -> open3d.bar"
+    // To:
+    // "foo(, arg0: float, arg1: float = 1.0, arg2: int = 1) -> open3d.bar"
+    std::string str = pybind_doc;
+    size_t parenthesis_pos = str.find("(");
+    if (parenthesis_pos == std::string::npos) {
+        return {};
+    } else {
+        str.replace(parenthesis_pos + 1, 0, ", ");
+    }
+
+    // Get start positions
+    std::regex pattern("(, [A-Za-z_][A-Za-z\\d_]*:)");
+    std::smatch res;
+    std::string::const_iterator start_iter(str.cbegin());
+    std::vector<size_t> argument_start_positions;
+    while (std::regex_search(start_iter, str.cend(), res, pattern)) {
+        size_t pos = res.position(0) + (start_iter - str.cbegin());
+        start_iter = res.suffix().first;
+        // Now the pos include ", ", which needs to be removed
+        argument_start_positions.push_back(pos + 2);
+    }
+
+    // Get end positions (non-inclusive)
+    // The 1st argument's end pos is 2nd argument's start pos - 2 and etc.
+    // The last argument's end pos is the location of the parenthesis before ->
+    std::vector<size_t> argument_end_positions;
+    for (size_t i = 0; i < argument_start_positions.size() - 1; ++i) {
+        argument_end_positions.push_back(argument_start_positions[i + 1] - 2);
+    }
+    std::size_t arrow_pos = str.rfind(") -> ");
+    if (arrow_pos == std::string::npos) {
+        return {};
+    } else {
+        argument_end_positions.push_back(arrow_pos);
+    }
+
+    std::vector<std::string> argument_tokens;
+    for (size_t i = 0; i < argument_start_positions.size(); ++i) {
+        std::string token = str.substr(
+                argument_start_positions[i],
+                argument_end_positions[i] - argument_start_positions[i]);
+        argument_tokens.push_back(token);
+    }
+    return argument_tokens;
+}
+
+}  // namespace docstring
+}  // namespace open3d
--- src/Python/io/io.cpp
+++ src/Python/io/io.cpp
@@ -25,7 +25,7 @@
 // ----------------------------------------------------------------------------
 
 #include "Python/io/io.h"
-
+#include "Python/docstring.h"
 #include "Open3D/Camera/PinholeCameraIntrinsic.h"
 #include "Open3D/Camera/PinholeCameraTrajectory.h"
 #include "Open3D/IO/ClassIO/FeatureIO.h"
@@ -51,14 +51,17 @@ void pybind_io(py::module &m) {
                  io::ReadImage(filename, image);
                  return image;
              },
-             "Function to read geometry::Image from file", "filename"_a);
+             "Function to read Image from file", "filename"_a);
+    docstring::FunctionDocInject(m_io, "read_image");
+
     m_io.def("write_image",
              [](const std::string &filename, const geometry::Image &image,
                 int quality) {
                  return io::WriteImage(filename, image, quality);
              },
-             "Function to write geometry::Image to file", "filename"_a,
-             "image"_a, "quality"_a = 90);
+             "Function to write Image to file", "filename"_a, "image"_a,
+             "quality"_a = 90);
+    docstring::FunctionDocInject(m_io, "write_image");
 
     // open3d::geometry::LineSet
     m_io.def("read_line_set",
@@ -67,16 +70,19 @@ void pybind_io(py::module &m) {
                  io::ReadLineSet(filename, line_set, format);
                  return line_set;
              },
-             "Function to read geometry::LineSet from file", "filename"_a,
+             "Function to read LineSet from file", "filename"_a,
              "format"_a = "auto");
+    docstring::FunctionDocInject(m_io, "read_line_set");
+
     m_io.def("write_line_set",
              [](const std::string &filename, const geometry::LineSet &line_set,
                 bool write_ascii, bool compressed) {
                  return io::WriteLineSet(filename, line_set, write_ascii,
                                          compressed);
              },
-             "Function to write geometry::LineSet to file", "filename"_a,
-             "line_set"_a, "write_ascii"_a = false, "compressed"_a = false);
+             "Function to write LineSet to file", "filename"_a, "line_set"_a,
+             "write_ascii"_a = false, "compressed"_a = false);
+    docstring::FunctionDocInject(m_io, "write_line_set");
 
     // open3d::geometry::PointCloud
     m_io.def("read_point_cloud",
@@ -85,8 +91,10 @@ void pybind_io(py::module &m) {
                  io::ReadPointCloud(filename, pcd, format);
                  return pcd;
              },
-             "Function to read geometry::PointCloud from file", "filename"_a,
+             "Function to read PointCloud from file", "filename"_a,
              "format"_a = "auto");
+    docstring::FunctionDocInject(m_io, "read_point_cloud");
+
     m_io.def("write_point_cloud",
              [](const std::string &filename,
                 const geometry::PointCloud &pointcloud, bool write_ascii,
@@ -94,8 +102,9 @@ void pybind_io(py::module &m) {
                  return io::WritePointCloud(filename, pointcloud, write_ascii,
                                             compressed);
              },
-             "Function to write geometry::PointCloud to file", "filename"_a,
+             "Function to write PointCloud to file", "filename"_a,
              "pointcloud"_a, "write_ascii"_a = false, "compressed"_a = false);
+    docstring::FunctionDocInject(m_io, "write_point_cloud");
 
     // open3d::geometry::TriangleMesh
     m_io.def("read_triangle_mesh",
@@ -104,15 +113,18 @@ void pybind_io(py::module &m) {
                  io::ReadTriangleMesh(filename, mesh);
                  return mesh;
              },
-             "Function to read geometry::TriangleMesh from file", "filename"_a);
+             "Function to read TriangleMesh from file", "filename"_a);
+    docstring::FunctionDocInject(m_io, "read_triangle_mesh");
+
     m_io.def("write_triangle_mesh",
              [](const std::string &filename, const geometry::TriangleMesh &mesh,
                 bool write_ascii, bool compressed) {
                  return io::WriteTriangleMesh(filename, mesh, write_ascii,
                                               compressed);
              },
-             "Function to write geometry::TriangleMesh to file", "filename"_a,
-             "mesh"_a, "write_ascii"_a = false, "compressed"_a = false);
+             "Function to write TriangleMesh to file", "filename"_a, "mesh"_a,
+             "write_ascii"_a = false, "compressed"_a = false);
+    docstring::FunctionDocInject(m_io, "write_triangle_mesh");
 
     // open3d::geometry::VoxelGrid
     m_io.def("read_voxel_grid",
@@ -121,8 +133,10 @@ void pybind_io(py::module &m) {
                  io::ReadVoxelGrid(filename, voxel_grid, format);
                  return voxel_grid;
              },
-             "Function to read geometry::VoxelGrid from file", "filename"_a,
+             "Function to read VoxelGrid from file", "filename"_a,
              "format"_a = "auto");
+    docstring::FunctionDocInject(m_io, "read_voxel_grid");
+
     m_io.def("write_voxel_grid",
              [](const std::string &filename,
                 const geometry::VoxelGrid &voxel_grid, bool write_ascii,
@@ -130,8 +144,9 @@ void pybind_io(py::module &m) {
                  return io::WriteVoxelGrid(filename, voxel_grid, write_ascii,
                                            compressed);
              },
-             "Function to write geometry::VoxelGrid to file", "filename"_a,
+             "Function to write VoxelGrid to file", "filename"_a,
              "voxel_grid"_a, "write_ascii"_a = false, "compressed"_a = false);
+    docstring::FunctionDocInject(m_io, "write_voxel_grid");
 
     // open3d::camera
     m_io.def("read_pinhole_camera_intrinsic",
@@ -140,45 +155,55 @@ void pybind_io(py::module &m) {
                  io::ReadIJsonConvertible(filename, intrinsic);
                  return intrinsic;
              },
-             "Function to read camera::PinholeCameraIntrinsic from file",
-             "filename"_a);
+             "Function to read PinholeCameraIntrinsic from file", "filename"_a);
+    docstring::FunctionDocInject(m_io, "read_pinhole_camera_intrinsic");
+
     m_io.def("write_pinhole_camera_intrinsic",
              [](const std::string &filename,
                 const camera::PinholeCameraIntrinsic &intrinsic) {
                  return io::WriteIJsonConvertible(filename, intrinsic);
              },
-             "Function to write camera::PinholeCameraIntrinsic to file",
-             "filename"_a, "intrinsic"_a);
+             "Function to write PinholeCameraIntrinsic to file", "filename"_a,
+             "intrinsic"_a);
+    docstring::FunctionDocInject(m_io, "write_pinhole_camera_intrinsic");
+
     m_io.def("read_pinhole_camera_parameters",
              [](const std::string &filename) {
                  camera::PinholeCameraParameters parameters;
                  io::ReadIJsonConvertible(filename, parameters);
                  return parameters;
              },
-             "Function to read camera::PinholeCameraParameters from file",
+             "Function to read PinholeCameraParameters from file",
              "filename"_a);
+    docstring::FunctionDocInject(m_io, "read_pinhole_camera_parameters");
+
     m_io.def("write_pinhole_camera_parameters",
              [](const std::string &filename,
                 const camera::PinholeCameraParameters &parameters) {
                  return io::WriteIJsonConvertible(filename, parameters);
              },
-             "Function to write camera::PinholeCameraParameters to file",
-             "filename"_a, "parameters"_a);
+             "Function to write PinholeCameraParameters to file", "filename"_a,
+             "parameters"_a);
+    docstring::FunctionDocInject(m_io, "write_pinhole_camera_parameters");
+
     m_io.def("read_pinhole_camera_trajectory",
              [](const std::string &filename) {
                  camera::PinholeCameraTrajectory trajectory;
                  io::ReadPinholeCameraTrajectory(filename, trajectory);
                  return trajectory;
              },
-             "Function to read camera::PinholeCameraTrajectory from file",
+             "Function to read PinholeCameraTrajectory from file",
              "filename"_a);
+    docstring::FunctionDocInject(m_io, "read_pinhole_camera_trajectory");
+
     m_io.def("write_pinhole_camera_trajectory",
              [](const std::string &filename,
                 const camera::PinholeCameraTrajectory &trajectory) {
                  return io::WritePinholeCameraTrajectory(filename, trajectory);
              },
-             "Function to write camera::PinholeCameraTrajectory to file",
-             "filename"_a, "trajectory"_a);
+             "Function to write PinholeCameraTrajectory to file", "filename"_a,
+             "trajectory"_a);
+    docstring::FunctionDocInject(m_io, "write_pinhole_camera_trajectory");
 
     // open3d::registration
     m_io.def("read_feature",
@@ -187,27 +212,33 @@ void pybind_io(py::module &m) {
                  io::ReadFeature(filename, feature);
                  return feature;
              },
-             "Function to read registration::Feature from file", "filename"_a);
+             "Function to read registration.Feature from file", "filename"_a);
+    docstring::FunctionDocInject(m_io, "read_feature",
+                                 {{"filename", "Path to file"}});
+
     m_io.def("write_feature",
              [](const std::string &filename,
                 const registration::Feature &feature) {
                  return io::WriteFeature(filename, feature);
              },
-             "Function to write registration::Feature to file", "filename"_a,
-             "feature"_a);
+             "Function to write Feature to file", "filename"_a, "feature"_a);
+    docstring::FunctionDocInject(m_io, "write_feature");
+
     m_io.def("read_pose_graph",
              [](const std::string &filename) {
                  registration::PoseGraph pose_graph;
                  io::ReadPoseGraph(filename, pose_graph);
                  return pose_graph;
              },
-             "Function to read registration::PoseGraph from file",
-             "filename"_a);
+             "Function to read PoseGraph from file", "filename"_a);
+    docstring::FunctionDocInject(m_io, "read_pose_graph");
+
     m_io.def("write_pose_graph",
              [](const std::string &filename,
                 const registration::PoseGraph pose_graph) {
                  io::WritePoseGraph(filename, pose_graph);
              },
-             "Function to write registration::PoseGraph to file", "filename"_a,
+             "Function to write PoseGraph to file", "filename"_a,
              "pose_graph"_a);
+    docstring::FunctionDocInject(m_io, "write_pose_graph");
 }
