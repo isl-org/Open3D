88405975aa3f31aa025a969ec9e66cce22c69a6a
takanokage <danut.petre@gmail.com>
2019-03-12 16:23:51 -0700

    moved the setup code to a higher layer.

--- src/Open3D/Geometry/PointCloud.cu
+++ src/Open3D/Geometry/PointCloud.cu
@@ -30,15 +30,13 @@
 #include "Open3D/Types/Matrix3f.h"
 using namespace open3d;
 
-#include "Open3D/Utility/CUDA.cuh"
-
 #include <iostream>
 using namespace std;
 
 // ---------------------------------------------------------------------------
 // dummy kernel
 // ---------------------------------------------------------------------------
-__global__ void dummy(float* data, int size, float* output) {
+__global__ void dummy(float* data, int nrPoints, float* output) {
     int gid = blockIdx.x * blockDim.x + threadIdx.x;
 
     Vector3f* points = (Vector3f*)data;
@@ -76,38 +74,9 @@ __global__ void dummy(float* data, int size, float* output) {
 // ---------------------------------------------------------------------------
 // helper function calls the dummy kernel
 // ---------------------------------------------------------------------------
-void dummyHost() {
+void dummyHost(float* const d_A, const int& nrPoints, float* const d_C) {
     cout << "dummyHost::START" << endl;
     cout << endl;
-
-    // Error code to check return values for CUDA calls
-    cudaError_t status = cudaSuccess;
-
-    // nr. of dimensions
-    int nrPoints = 1 << 8;
-    cout << "nr. of points:" << nrPoints << endl;
-
-    int inputSize = nrPoints * Vector3f::SIZE;
-    int outputSize = nrPoints * Matrix3f::SIZE;
-
-    // host memory
-    float *h_A = NULL;
-    float *h_C = NULL;
-
-    // device memory
-    float *d_A = NULL;
-    float *d_C = NULL;
-
-    if (!AlocateHstMemory(&h_A, inputSize, "h_A")) exit(1);
-    if (!AlocateHstMemory(&h_C, outputSize, "h_C")) exit(1);
-
-    RandInit(h_A, inputSize);
-
-    if (!AlocateDevMemory(&d_A, inputSize, "d_A")) exit(1);
-    if (!AlocateDevMemory(&d_C, outputSize, "d_C")) exit(1);
-
-    // Copy input to the device
-    CopyHst2DevMemory(h_A, d_A, inputSize);
 
     // Launch the dummy CUDA kernel
     int threadsPerBlock = 256;
@@ -118,35 +87,6 @@ void dummyHost() {
 
     dummy<<<blocksPerGrid, threadsPerBlock>>>(d_A, nrPoints, d_C);
     cudaDeviceSynchronize();
-    status = cudaGetLastError();
-
-    if (status != cudaSuccess)
-    {
-        cout << "status: " << cudaGetErrorString(status) << endl;
-        cout << "Failed to launch vectorAdd kernel" << endl;
-        exit(1);
-    }
-
-    // Copy results to the host
-    CopyDev2HstMemory(d_C, h_C, outputSize);
-
-    Matrix3f* cumulants = (Matrix3f*)h_C;
-    cout << endl;
-    cout << endl;
-    for (int i = 0; i < nrPoints; i++)
-    {
-        Matrix3f c = cumulants[i];
-        printf("%4d: %+6.3f %+6.3f %+6.3f\n      %+6.3f %+6.3f %+6.3f\n      %+6.3f %+6.3f %+6.3f\n",
-        i, c[0][0], c[0][1], c[0][2], c[1][0], c[1][1], c[1][2], c[2][0], c[2][1], c[2][2]);
-    }
-
-    // Free device global memory
-    freeDev(&d_A, "d_A");
-    freeDev(&d_C, "d_C");
-
-    // Free host memory
-    free(h_A);
-    free(h_C);
 
     cout << endl;
     cout << "dummyHost::END" << endl;
