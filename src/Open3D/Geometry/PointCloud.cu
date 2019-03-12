a80a1371d45a977dc217c6bb23e21fbf33205661
takanokage <danut.petre@gmail.com>
2019-03-12 09:52:57 -0700

    dummy code to test use of vector op[] inside kernel.

--- src/Open3D/Geometry/PointCloud.cu
+++ src/Open3D/Geometry/PointCloud.cu
@@ -24,7 +24,24 @@
 // IN THE SOFTWARE.
 // ----------------------------------------------------------------------------
 
-__global__ void dummy(float * points, int dim, int size) {
+#include "Open3D/Types/Vector3f.h"
+using namespace open3d;
+
+__global__ void dummy(Vector3f* points, int size) {
+    // global thread id x
+    int gid = blockIdx.x * blockDim.x + threadIdx.x;
+
+    // nr points / thread
+    int chunkSize = size / gridDim.x / blockDim.x;
+
+    // index 0 of points corresponding to current thread
+    int globalOffset = gid * chunkSize;
+
+    Vector3f* localPoints = &points[globalOffset];
+
+    Vector3f point = localPoints[gid];
+
+    point[0] + point[1] + point[2];
 }
 
 void dummy() {
