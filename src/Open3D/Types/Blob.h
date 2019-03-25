ed8a28a10be7c2c2db42c086bec396789be3e752
takanokage <danut.petre@gmail.com>
2019-03-25 15:04:45 -0700

    added copy constructor and assignment operators. question: how to deal with device data in such cases? updated comments: 'forward the call' -> 'redirect'.

--- src/Open3D/Types/Blob.h
+++ src/Open3D/Types/Blob.h
@@ -36,7 +36,14 @@ template <typename V, typename T>
 struct Blob {
     typedef struct _Type {
         _Type() {}
-        _Type(const int& size) : h_data(size) {}
+        _Type(const int &size) : h_data(size) {}
+
+        // copy constructor
+        // note: how to deal with device data?
+        _Type(const _Type &t)
+            : h_data(t.h_data),
+            //   d_data(t.d_data),
+              cuda_device_id(t.cuda_device_id) {}
 
         // host data container
         std::vector<V> h_data{};
@@ -51,44 +58,67 @@ struct Blob {
         // subscript operator: readonly, host side only
         inline const V &operator[](const uint &i) const { return h_data[i]; }
 
-        // forward the call to std:vector<V>::data()
+        // redirect to std:vector<V>::operator=(...)
+        // note: how to deal with device data?
+        inline _Type &operator=(const _Type &t) {
+            h_data = std::vector<V>(t.h_data);
+            // d_data = t.d_data;
+
+            return *this;
+        }
+        // redirect to std:vector<V>::operator=(...)
+        // note: how to deal with device data?
+        inline _Type &operator=(_Type &&t) {
+            h_data = t.h_data;
+            // d_data = t.d_data;
+
+            return *this;
+        }
+        // redirect to std:vector<V>::operator=(...)
+        inline _Type &operator=(std::initializer_list<V> il) {
+            return h_data = il;
+
+            return *this;
+        }
+
+        // redirect to std:vector<V>::data()
         inline V *data() noexcept { return h_data.data(); }
-        // forward the call to std:vector<V>::data()
+        // redirect to std:vector<V>::data()
         inline const V *data() const noexcept { return h_data.data(); }
-        // forward the call to std:vector<V>::begin()
+        // redirect to std:vector<V>::begin()
         inline typename std::vector<V>::iterator begin() noexcept {
             return h_data.begin();
         }
-        // forward the call to std:vector<V>::begin()
+        // redirect to std:vector<V>::begin()
         inline typename std::vector<V>::const_iterator begin() const noexcept {
             return h_data.begin();
         }
-        // forward the call to std:vector<V>::clear()
+        // redirect to std:vector<V>::clear()
         inline void clear() noexcept { h_data.clear(); }
-        // forward the call to std:vector<V>::end()
+        // redirect to std:vector<V>::end()
         inline typename std::vector<V>::iterator end() noexcept {
             return h_data.end();
         }
-        // forward the call to std:vector<V>::end()
+        // redirect to std:vector<V>::end()
         inline typename std::vector<V>::const_iterator end() const noexcept {
             return h_data.end();
         }
-        // forward the call to std:vector<V>::empty()
+        // redirect to std:vector<V>::empty()
         inline bool empty() const noexcept { return h_data.empty(); }
-        // forward the call to std:vector<V>::insert(...)
+        // redirect to std:vector<V>::insert(...)
         inline typename std::vector<V>::iterator insert(
                 typename std::vector<V>::const_iterator position,
                 const V &val) {
             h_data.insert(position, val);
         }
-        // forward the call to std:vector<V>::insert(...)
+        // redirect to std:vector<V>::insert(...)
         inline typename std::vector<V>::iterator insert(
                 typename std::vector<V>::const_iterator position,
                 size_t n,
                 const V &val) {
             h_data.insert(position, n, val);
         }
-        // forward the call to std:vector<V>::insert(...)
+        // redirect to std:vector<V>::insert(...)
         template <typename InputIterator>
         inline typename std::vector<V>::iterator insert(
                 typename std::vector<V>::const_iterator position,
@@ -96,25 +126,26 @@ struct Blob {
                 InputIterator last) {
             h_data.insert(position, first, last);
         }
-        // forward the call to std:vector<V>::insert(...)
+        // redirect to std:vector<V>::insert(...)
         inline typename std::vector<V>::iterator insert(
                 typename std::vector<V>::const_iterator position, V &&val) {
             h_data.insert(position, val);
         }
-        // forward the call to std:vector<V>::insert(...)
-        inline typename std::vector<V>::iterator insert (typename
-        std::vector<V>::const_iterator position, std::initializer_list<V> il) {
+        // redirect to std:vector<V>::insert(...)
+        inline typename std::vector<V>::iterator insert(
+                typename std::vector<V>::const_iterator position,
+                std::initializer_list<V> il) {
             h_data.insert(position, il);
         }
-        // forward the call to std:vector<V>::push_back(...)
+        // redirect to std:vector<V>::push_back(...)
         inline void push_back(const V &val) { h_data.push_back(val); }
-        // forward the call to std:vector<V>::push_back(...)
+        // redirect to std:vector<V>::push_back(...)
         inline void push_back(V &&val) { h_data.push_back(val); }
-        // forward the call to std:vector<V>::resize(...)
+        // redirect to std:vector<V>::resize(...)
         inline void resize(size_t n) { h_data.resize(n); }
-        // forward the call to std:vector<V>::resize(...)
+        // redirect to std:vector<V>::resize(...)
         inline void resize(size_t n, const V &val) { h_data.resize(n, val); }
-        // forward the call to std:vector<V>::size()
+        // redirect to std:vector<V>::size()
         inline size_t size() const { return h_data.size(); }
     } Type;
 };
