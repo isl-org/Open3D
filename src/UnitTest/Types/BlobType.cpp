7db38c7d110933b625e68273b5810d64f9629f2e
takanokage <danut.petre@gmail.com>
2019-04-03 13:51:01 -0700

    BlobType: added unit test for Initialize(...) with args.

--- src/UnitTest/Types/BlobType.cpp
+++ src/UnitTest/Types/BlobType.cpp
@@ -331,6 +331,34 @@ TEST(BlobType, Initialize) {
 }
 
 // ----------------------------------------------------------------------------
+// Initialize - with args - CPU and GPU.
+// ----------------------------------------------------------------------------
+TEST(BlobType, Initialize_args) {
+    size_t num_elements = 100;
+    size_t num_doubles = num_elements * sizeof(Eigen::Vector3d) / sizeof(double);
+    open3d::cuda::DeviceID::Type device_id = (open3d::cuda::DeviceID::Type)(
+            open3d::cuda::DeviceID::CPU | open3d::cuda::DeviceID::GPU_00);
+
+    open3d::Blob3d b0;
+
+    EXPECT_EQ(b0.num_elements, 0);
+    EXPECT_TRUE(open3d::cuda::DeviceID::CPU & b0.device_id);
+    EXPECT_EQ(b0.h_data.size(), 0);
+    EXPECT_TRUE(b0.h_data.empty());
+    EXPECT_TRUE(NULL == b0.d_data);
+    EXPECT_EQ(b0.size(), 0);
+
+    b0.Initialize(num_elements, device_id);
+
+    EXPECT_EQ(b0.num_elements, num_elements);
+    EXPECT_TRUE(b0.device_id == device_id);
+    EXPECT_EQ(b0.h_data.size(), num_elements);
+    EXPECT_FALSE(b0.h_data.empty());
+    EXPECT_TRUE(NULL != b0.d_data);
+    EXPECT_EQ(b0.size(), num_elements);
+}
+
+// ----------------------------------------------------------------------------
 // Reset - CPU and GPU.
 // ----------------------------------------------------------------------------
 TEST(BlobType, Reset) {
