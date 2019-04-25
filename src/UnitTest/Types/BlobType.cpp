7deae749c54292352f760f065505258fa14a1f43
takanokage <danut.petre@gmail.com>
2019-04-25 14:17:08 -0700

    added Reset unit test for CPU only.

--- src/UnitTest/Types/BlobType.cpp
+++ src/UnitTest/Types/BlobType.cpp
@@ -381,6 +381,34 @@ TEST(BlobType, Initialize_args) {
 }
 #endif
 
+// ----------------------------------------------------------------------------
+// Reset - CPU.
+// ----------------------------------------------------------------------------
+TEST(BlobType, Reset_CPU) {
+    size_t num_elements = 100;
+    size_t num_doubles =
+            num_elements * sizeof(Eigen::Vector3d) / sizeof(double);
+    open3d::DeviceID::Type device_id = open3d::DeviceID::CPU;
+
+    open3d::Blob3d b0(num_elements, device_id);
+
+    EXPECT_EQ(b0.num_elements, num_elements);
+    EXPECT_EQ(b0.device_id, open3d::DeviceID::CPU);
+    EXPECT_EQ(b0.h_data.size(), num_elements);
+    EXPECT_FALSE(b0.h_data.empty());
+    EXPECT_TRUE(NULL == b0.d_data);
+    EXPECT_EQ(b0.size(), num_elements);
+
+    b0.Reset();
+
+    EXPECT_EQ(b0.num_elements, 0);
+    EXPECT_EQ(b0.device_id, open3d::DeviceID::CPU);
+    EXPECT_EQ(b0.h_data.size(), 0);
+    EXPECT_TRUE(b0.h_data.empty());
+    EXPECT_TRUE(NULL == b0.d_data);
+    EXPECT_EQ(b0.size(), 0);
+}
+
 #ifdef OPEN3D_USE_CUDA
 // ----------------------------------------------------------------------------
 // Reset - CPU and GPU.
