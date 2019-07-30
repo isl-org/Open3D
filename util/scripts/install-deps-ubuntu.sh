e0661d92772994f33aa54eaea70aa5b4201622ba
Yixing Lao <yixing.lao@gmail.com>
2019-07-30 16:59:30 -0700

    clean up

--- util/scripts/install-deps-ubuntu.sh
+++ util/scripts/install-deps-ubuntu.sh
@@ -10,6 +10,5 @@ sudo apt-get --yes install libjsoncpp-dev || true
 sudo apt-get --yes install libeigen3-dev || true
 sudo apt-get --yes install libpng-dev || true
 sudo apt-get --yes install libpng16-dev || true
-sudo apt-get --yes install libjpeg-dev || true
 sudo apt-get --yes install python-dev python-tk || true
 sudo apt-get --yes install python3-dev python3-tk || true
