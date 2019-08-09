dfb9500fff5c8474495031c3aa2fde3fb4af458a
Yixing Lao <yixing.lao@gmail.com>
2019-08-09 01:26:07 -0700

    implement ConvertToK4AConfig

--- src/Open3D/IO/Sensor/AzureKinect/AzureKinectSensorConfig.cpp
+++ src/Open3D/IO/Sensor/AzureKinect/AzureKinectSensorConfig.cpp
@@ -97,6 +97,67 @@ static std::string k4a_wired_sync_mode_t_to_string(
     return convert_map[k4a_wired_sync_mode];
 }
 
+///////////////////////////////////
+static k4a_image_format_t string_to_k4a_image_format_t(
+        const std::string &value) {
+    static std::unordered_map<std::string, k4a_image_format_t> convert_map({
+            {"K4A_IMAGE_FORMAT_COLOR_MJPG", K4A_IMAGE_FORMAT_COLOR_MJPG},
+            {"K4A_IMAGE_FORMAT_COLOR_NV12", K4A_IMAGE_FORMAT_COLOR_NV12},
+            {"K4A_IMAGE_FORMAT_COLOR_YUY2", K4A_IMAGE_FORMAT_COLOR_YUY2},
+            {"K4A_IMAGE_FORMAT_COLOR_BGRA32", K4A_IMAGE_FORMAT_COLOR_BGRA32},
+            {"K4A_IMAGE_FORMAT_DEPTH16", K4A_IMAGE_FORMAT_DEPTH16},
+            {"K4A_IMAGE_FORMAT_IR16", K4A_IMAGE_FORMAT_IR16},
+            {"K4A_IMAGE_FORMAT_CUSTOM", K4A_IMAGE_FORMAT_CUSTOM},
+    });
+    return convert_map[value];
+}
+
+static k4a_color_resolution_t string_to_k4a_color_resolution_t(
+        const std::string &value) {
+    static std::unordered_map<std::string, k4a_color_resolution_t> convert_map({
+            {"K4A_COLOR_RESOLUTION_OFF", K4A_COLOR_RESOLUTION_OFF},
+            {"K4A_COLOR_RESOLUTION_720P", K4A_COLOR_RESOLUTION_720P},
+            {"K4A_COLOR_RESOLUTION_1080P", K4A_COLOR_RESOLUTION_1080P},
+            {"K4A_COLOR_RESOLUTION_1440P", K4A_COLOR_RESOLUTION_1440P},
+            {"K4A_COLOR_RESOLUTION_1536P", K4A_COLOR_RESOLUTION_1536P},
+            {"K4A_COLOR_RESOLUTION_2160P", K4A_COLOR_RESOLUTION_2160P},
+            {"K4A_COLOR_RESOLUTION_3072P", K4A_COLOR_RESOLUTION_3072P},
+    });
+    return convert_map[value];
+}
+
+static k4a_depth_mode_t string_to_k4a_depth_mode_t(const std::string &value) {
+    static std::unordered_map<std::string, k4a_depth_mode_t> convert_map({
+            {"K4A_DEPTH_MODE_OFF", K4A_DEPTH_MODE_OFF},
+            {"K4A_DEPTH_MODE_NFOV_2X2BINNED", K4A_DEPTH_MODE_NFOV_2X2BINNED},
+            {"K4A_DEPTH_MODE_NFOV_UNBINNED", K4A_DEPTH_MODE_NFOV_UNBINNED},
+            {"K4A_DEPTH_MODE_WFOV_2X2BINNED", K4A_DEPTH_MODE_WFOV_2X2BINNED},
+            {"K4A_DEPTH_MODE_WFOV_UNBINNED", K4A_DEPTH_MODE_WFOV_UNBINNED},
+            {"K4A_DEPTH_MODE_PASSIVE_IR", K4A_DEPTH_MODE_PASSIVE_IR},
+    });
+    return convert_map[value];
+}
+
+static k4a_fps_t string_to_k4a_fps_t(const std::string &value) {
+    static std::unordered_map<std::string, k4a_fps_t> convert_map({
+            {"K4A_FRAMES_PER_SECOND_5", K4A_FRAMES_PER_SECOND_5},
+            {"K4A_FRAMES_PER_SECOND_15", K4A_FRAMES_PER_SECOND_15},
+            {"K4A_FRAMES_PER_SECOND_30", K4A_FRAMES_PER_SECOND_30},
+    });
+    return convert_map[value];
+}
+
+static k4a_wired_sync_mode_t string_to_k4a_wired_sync_mode_t(
+        const std::string &value) {
+    static std::unordered_map<std::string, k4a_wired_sync_mode_t> convert_map({
+            {"K4A_WIRED_SYNC_MODE_STANDALONE", K4A_WIRED_SYNC_MODE_STANDALONE},
+            {"K4A_WIRED_SYNC_MODE_MASTER", K4A_WIRED_SYNC_MODE_MASTER},
+            {"K4A_WIRED_SYNC_MODE_SUBORDINATE",
+             K4A_WIRED_SYNC_MODE_SUBORDINATE},
+    });
+    return convert_map[value];
+}
+
 AzureKinectSensorConfig::AzureKinectSensorConfig() {
     config_["color_format"] = "K4A_IMAGE_FORMAT_COLOR_MJPG";
     config_["color_resolution"] = "K4A_COLOR_RESOLUTION_1080P";
@@ -142,24 +203,30 @@ bool AzureKinectSensorConfig::ConvertFromJsonValue(const Json::Value &value) {
 
 void AzureKinectSensorConfig::ConvertFromNativeConfig(
         const k4a_device_configuration_t &k4a_config) {
-    config_["color_format"] =
-            k4a_image_format_t_to_string(k4a_config.color_format);
-    config_["color_resolution"] =
-            k4a_color_resolution_t_to_string(k4a_config.color_resolution);
-    config_["depth_mode"] = k4a_depth_mode_t_to_string(k4a_config.depth_mode);
-    config_["camera_fps"] = k4a_fps_t_to_string(k4a_config.camera_fps);
-    config_["wired_sync_mode"] =
-            k4a_wired_sync_mode_t_to_string(k4a_config.wired_sync_mode);
-    config_["depth_delay_off_color_usec"] =
-            std::to_string(k4a_config.depth_delay_off_color_usec);
-    config_["subordinate_delay_off_master_usec"] =
-            std::to_string(k4a_config.subordinate_delay_off_master_usec);
-    config_["disable_streaming_indicator"] =
-            k4a_config.disable_streaming_indicator ? "true" : "false";
+    // clang-format off
+    config_["color_format"]                      = k4a_image_format_t_to_string(k4a_config.color_format);
+    config_["color_resolution"]                  = k4a_color_resolution_t_to_string(k4a_config.color_resolution);
+    config_["depth_mode"]                        = k4a_depth_mode_t_to_string(k4a_config.depth_mode);
+    config_["camera_fps"]                        = k4a_fps_t_to_string(k4a_config.camera_fps);
+    config_["wired_sync_mode"]                   = k4a_wired_sync_mode_t_to_string(k4a_config.wired_sync_mode);
+    config_["depth_delay_off_color_usec"]        = std::to_string(k4a_config.depth_delay_off_color_usec);
+    config_["subordinate_delay_off_master_usec"] = std::to_string(k4a_config.subordinate_delay_off_master_usec);
+    config_["disable_streaming_indicator"]       = k4a_config.disable_streaming_indicator ? "true" : "false";
+    // clang-format on
 }
 
 k4a_device_configuration_t AzureKinectSensorConfig::ConvertToK4AConfig() {
     k4a_device_configuration_t k4a_config;
+    // clang-format off
+    k4a_config.color_format                      = string_to_k4a_image_format_t(config_["color_format"]);
+    k4a_config.color_resolution                  = string_to_k4a_color_resolution_t(config_["color_resolution"]);
+    k4a_config.depth_mode                        = string_to_k4a_depth_mode_t(config_["depth_mode"]);
+    k4a_config.camera_fps                        = string_to_k4a_fps_t(config_["camera_fps"]);
+    k4a_config.wired_sync_mode                   = string_to_k4a_wired_sync_mode_t(config_["wired_sync_mode"]);
+    k4a_config.depth_delay_off_color_usec        = static_cast<int32_t>(std::stoi(config_["depth_delay_off_color_usec"]));
+    k4a_config.subordinate_delay_off_master_usec = static_cast<uint32_t>(std::stoi(config_["subordinate_delay_off_master_usec"]));
+    k4a_config.disable_streaming_indicator       = config_["disable_streaming_indicator"] == "true";
+    // clang-format on
     return k4a_config;
 }
 
