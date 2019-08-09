1b8b86cd151d4e22f36a5abc3f9d8b3586829882
Yixing Lao <yixing.lao@gmail.com>
2019-08-09 01:11:29 -0700

    conversion working

--- src/UnitTest/IO/Sensor/AzureKinect/AzureKinectSensorConfig.cpp
+++ src/UnitTest/IO/Sensor/AzureKinect/AzureKinectSensorConfig.cpp
@@ -64,18 +64,30 @@ TEST(AzureKinectSensorConfig, CustomConstructor) {
 }
 
 TEST(AzureKinectSensorConfig, ConvertFromNativeConfig) {
+    // Use non-default values to check
     k4a_device_configuration_t k4a_config = K4A_DEVICE_CONFIG_INIT_DISABLE_ALL;
-    k4a_config.color_format = K4A_IMAGE_FORMAT_COLOR_MJPG;
-    k4a_config.color_resolution = K4A_COLOR_RESOLUTION_1080P;
-    k4a_config.depth_mode = K4A_DEPTH_MODE_WFOV_2X2BINNED;
-    k4a_config.camera_fps = K4A_FRAMES_PER_SECOND_30;
-    k4a_config.wired_sync_mode = K4A_WIRED_SYNC_MODE_STANDALONE;
-    k4a_config.depth_delay_off_color_usec = 0;
-    k4a_config.subordinate_delay_off_master_usec = 0;
-    k4a_config.disable_streaming_indicator = false;
+    k4a_config.color_format = K4A_IMAGE_FORMAT_COLOR_NV12;
+    k4a_config.color_resolution = K4A_COLOR_RESOLUTION_720P;
+    k4a_config.depth_mode = K4A_DEPTH_MODE_WFOV_UNBINNED;
+    k4a_config.camera_fps = K4A_FRAMES_PER_SECOND_5;
+    k4a_config.wired_sync_mode = K4A_WIRED_SYNC_MODE_MASTER;
+    k4a_config.depth_delay_off_color_usec = 12;
+    k4a_config.subordinate_delay_off_master_usec = 34;
+    k4a_config.disable_streaming_indicator = true;
+
+    std::unordered_map<std::string, std::string> ref_config;
+    ref_config["color_format"] = "K4A_IMAGE_FORMAT_COLOR_NV12";
+    ref_config["color_resolution"] = "K4A_COLOR_RESOLUTION_720P";
+    ref_config["depth_mode"] = "K4A_DEPTH_MODE_WFOV_UNBINNED";
+    ref_config["camera_fps"] = "K4A_FRAMES_PER_SECOND_5";
+    ref_config["wired_sync_mode"] = "K4A_WIRED_SYNC_MODE_MASTER";
+    ref_config["depth_delay_off_color_usec"] = "12";
+    ref_config["subordinate_delay_off_master_usec"] = "34";
+    ref_config["disable_streaming_indicator"] = "true";
 
     io::AzureKinectSensorConfig kinect_config;
     kinect_config.ConvertFromNativeConfig(k4a_config);
+    EXPECT_TRUE(kinect_config.config_ == ref_config);
 }
 
 TEST(AzureKinectSensorConfig, ConvertToNativeConfig) {}
