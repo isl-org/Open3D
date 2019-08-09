2c98178fc5873032dea95c045f2df79fecd51131
Yixing Lao <yixing.lao@gmail.com>
2019-08-09 00:59:26 -0700

    config to string conversion

--- src/Open3D/IO/Sensor/AzureKinect/AzureKinectSensorConfig.cpp
+++ src/Open3D/IO/Sensor/AzureKinect/AzureKinectSensorConfig.cpp
@@ -37,7 +37,7 @@
 namespace open3d {
 namespace io {
 
-static std::string k4a_image_format_to_string(
+static std::string k4a_image_format_t_to_string(
         k4a_image_format_t k4a_image_format) {
     static std::unordered_map<k4a_image_format_t, std::string> convert_map({
             {K4A_IMAGE_FORMAT_COLOR_MJPG, "K4A_IMAGE_FORMAT_COLOR_MJPG"},
@@ -49,6 +49,52 @@ static std::string k4a_image_format_to_string(
             {K4A_IMAGE_FORMAT_CUSTOM, "K4A_IMAGE_FORMAT_CUSTOM"},
     });
     return convert_map[k4a_image_format];
+}
+
+static std::string k4a_color_resolution_t_to_string(
+        k4a_color_resolution_t k4a_color_resolution) {
+    static std::unordered_map<k4a_color_resolution_t, std::string> convert_map({
+            {K4A_COLOR_RESOLUTION_OFF, "K4A_COLOR_RESOLUTION_OFF"},
+            {K4A_COLOR_RESOLUTION_720P, "K4A_COLOR_RESOLUTION_720P"},
+            {K4A_COLOR_RESOLUTION_1080P, "K4A_COLOR_RESOLUTION_1080P"},
+            {K4A_COLOR_RESOLUTION_1440P, "K4A_COLOR_RESOLUTION_1440P"},
+            {K4A_COLOR_RESOLUTION_1536P, "K4A_COLOR_RESOLUTION_1536P"},
+            {K4A_COLOR_RESOLUTION_2160P, "K4A_COLOR_RESOLUTION_2160P"},
+            {K4A_COLOR_RESOLUTION_3072P, "K4A_COLOR_RESOLUTION_3072P"},
+    });
+    return convert_map[k4a_color_resolution];
+}
+
+static std::string k4a_depth_mode_t_to_string(k4a_depth_mode_t k4a_depth_mode) {
+    static std::unordered_map<k4a_depth_mode_t, std::string> convert_map({
+            {K4A_DEPTH_MODE_OFF, "K4A_DEPTH_MODE_OFF"},
+            {K4A_DEPTH_MODE_NFOV_2X2BINNED, "K4A_DEPTH_MODE_NFOV_2X2BINNED"},
+            {K4A_DEPTH_MODE_NFOV_UNBINNED, "K4A_DEPTH_MODE_NFOV_UNBINNED"},
+            {K4A_DEPTH_MODE_WFOV_2X2BINNED, "K4A_DEPTH_MODE_WFOV_2X2BINNED"},
+            {K4A_DEPTH_MODE_WFOV_UNBINNED, "K4A_DEPTH_MODE_WFOV_UNBINNED"},
+            {K4A_DEPTH_MODE_PASSIVE_IR, "K4A_DEPTH_MODE_PASSIVE_IR"},
+    });
+    return convert_map[k4a_depth_mode];
+}
+
+static std::string k4a_fps_t_to_string(k4a_fps_t k4a_fps) {
+    static std::unordered_map<k4a_fps_t, std::string> convert_map({
+            {K4A_FRAMES_PER_SECOND_5, "K4A_FRAMES_PER_SECOND_5"},
+            {K4A_FRAMES_PER_SECOND_15, "K4A_FRAMES_PER_SECOND_15"},
+            {K4A_FRAMES_PER_SECOND_30, "K4A_FRAMES_PER_SECOND_30"},
+    });
+    return convert_map[k4a_fps];
+}
+
+static std::string k4a_wired_sync_mode_t_to_string(
+        k4a_wired_sync_mode_t k4a_wired_sync_mode) {
+    static std::unordered_map<k4a_wired_sync_mode_t, std::string> convert_map({
+            {K4A_WIRED_SYNC_MODE_STANDALONE, "K4A_WIRED_SYNC_MODE_STANDALONE"},
+            {K4A_WIRED_SYNC_MODE_MASTER, "K4A_WIRED_SYNC_MODE_MASTER"},
+            {K4A_WIRED_SYNC_MODE_SUBORDINATE,
+             "K4A_WIRED_SYNC_MODE_SUBORDINATE"},
+    });
+    return convert_map[k4a_wired_sync_mode];
 }
 
 AzureKinectSensorConfig::AzureKinectSensorConfig() {
@@ -97,7 +143,13 @@ bool AzureKinectSensorConfig::ConvertFromJsonValue(const Json::Value &value) {
 void AzureKinectSensorConfig::ConvertFromNativeConfig(
         const k4a_device_configuration_t &k4a_config) {
     config_["color_format"] =
-            k4a_image_format_to_string(k4a_config.color_format);
+            k4a_image_format_t_to_string(k4a_config.color_format);
+    config_["color_resolution"] =
+            k4a_color_resolution_t_to_string(k4a_config.color_resolution);
+    config_["depth_mode"] = k4a_depth_mode_t_to_string(k4a_config.depth_mode);
+    config_["camera_fps"] = k4a_fps_t_to_string(k4a_config.camera_fps);
+    config_["wired_sync_mode"] =
+            k4a_wired_sync_mode_t_to_string(k4a_config.wired_sync_mode);
 }
 
 k4a_device_configuration_t AzureKinectSensorConfig::ConvertToK4AConfig() {
