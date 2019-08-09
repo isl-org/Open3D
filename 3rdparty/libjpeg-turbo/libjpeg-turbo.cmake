ca3646afb7347467c434834bfa8e804f50544cdc
Yixing Lao <yixing.lao@gmail.com>
2019-08-08 21:44:33 -0700

    Replace libjpeg with libjpeg-turbo (#1097)
    * add jpeg turbo
    
    * clean up
    
    * add cmake error message
    
    * upgrade cmake version for travis
    
    * try to fix windows shared library name
    
    * add policy cmake_policy(SET CMP0054 NEW)
    
    * fix policy
    
    * try resetting travis path
    
    * try resetting travis path
    
    * do not use export
    
    * use external project approach
    
    * fix policy
    
    * support older cmake version
    
    * integrate turbojpeg
    
    * merge changes from recorder branch
    
    * use a dedicated 3rdparty install directory
    
    * turbo jpeg enabled_shared=off
    
    * disable shared lib
    
    * fix library name
    
    * fix link directories on windows

---
+++ 3rdparty/libjpeg-turbo/libjpeg-turbo.cmake
@@ -0,0 +1,87 @@
+include(ExternalProject)
+
+# Set compiler flags
+if ("${CMAKE_C_COMPILER_ID}" STREQUAL "MSVC")
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} /D_CRT_SECURE_NO_WARNINGS")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /D_CRT_SECURE_NO_WARNINGS")
+endif()
+
+# Set WITH_SIMD
+include(CheckLanguage)
+check_language(ASM_NASM)
+if (CMAKE_ASM_NASM_COMPILER)
+    if (APPLE)
+        # macOS might have /usr/bin/nasm but it cannot be used
+        # https://stackoverflow.com/q/53974320
+        # To fix this, run `brew install nasm`
+        execute_process(COMMAND nasm --version RESULT_VARIABLE return_code)
+        if("${return_code}" STREQUAL "0")
+            enable_language(ASM_NASM)
+            option(WITH_SIMD "" ON)
+        else()
+            message(STATUS "nasm found but can't be used, run `brew install nasm`")
+            option(WITH_SIMD "" OFF)
+        endif()
+    else()
+        enable_language(ASM_NASM)
+        option(WITH_SIMD "" ON)
+    endif()
+else()
+    option(WITH_SIMD "" OFF)
+endif()
+if (WITH_SIMD)
+    message(STATUS "NASM assembler enabled")
+else()
+    message(STATUS "NASM assembler not found - libjpeg-turbo performance may suffer")
+endif()
+
+ExternalProject_Add(
+    ext_turbojpeg
+    PREFIX turbojpeg
+    SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/libjpeg-turbo/libjpeg-turbo
+    UPDATE_COMMAND ""
+    CMAKE_GENERATOR ${CMAKE_GENERATOR}
+    CMAKE_GENERATOR_PLATFORM ${CMAKE_GENERATOR_PLATFORM}
+    CMAKE_GENERATOR_TOOLSET ${CMAKE_GENERATOR_TOOLSET}
+    CMAKE_ARGS
+        -DCMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS}
+        -DCMAKE_C_FLAGS=${DCMAKE_C_FLAGS}
+        -DENABLE_STATIC=ON
+        -DENABLE_SHARED=OFF
+        -DWITH_SIMD=${WITH_SIMD}
+        -DCMAKE_INSTALL_PREFIX=${3RDPARTY_INSTALL_PREFIX}
+        -DCMAKE_POSITION_INDEPENDENT_CODE=ON
+)
+
+add_library(turbojpeg INTERFACE)
+add_dependencies(turbojpeg ext_turbojpeg)
+
+# If MSVC, the OUTPUT_NAME was set to turbojpeg-static
+if(MSVC)
+    set(lib_name "turbojpeg-static")
+else()
+    set(lib_name "turbojpeg")
+endif()
+
+# For linking with Open3D's after installation
+set(JPEG_TURBO_LIBRARIES ${lib_name})
+
+set(turbojpeg_LIB_FILES
+    ${3RDPARTY_INSTALL_PREFIX}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}${lib_name}${CMAKE_STATIC_LIBRARY_SUFFIX}
+)
+
+target_include_directories(turbojpeg SYSTEM INTERFACE
+    ${3RDPARTY_INSTALL_PREFIX}/include
+)
+target_link_libraries(turbojpeg INTERFACE
+    ${turbojpeg_LIB_FILES}
+)
+
+if (NOT BUILD_SHARED_LIBS)
+    install(FILES ${turbojpeg_LIB_FILES}
+            DESTINATION ${CMAKE_INSTALL_PREFIX}/lib)
+endif()
+
+add_dependencies(build_all_3rd_party_libs turbojpeg)
+
+
--- util/scripts/install-deps-osx.sh
+++ util/scripts/install-deps-osx.sh
@@ -2,7 +2,7 @@
 set -e
 
 which -s brew
-if [[ $? != 0 ]] ; then
+if [[ $? != 0 ]]; then
     echo "Please install Homebrew, follow the instructions on:"
     echo ""
     echo "        http://brew.sh/"
@@ -15,10 +15,10 @@ else
     brew update
 fi
 
-for pkg in libusb glew glfw3 libpng jpeg pkg-config jsoncpp eigen; do
+for pkg in libusb glew glfw3 libpng pkg-config jsoncpp eigen; do
     if brew list -1 | grep -q "^${pkg}\$"; then
         echo "Package '$pkg' has already been installed and is being upgraded ..."
-        brew upgrade $pkg || true  # Upgrade might cause error when already installed
+        brew upgrade $pkg || true # Upgrade might cause error when already installed
     else
         echo "Package '$pkg' is being installed ..."
         brew install $pkg
