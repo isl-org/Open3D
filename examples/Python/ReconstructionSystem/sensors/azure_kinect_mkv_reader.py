d75e25b044b9b3e9fffa5acbac478b2790499438
Yixing Lao <yixing.lao@gmail.com>
2019-08-27 05:52:04 -0700

    convert to class style

--- examples/Python/ReconstructionSystem/sensors/azure_kinect_mkv_reader.py
+++ examples/Python/ReconstructionSystem/sensors/azure_kinect_mkv_reader.py
@@ -8,79 +8,87 @@ pwd = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.join(pwd, '..'))
 from initialize_config import initialize_config
 
-flag_stop = False
-flag_play = True
+class ReaderWithCallback:
+
+    def __init__(self, input, output):
+        self.flag_exit = False
+        self.flag_play = True
+        self.input = input
+        self.output = output
+
+        self.reader = o3d.io.AzureKinectMKVReader()
+        self.reader.open(self.input)
+        if not self.reader.is_opened():
+            raise RuntimeError("Unable to open file {}".format(args.input))
+
+    def escape_callback(self, vis):
+        self.flag_exit = True
+        return False
 
 
-def escape_callback(vis):
-    global flag_stop
-    flag_stop = True
-    return False
+    def space_callback(self, vis):
+        if self.flag_play:
+            print('Playback paused, press [SPACE] to continue.')
+        else:
+            print('Playback resumed, press [SPACE] to pause.')
+        self.flag_play = not self.flag_play
+        return False
 
 
-def space_callback(vis):
-    global flag_play
-    if flag_play:
-        print('Playback paused, press [SPACE] to continue.')
-    else:
-        print('Playback resumed, press [SPACE] to pause.')
+    def run(self):
+        glfw_key_escape = 256
+        glfw_key_space = 32
+        vis = o3d.visualization.VisualizerWithKeyCallback()
+        vis.register_key_callback(glfw_key_escape, self.escape_callback)
+        vis.register_key_callback(glfw_key_space, self.space_callback)
 
-    flag_play = not flag_play
-    return False
+        vis_geometry_added = False
+        vis.create_window('reader', 1920, 540)
 
+        print("MKV reader initialized. Press [SPACE] to start, [ESC] to exit.")
 
-def main(reader, output_path):
-    glfw_key_escape = 256
-    glfw_key_space = 32
-    vis = o3d.visualization.VisualizerWithKeyCallback()
-    vis.register_key_callback(glfw_key_escape, escape_callback)
-    vis.register_key_callback(glfw_key_space, space_callback)
+        if self.output is not None:
+            abspath = os.path.abspath(self.output)
+            metadata = self.reader.get_metadata()
+            o3d.io.write_azure_kinect_mkv_metadata(
+                '{}/intrinsic.json'.format(abspath), metadata)
 
-    vis_geometry_added = False
-    vis.create_window('reader', 1920, 540)
+            config = {
+                'path_dataset': abspath,
+                'path_intrinsic': '{}/intrinsic.json'.format(abspath)
+            }
+            initialize_config(config)
+            with open('{}/config.json'.format(abspath), 'w') as f:
+                json.dump(config, f, indent=4)
 
-    print("MKV reader initialized. Press [SPACE] to start, [ESC] to exit.")
+        idx = 0
+        while not self.reader.is_eof() and not self.flag_exit:
+            if self.flag_play:
+                rgbd = self.reader.next_frame()
+                if rgbd is None:
+                    continue
 
-    if output_path is not None:
-        abspath = os.path.abspath(output_path)
-        metadata = reader.get_metadata()
-        o3d.io.write_azure_kinect_mkv_metadata(
-            '{}/intrinsic.json'.format(abspath), metadata)
+                if not vis_geometry_added:
+                    vis.add_geometry(rgbd)
+                    vis_geometry_added = True
 
-        config = {
-            'path_dataset': abspath,
-            'path_intrinsic': '{}/intrinsic.json'.format(abspath)
-        }
-        initialize_config(config)
-        with open('{}/config.json'.format(abspath), 'w') as f:
-            json.dump(config, f, indent=4)
+                if self.output is not None:
+                    color_filename = '{0}/color/{1:05d}.jpg'.format(
+                        self.output, idx)
+                    print('Writing to {}'.format(color_filename))
+                    o3d.io.write_image(color_filename, rgbd.color)
 
-    idx = 0
-    while not reader.is_eof() and not flag_stop:
-        if flag_play:
-            rgbd = reader.next_frame()
-            if rgbd is None:
-                continue
+                    depth_filename = '{0}/depth/{1:05d}.png'.format(
+                        self.output, idx)
+                    print('Writing to {}'.format(depth_filename))
+                    o3d.io.write_image(depth_filename, rgbd.depth)
+                    idx += 1
 
-            if not vis_geometry_added:
-                vis.add_geometry(rgbd)
-                vis_geometry_added = True
+            vis.update_geometry()
+            vis.poll_events()
+            vis.update_renderer()
 
-            if output_path is not None:
-                color_filename = '{0}/color/{1:05d}.jpg'.format(
-                    output_path, idx)
-                print('Writing to {}'.format(color_filename))
-                o3d.io.write_image(color_filename, rgbd.color)
-
-                depth_filename = '{0}/depth/{1:05d}.png'.format(
-                    output_path, idx)
-                print('Writing to {}'.format(depth_filename))
-                o3d.io.write_image(depth_filename, rgbd.depth)
-                idx += 1
-
-        vis.update_geometry()
-        vis.poll_events()
-        vis.update_renderer()
+        self.reader.close()
 
 
 if __name__ == '__main__':
@@ -113,12 +121,5 @@ if __name__ == '__main__':
             print('Unable to mkdir {}, only play mkv'.format(args.output))
             args.output = None
 
-    azure_mkv_reader = o3d.io.AzureKinectMKVReader()
-    azure_mkv_reader.open(args.input)
-    if not azure_mkv_reader.is_opened():
-        print('Unable to open file {}'.format(args.input))
-        exit()
-
-    main(azure_mkv_reader, args.output)
-
-    azure_mkv_reader.close()
+    reader = ReaderWithCallback(args.input, args.output)
+    reader.run()
