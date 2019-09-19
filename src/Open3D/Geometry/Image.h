6590e86e8e35f2cfaf3555de5d888490986e92cc
Wei Dong <theNded@users.noreply.github.com>
2019-09-19 04:31:09 -0400

    Implement image flip functions (#1186)
    * implement image flip functions
    
    * minor change in doc
    
    * better naming
    
    * add test for flip and fix problems in transpose
    
    * support different bytes_per_channel, and utest

--- src/Open3D/Geometry/Image.h
+++ src/Open3D/Geometry/Image.h
@@ -128,7 +128,12 @@ public:
     std::shared_ptr<Image> ConvertDepthToFloatImage(
             double depth_scale = 1000.0, double depth_trunc = 3.0) const;
 
-    std::shared_ptr<Image> Flip() const;
+    std::shared_ptr<Image> Transpose() const;
+
+    /// Function to flip image horizontally (from left to right)
+    std::shared_ptr<Image> FlipHorizontal() const;
+    /// Function to flip image vertically (upside down)
+    std::shared_ptr<Image> FlipVertical() const;
 
     /// Function to filter image with pre-defined filtering type
     std::shared_ptr<Image> Filter(Image::FilterType type) const;
