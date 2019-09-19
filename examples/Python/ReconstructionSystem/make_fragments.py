766747ca369a4ef188647b876653ed1a558f6235
Gernot Riegler <gernotriegler@gmail.com>
2019-09-19 11:49:56 +0200

    read_rgbd_image fix

--- examples/Python/ReconstructionSystem/make_fragments.py
+++ examples/Python/ReconstructionSystem/make_fragments.py
@@ -23,12 +23,11 @@ if with_opencv:
 def read_rgbd_image(color_file, depth_file, convert_rgb_to_intensity, config):
     color = o3d.io.read_image(color_file)
     depth = o3d.io.read_image(depth_file)
-    if config["depth_map_type"] == "redwood":
-        rgbd_image = o3d.geometry.RGBDImage.create_from_color_and_depth(
-            color,
-            depth,
-            depth_trunc=config["max_depth"],
-            convert_rgb_to_intensity=convert_rgb_to_intensity)
+    rgbd_image = o3d.geometry.RGBDImage.create_from_color_and_depth(
+        color,
+        depth,
+        depth_trunc=config["max_depth"],
+        convert_rgb_to_intensity=convert_rgb_to_intensity)
     return rgbd_image
 
 
