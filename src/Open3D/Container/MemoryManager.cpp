bfa5e8b1c44e099afc5a4dcbe0bb3cba76bff281
Yixing Lao <yixing.lao@gmail.com>
2019-09-20 01:48:48 -0700

    registry and blob

---
+++ src/Open3D/Container/MemoryManager.cpp
@@ -0,0 +1,89 @@
+// ----------------------------------------------------------------------------
+// -                        Open3D: www.open3d.org                            -
+// ----------------------------------------------------------------------------
+// The MIT License (MIT)
+//
+// Copyright (c) 2018 www.open3d.org
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+// ----------------------------------------------------------------------------
+
+#include "Open3D/Container/MemoryManager.h"
+
+#include <cstring>
+#include <iostream>
+#include <stdexcept>
+#include <string>
+
+#include "Open3D/Utility/Console.h"
+
+namespace open3d {
+
+OPEN3D_DEFINE_REGISTRY_FOR_SINGLETON(MemoryManagerBackendRegistry,
+                                     std::shared_ptr<MemoryManagerBackend>);
+
+std::shared_ptr<MemoryManagerBackend> MemoryManager::GetImpl(
+        const std::string& device) {
+    if (MemoryManagerBackendRegistry()->Has(device)) {
+        return MemoryManagerBackendRegistry()->GetSingletonObject(device);
+    } else {
+        throw std::runtime_error("Cannot get MemoryManager impl for " + device);
+    }
+}
+
+void* MemoryManager::Allocate(size_t byte_size, const std::string& device) {
+    return GetImpl(device)->Allocate(byte_size);
+}
+
+// TODO: consider removing the "device" argument, check ptr device first
+void MemoryManager::Free(void* ptr) {
+    if (ptr) {
+        if (IsCUDAPointer(ptr)) {
+            GetImpl("gpu")->Free(ptr);
+        } else {
+            GetImpl("cpu")->Free(ptr);
+        }
+    }
+}
+
+void MemoryManager::CopyTo(void* dst_ptr,
+                           const void* src_ptr,
+                           std::size_t num_bytes) {
+    if (dst_ptr == nullptr || src_ptr == nullptr) {
+        throw std::runtime_error("CopyTo: nullptr detected");
+    }
+    std::string dst_device = IsCUDAPointer(dst_ptr) ? "gpu" : "cpu";
+    std::string src_device = IsCUDAPointer(src_ptr) ? "gpu" : "cpu";
+
+    if (src_device == "gpu" || dst_device == "gpu") {
+        GetImpl("gpu")->CopyTo(dst_ptr, src_ptr, num_bytes);
+    } else {
+        GetImpl("cpu")->CopyTo(dst_ptr, src_ptr, num_bytes);
+    }
+}
+
+bool MemoryManager::IsCUDAPointer(const void* ptr) {
+    if (MemoryManagerBackendRegistry()->Has("gpu")) {
+        return GetImpl("gpu")->IsCUDAPointer(ptr);
+    } else {
+        return GetImpl("cpu")->IsCUDAPointer(ptr);
+    }
+}
+
+}  // namespace open3d
---
+++ src/UnitTest/Container/Tensor.cpp
@@ -0,0 +1,85 @@
+// ----------------------------------------------------------------------------
+// -                        Open3D: www.open3d.org                            -
+// ----------------------------------------------------------------------------
+// The MIT License (MIT)
+//
+// Copyright (c) 2018 www.open3d.org
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+// ----------------------------------------------------------------------------
+
+#include "Open3D/Container/Blob.h"
+#include "Open3D/Container/MemoryManager.h"
+#include "Open3D/Container/TensorArray.h"
+#include "TestUtility/UnitTest.h"
+
+using namespace std;
+using namespace open3d;
+
+TEST(Container, Registry) {
+    EXPECT_TRUE(MemoryManagerBackendRegistry()->Has("cpu"));
+    EXPECT_NE(MemoryManagerBackendRegistry()->GetSingletonObject("cpu"),
+              nullptr);
+}
+
+TEST(Container, CPU_Blob) {
+    Shape shape;
+
+    // Create tensor
+    shape = {4, 4};
+    Blob<float> matrix4f(shape, "cpu");
+
+    // Create tensor with init values
+    shape = {3};
+    std::vector<float> init_val({1, 2, 3});
+    Blob<float> vector3f(init_val, shape, "cpu");
+
+    // Check that the values are actually copied
+    std::vector<float> out_val = vector3f.ToStdVector();
+    unit_test::ExpectEQ(out_val, {1, 2, 3});
+}
+
+TEST(Container, CPU_Array) {
+    Shape tensor_shape = {3};
+    size_t max_size = 10000;
+    TensorArray<float> points(tensor_shape, max_size, "cpu");
+}
+
+TEST(Container, GPU_CONDITIONAL_TEST(GPU_Blob)) {
+    Shape shape;
+
+    // Create tensor
+    shape = {4, 4};
+    Blob<float> matrix4f(shape, "gpu");
+
+    // Create tensor with init values
+    shape = {3};
+    std::vector<float> init_val({1, 2, 3});
+    Blob<float> vector3f(init_val, shape, "gpu");
+
+    // Check that the values are actually copied
+    std::vector<float> out_val = vector3f.ToStdVector();
+    unit_test::ExpectEQ(out_val, {1, 2, 3});
+}
+
+TEST(Container, GPU_CONDITIONAL_TEST(GPU_Array)) {
+    Shape tensor_shape = {3};
+    size_t max_size = 100000;
+    TensorArray<float> points(tensor_shape, max_size, "gpu");
+}
