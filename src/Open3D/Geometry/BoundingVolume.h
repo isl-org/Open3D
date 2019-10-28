f4c44a2392d6ad889e148d2ee41a5e57f36b01dc
Rohan Rathi <rohanrathi@uber.com>
2019-10-29 02:07:04 +0530

    Docs for AxisAlignedBoundingBox

--- src/Open3D/Geometry/BoundingVolume.h
+++ src/Open3D/Geometry/BoundingVolume.h
@@ -94,6 +94,7 @@ public:
     Eigen::Vector3d color_;
 };
 
+/// Class that defines an axis_aligned box that can be computed from 3D geometries. The axis aligned bounding box uses the cooridnate axes for bounding box generation
 class AxisAlignedBoundingBox : public Geometry3D {
 public:
     AxisAlignedBoundingBox()
@@ -110,19 +111,40 @@ public:
     ~AxisAlignedBoundingBox() override {}
 
 public:
+    /// Clear all elements in the geometry
     AxisAlignedBoundingBox& Clear() override;
+    /// Returns True iff the geometry is empty
     bool IsEmpty() const override;
+    /// Returns min bounds for geometry coordinates
     virtual Eigen::Vector3d GetMinBound() const override;
+    /// Returns max bounds for geometry coordinates
     virtual Eigen::Vector3d GetMaxBound() const override;
+    /// Returns the center of the geometry coordinates
     virtual Eigen::Vector3d GetCenter() const override;
+    /// Returns an axis-aligned bounding box of the geometry
     virtual AxisAlignedBoundingBox GetAxisAlignedBoundingBox() const override;
+    /// Returns an oriented bounding box of the geometry
     virtual OrientedBoundingBox GetOrientedBoundingBox() const override;
+    /// Apply transformation (4x4 matrix) to the geometry coordinates
     virtual AxisAlignedBoundingBox& Transform(
             const Eigen::Matrix4d& transformation) override;
+    /// Apply translation to the geometry coordinates.
+    /// Parameters:
+    /// translation - A 3D vector to transform the geometry
+    /// relative - If true, the translation vector is directly added to the geometry coordinates. Otherwise, the center is moved to the translation vector.
     virtual AxisAlignedBoundingBox& Translate(
             const Eigen::Vector3d& translation, bool relative = true) override;
+    /// Apply scaling to the geometry coordinates
+    /// Parameters:
+    /// scale - The scale parameter that is multiplied to the points/vertices of the geometry
+    /// center - If true, then the scale is applied to the centered geometry
     virtual AxisAlignedBoundingBox& Scale(const double scale,
                                           bool center = true) override;
+    /// Apply rotation to the geometry coordinates and normals
+    /// Parameters:
+    /// rotation -  A 3D vector that either defines the three angles for Euler rotation, or in the axis-angle representation the normalized vector defines the axis of rotation and the norm the angle around this axis
+    /// center -  If true, then the rotation is applied to the centered geometry
+    /// type - Type of rotation, i.e., an Euler format, or axis-angle.
     virtual AxisAlignedBoundingBox& Rotate(const Eigen::Matrix3d& R,
                                            bool center = true) override;
 
@@ -146,20 +168,26 @@ public:
         return (z - min_bound_(2)) / (max_bound_(2) - min_bound_(2));
     }
 
+    /// Returns the volume of the bounding box
     double Volume() const;
+    /// Returns the eight points that define the bounding box
     std::vector<Eigen::Vector3d> GetBoxPoints() const;
 
     /// Return indices to points that are within the bounding box.
     std::vector<size_t> GetPointIndicesWithinBoundingBox(
             const std::vector<Eigen::Vector3d>& points) const;
 
+    /// Returns the 3D dimensions of the bounding box in string format
     std::string GetPrintInfo() const;
 
+    /// Creates the bounding box that encloses the set of points
     static AxisAlignedBoundingBox CreateFromPoints(
             const std::vector<Eigen::Vector3d>& points);
 
 public:
+    /// The lower x, y, z bounds of the bounding box
     Eigen::Vector3d min_bound_;
+    /// The upper x, y, z bounds of the bounding box
     Eigen::Vector3d max_bound_;
     Eigen::Vector3d color_;
 };
--- src/Python/open3d_pybind/geometry/boundingvolume.cpp
+++ src/Python/open3d_pybind/geometry/boundingvolume.cpp
@@ -77,7 +77,9 @@ void pybind_boundingvolume(py::module &m) {
             axis_aligned_bounding_box(m, "AxisAlignedBoundingBox",
                                       "Class that defines an axis_aligned box "
                                       "that can be computed from 3D "
-                                      "geometries.");
+                                      "geometries. The axis aligned bounding "
+                                      "box uses the cooridnate axes for "
+                                      "bounding box generation.");
     py::detail::bind_default_constructor<geometry::AxisAlignedBoundingBox>(
             axis_aligned_bounding_box);
     py::detail::bind_copy_functions<geometry::AxisAlignedBoundingBox>(
