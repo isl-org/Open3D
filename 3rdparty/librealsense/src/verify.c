619d4766d0c12cfb61251f55ab605b81f9f0a040
jtrantow <jerry.trantow@gmail.com>
2019-10-29 16:13:48 -0500

    revert header

--- 3rdparty/librealsense/src/verify.c
+++ 3rdparty/librealsense/src/verify.c
@@ -2,4 +2,4 @@
    Copyright(c) 2015 Intel Corporation. All Rights Reserved. */
 
 /* This file simply verifies that rs.h can be correctly included by a C compiler */
-#include <C:\Users\JJT\Documents\MTSI\librealsenseMaster\include\librealsense2/rs.h> //C:\Users\JJT\Documents\MTSI\librealsenseMaster\include\librealsense2
+#include <librealsense/rs.h> 
