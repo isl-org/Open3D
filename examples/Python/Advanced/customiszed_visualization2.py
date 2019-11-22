cacfc81c58079c9653456af4fc63aa61a2165a60
Samson Lee <samson.lee@visionarymachines.com>
2019-11-22 20:42:54 +1100

    Make example similar to customized_visualization.py

--- examples/Python/Advanced/customiszed_visualization2.py
+++ examples/Python/Advanced/customiszed_visualization2.py
@@ -1,33 +1,40 @@
+# Open3D: www.open3d.org
+# The MIT License (MIT)
+# See license file or visit www.open3d.org for details
+
+# examples/Python/Advanced/customized_visualization2.py
+
 import open3d as o3d
 
-mesh_box = o3d.geometry.TriangleMesh.create_box(width=1.0, height=1.0, depth=1.0)
+def custom_key_action_without_kb_repeat_delay(pcd):
+    rotating = False
 
-vis = o3d.visualization.VisualizerWithKeyCallback()
+    vis = o3d.visualization.VisualizerWithKeyCallback()
 
-rotating = False
+    def key_action_callback(vis, action, mods):
+        nonlocal rotating
+        print(action)
+        if action == 1:  # key down
+            rotating = True
+        elif action == 0:  # key up
+            rotating = False
 
-def rotate_view(vis):
-    ctr = vis.get_view_control()
-    ctr.rotate(10.0, 0.0)
+    def animation_callback(vis):
+        nonlocal rotating
+        if rotating:
+            ctr = vis.get_view_control()
+            ctr.rotate(10.0, 0.0)
 
-def key_action_callback(vis, action, mods):
-    print(action)
-    global rotating
-    if action == 1:  # key down
-        rotating = True
-    elif action == 0:  # key up
-        rotating = False
+    vis.register_key_action_callback(32, key_action_callback)  # space
+    vis.register_animation_callback(animation_callback)
 
-vis.register_key_action_callback(32, key_action_callback)  # space
+    vis.create_window()
+    vis.add_geometry(pcd)
+    vis.run()
 
-vis.create_window()
-vis.add_geometry(mesh_box)
 
-while True:
-    if not vis.poll_events():
-        break
-    if rotating:
-        rotate_view(vis)
-    vis.update_renderer()
+if __name__ == "__main__":
+    pcd = o3d.io.read_point_cloud("../../TestData/fragment.ply")
 
-vis.destroy_window()
+    print("7. Customized visualization with smooth key action (without keyboard repeat delay)")
+    custom_key_action_without_kb_repeat_delay(pcd)
