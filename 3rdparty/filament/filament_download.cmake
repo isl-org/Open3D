cc72312c179d019b8452d88626191cce49d18228
Geoffrey Prewett <prewettg@gmail.com>
2020-02-03 18:39:37 -0800

    Updated to use macOS 10.13 build of Filament

--- 3rdparty/filament/filament_download.cmake
+++ 3rdparty/filament/filament_download.cmake
@@ -24,7 +24,7 @@ if ("${PATH_TO_FILAMENT}" STREQUAL "")
                 file(MAKE_DIRECTORY ${FILAMENT_ROOT})
                 set(TAR_PWD ${FILAMENT_ROOT})
             elseif (APPLE)
-                set(DOWNLOAD_URL_PRIMARY "https://storage.googleapis.com/isl-datasets/open3d-dev/filament-20191127-mac.tgz")
+                set(DOWNLOAD_URL_PRIMARY "https://storage.googleapis.com/isl-datasets/open3d-dev/filament-20191127-mac-10.13.tgz")
                 set(DOWNLOAD_URL_FALLBACK "https://github.com/google/filament/releases/download/v1.4.3/filament-20191127-mac.tgz")
             endif()
             # =================================================================================================
