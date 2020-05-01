a676bb3569e07d6885efec1d9cf68ffad19cf5ea
Yixing Lao <yixing.lao@gmail.com>
2020-05-01 02:43:50 -0700

    binary op output must fit in the output tensor (#1777)

--- src/Open3D/Core/Kernel/BinaryEW.cpp
+++ src/Open3D/Core/Kernel/BinaryEW.cpp
@@ -48,6 +48,7 @@ void BinaryEW(const Tensor& lhs,
               const Tensor& rhs,
               Tensor& dst,
               BinaryEWOpCode op_code) {
+    // lhs, rhs and dst must be on the same device.
     for (auto device :
          std::vector<Device>({rhs.GetDevice(), dst.GetDevice()})) {
         if (lhs.GetDevice() != device) {
@@ -55,6 +56,17 @@ void BinaryEW(const Tensor& lhs,
                               lhs.GetDevice().ToString(), device.ToString());
         }
     }
+
+    // broadcast(lhs.shape, rhs.shape) must be dst.shape.
+    const SizeVector broadcasted_input_shape =
+            shape_util::BroadcastedShape(lhs.GetShape(), rhs.GetShape());
+    if (broadcasted_input_shape != dst.GetShape()) {
+        utility::LogError(
+                "The broadcasted input shape {} does not match the output "
+                "shape {}.",
+                broadcasted_input_shape, dst.GetShape());
+    }
+
     Device::DeviceType device_type = lhs.GetDevice().GetType();
     if (device_type == Device::DeviceType::CPU) {
         BinaryEWCPU(lhs, rhs, dst, op_code);
