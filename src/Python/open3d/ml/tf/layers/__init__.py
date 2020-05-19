562245707cdb89780ce8d161a9684350fda1dcd8
Benjamin Ummenhofer <benjaminum@gmail.com>
2020-05-19 18:59:20 +0200

    added sparseconv layer and unit test

--- src/Python/open3d/ml/tf/layers/__init__.py
+++ src/Python/open3d/ml/tf/layers/__init__.py
@@ -1,4 +1,5 @@
 from open3d.ml.tf.python.layers.convolutions import ContinuousConv
+from open3d.ml.tf.python.layers.convolutions import SparseConv
 from open3d.ml.tf.python.layers.neighbor_search import FixedRadiusSearch
 from open3d.ml.tf.python.layers.neighbor_search import RadiusSearch
 from open3d.ml.tf.python.layers.neighbor_search import KNNSearch
