fb0cd7ae4787cd0a5a1f54e349dbced742338762
scimad <scimad2.71@gmail.com>
2020-05-28 08:07:27 +0545

    Fix code style

--- examples/Python/ReconstructionSystem/refine_registration.py
+++ examples/Python/ReconstructionSystem/refine_registration.py
@@ -140,7 +140,7 @@ def make_posegraph_for_refined_scene(ply_file_names, config):
         matching_results[s * n_files + t] = \
                 matching_result(s, t, edge.transformation)
 
-    if config["python_multi_threading"].lower()=='true':
+    if config["python_multi_threading"].lower() == "true":
         from joblib import Parallel, delayed
         import multiprocessing
         import subprocess
