a18a87259dc372972608b7c91b747d568d19025b
Yixing Lao <yixing.lao@gmail.com>
2020-06-14 15:21:15 -0700

    run cmake-format

--- 3rdparty/CMake/FindPytorch.cmake
+++ 3rdparty/CMake/FindPytorch.cmake
@@ -1,57 +1,65 @@
 # Find the Pytorch root and use the provided cmake module
 #
-# The following variables will be set
-#   Pytorch_FOUND
-#   Pytorch_VERSION
-#   Pytorch_ROOT
-#   Pytorch_DEFINITIONS
+# The following variables will be set:
+# - Pytorch_FOUND
+# - Pytorch_VERSION
+# - Pytorch_ROOT
+# - Pytorch_DEFINITIONS
 #
-# This script will call find_package( Torch ) which will define
-#   TORCH_FOUND
-#   TORCH_INCLUDE_DIRS
-#   TORCH_LIBRARIES
-#   TORCH_CXX_FLAGS
+# This script will call find_package( Torch ) which will define:
+# - TORCH_FOUND
+# - TORCH_INCLUDE_DIRS
+# - TORCH_LIBRARIES
+# - TORCH_CXX_FLAGS
 #
-# and import the target 'torch'
-#
-if (NOT Pytorch_FOUND)
-    # Searching for pytorch requires the python executable
-    find_package( PythonExecutable REQUIRED )
+# and import the target 'torch'.
 
-    message( STATUS "Getting Pytorch properties ..." )
+if(NOT Pytorch_FOUND)
+  # Searching for pytorch requires the python executable
+  find_package(PythonExecutable REQUIRED)
 
-    execute_process( COMMAND ${PYTHON_EXECUTABLE} "-c" "import torch; print(torch.__version__, end='')"
-            OUTPUT_VARIABLE Pytorch_VERSION )
-    execute_process( COMMAND ${PYTHON_EXECUTABLE} "-c" "import os; import torch; print(os.path.dirname(torch.__file__), end='')"
-            OUTPUT_VARIABLE Pytorch_ROOT )
+  message(STATUS "Getting Pytorch properties ...")
 
-    # Use the cmake config provided by torch
-    find_package( Torch REQUIRED
-                  PATHS "${Pytorch_ROOT}/share/cmake/Torch"
-                  NO_DEFAULT_PATH )
+  execute_process(
+    COMMAND ${PYTHON_EXECUTABLE} "-c"
+            "import torch; print(torch.__version__, end='')"
+    OUTPUT_VARIABLE Pytorch_VERSION)
+  execute_process(
+    COMMAND
+      ${PYTHON_EXECUTABLE} "-c"
+      "import os; import torch; print(os.path.dirname(torch.__file__), end='')"
+    OUTPUT_VARIABLE Pytorch_ROOT)
 
-    # Note: older versions of Pytorch have hard-coded cuda library paths, see:
-    # https://github.com/pytorch/pytorch/issues/15476. For PyTorch
-    # version >= 1.4.0 this has been addressed.
+  # Use the cmake config provided by torch
+  find_package(Torch REQUIRED PATHS "${Pytorch_ROOT}/share/cmake/Torch"
+               NO_DEFAULT_PATH)
+
+  # Note: older versions of Pytorch have hard-coded cuda library paths, see:
+  # https://github.com/pytorch/pytorch/issues/15476. For PyTorch version >=
+  # 1.4.0 this has been addressed.
 endif()
 
-message( STATUS "Pytorch         version: ${Pytorch_VERSION}" )
-message( STATUS "               root dir: ${Pytorch_ROOT}" )
-message( STATUS "          compile flags: ${TORCH_CXX_FLAGS}" )
-foreach( idir ${TORCH_INCLUDE_DIRS} )
-message( STATUS "           include dirs: ${idir}" )
-endforeach( idir )
-foreach( lib ${TORCH_LIBRARIES} )
-message( STATUS "              libraries: ${lib}" )
-endforeach( lib )
+message(STATUS "Pytorch         version: ${Pytorch_VERSION}")
+message(STATUS "               root dir: ${Pytorch_ROOT}")
+message(STATUS "          compile flags: ${TORCH_CXX_FLAGS}")
+foreach(idir ${TORCH_INCLUDE_DIRS})
+  message(STATUS "           include dirs: ${idir}")
+endforeach(idir)
+foreach(lib ${TORCH_LIBRARIES})
+  message(STATUS "              libraries: ${lib}")
+endforeach(lib)
 
-# check if the c++11 ABI is compatible
-if( ${TORCH_CXX_FLAGS} MATCHES "_GLIBCXX_USE_CXX11_ABI=([01])" )
-        if( (GLIBCXX_USE_CXX11_ABI AND NOT CMAKE_MATCH_1) OR (NOT GLIBCXX_USE_CXX11_ABI AND CMAKE_MATCH_1) )
-                message( FATAL_ERROR "_GLIBCXX_USE_CXX11_ABI mismatch: Open3D ${GLIBCXX_USE_CXX11_ABI} vs Torch ${CMAKE_MATCH_1}")
-        endif()
+# Check if the c++11 ABI is compatible
+if(${TORCH_CXX_FLAGS} MATCHES "_GLIBCXX_USE_CXX11_ABI=([01])")
+  if((GLIBCXX_USE_CXX11_ABI AND NOT CMAKE_MATCH_1) OR (NOT GLIBCXX_USE_CXX11_ABI
+                                                       AND CMAKE_MATCH_1))
+    message(
+      FATAL_ERROR
+        "_GLIBCXX_USE_CXX11_ABI mismatch: Open3D ${GLIBCXX_USE_CXX11_ABI} vs Torch ${CMAKE_MATCH_1}"
+    )
+  endif()
 endif()
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Pytorch DEFAULT_MSG Pytorch_VERSION Pytorch_ROOT )
-
+find_package_handle_standard_args(Pytorch DEFAULT_MSG Pytorch_VERSION
+                                  Pytorch_ROOT)
--- 3rdparty/CMake/FindTensorflow.cmake
+++ 3rdparty/CMake/FindTensorflow.cmake
@@ -1,42 +1,57 @@
 # Find Tensorflow include dir and library
 #
-# The following variables will be set
-#   Tensorflow_FOUND
-#   Tensorflow_INCLUDE_DIR
-#   Tensorflow_LIB_DIR
-#   Tensorflow_FRAMEWORK_LIB
-#   Tensorflow_DEFINITIONS
-#
-if (NOT Tensorflow_FOUND)
-    # searching for tensorflow requires the python executable
-    find_package( PythonExecutable REQUIRED )
+# The following variables will be set:
+# - Tensorflow_FOUND
+# - Tensorflow_INCLUDE_DIR
+# - Tensorflow_LIB_DIR
+# - Tensorflow_FRAMEWORK_LIB
+# - Tensorflow_DEFINITIONS
 
-    message( STATUS "Getting Tensorflow properties ..." )
+if(NOT Tensorflow_FOUND)
+  # Searching for tensorflow requires the python executable
+  find_package(PythonExecutable REQUIRED)
 
-    execute_process( COMMAND ${PYTHON_EXECUTABLE} "-c" "from __future__ import print_function; import tensorflow as tf; print(tf.sysconfig.get_include(), end='')" 
-                     OUTPUT_VARIABLE Tensorflow_INCLUDE_DIR )
-    execute_process( COMMAND ${PYTHON_EXECUTABLE} "-c" "from __future__ import print_function; import tensorflow as tf; print(tf.sysconfig.get_lib(), end='')" 
-                     OUTPUT_VARIABLE Tensorflow_LIB_DIR )
-    find_library( Tensorflow_FRAMEWORK_LIB 
-        NAMES tensorflow_framework libtensorflow_framework.so.2 
-        PATHS "${Tensorflow_LIB_DIR}" 
-        NO_DEFAULT_PATH )
-    execute_process( COMMAND ${PYTHON_EXECUTABLE} "-c" "from __future__ import print_function; import tensorflow as tf; defs = [ x[2:] for x in tf.sysconfig.get_compile_flags() if x.startswith('-D')]; print(';'.join(defs), end='')" 
-                     OUTPUT_VARIABLE Tensorflow_DEFINITIONS )
+  message(STATUS "Getting Tensorflow properties ...")
+
+  execute_process(
+    COMMAND
+      ${PYTHON_EXECUTABLE} "-c"
+      "from __future__ import print_function; import tensorflow as tf; print(tf.sysconfig.get_include(), end='')"
+    OUTPUT_VARIABLE Tensorflow_INCLUDE_DIR)
+  execute_process(
+    COMMAND
+      ${PYTHON_EXECUTABLE} "-c"
+      "from __future__ import print_function; import tensorflow as tf; print(tf.sysconfig.get_lib(), end='')"
+    OUTPUT_VARIABLE Tensorflow_LIB_DIR)
+  find_library(
+    Tensorflow_FRAMEWORK_LIB
+    NAMES tensorflow_framework libtensorflow_framework.so.2
+    PATHS "${Tensorflow_LIB_DIR}"
+    NO_DEFAULT_PATH)
+  execute_process(
+    COMMAND
+      ${PYTHON_EXECUTABLE} "-c"
+      "from __future__ import print_function; import tensorflow as tf; defs = [ x[2:] for x in tf.sysconfig.get_compile_flags() if x.startswith('-D')]; print(';'.join(defs), end='')"
+    OUTPUT_VARIABLE Tensorflow_DEFINITIONS)
 endif()
 
-message( STATUS "Tensorflow   include dir: ${Tensorflow_INCLUDE_DIR}" )
-message( STATUS "             library dir: ${Tensorflow_LIB_DIR}" )
-message( STATUS "           framework lib: ${Tensorflow_FRAMEWORK_LIB}" )
-message( STATUS "             definitions: ${Tensorflow_DEFINITIONS}" )
+message(STATUS "Tensorflow   include dir: ${Tensorflow_INCLUDE_DIR}")
+message(STATUS "             library dir: ${Tensorflow_LIB_DIR}")
+message(STATUS "           framework lib: ${Tensorflow_FRAMEWORK_LIB}")
+message(STATUS "             definitions: ${Tensorflow_DEFINITIONS}")
 
-# check if the c++11 ABI is compatible
-if( ${Tensorflow_DEFINITIONS} MATCHES "_GLIBCXX_USE_CXX11_ABI=([01])" )
-        if( (GLIBCXX_USE_CXX11_ABI AND NOT CMAKE_MATCH_1) OR (NOT GLIBCXX_USE_CXX11_ABI AND CMAKE_MATCH_1) )
-                message( FATAL_ERROR "_GLIBCXX_USE_CXX11_ABI mismatch: Open3D ${GLIBCXX_USE_CXX11_ABI} vs Tensorflow ${CMAKE_MATCH_1}")
-        endif()
+# Check if the c++11 ABI is compatible
+if(${Tensorflow_DEFINITIONS} MATCHES "_GLIBCXX_USE_CXX11_ABI=([01])")
+  if((GLIBCXX_USE_CXX11_ABI AND NOT CMAKE_MATCH_1) OR (NOT GLIBCXX_USE_CXX11_ABI
+                                                       AND CMAKE_MATCH_1))
+    message(
+      FATAL_ERROR
+        "_GLIBCXX_USE_CXX11_ABI mismatch: Open3D ${GLIBCXX_USE_CXX11_ABI} vs Tensorflow ${CMAKE_MATCH_1}"
+    )
+  endif()
 endif()
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Tensorflow DEFAULT_MSG Tensorflow_INCLUDE_DIR Tensorflow_LIB_DIR Tensorflow_FRAMEWORK_LIB Tensorflow_DEFINITIONS )
-
+find_package_handle_standard_args(
+  Tensorflow DEFAULT_MSG Tensorflow_INCLUDE_DIR Tensorflow_LIB_DIR
+  Tensorflow_FRAMEWORK_LIB Tensorflow_DEFINITIONS)
