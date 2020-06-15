cbde8c8094a1050aa490ada37e05f23a83a82d1e
Yixing Lao <yixing.lao@gmail.com>
2020-06-15 03:13:26 -0700

    fix ABI flags, tf_ops and th_ops links Open3D

--- 3rdparty/CMake/FindPytorch.cmake
+++ 3rdparty/CMake/FindPytorch.cmake
@@ -15,49 +15,58 @@
 # and import the target 'torch'.
 
 if(NOT Pytorch_FOUND)
-  # Searching for pytorch requires the python executable
-  find_package(PythonExecutable REQUIRED)
+    # Searching for pytorch requires the python executable
+    find_package(PythonExecutable REQUIRED)
 
-  message(STATUS "Getting Pytorch properties ...")
+    message(STATUS "Getting Pytorch properties ...")
 
-  execute_process(
-    COMMAND ${PYTHON_EXECUTABLE} "-c"
-            "import torch; print(torch.__version__, end='')"
-    OUTPUT_VARIABLE Pytorch_VERSION)
-  execute_process(
-    COMMAND
-      ${PYTHON_EXECUTABLE} "-c"
-      "import os; import torch; print(os.path.dirname(torch.__file__), end='')"
-    OUTPUT_VARIABLE Pytorch_ROOT)
+    # Get Pytorch_VERSION
+    execute_process(
+        COMMAND ${PYTHON_EXECUTABLE} "-c"
+                "import torch; print(torch.__version__, end='')"
+        OUTPUT_VARIABLE Pytorch_VERSION)
 
-  # Use the cmake config provided by torch
-  find_package(Torch REQUIRED PATHS "${Pytorch_ROOT}/share/cmake/Torch"
-               NO_DEFAULT_PATH)
+    # Get Pytorch_ROOT
+    execute_process(
+        COMMAND
+            ${PYTHON_EXECUTABLE} "-c"
+            "import os; import torch; print(os.path.dirname(torch.__file__), end='')"
+        OUTPUT_VARIABLE Pytorch_ROOT)
 
-  # Note: older versions of Pytorch have hard-coded cuda library paths, see:
-  # https://github.com/pytorch/pytorch/issues/15476. For PyTorch version >=
-  # 1.4.0 this has been addressed.
+    # Use the cmake config provided by torch
+    find_package(Torch REQUIRED PATHS "${Pytorch_ROOT}/share/cmake/Torch"
+                 NO_DEFAULT_PATH)
+
+    # Note: older versions of Pytorch have hard-coded cuda library paths, see:
+    # https://github.com/pytorch/pytorch/issues/15476. For PyTorch version >=
+    # 1.4.0 this has been addressed.
+
+    # Get Pytorch_CXX11_ABI: True/False
+    execute_process(
+        COMMAND
+            ${PYTHON_EXECUTABLE} "-c"
+            "import torch; print(torch._C._GLIBCXX_USE_CXX11_ABI, end='')"
+        OUTPUT_VARIABLE Pytorch_CXX11_ABI
+    )
 endif()
 
 message(STATUS "Pytorch         version: ${Pytorch_VERSION}")
 message(STATUS "               root dir: ${Pytorch_ROOT}")
 message(STATUS "          compile flags: ${TORCH_CXX_FLAGS}")
+message(STATUS "          use cxx11 abi: ${Pytorch_CXX11_ABI}")
 foreach(idir ${TORCH_INCLUDE_DIRS})
-  message(STATUS "           include dirs: ${idir}")
+    message(STATUS "           include dirs: ${idir}")
 endforeach(idir)
 foreach(lib ${TORCH_LIBRARIES})
-  message(STATUS "              libraries: ${lib}")
+    message(STATUS "              libraries: ${lib}")
 endforeach(lib)
 
 # Check if the c++11 ABI is compatible
-if(${TORCH_CXX_FLAGS} MATCHES "_GLIBCXX_USE_CXX11_ABI=([01])")
-  if((GLIBCXX_USE_CXX11_ABI AND NOT CMAKE_MATCH_1) OR (NOT GLIBCXX_USE_CXX11_ABI
-                                                       AND CMAKE_MATCH_1))
-    message(
-      FATAL_ERROR
-        "_GLIBCXX_USE_CXX11_ABI mismatch: Open3D ${GLIBCXX_USE_CXX11_ABI} vs Torch ${CMAKE_MATCH_1}"
-    )
-  endif()
+if((Pytorch_CXX11_ABI AND (NOT GLIBCXX_USE_CXX11_ABI)) OR
+   (NOT Pytorch_CXX11_ABI AND GLIBCXX_USE_CXX11_ABI))
+    message(FATAL_ERROR "PyTorch and Open3D ABI mismatch: ${Pytorch_CXX11_ABI} != ${GLIBCXX_USE_CXX11_ABI}")
+else()
+    message(STATUS "PyTorch matches Open3D ABI: ${Pytorch_CXX11_ABI} == ${GLIBCXX_USE_CXX11_ABI}")
 endif()
 
 include(FindPackageHandleStandardArgs)
--- 3rdparty/CMake/FindTensorflow.cmake
+++ 3rdparty/CMake/FindTensorflow.cmake
@@ -8,50 +8,65 @@
 # - Tensorflow_DEFINITIONS
 
 if(NOT Tensorflow_FOUND)
-  # Searching for tensorflow requires the python executable
-  find_package(PythonExecutable REQUIRED)
+    # Searching for tensorflow requires the python executable
+    find_package(PythonExecutable REQUIRED)
 
-  message(STATUS "Getting Tensorflow properties ...")
+    message(STATUS "Getting Tensorflow properties ...")
 
-  execute_process(
-    COMMAND
-      ${PYTHON_EXECUTABLE} "-c"
-      "from __future__ import print_function; import tensorflow as tf; print(tf.sysconfig.get_include(), end='')"
-    OUTPUT_VARIABLE Tensorflow_INCLUDE_DIR)
-  execute_process(
-    COMMAND
-      ${PYTHON_EXECUTABLE} "-c"
-      "from __future__ import print_function; import tensorflow as tf; print(tf.sysconfig.get_lib(), end='')"
-    OUTPUT_VARIABLE Tensorflow_LIB_DIR)
-  find_library(
-    Tensorflow_FRAMEWORK_LIB
-    NAMES tensorflow_framework libtensorflow_framework.so.2
-    PATHS "${Tensorflow_LIB_DIR}"
-    NO_DEFAULT_PATH)
-  execute_process(
-    COMMAND
-      ${PYTHON_EXECUTABLE} "-c"
-      "from __future__ import print_function; import tensorflow as tf; defs = [ x[2:] for x in tf.sysconfig.get_compile_flags() if x.startswith('-D')]; print(';'.join(defs), end='')"
-    OUTPUT_VARIABLE Tensorflow_DEFINITIONS)
+    # Get Tensorflow_INCLUDE_DIR
+    execute_process(
+        COMMAND
+            ${PYTHON_EXECUTABLE} "-c"
+            "import tensorflow as tf; print(tf.sysconfig.get_include(), end='')"
+        OUTPUT_VARIABLE Tensorflow_INCLUDE_DIR)
+
+    # Get Tensorflow_LIB_DIR
+    execute_process(
+        COMMAND
+            ${PYTHON_EXECUTABLE} "-c"
+            "import tensorflow as tf; print(tf.sysconfig.get_lib(), end='')"
+        OUTPUT_VARIABLE Tensorflow_LIB_DIR)
+
+    # Get Tensorflow_FRAMEWORK_LIB
+    find_library(
+        Tensorflow_FRAMEWORK_LIB
+        NAMES tensorflow_framework libtensorflow_framework.so.2
+        PATHS "${Tensorflow_LIB_DIR}"
+        NO_DEFAULT_PATH
+    )
+
+    # Get Tensorflow_DEFINITIONS
+    execute_process(
+        COMMAND
+            ${PYTHON_EXECUTABLE} "-c"
+            "import tensorflow as tf; defs = [ x[2:] for x in tf.sysconfig.get_compile_flags() if x.startswith('-D')]; print(';'.join(defs), end='')"
+        OUTPUT_VARIABLE Tensorflow_DEFINITIONS
+    )
+
+    # Get TensorFlow CXX11_ABI: 0/1
+    execute_process(
+        COMMAND
+            ${PYTHON_EXECUTABLE} "-c"
+            "import tensorflow; print(tensorflow.__cxx11_abi_flag__, end='')"
+        OUTPUT_VARIABLE Tensorflow_CXX11_ABI
+    )
 endif()
 
 message(STATUS "Tensorflow   include dir: ${Tensorflow_INCLUDE_DIR}")
 message(STATUS "             library dir: ${Tensorflow_LIB_DIR}")
 message(STATUS "           framework lib: ${Tensorflow_FRAMEWORK_LIB}")
 message(STATUS "             definitions: ${Tensorflow_DEFINITIONS}")
+message(STATUS "           use cxx11 abi: ${Tensorflow_CXX11_ABI}")
 
 # Check if the c++11 ABI is compatible
-if(${Tensorflow_DEFINITIONS} MATCHES "_GLIBCXX_USE_CXX11_ABI=([01])")
-  if((GLIBCXX_USE_CXX11_ABI AND NOT CMAKE_MATCH_1) OR (NOT GLIBCXX_USE_CXX11_ABI
-                                                       AND CMAKE_MATCH_1))
-    message(
-      FATAL_ERROR
-        "_GLIBCXX_USE_CXX11_ABI mismatch: Open3D ${GLIBCXX_USE_CXX11_ABI} vs Tensorflow ${CMAKE_MATCH_1}"
-    )
-  endif()
+if((Tensorflow_CXX11_ABI AND (NOT GLIBCXX_USE_CXX11_ABI)) OR
+   (NOT Tensorflow_CXX11_ABI AND GLIBCXX_USE_CXX11_ABI))
+    message(FATAL_ERROR "TensorFlow and Open3D ABI mismatch: ${Tensorflow_CXX11_ABI} != ${GLIBCXX_USE_CXX11_ABI}")
+else()
+    message(STATUS "TensorFlow matches Open3D ABI: ${Tensorflow_CXX11_ABI} == ${GLIBCXX_USE_CXX11_ABI}")
 endif()
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(
-  Tensorflow DEFAULT_MSG Tensorflow_INCLUDE_DIR Tensorflow_LIB_DIR
-  Tensorflow_FRAMEWORK_LIB Tensorflow_DEFINITIONS)
+    Tensorflow DEFAULT_MSG Tensorflow_INCLUDE_DIR Tensorflow_LIB_DIR
+    Tensorflow_FRAMEWORK_LIB Tensorflow_DEFINITIONS)
