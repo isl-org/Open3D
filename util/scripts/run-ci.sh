603cf842b0f4527bd381d622532561cbadf61afc
Benjamin Ummenhofer <benjaminum@gmail.com>
2020-06-17 07:44:20 +0200

    use https and improved message for CMAKE_CUDA_COMPILER

--- util/scripts/run-ci.sh
+++ util/scripts/run-ci.sh
@@ -53,7 +53,7 @@ cmake --version
 date
 if [ "$BUILD_CUDA_MODULE" == "ON" ]; then
     CUDA_TOOLKIT_DIR=~/cuda
-    reportRun curl -LO http://developer.download.nvidia.com/compute/cuda/10.1/Prod/local_installers/cuda_10.1.243_418.87.00_linux.run
+    reportRun curl -LO https://developer.download.nvidia.com/compute/cuda/10.1/Prod/local_installers/cuda_10.1.243_418.87.00_linux.run
     reportRun sh cuda_10.1.243_418.87.00_linux.run --silent --toolkit --toolkitpath="$CUDA_TOOLKIT_DIR" --defaultroot="$CUDA_TOOLKIT_DIR"
     export PATH="$CUDA_TOOLKIT_DIR/bin:$PATH"
     export LD_LIBRARY_PATH="$CUDA_TOOLKIT_DIR/extras/CUPTI/lib64:$CUDA_TOOLKIT_DIR/lib64"
