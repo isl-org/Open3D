cd0193d61eadd0a9de8f6e40e45c5d1e114de51e
Yixing Lao <yixing.lao@gmail.com>
2020-06-29 22:01:22 -0700

    update jupyter example

--- python/test/test_color_map.py
+++ python/test/test_color_map.py
@@ -6,33 +6,33 @@ import sys
 from open3d_test import download_fountain_dataset
 
 
-def get_file_list(path, extension=None):
-
-    def sorted_alphanum(file_list_ordered):
-        convert = lambda text: int(text) if text.isdigit() else text
-        alphanum_key = lambda key: [
-            convert(c) for c in re.split('([0-9]+)', key)
-        ]
-        return sorted(file_list_ordered, key=alphanum_key)
-
-    if extension is None:
-        file_list = [
-            path + f
-            for f in os.listdir(path)
-            if os.path.isfile(os.path.join(path, f))
-        ]
-    else:
-        file_list = [
-            path + f
-            for f in os.listdir(path)
-            if os.path.isfile(os.path.join(path, f)) and
-            os.path.splitext(f)[1] == extension
-        ]
-    file_list = sorted_alphanum(file_list)
-    return file_list
-
-
 def load_fountain_dataset():
+
+    def get_file_list(path, extension=None):
+
+        def sorted_alphanum(file_list_ordered):
+            convert = lambda text: int(text) if text.isdigit() else text
+            alphanum_key = lambda key: [
+                convert(c) for c in re.split('([0-9]+)', key)
+            ]
+            return sorted(file_list_ordered, key=alphanum_key)
+
+        if extension is None:
+            file_list = [
+                path + f
+                for f in os.listdir(path)
+                if os.path.isfile(os.path.join(path, f))
+            ]
+        else:
+            file_list = [
+                path + f
+                for f in os.listdir(path)
+                if os.path.isfile(os.path.join(path, f)) and
+                os.path.splitext(f)[1] == extension
+            ]
+        file_list = sorted_alphanum(file_list)
+        return file_list
+
     path = download_fountain_dataset()
     depth_image_path = get_file_list(os.path.join(path, "depth/"),
                                      extension=".png")
