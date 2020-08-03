1e4f559ef4d5e70a99e205da071068b5e10fdb6a
Yixing Lao <yixing.lao@gmail.com>
2020-08-03 10:51:12 -0700

    MKL integration with tests (#2128)
    * mkl integration with unit test
    
    * use python 3.6
    
    * install wheel python package

--- cpp/pybind/core/kernel.cpp
+++ cpp/pybind/core/kernel.cpp
@@ -25,21 +25,16 @@
 // ----------------------------------------------------------------------------
 
 #include "pybind/core/core.h"
+#include "pybind/docstring.h"
 #include "pybind/open3d_pybind.h"
+
+#include "open3d/core/kernel/Kernel.h"
 
 namespace open3d {
 
-void pybind_core(py::module &m) {
-    py::module m_core = m.def_submodule("core");
-    pybind_cuda_utils(m_core);
-    pybind_core_blob(m_core);
-    pybind_core_dtype(m_core);
-    pybind_core_device(m_core);
-    pybind_core_size_vector(m_core);
-    pybind_core_tensor_key(m_core);
-    pybind_core_tensor(m_core);
-    pybind_core_tensorlist(m_core);
-    pybind_core_kernel(m_core);
+void pybind_core_kernel(py::module &m) {
+    py::module m_kernel = m.def_submodule("kernel");
+    m_kernel.def("test_mkl_integration", &core::kernel::TestMKLIntegration);
 }
 
 }  // namespace open3d
