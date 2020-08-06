3e1fa8db928d506456ea4490f8b84b3b923ba45d
Rene Sepulveda <errissa@gmail.com>
2020-08-05 23:20:42 -0400

    Set correct link libraries for windows

--- 3rdparty/assimp/assimp.cmake
+++ 3rdparty/assimp/assimp.cmake
@@ -11,13 +11,14 @@ ExternalProject_Add(
     GIT_TAG v5.0.1 # Jan 2020
     UPDATE_COMMAND ""
     CMAKE_ARGS
-        -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+        -DCMAKE_BUILD_TYPE=Release
         -DBUILD_SHARED_LIBS=OFF
         -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
         -DCMAKE_POSITION_INDEPENDENT_CODE=ON
         -DASSIMP_NO_EXPORT=ON
         -DASSIMP_BUILD_ASSIMP_TOOLS=OFF
         -DASSIMP_BUILD_TESTS=OFF
+        -DASSIMP_INSTALL_PDB=OFF
 )
 
 ExternalProject_Get_Property(ext_assimp INSTALL_DIR)
@@ -29,4 +30,8 @@ ExternalProject_Add_Step(ext_assimp patch-copy
   DEPENDERS update)
 set(ASSIMP_INCLUDE_DIR ${INSTALL_DIR}/include/)
 set(ASSIMP_LIB_DIR ${INSTALL_DIR}/lib)
-set(ASSIMP_LIBRARIES assimp IrrXML)
+if (UNIX OR APPLE)
+  set(ASSIMP_LIBRARIES assimp IrrXML)
+else ()
+  set(ASSIMP_LIBRARIES assimp-vc142-mt IrrXML zlibstatic)
+  
