36fb5c29ade6c95c7237e609ca33628d616d35f6
Rene Sepulveda <errissa@gmail.com>
2020-08-18 23:17:04 -0400

    Remove SceneWidget::SetBackgroundColor which is redundant

--- cpp/open3d/visualization/visualizer/GuiVisualizer.cpp
+++ cpp/open3d/visualization/visualizer/GuiVisualizer.cpp
@@ -486,7 +486,8 @@ struct GuiVisualizer::Impl {
     }
 
     void UpdateFromModel(rendering::Renderer &renderer, bool material_changed) {
-        scene_wgt_->SetBackgroundColor(settings_.model_.GetBackgroundColor());
+        auto bcolor = settings_.model_.GetBackgroundColor();
+        renderer.SetClearColor({bcolor.x(), bcolor.y(), bcolor.z(), 1.f});
 
         if (settings_.model_.GetShowSkybox()) {
             scene_wgt_->GetScene()->ShowSkybox(true);
