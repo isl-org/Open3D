bce2a206d9bbbb601aa2c0504b61007dc84f7e0d
Wei Dong <weidong@andrew.cmu.edu>
2020-08-21 12:32:42 -0400

    minor

--- cpp/open3d/pipelines/integration/ScalableTSDFVolume.cpp
+++ cpp/open3d/pipelines/integration/ScalableTSDFVolume.cpp
@@ -66,12 +66,13 @@ void ScalableTSDFVolume::Integrate(
         (color_type_ == TSDFVolumeColorType::Gray32 &&
          image.color_.bytes_per_channel_ != 4)) {
         utility::LogError(
-                "[UniformTSDFVolume::Integrate] Unsupported image format.");
+                "[ScalableTSDFVolume::Integrate] Unsupported image format.");
     }
     if ((image.depth_.width_ != intrinsic.width_) ||
         (image.depth_.height_ != intrinsic.height_)) {
         utility::LogError(
-                "[UniformTSDFVolume::Integrate] depth image size is ({} x {}), "
+                "[ScalableTSDFVolume::Integrate] depth image size is ({} x "
+                "{}), "
                 "but got ({} x {}) from intrinsic.",
                 image.depth_.width_, image.depth_.height_, intrinsic.width_,
                 intrinsic.height_);
@@ -80,7 +81,8 @@ void ScalableTSDFVolume::Integrate(
         (image.color_.width_ != intrinsic.width_ ||
          image.color_.height_ != intrinsic.height_)) {
         utility::LogError(
-                "[UniformTSDFVolume::Integrate] color image size is ({} x {}), "
+                "[ScalableTSDFVolume::Integrate] color image size is ({} x "
+                "{}), "
                 "but got ({} x {}) from intrinsic.",
                 image.color_.width_, image.color_.height_, intrinsic.width_,
                 intrinsic.height_);
