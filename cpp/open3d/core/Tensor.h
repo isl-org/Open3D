447d95adcbc70890f0e94833fd6930740ce78419
Yixing Lao <yixing.lao@gmail.com>
2020-08-22 16:26:46 -0700

    speeds up legacy pointcloud converter (#2216)
    * speeds up legacy pointcloud converter
    
    * clean up includes
    
    * clean up

--- cpp/open3d/core/Tensor.h
+++ cpp/open3d/core/Tensor.h
@@ -321,8 +321,8 @@ public:
     ///      aten/src/ATen/TensorUtils.cpp
     Tensor View(const SizeVector& dst_shape) const;
 
-    /// Copy Tensor to a specified device
-    /// The resulting Tensor will be compacted and contiguous
+    /// Copy Tensor to a specified device.
+    /// The resulting Tensor will be compacted and contiguous.
     Tensor Copy(const Device& device) const;
 
     /// Copy Tensor to the same device.
