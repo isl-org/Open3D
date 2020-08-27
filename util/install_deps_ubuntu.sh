c11c499da8689cb71a43a64ae1598672b295d71e
Sameer Sheorey <sameer.sheorey@intel.com>
2020-08-26 18:12:16 -0700

    Separate CUDA/cuDNN install from other dependencies

--- util/install_deps_ubuntu.sh
+++ util/install_deps_ubuntu.sh
@@ -1,33 +1,24 @@
 #!/usr/bin/env bash
-set -ev
-
 # Install Open3D build dependencies from Ubuntu repositories
 # Use: install_deps_ubuntu.sh [ assume-yes ] [ install-cuda ]
 
-SUDO=${SUDO:=sudo}      # SUDO=command in docker (running as root, sudo not available)
-CUDA_VERSION=("10-1" "10.1")
-CUDNN_MAJOR_VERSION=7
-CUDNN_VERSION="7.6.5.32-1+cuda10.1"
+set -ev
 
-APT_ARG=""
+SUDO=${SUDO:=sudo}      # SUDO=command in docker (running as root, sudo not available)
+APT_CONFIRM=""
 INSTALL_CUDA=""
 for arg in "$@" ; do
     case $arg in
-        "assume-yes")
-            APT_ARG="--yes"
-            ;;
-        "install-cuda")
-            INSTALL_CUDA=ON
-            ;;
-        *)
-            echo "Use: install_deps_ubuntu.sh [ assume-yes ] [ install-cuda ]"
-            echo "install-cuda: Install CUDA and cuDNN"
-            exit 1
-            ;;
+        "assume-yes") APT_CONFIRM="--assume-yes" ;;
+        "install-cuda") INSTALL_CUDA=ON ;;
+        *) echo "Use: install_deps_ubuntu.sh [ assume-yes ] [ install-cuda ]"
+           echo "install-cuda: Install CUDA and cuDNN"
+           exit 1
+           ;;
     esac
 done
 
-packages=(
+dependencies=(
     # Open3D deps
     xorg-dev
     libglu1-mesa-dev
@@ -44,29 +35,37 @@ packages=(
     libosmesa6-dev
 )
 
+$SUDO apt-get update
+for package in "${dependencies[@]}" ; do
+    $SUDO apt-get install "$APT_CONFIRM" "$package"
+done
+
+######################################################
+# The following scripts are disabled by default.
+# They are intended to be used by the Open3D CI system only.
+######################################################
+
+CUDA_VERSION=("10-1" "10.1")
+CUDNN_MAJOR_VERSION=7
+CUDNN_VERSION="7.6.5.32-1+cuda10.1"
+
 if [ $INSTALL_CUDA == ON ] ; then
     if ! nvcc --version >/dev/null ; then
         echo "Installing CUDA ${CUDA_VERSION[1]} with apt ..."
         $SUDO apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
         $SUDO apt-add-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64 /"
-        packages=("${packages[@]}" "cuda-toolkit-${CUDA_VERSION[0]}")
+        $SUDO apt-get install "$APT_CONFIRM" "cuda-toolkit-${CUDA_VERSION[0]}"
         echo "Add CUDA executables location to PATH in /etc/environment"
         echo "(system) or ~/.bashrc (user) and restart your shell:"
-        echo "# CUDA installation path"
         echo "export PATH=/usr/local/cuda-${CUDA_VERSION[1]}/bin\${PATH:+:\${PATH}}"
+        echo "export LD_LIBRARY_PATH=/usr/local/cuda-${CUDA_VERSION[1]}/lib64\${LD_LIBRARY_PATH:+:\${LD_LIBRARY_PATH}}"
     else
         echo "CUDA found:"
         nvcc --version
     fi
     echo "Installing cuDNN ${CUDNN_VERSION} with apt ..."
     $SUDO apt-add-repository "deb https://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1804/x86_64 /"
-    packages=("${packages[@]}" \
+    $SUDO apt-get install "$APT_CONFIRM" \
         "libcudnn${CUDNN_MAJOR_VERSION}=$CUDNN_VERSION" \
-        "libcudnn${CUDNN_MAJOR_VERSION}-dev=$CUDNN_VERSION" \
-    )
+        "libcudnn${CUDNN_MAJOR_VERSION}-dev=$CUDNN_VERSION"
 fi
-
-$SUDO apt-get update
-for package in "${packages[@]}" ; do
-    $SUDO apt-get install "$APT_ARG" "$package"
-done
