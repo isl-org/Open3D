d9e8636a5bdfdc1277e313532d6a700a5ed4b8c1
Sanskar Agrawal <sanskaragrawal107@gmail.com>
2020-08-31 22:49:54 +0530

    apply style

--- python/test/ml/test_contrib.py
+++ python/test/ml/test_contrib.py
@@ -4,9 +4,11 @@ from open3d.pybind.ml.contrib import subsample, subsample_batch
 
 
 def test_subsample():
-    points = np.array([[0, 0, 0], [1, 0, 0], [0, 1, 0], [0, 0 ,1], [1, 1, 1], [5, 0, 0], [5, 1, 0]], dtype=np.float32)
+    points = np.array([[0, 0, 0], [1, 0, 0], [0, 1, 0], [0, 0, 1], [1, 1, 1],
+                       [5, 0, 0], [5, 1, 0]],
+                      dtype=np.float32)
     features = np.array(range(21), dtype=np.float32).reshape(-1, 3)
-    labels = np.array(range(7), dtype = np.int32)
+    labels = np.array(range(7), dtype=np.int32)
 
     # Passing only points.
     sub_points_ref = np.array([[5, 0.5, 0], [0.4, 0.4, 0.4]], dtype=np.float32)
@@ -15,7 +17,8 @@ def test_subsample():
     np.testing.assert_equal(sub_points, sub_points_ref)
 
     #Passing points and features.
-    sub_features_ref = np.array([[16.5, 17.5, 18.5], [6, 7, 8]], dtype=np.float32)
+    sub_features_ref = np.array([[16.5, 17.5, 18.5], [6, 7, 8]],
+                                dtype=np.float32)
     sub_points_feat = subsample(points, features=features, sampleDl=1.1)
 
     assert len(sub_points_feat) == 2
@@ -28,7 +31,10 @@ def test_subsample():
 
     #Passing points, features and labels.
     sub_labels_ref = np.array([6, 4], dtype=np.int32)
-    sub_points_feat_lab = subsample(points, features=features, classes=labels, sampleDl=1.1)
+    sub_points_feat_lab = subsample(points,
+                                    features=features,
+                                    classes=labels,
+                                    sampleDl=1.1)
 
     assert len(sub_points_feat_lab) == 3
 
@@ -46,13 +52,21 @@ def test_subsample():
         sub_points = subsample(np.array(points, dtype=np.float64), sampleDl=1.1)
 
     with pytest.raises(RuntimeError):
-        sub_points = subsample(points, features=np.array(features, dtype=np.int32), sampleDl=1.1)
-    
-    with pytest.raises(RuntimeError):
-        sub_points = subsample(points, features=features, classes=np.array(labels, dtype=np.float32), sampleDl=1.1)
+        sub_points = subsample(points,
+                               features=np.array(features, dtype=np.int32),
+                               sampleDl=1.1)
 
     with pytest.raises(RuntimeError):
-        sub_points = subsample(points, features=np.array(features, np.int32), classes=np.array(labels, dtype=np.float32), sampleDl=1.1)
+        sub_points = subsample(points,
+                               features=features,
+                               classes=np.array(labels, dtype=np.float32),
+                               sampleDl=1.1)
+
+    with pytest.raises(RuntimeError):
+        sub_points = subsample(points,
+                               features=np.array(features, np.int32),
+                               classes=np.array(labels, dtype=np.float32),
+                               sampleDl=1.1)
 
     # Test shape mismatch
     with pytest.raises(RuntimeError):
@@ -70,14 +84,19 @@ def test_subsample():
     with pytest.raises(TypeError):
         sub_points = subsample(None, sampleDl=1.1)
 
+
 def test_subsample_batch():
-    points = np.array([[0, 0, 0], [1, 0, 0], [0, 1, 0], [0, 0 ,1], [1, 1, 1], [5, 0, 0], [5, 1, 0]], dtype=np.float32)
+    points = np.array([[0, 0, 0], [1, 0, 0], [0, 1, 0], [0, 0, 1], [1, 1, 1],
+                       [5, 0, 0], [5, 1, 0]],
+                      dtype=np.float32)
     features = np.array(range(28), dtype=np.float32).reshape(-1, 4)
-    labels = np.array(range(7), dtype = np.int32)
+    labels = np.array(range(7), dtype=np.int32)
     batches = np.array([3, 2, 2], dtype=np.int32)
 
     #Passing only points.
-    sub_points_ref = np.array([[0.3333333, 0.3333333, 0], [0.5, 0.5, 1], [5, 0.5, 0]], dtype=np.float32)
+    sub_points_ref = np.array(
+        [[0.3333333, 0.3333333, 0], [0.5, 0.5, 1], [5, 0.5, 0]],
+        dtype=np.float32)
     sub_batch_ref = np.array([1, 1, 1], dtype=np.int32)
 
     (sub_points, sub_batch) = subsample_batch(points, batches, sampleDl=1.1)
@@ -86,8 +105,12 @@ def test_subsample_batch():
     np.testing.assert_almost_equal(sub_batch, sub_batch_ref)
 
     #Passing points and features.
-    sub_features_ref = np.array([[4, 5, 6, 7], [14, 15, 16, 17], [22, 23, 24, 25]], dtype=np.float32)
-    sub_points_feat = subsample_batch(points, batches, features=features, sampleDl=1.1)
+    sub_features_ref = np.array(
+        [[4, 5, 6, 7], [14, 15, 16, 17], [22, 23, 24, 25]], dtype=np.float32)
+    sub_points_feat = subsample_batch(points,
+                                      batches,
+                                      features=features,
+                                      sampleDl=1.1)
 
     assert len(sub_points_feat) == 3
 
@@ -100,7 +123,11 @@ def test_subsample_batch():
 
     #Passing points, features and labels.
     sub_labels_ref = np.array([2, 4, 6], dtype=np.int32)
-    sub_points_feat_lab = subsample_batch(points, batches, features=features, classes=labels, sampleDl=1.1)
+    sub_points_feat_lab = subsample_batch(points,
+                                          batches,
+                                          features=features,
+                                          classes=labels,
+                                          sampleDl=1.1)
 
     assert len(sub_points_feat_lab) == 4
 
@@ -113,32 +140,55 @@ def test_subsample_batch():
 
     # Test wrong dtype.
     with pytest.raises(RuntimeError):
-        sub_points = subsample_batch(np.array(points, dtype=np.int32), batches, sampleDl=1.1)
+        sub_points = subsample_batch(np.array(points, dtype=np.int32),
+                                     batches,
+                                     sampleDl=1.1)
 
     with pytest.raises(RuntimeError):
-        sub_points = subsample_batch(points, np.array(batches, dtype=np.float32), sampleDl=1.1)
+        sub_points = subsample_batch(points,
+                                     np.array(batches, dtype=np.float32),
+                                     sampleDl=1.1)
 
     with pytest.raises(RuntimeError):
-        sub_points = subsample_batch(np.array(points, dtype=np.float64), batches, sampleDl=1.1)
+        sub_points = subsample_batch(np.array(points, dtype=np.float64),
+                                     batches,
+                                     sampleDl=1.1)
 
     with pytest.raises(RuntimeError):
-        sub_points = subsample_batch(points, batches, features=np.array(features, dtype=np.int32), sampleDl=1.1)
-    
-    with pytest.raises(RuntimeError):
-        sub_points = subsample_batch(points, batches, features=features, classes=np.array(labels, dtype=np.float32), sampleDl=1.1)
+        sub_points = subsample_batch(points,
+                                     batches,
+                                     features=np.array(features,
+                                                       dtype=np.int32),
+                                     sampleDl=1.1)
 
     with pytest.raises(RuntimeError):
-        sub_points = subsample_batch(points, batches, features=np.array(features, np.int32), classes=np.array(labels, dtype=np.float32), sampleDl=1.1)
+        sub_points = subsample_batch(points,
+                                     batches,
+                                     features=features,
+                                     classes=np.array(labels, dtype=np.float32),
+                                     sampleDl=1.1)
+
+    with pytest.raises(RuntimeError):
+        sub_points = subsample_batch(points,
+                                     batches,
+                                     features=np.array(features, np.int32),
+                                     classes=np.array(labels, dtype=np.float32),
+                                     sampleDl=1.1)
 
     # Test shape mismatch
     with pytest.raises(RuntimeError):
         sub_points = subsample_batch(points[0], batches, sampleDl=1.1)
 
     with pytest.raises(RuntimeError):
-        sub_points = subsample_batch(np.ones((10, 4), dtype=np.float32), batches, sampleDl=1.1)
+        sub_points = subsample_batch(np.ones((10, 4), dtype=np.float32),
+                                     batches,
+                                     sampleDl=1.1)
 
     with pytest.raises(RuntimeError):
-        sub_points = subsample_batch(points, batches, features=features[0], sampleDl=1.1)
+        sub_points = subsample_batch(points,
+                                     batches,
+                                     features=features[0],
+                                     sampleDl=1.1)
 
     with pytest.raises(RuntimeError):
         sub_points = subsample_batch(points[0], batches, sampleDl=1.1)
@@ -148,10 +198,18 @@ def test_subsample_batch():
 
     # Test sum(batch) != num_points
     with pytest.raises(RuntimeError):
-        sub_points = subsample_batch(points, np.array([3, 3, 2], dtype=np.int32), sampleDl=1.1)
+        sub_points = subsample_batch(points,
+                                     np.array([3, 3, 2], dtype=np.int32),
+                                     sampleDl=1.1)
 
     with pytest.raises(RuntimeError):
-        sub_points = subsample_batch(points, np.array([3, 3, 2], dtype=np.int32), features=features, classes=labels, sampleDl=1.1)
+        sub_points = subsample_batch(points,
+                                     np.array([3, 3, 2], dtype=np.int32),
+                                     features=features,
+                                     classes=labels,
+                                     sampleDl=1.1)
 
     with pytest.raises(RuntimeError):
-        sub_points = subsample_batch(points, np.array([1], dtype=np.int32), sampleDl=1.1)
+        sub_points = subsample_batch(points,
+                                     np.array([1], dtype=np.int32),
+                                     sampleDl=1.1)
