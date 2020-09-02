5d1110b90659964aaba6a07887e513077d914fa7
Sanskar Agrawal <sanskaragrawal107@gmail.com>
2020-09-02 23:07:25 +0530

    apply style

--- python/open3d/ml/tf/python/ops/sampling.py
+++ python/open3d/ml/tf/python/ops/sampling.py
@@ -1,13 +1,14 @@
 from .lib import _lib
 
+
 def tf_batch_subsampling(points, batches_len, sampleDl):
-    return _lib.batch_grid_subsampling(
-        points, batches_len, sampleDl)
+    return _lib.batch_grid_subsampling(points, batches_len, sampleDl)
+
 
 def tf_subsampling(points, sampleDl):
-    return _lib.grid_subsampling(
-        points, sampleDl)
+    return _lib.grid_subsampling(points, sampleDl)
+
 
 def tf_batch_neighbors(queries, supports, q_batches, s_batches, radius):
-    return _lib.batch_ordered_neighbors(
-        queries, supports, q_batches, s_batches, radius)
+    return _lib.batch_ordered_neighbors(queries, supports, q_batches, s_batches,
+                                        radius)
