1a53667fc1380a0ed3cd836a7034a3ab9164218f
ssheorey <41028320+ssheorey@users.noreply.github.com>
2020-09-02 15:54:17 -0700

    GCE GPU CI docker (PyTorch + cuDNN) (#2211)
    * PyTorch Ops + CUDA
    
    * Fix for deb CUDA path
    
    * Recommend PATH addition instead of adding directly for CUDA.
    
    * Bugfix PATH update
    
    * Installing CUDA and cuDNN is optional
    
    * Clear apt and pip cache to save space
    
    * pip cache purge should not fail
    
    * CI in release mode, build examples iteratively
    
    to save disk space on Ubuntu CI
    
    * Separate CUDA/cuDNN install from other dependencies
    
    * Specify CUBLAS version
    
    To prevent incorrect version install
    
    * Move CUDA install to run_ci.sh
    
    Since deb install requires manual symlinking for cmake
    
    * cublas_v2.h error debug
    
    Co-authored-by: Yixing Lao <yixing.lao@gmail.com>

--- util/install_deps_ubuntu.sh
+++ util/install_deps_ubuntu.sh
@@ -1,38 +1,36 @@
 #!/usr/bin/env bash
+# Install Open3D build dependencies from Ubuntu repositories
+# CUDA (v10.1) and CUDNN (v7.6.5) are optional dependencies and are not
+# installed here
+# Use: install_deps_ubuntu.sh [ assume-yes ]
+
 set -ev
 
 SUDO=${SUDO:=sudo}      # SUDO=command in docker (running as root, sudo not available)
+if [ "$1" == "assume-yes" ] ; then
+    APT_CONFIRM="--assume-yes"
+else
+    APT_CONFIRM=""
+fi
+
+dependencies=(
+    # Open3D deps
+    xorg-dev
+    libglu1-mesa-dev
+    python3-dev
+    # Filament build-from-source deps
+    libsdl2-dev
+    libc++-7-dev
+    libc++abi-7-dev
+    ninja-build
+    libxi-dev
+    # ML deps
+    libtbb-dev
+    # Headless rendering deps
+    libosmesa6-dev
+)
 
 $SUDO apt-get update
-
-if [ "$1" == "assume-yes" ]; then
-    # Open3D deps
-    $SUDO apt-get --yes install xorg-dev
-    $SUDO apt-get --yes install libglu1-mesa-dev
-    $SUDO apt-get --yes install python3-dev
-    # Filament build-from-source dpes
-    $SUDO apt-get --yes install libsdl2-dev
-    $SUDO apt-get --yes install libc++-7-dev
-    $SUDO apt-get --yes install libc++abi-7-dev
-    $SUDO apt-get --yes install ninja-build
-    $SUDO apt-get --yes install libxi-dev
-    # ML deps
-    $SUDO apt-get --yes install libtbb-dev
-    # Headless rendering deps
-    $SUDO apt-get --yes install libosmesa6-dev
-else
-    # Open3D deps
-    $SUDO apt-get install xorg-dev
-    $SUDO apt-get install libglu1-mesa-dev
-    $SUDO apt-get install python3-dev
-    # Filament build-from-source dpes
-    $SUDO apt-get install libsdl2-dev
-    $SUDO apt-get install libc++-7-dev
-    $SUDO apt-get install libc++abi-7-dev
-    $SUDO apt-get install ninja-build
-    $SUDO apt-get install libxi-dev
-    # ML deps
-    $SUDO apt-get install libtbb-dev
-    # Headless rendering deps
-    $SUDO apt-get install libosmesa6-dev
-fi
+for package in "${dependencies[@]}" ; do
+    $SUDO apt-get install "$APT_CONFIRM" "$package"
+done
