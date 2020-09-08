e50d0ca094f98455422762b11f3105b439d73d5e
Benjamin Ummenhofer <benjaminum@gmail.com>
2020-09-08 21:24:28 +0200

    custom rst files for layers and module doc strings

--- python/open3d/ml/tf/ops/__init__.py
+++ python/open3d/ml/tf/ops/__init__.py
@@ -1,2 +1,7 @@
+"""Functional API with operators.
+
+These are the building blocks for the layers. See The layer API for an easy to
+use high level interface.
+"""
 from open3d.ml.tf.python.ops.ops import *
 from open3d.ml.tf.python.ops.gradients import *
