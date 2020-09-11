f2c1731d5f47b95017de6c4edb3794fb9f7d402d
Benjamin Ummenhofer <benjaminum@gmail.com>
2020-09-11 16:20:14 +0200

    rm trailing whitespace

--- python/open3d/ml/tf/python/layers/voxel_pooling.py
+++ python/open3d/ml/tf/python/layers/voxel_pooling.py
@@ -8,16 +8,16 @@ class VoxelPooling(tf.keras.layers.Layer):
     """Voxel pooling for 3D point clouds
 
     Spatial pooling for point clouds by combining points that fall into the same voxel bin.
-    
-    The voxel grid used for pooling is always aligned to the origin (0,0,0) to 
+
+    The voxel grid used for pooling is always aligned to the origin (0,0,0) to
     simplify building voxel grid hierarchies. The order of the returned voxels is
     not defined as can be seen in the following example::
 
       import open3d.ml.tf as ml3d
 
       positions = [
-          [0.1,0.1,0.1], 
-          [0.5,0.5,0.5], 
+          [0.1,0.1,0.1],
+          [0.5,0.5,0.5],
           [1.7,1.7,1.7],
           [1.8,1.8,1.8],
           [0.3,2.4,1.4]]
@@ -27,17 +27,17 @@ class VoxelPooling(tf.keras.layers.Layer):
                   [4.2,0.1],
                   [1.3,3.4],
                   [2.3,1.9]]
-                  
+
       voxel_pooling = ml3d.layers.VoxelPooling(position_fn='center', feature_fn='max')
 
       ans = voxel_pooling(positions, features, 1.0)
 
-      # returns the voxel centers in 
+      # returns the voxel centers in
       # ans.pooled_positions = [[0.5, 2.5, 1.5],
       #                         [1.5, 1.5, 1.5],
       #                         [0.5, 0.5, 0.5]]
       #
-      # and the max pooled features for each voxel in 
+      # and the max pooled features for each voxel in
       # ans.pooled_features = [[2.3, 1.9],
       #                        [4.2, 3.4],
       #                        [1.1, 2.3]]
@@ -68,21 +68,21 @@ class VoxelPooling(tf.keras.layers.Layer):
         """This function computes the pooled positions and features.
 
         Arguments:
-        
+
           positions: The point positions with shape [N,3] with N as the number of points.
             *This argument must be given as a positional argument!*
 
           features: The feature vector with shape [N,channels].
 
           voxel_size: The voxel size.
-          
+
         Returns:
           2 Tensors in the following order
 
           pooled_positions
             The output point positions with shape [M,3] and M <= N.
 
-          pooled_features: 
+          pooled_features:
             The output point features with shape [M,channnels] and M <= N.
         """
         result = ops.voxel_pooling(positions,
