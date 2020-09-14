f078b7191672126ab96103eb6d2fdd1ccbad61ae
Yixing Lao <yixing.lao@gmail.com>
2020-09-14 03:57:06 -0700

    patch CMP0091 to TBB

--- 3rdparty/mkl/mkl.cmake
+++ 3rdparty/mkl/mkl.cmake
@@ -46,15 +46,23 @@ set(STATIC_TBB_LIBRARIES tbb_static tbbmalloc_static)
 
 # Need to put TBB right next to MKL in the link flags. So instead of creating a
 # new tbb.cmake, it is also put here.
+#
+# To generate a new patch, go inside TBB source folder:
+# 1. Checkout to a new branch
+# 2. Make changes, commit
+# 3. git format-patch master
+# 4. Copy the new patch file back to Open3d.
 ExternalProject_Add(
     ext_tbb
     PREFIX tbb
     GIT_REPOSITORY https://github.com/wjakob/tbb.git
     GIT_TAG 141b0e310e1fb552bdca887542c9c1a8544d6503 # Sept 2020
     UPDATE_COMMAND ""
+    PATCH_COMMAND git apply ${Open3D_3RDPARTY_DIR}/mkl/0001-Allow-selecttion-of-static-dynamic-MSVC-runtime.patch
     CMAKE_ARGS
         -DCMAKE_INSTALL_PREFIX=${MKL_INSTALL_PREFIX}
         -DCMAKE_POSITION_INDEPENDENT_CODE=ON
+        -DSTATIC_WINDOWS_RUNTIME=${STATIC_WINDOWS_RUNTIME}
         -DTBB_BUILD_TBBMALLOC=ON
         -DTBB_BUILD_TBBMALLOC_PROXYC=OFF
         -DTBB_BUILD_SHARED=OFF
