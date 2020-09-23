d848cd024f87ecc6c1eab6efff50bea726a05ae4
Junha Lee <junhakiwi@postech.ac.kr>
2020-09-24 02:34:40 +0900

    tbb::parallel_scan

--- cpp/open3d/core/kernel/CumSumCPU.cpp
+++ cpp/open3d/core/kernel/CumSumCPU.cpp
@@ -23,26 +23,65 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 // IN THE SOFTWARE.
 // ----------------------------------------------------------------------------
+#include <tbb/parallel_for.h>
+
 #include "open3d/core/SizeVector.h"
 #include "open3d/core/kernel/CumSum.h"
 #include "open3d/utility/Console.h"
+#include "tbb/parallel_scan.h"
 
 namespace open3d {
 namespace core {
 namespace kernel {
 
+namespace {
+class ScanSumBody {
+    Tensor* out;
+    const Tensor* in;
+    int64_t dim;
+    Tensor sum;
+
+public:
+    ScanSumBody(Tensor* out_, const Tensor* in_, int64_t dim_)
+        : out(out_), in(in_), dim(dim_) {
+        sum = Tensor::Zeros(
+                shape_util::ReductionShape(in->GetShape(), {dim}, true),
+                in->GetDtype(), in->GetDevice());
+    }
+    template <class Tag>
+    void operator()(const tbb::blocked_range<size_t>& r, Tag) {
+        Tensor temp = sum;
+        for (size_t i = r.begin(); i < r.end(); ++i) {
+            temp.Add_(in->Slice(dim, i, i + 1));
+            if (Tag::is_final_scan()) {
+                out->Slice(dim, i, i + 1).AsRvalue() = temp;
+            }
+        }
+        sum = temp;
+    }
+    ScanSumBody(ScanSumBody& b, tbb::split) : out(b.out), in(b.in), dim(b.dim) {
+        sum = Tensor::Zeros(
+                shape_util::ReductionShape(in->GetShape(), {dim}, true),
+                in->GetDtype(), in->GetDevice());
+    }
+    void reverse_join(ScanSumBody& a) { sum = a.sum.Add(sum); }
+    void assign(ScanSumBody& b) { sum = b.sum; }
+};
+}  // namespace
+
 void CumSumCPU(const Tensor& src, Tensor& dst, int64_t dim) {
     // Copy first slice of source Tensor to destination Tensor.
     dst.Slice(dim, 0, 1).CopyFrom(src.Slice(dim, 0, 1));
-
     int64_t num_elements = src.GetShapeRef()[dim];
 
-    for (int64_t i = 1; i < num_elements; ++i) {
-        Tensor src_slice = src.Slice(dim, i, i + 1);
-        Tensor prev_slice = dst.Slice(dim, i - 1, i);
-        Tensor dst_slice = dst.Slice(dim, i, i + 1);
-        dst_slice.AsRvalue() = src_slice.Add(prev_slice);
+    // Return if there are no elements;
+    if (num_elements <= 0) {
+        return;
     }
+
+    // Parallel scan.
+    ScanSumBody body(&dst, &src, dim);
+    tbb::parallel_scan(tbb::blocked_range<size_t>(0, num_elements), body);
 }
 
 }  // namespace kernel
