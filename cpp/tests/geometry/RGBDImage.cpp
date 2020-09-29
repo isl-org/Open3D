53c55a7eca014f70428fd3a4a3b088107578d42d
Yixing Lao <yixing.lao@gmail.com>
2020-09-29 09:49:00 -0700

    refactor two more tests

--- cpp/tests/geometry/RGBDImage.cpp
+++ cpp/tests/geometry/RGBDImage.cpp
@@ -238,129 +238,93 @@ TEST(RGBDImage, CreateFromNYUFormat) {
     // dataset loader, just with a different scaling and truncation factor.
 }
 
+template <typename T>
+static std::vector<T> ImageAsVector(const geometry::Image& im) {
+    const T* data_ptr = reinterpret_cast<const T*>(im.data_.data());
+    int num_elements = im.height_ * im.width_ * im.num_of_channels_;
+    std::vector<T> vals(data_ptr, data_ptr + num_elements);
+    return vals;
+}
+
 TEST(RGBDImage, FilterPyramid) {
-    std::vector<std::vector<uint8_t>> ref_color = {
-            {49,  63,  46,  63,  234, 198, 45,  63,  152, 189, 39,  63,  151,
-             141, 36,  63,  165, 233, 38,  63,  44,  66,  47,  63,  54,  137,
-             40,  63,  10,  229, 34,  63,  34,  30,  36,  63,  102, 55,  42,
-             63,  230, 110, 48,  63,  133, 79,  41,  63,  33,  69,  37,  63,
-             126, 99,  38,  63,  234, 215, 40,  63,  91,  21,  49,  63,  92,
-             34,  42,  63,  75,  230, 36,  63,  181, 45,  37,  63,  210, 213,
-             37,  63,  82,  65,  49,  63,  72,  187, 41,  63,  106, 229, 37,
-             63,  255, 106, 40,  63,  94,  171, 45,  63},
-            {159, 3, 43, 63, 135, 253, 38, 63, 128, 50, 43, 63, 2, 65, 39, 63}};
+    int width = 4;
+    int height = 4;
 
-    std::vector<std::vector<uint8_t>> ref_depth = {
-            {38,  31,  30,  58,  91,  210, 16,  58,  248, 34,  42,  58,  238,
-             234, 63,  58,  236, 245, 76,  58,  110, 243, 222, 57,  98,  12,
-             254, 57,  47,  168, 17,  58,  162, 44,  25,  58,  168, 28,  48,
-             58,  39,  160, 9,   58,  4,   196, 14,  58,  243, 74,  4,   58,
-             173, 65,  5,   58,  160, 171, 45,  58,  11,  239, 20,  58,  154,
-             141, 11,  58,  214, 133, 248, 57,  250, 97,  243, 57,  128, 174,
-             11,  58,  56,  200, 138, 57,  214, 164, 156, 57,  33,  250, 200,
-             57,  206, 160, 238, 57,  123, 32,  188, 57},
-            {196, 181, 8, 58, 174, 43, 22, 58, 190, 83, 23, 58, 152, 174, 7,
-             58}};
+    geometry::Image im_color;
+    im_color.Prepare(width, height, 3, 4);
+    float* im_color_data = reinterpret_cast<float*>(im_color.data_.data());
+    std::vector<float> im_color_val{
+            0.0, 0.0, 0.0, 0.1, 0.1, 0.1, 0.2, 0.2, 0.2, 0.3, 0.3, 0.3,
+            0.4, 0.4, 0.4, 0.5, 0.5, 0.5, 0.6, 0.6, 0.6, 0.7, 0.7, 0.7,
+            0.8, 0.8, 0.8, 0.9, 0.9, 0.9, 1.0, 1.0, 1.0, 1.1, 1.1, 1.1,
+            1.2, 1.2, 1.2, 1.3, 1.3, 1.3, 1.4, 1.4, 1.4, 1.5, 1.5, 1.5};
+    std::copy(im_color_val.begin(), im_color_val.end(), im_color_data);
 
-    geometry::Image depth;
-    geometry::Image color;
+    geometry::Image im_depth;
+    im_depth.Prepare(width, height, 1, 2);
+    uint16_t* im_depth_data =
+            reinterpret_cast<uint16_t*>(im_depth.data_.data());
+    std::vector<uint16_t> im_depth_val{0, 1, 2,  3,  4,  5,  6,  7,
+                                       8, 9, 10, 11, 12, 13, 14, 15};
+    std::copy(im_depth_val.begin(), im_depth_val.end(), im_depth_data);
 
-    const int size = 5;
+    std::shared_ptr<geometry::RGBDImage> im_rgbd =
+            geometry::RGBDImage::CreateFromColorAndDepth(im_color, im_depth, 1,
+                                                         1000, true);
+    geometry::RGBDImagePyramid pyramid = im_rgbd->CreatePyramid(2, false);
+    geometry::RGBDImagePyramid pyramid_filtered =
+            geometry::RGBDImage::FilterPyramid(
+                    pyramid, geometry::Image::FilterType::Sobel3Dx);
 
-    // test image dimensions
-    const int depth_width = size;
-    const int depth_height = size;
-    const int depth_num_of_channels = 1;
-    const int depth_bytes_per_channel = 4;
-
-    const int color_width = size;
-    const int color_height = size;
-    const int color_num_of_channels = 3;
-    const int color_bytes_per_channel = 1;
-
-    color.Prepare(color_width, color_height, color_num_of_channels,
-                  color_bytes_per_channel);
-
-    depth.Prepare(depth_width, depth_height, depth_num_of_channels,
-                  depth_bytes_per_channel);
-
-    float* const float_data = depth.PointerAs<float>();
-    Rand(float_data, depth_width * depth_height, 0.0, 1.0, 0);
-    Rand(color.data_, 130, 200, 0);
-
-    size_t num_of_levels = 2;
-    auto rgbd_image =
-            geometry::RGBDImage::CreateFromColorAndDepth(color, depth);
-    auto pyramid = rgbd_image->CreatePyramid(num_of_levels);
-    auto filtered = geometry::RGBDImage::FilterPyramid(
-            pyramid, geometry::Image::FilterType::Gaussian3);
-
-    for (size_t j = 0; j < num_of_levels; j++) {
-        ExpectEQ(ref_color[j], filtered[j]->color_.data_);
-        ExpectEQ(ref_depth[j], filtered[j]->depth_.data_);
-    }
+    ExpectEQ(ImageAsVector<float>(pyramid_filtered[0]->color_),
+             std::vector<float>({0.4, 0.8, 0.8, 0.4, 0.4, 0.8, 0.8, 0.4, 0.4,
+                                 0.8, 0.8, 0.4, 0.4, 0.8, 0.8, 0.4}));
+    ExpectEQ(ImageAsVector<float>(pyramid_filtered[0]->depth_),
+             std::vector<float>(
+                     {4, 8, 8, 4, 4, 8, 8, 4, 4, 8, 8, 4, 4, 8, 8, 4}));
+    ExpectEQ(ImageAsVector<float>(pyramid_filtered[1]->color_),
+             std::vector<float>({0.8, 0.8, 0.8, 0.8}));
+    ExpectEQ(ImageAsVector<float>(pyramid_filtered[1]->depth_),
+             std::vector<float>({8, 8, 8, 8}));
 }
 
 TEST(RGBDImage, CreatePyramid) {
-    std::vector<std::vector<uint8_t>> ref_color = {
-            {216, 2,   42,  63,  21,  162, 57,  63,  62,  210, 42,  63,  216,
-             72,  38,  63,  116, 49,  38,  63,  55,  245, 52,  63,  150, 19,
-             30,  63,  123, 6,   19,  63,  193, 10,  23,  63,  83,  253, 46,
-             63,  141, 0,   52,  63,  9,   144, 38,  63,  193, 19,  55,  63,
-             19,  179, 45,  63,  56,  160, 49,  63,  26,  10,  50,  63,  121,
-             185, 46,  63,  168, 239, 16,  63,  183, 184, 35,  63,  63,  137,
-             21,  63,  135, 1,   54,  63,  220, 15,  35,  63,  177, 246, 44,
-             63,  207, 89,  38,  63,  56,  66,  57,  63},
-            {96, 244, 44, 63, 151, 211, 36, 63, 137, 61, 45, 63, 40, 111, 37,
-             63}};
+    int width = 4;
+    int height = 4;
 
-    std::vector<std::vector<uint8_t>> ref_depth = {
-            {208, 254, 91,  58,  103, 154, 205, 57,  59,  147, 76,  58,  236,
-             175, 80,  58,  232, 127, 110, 58,  103, 154, 77,  57,  62,  195,
-             174, 57,  139, 118, 72,  58,  22,  236, 143, 57,  66,  243, 16,
-             58,  161, 199, 248, 57,  134, 123, 36,  58,  255, 53,  191, 57,
-             93,  164, 5,   58,  161, 199, 120, 58,  20,  135, 111, 58,  222,
-             137, 38,  58,  79,  25,  59,  58,  198, 8,   20,  57,  126, 80,
-             30,  58,  5,   150, 131, 55,  249, 213, 122, 57,  101, 207, 11,
-             57,  68,  190, 82,  58,  214, 94,  32,  57},
-            {30, 202, 230, 57, 240, 107, 43, 58, 18, 188, 45, 58, 111, 173, 226,
-             57}};
+    geometry::Image im_color;
+    im_color.Prepare(width, height, 3, 4);
+    float* im_color_data = reinterpret_cast<float*>(im_color.data_.data());
+    std::vector<float> im_color_val{
+            0.0, 0.0, 0.0, 0.1, 0.1, 0.1, 0.2, 0.2, 0.2, 0.3, 0.3, 0.3,
+            0.4, 0.4, 0.4, 0.5, 0.5, 0.5, 0.6, 0.6, 0.6, 0.7, 0.7, 0.7,
+            0.8, 0.8, 0.8, 0.9, 0.9, 0.9, 1.0, 1.0, 1.0, 1.1, 1.1, 1.1,
+            1.2, 1.2, 1.2, 1.3, 1.3, 1.3, 1.4, 1.4, 1.4, 1.5, 1.5, 1.5};
+    std::copy(im_color_val.begin(), im_color_val.end(), im_color_data);
 
-    geometry::Image depth;
-    geometry::Image color;
+    geometry::Image im_depth;
+    im_depth.Prepare(width, height, 1, 2);
+    uint16_t* im_depth_data =
+            reinterpret_cast<uint16_t*>(im_depth.data_.data());
+    std::vector<uint16_t> im_depth_val{0, 1, 2,  3,  4,  5,  6,  7,
+                                       8, 9, 10, 11, 12, 13, 14, 15};
+    std::copy(im_depth_val.begin(), im_depth_val.end(), im_depth_data);
 
-    const int size = 5;
+    std::shared_ptr<geometry::RGBDImage> im_rgbd =
+            geometry::RGBDImage::CreateFromColorAndDepth(im_color, im_depth, 1,
+                                                         1000, true);
+    geometry::RGBDImagePyramid pyramid = im_rgbd->CreatePyramid(2, false);
 
-    // test image dimensions
-    const int depth_width = size;
-    const int depth_height = size;
-    const int depth_num_of_channels = 1;
-    const int depth_bytes_per_channel = 4;
-
-    const int color_width = size;
-    const int color_height = size;
-    const int color_num_of_channels = 3;
-    const int color_bytes_per_channel = 1;
-
-    color.Prepare(color_width, color_height, color_num_of_channels,
-                  color_bytes_per_channel);
-
-    depth.Prepare(depth_width, depth_height, depth_num_of_channels,
-                  depth_bytes_per_channel);
-
-    float* const float_data = depth.PointerAs<float>();
-    Rand(float_data, depth_width * depth_height, 0.0, 1.0, 0);
-    Rand(color.data_, 130, 200, 0);
-
-    size_t num_of_levels = 2;
-    auto rgbd_image =
-            geometry::RGBDImage::CreateFromColorAndDepth(color, depth);
-    auto pyramid = rgbd_image->CreatePyramid(num_of_levels);
-
-    for (size_t j = 0; j < num_of_levels; j++) {
-        EXPECT_EQ(ref_color[j], pyramid[j]->color_.data_);
-        EXPECT_EQ(ref_depth[j], pyramid[j]->depth_.data_);
-    }
+    ExpectEQ(ImageAsVector<float>(pyramid[0]->color_),
+             std::vector<float>({0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8,
+                                 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5}));
+    ExpectEQ(ImageAsVector<float>(pyramid[0]->depth_),
+             std::vector<float>(
+                     {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}));
+    ExpectEQ(ImageAsVector<float>(pyramid[1]->color_),
+             std::vector<float>({0.25, 0.45, 1.05, 1.25}));
+    ExpectEQ(ImageAsVector<float>(pyramid[1]->depth_),
+             std::vector<float>({2.5, 4.5, 10.5, 12.5}));
 }
 
 }  // namespace tests
