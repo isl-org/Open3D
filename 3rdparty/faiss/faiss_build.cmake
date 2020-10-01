837001ebe30c49e551c1c47cddb7d37ebb3b68bc
Junha Lee <junhakiwi@postech.ac.kr>
2020-10-01 11:25:56 +0900

    faiss openmp on osx

--- 3rdparty/faiss/faiss_build.cmake
+++ 3rdparty/faiss/faiss_build.cmake
@@ -15,7 +15,7 @@ ExternalProject_Add(
     GIT_REPOSITORY https://github.com/facebookresearch/faiss.git
     GIT_TAG 27393c436cfd621544a89711fd0694188aa90277
     UPDATE_COMMAND ""
-    PATCH_COMMAND git apply ${Open3D_3RDPARTY_DIR}/faiss/0001-Allow-selection-of-blas-library.patch
+    PATCH_COMMAND git apply ${Open3D_3RDPARTY_DIR}/faiss/0001-Allow-selection-of-blas-library-and-resolve-openmp-error.patch
     CMAKE_ARGS
         -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
         -DCMAKE_INSTALL_PREFIX=${FAISS_ROOT}
