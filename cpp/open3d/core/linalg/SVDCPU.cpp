430989a35ca3c3212b4885f807a6d22e6d2754a1
Yixing Lao <yixing.lao@gmail.com>
2020-10-01 21:20:08 -0700

    OpenBLAS/LAPACK and support for ARM (#2205)

--- cpp/open3d/core/linalg/SVDCPU.cpp
+++ cpp/open3d/core/linalg/SVDCPU.cpp
@@ -24,10 +24,7 @@
 // IN THE SOFTWARE.
 // ----------------------------------------------------------------------------
 
-#include <stdio.h>
-#include <stdlib.h>
-
-#include "open3d/core/linalg/LAPACK.h"
+#include "open3d/core/linalg/LapackWrapper.h"
 #include "open3d/core/linalg/LinalgUtils.h"
 #include "open3d/core/linalg/SVD.h"
 
