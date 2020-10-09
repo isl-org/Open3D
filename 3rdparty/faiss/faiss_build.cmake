5a1eaa655910dbdcf03b175e05dfa3a884a95804
Junha Lee <junhakiwi@postech.ac.kr>
2020-10-09 12:50:55 +0900

    change faiss to junha's fork

--- 3rdparty/faiss/faiss_build.cmake
+++ 3rdparty/faiss/faiss_build.cmake
@@ -12,16 +12,16 @@ set(FAISS_ROOT "${CMAKE_CURRENT_BINARY_DIR}/libfaiss_install")
 ExternalProject_Add(
     ext_faiss
     PREFIX faiss
-    GIT_REPOSITORY https://github.com/facebookresearch/faiss.git
-    GIT_TAG 27393c436cfd621544a89711fd0694188aa90277
+    GIT_REPOSITORY https://github.com/junha-l/faiss.git 
+    GIT_TAG 22e78bb09672478d5aa83a83f6f8bf06f676e76d 
     UPDATE_COMMAND ""
-    PATCH_COMMAND git apply ${Open3D_3RDPARTY_DIR}/faiss/0001-Allow-selection-of-blas-library.patch
     CMAKE_ARGS
         -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
         -DCMAKE_INSTALL_PREFIX=${FAISS_ROOT}
         -DCMAKE_POSITION_INDEPENDENT_CODE=ON
         -DFAISS_ENABLE_GPU=${BUILD_CUDA_MODULE}
         -DFAISS_ENABLE_PYTHON=OFF
+        -DFAISS_USE_SYSTEM_BLAS=OFF
         -DFAISS_BLAS_TARGET=${FAISS_BLAS_TARGET}
         -DBUILD_TESTING=OFF
 )
