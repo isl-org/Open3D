f2cef3c2abdc082dc981aff7838daa30a372e19f
Yixing Lao <yixing.lao@gmail.com>
2020-10-16 05:19:09 -0700

    remove conda env check for debugging

--- cpp/pybind/check_and_install_conda_deps.cmake
+++ cpp/pybind/check_and_install_conda_deps.cmake
@@ -1,16 +1,3 @@
-# Assert that we're inside a conda environemnt
-message(STATUS "Checking Conda environment...")
-execute_process(
-    COMMAND bash "-c" "conda info | grep -q \"active environment : None\""
-    RESULT_VARIABLE return_code
-)
-
-if (${return_code})
-    message("Conda environment is active.")
-else()
-    message(FATAL_ERROR "Not in a conda environment: 1) activate conda, 2) rerun cmake and make")
-endif()
-
 # Check if conda-build is installed, if not, install it
 find_program(CONDA_BUILD "conda-build")
 if (NOT CONDA_BUILD)
