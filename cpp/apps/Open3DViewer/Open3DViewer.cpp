a2550c9839478c5565054046b7681c4c4caec9f9
Benjamin Ummenhofer <benjaminum@gmail.com>
2020-11-23 13:09:03 +0100

    only start RPC interface if started with '--bind' on linux

--- cpp/apps/Open3DViewer/Open3DViewer.cpp
+++ cpp/apps/Open3DViewer/Open3DViewer.cpp
@@ -55,10 +55,12 @@ int Run(int argc, const char *argv[]) {
 
     auto vis = std::make_shared<GuiVisualizer>("Open3D", WIDTH, HEIGHT);
 #ifdef BUILD_RPC_INTERFACE
-    std::string address = utility::GetProgramOptionAsString(
-            argc, (char **)argv, "--bind",
-            io::rpc::Connection::DefaultAddress());
-    vis->StartRPCInterface(address, 1000);
+    if (utility::ProgramOptionExists(argc, (char **)argv, "--bind")) {
+        std::string address = utility::GetProgramOptionAsString(
+                argc, (char **)argv, "--bind",
+                io::rpc::Connection::DefaultAddress());
+        vis->StartRPCInterface(address, 1000);
+    }
 #endif
     bool is_path_valid = (path && path[0] != '\0');
     if (is_path_valid) {
