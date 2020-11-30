f63524f30829315eada1264888a3b00e9c3f9c30
Rene <errissa@gmail.com>
2020-11-30 17:12:35 -0500

    Integration of Custom Filament Headless Backend (#2572)
    * Update download links to Filament 1.9.4
    
    * Update to build against filament 1.9.5
    
    * Update download link for 20.04 builds to Filament 1.9.5
    
    * Merge branch 'master' into errissa/filament-1.9.5
    
    * Update to build against 1.9.7 in general and 1.9.7 custom build for Linux
    
    * Add method to configure Engine for headless rendering
    
    * Link against EGL
    
    * Add ability to specify headless rendering when creating OffscreenRenderer
    
    * Don't link in EGL on Apple
    
    * Signal error if user tries to configure rendering for headless on unsupported platforms
    
    * Style fixes
    
    * Filament uses main not master for it's main development
    
    * Make sure EGL is linked correctly when building shared libraries
    
    * Merge branch 'master' into errissa/filament-1.9.5
    
    * Updates to work with Intel-ISL Filament fork
    
    * Download Ubuntu 20.04 specific build when necessary

--- 3rdparty/filament/filament_build.cmake
+++ 3rdparty/filament/filament_build.cmake
@@ -2,8 +2,8 @@ include(ExternalProject)
 
 set(FILAMENT_ROOT "${CMAKE_BINARY_DIR}/filament-binaries")
 
-set(FILAMENT_GIT_REPOSITORY "https://github.com/google/filament.git")
-set(FILAMENT_GIT_TAG "v1.9.5")
+set(FILAMENT_GIT_REPOSITORY "https://github.com/intel-isl/filament.git")
+set(FILAMENT_GIT_TAG "release")
 
 ExternalProject_Add(
     ext_filament
@@ -24,6 +24,7 @@ ExternalProject_Add(
         -DUSE_STATIC_LIBCXX=ON
         -DFILAMENT_SUPPORTS_VULKAN=OFF
         -DFILAMENT_SKIP_SAMPLES=ON
+        -DFILAMENT_OPENGL_HANDLE_ARENA_SIZE_IN_MB=20 # to support many small entities
 )
 
 set(filament_LIBRARIES
