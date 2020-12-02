635434b58f16373188fe207ebd097cbc0c03c14a
Rishabh Singh <rishabh.17iitkgp@gmail.com>
2020-12-03 00:26:05 +0530

    t-PointCloud UnitTest added

--- cpp/tests/t/geometry/PointCloud.cpp
+++ cpp/tests/t/geometry/PointCloud.cpp
@@ -118,6 +118,35 @@ TEST_P(PointCloudPermuteDevices, GetMinBound_GetMaxBound_GetCenter) {
               std::vector<float>({2.5, 3.5, 4.5}));
 }
 
+TEST_P(PointCloudPermuteDevices, Transform) {
+    core::Device device = GetParam();
+
+    t::geometry::PointCloud pcd(core::Dtype::Float64, device);
+
+    // i) Translated by -1, -1, -1 
+    // ii) Rotated by +30 degree about x-axis, +45 degree about y-axis
+    std::vector<double> transformation_v{
+                                0.707, 0.354, 0.612, -1.673,
+                                 0.0,   0.866, -0.5,  -0.366,
+                                -0.707, 0.354, 0.612, -0.259,
+                                 0.0,   0.0,   0.0,    1.0};    
+    
+    core::Tensor transformation(transformation_v,
+                                {4, 4}, core::Dtype::Float64, device);
+
+    pcd.SetPoints(core::Tensor(std::vector<double>{3, 2, 1}, {1, 3},
+                               core::Dtype::Float64, device));
+    pcd.SetPointNormals(core::Tensor(std::vector<double>{1, 1, 1}, {1, 3},
+                               core::Dtype::Float64, device));
+
+    pcd.Transform(transformation);
+
+    EXPECT_EQ(pcd.GetPoints().ToFlatVector<double>(),
+              std::vector<double>({1.768, 0.866, -1.061}));
+    EXPECT_EQ(pcd.GetPointNormals().ToFlatVector<double>(),
+              std::vector<double>({1.673, 0.366, 0.259}));
+}
+
 TEST_P(PointCloudPermuteDevices, Translate) {
     core::Device device = GetParam();
 
