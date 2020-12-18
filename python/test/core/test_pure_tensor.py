411b880bcaa6b9484fb65170184c1de25cd0f5cd
Yixing Lao <yixing.lao@gmail.com>
2020-12-18 04:49:18 -0800

    update test

--- python/test/core/test_pure_tensor.py
+++ python/test/core/test_pure_tensor.py
@@ -63,6 +63,11 @@ def test_list_creation():
                                 device=o3c.Device("CPU:0"))
     print(t1)
 
+    t1 = o3d.pybind.core.Tensor([[0, 1, 2], [3, 4, 5]],
+                                dtype=None,
+                                device=o3c.Device("CPU:0"))
+    print(t1)
+
     with pytest.raises(RuntimeError):
         t1 = o3d.pybind.core.Tensor([[0, 1, 2, 3, 4, 5], [2, 3]],
                                     dtype=None,
