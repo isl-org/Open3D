5b2bc5bbb6859f9c2f8ab69bc7f020627e982b35
Yixing Lao <yixing.lao@gmail.com>
2020-12-18 17:42:05 -0800

    const ref

--- python/test/core/test_pure_tensor.py
+++ python/test/core/test_pure_tensor.py
@@ -38,51 +38,51 @@ def test_creation():
     t_np = np.ones((2, 3))
 
     t0 = o3c.Tensor(t_np)
-    print(t0)
+    # print(t0)
 
     t1 = o3d.pybind.core.Tensor(t_np, o3c.Dtype.Int32)
-    print(t1)
+    # print(t1)
 
     t1 = o3d.pybind.core.Tensor(t_np, dtype=None, device=o3c.Device("CPU:0"))
-    print(t1)
+    # print(t1)
 
     t1 = o3d.pybind.core.Tensor(1,
                                 dtype=o3c.Dtype.Float32,
                                 device=o3c.Device("CPU:0"))
-    print(t1)
+    # print(t1)
 
     t1 = o3d.pybind.core.Tensor(3.14,
                                 dtype=o3c.Dtype.Int32,
                                 device=o3c.Device("CPU:0"))
-    print(t1)
+    # print(t1)
 
 
 def test_list_creation():
     t1 = o3d.pybind.core.Tensor([0, 1, 2, 3, 4, 5],
                                 dtype=None,
                                 device=o3c.Device("CPU:0"))
-    print(t1)
+    # print(t1)
 
     t1 = o3d.pybind.core.Tensor([[0, 1, 2], [3, 4, 5]],
                                 dtype=None,
                                 device=o3c.Device("CPU:0"))
-    print(t1)
+    # print(t1)
 
     with pytest.raises(RuntimeError):
         t1 = o3d.pybind.core.Tensor([[0, 1, 2, 3, 4, 5], [2, 3]],
                                     dtype=None,
                                     device=o3c.Device("CPU:0"))
-        print(t1)
+        # print(t1)
 
 
 def test_tuple_creation():
     t1 = o3d.pybind.core.Tensor((0, 1, 2, 3, 4, 5),
                                 dtype=None,
                                 device=o3c.Device("CPU:0"))
-    print(t1)
+    # print(t1)
 
     with pytest.raises(RuntimeError):
         t1 = o3d.pybind.core.Tensor(((0, 1, 2, 3, 4, 5), (2, 3)),
                                     dtype=None,
                                     device=o3c.Device("CPU:0"))
-        print(t1)
+        # print(t1)
