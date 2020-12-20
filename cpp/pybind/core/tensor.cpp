2ddda1bc66a3ab218f91b7da39744bd1c44b2d36
Yixing Lao <yixing.lao@gmail.com>
2020-12-20 01:07:28 -0800

    eye

--- cpp/pybind/core/tensor.cpp
+++ cpp/pybind/core/tensor.cpp
@@ -210,7 +210,6 @@ void pybind_core_tensor(py::module& m) {
     BindTensorFullCreation<uint8_t>(tensor);
     BindTensorFullCreation<bool>(tensor);
 
-    tensor.def_static("eye", &Tensor::Eye);
     tensor.def_static(
             "eye",
             [](int64_t n, utility::optional<Dtype> dtype,
