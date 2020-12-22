357510c306d3f330aaaae7e8e81f7cd3102f72ec
Yixing Lao <yixing.lao@gmail.com>
2020-12-22 01:46:44 -0800

    remove test_draw

--- test_draw.py
+++
@@ -1,6 +0,0 @@
-import open3d as o3d
-
-pcd = o3d.io.read_point_cloud(
-    "/Users/ylao/repo/Open3D/examples/test_data/fragment.pcd")
-o3d.visualization.draw_geometries([pcd])
-o3d.visualization.draw_geometries([pcd])
