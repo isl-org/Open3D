a73531539f66e501177b66a519467b2905558ab0
Sameer Sheorey <sameer.sheorey@intel.com>
2020-12-31 10:56:24 -0800

    Python unit test and data for realsense

---
+++ python/test/t/io/test_realsense.py
@@ -0,0 +1,88 @@
+# ----------------------------------------------------------------------------
+# -                        Open3D: www.open3d.org                            -
+# ----------------------------------------------------------------------------
+# The MIT License (MIT)
+#
+# Copyright (c) 2020 www.open3d.org
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+# ----------------------------------------------------------------------------
+
+# flake8: noqa: S101
+
+import sys
+import os
+import shutil
+import open3d as o3d
+import numpy as np
+import pytest
+
+sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../..")
+from open3d_test import test_data_dir
+
+
+def test_RSBagReader():
+
+    shutil.unpack_archive(test_data_dir +
+                          "/RGBD/other_formats/L515_test_s.bag.tar.xz")
+
+    bag_reader = o3d.t.io.RSBagReader()
+    bag_reader.open("L515_test_s.bag")
+
+    # Metadata
+    metadata = bag_reader.metadata
+    assert metadata.color_channels == 3
+    assert metadata.color_dt == o3d.core.Dtype.UInt16
+    assert metadata.color_format == 'RGB8'
+    assert metadata.depth_dt == o3d.core.Dtype.UInt8
+    assert metadata.depth_format == 'Z16'
+    assert np.allclose(metadata.depth_scale, 3999.999755859375)
+    assert metadata.device_name == "Intel RealSense L515"
+    assert metadata.fps == 30
+    assert metadata.height == 540
+    assert metadata.width == 960
+    assert metadata.stream_length_usec == 199868
+    assert np.allclose(
+        metadata.intrsics.intrinsic_matrix,
+        np.array([[689.3069458, 0., 491.23974609],
+                  [0., 689.74578857, 269.99111938], [0., 0., 1.]]))
+
+    # Frames
+    im_rgbd = bag_reader.next_frame()
+    assert not im_rgbd.is_empty() and im_rgbd.are_aligned()
+    assert im_rgbd.color.channels == 3
+    assert im_rgbd.color.dtype == o3d.core.Dtype.UInt8
+    assert im_rgbd.color.rows == 540
+    assert im_rgbd.color.columns == 960
+    assert im_rgbd.depth.channels == 1
+    assert im_rgbd.depth.dtype == o3d.core.Dtype.UInt16
+    assert im_rgbd.depth.rows == 540
+    assert im_rgbd.depth.columns == 960
+
+    n_frames = 1
+    while not bag_reader.is_eof():
+        im_rgbd = bag_reader.next_frame()
+        n_frames = n_frames + 1
+        print(im_rgbd)
+        # process im_rgbd.depth and im_rgbd.color
+
+    bag_reader.close()
+    assert n_frames == 6
+
+    os.remove("L515_test_s.bag")
