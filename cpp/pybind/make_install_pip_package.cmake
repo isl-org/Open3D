8adf34bd4668d773a540de325214a894115f0af5
Yixing Lao <yixing.lao@gmail.com>
2021-01-10 15:04:14 -0800

    make install-pip-package force update

--- cpp/pybind/make_install_pip_package.cmake
+++ cpp/pybind/make_install_pip_package.cmake
@@ -4,4 +4,10 @@
 # Note: Since `make python-package` clears PYTHON_COMPILED_MODULE_DIR every time,
 #       it is guaranteed that there is only one wheel in ${PYTHON_PACKAGE_DST_DIR}/pip_package/*.whl
 file(GLOB WHEEL_FILE "${PYTHON_PACKAGE_DST_DIR}/pip_package/*.whl")
-execute_process(COMMAND pip install ${WHEEL_FILE} -U)
+execute_process(
+    COMMAND pip install --upgrade ${WHEEL_FILE}
+    # `pip install --upgrade` sometimes does not install the wheel for
+    # uncommited changes. This is because the Open3D package version number
+    # `open3d-version_id+commit_id` remains the same.
+    COMMAND pip install --upgrade --no-deps --force-reinstall ${WHEEL_FILE}
+)
