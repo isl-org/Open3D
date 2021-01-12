03d9ab4813fb29cbdedbdeb296850e57dc999393
Sameer Sheorey <sameer.sheorey@intel.com>
2021-01-11 16:04:45 -0800

    restrict #pragma warning to MSVC

--- cpp/open3d/geometry/SurfaceReconstructionPoisson.cpp
+++ cpp/open3d/geometry/SurfaceReconstructionPoisson.cpp
@@ -37,18 +37,22 @@
 #include "open3d/utility/Console.h"
 
 // clang-format off
+#ifdef _MSC_VER
 #pragma warning(push)
-#pragma warning (disable: 4701 4245 4189)
+#pragma warning(disable: 4701 4245 4189)
 // 4701: potentially uninitialized local variable
-// 4245: signed/unsigned mismatch 
-// 4189: local variable is initialized but not referenced 
+// 4245: signed/unsigned mismatch
+// 4189: local variable is initialized but not referenced
+#endif
 #include "PoissonRecon/Src/PreProcessor.h"
 #include "PoissonRecon/Src/MyMiscellany.h"
 #include "PoissonRecon/Src/CmdLineParser.h"
 #include "PoissonRecon/Src/FEMTree.h"
 #include "PoissonRecon/Src/PPolynomial.h"
 #include "PoissonRecon/Src/PointStreamData.h"
+#ifdef _MSC_VER
 #pragma warning(pop)
+#endif
 // clang-format on
 
 namespace open3d {
