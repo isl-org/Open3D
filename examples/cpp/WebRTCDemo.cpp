bf0fb1d3a2e14f68187edfd5e9896f0d29fbcf26
Yixing Lao <yixing.lao@gmail.com>
2021-01-27 09:50:32 -0800

    use std::ref to pass refernce to threads

--- examples/cpp/WebRTCDemo.cpp
+++ examples/cpp/WebRTCDemo.cpp
@@ -17,29 +17,29 @@ public:
     std::string name_;
 };
 
-void Rename(Dog* dog) {
+void Rename(Dog& dog) {
     std::string new_name = "feifei";
     while (true) {
         std::this_thread::sleep_for(std::chrono::seconds(2));
-        if (dog->name_ == "feifei") {
-            dog->name_ = "fafa";
+        if (dog.name_ == "feifei") {
+            dog.name_ = "fafa";
         } else {
-            dog->name_ = "feifei";
+            dog.name_ = "feifei";
         }
     }
 }
 
-void Print(Dog* dog) {
+void Print(Dog& dog) {
     while (true) {
         std::this_thread::sleep_for(std::chrono::seconds(1));
-        dog->PrintName();
+        dog.PrintName();
     }
 }
 
 int main() {
-    Dog d("feifei");
-    std::thread first(Rename, &d);
-    std::thread second(Print, &d);
+    Dog dog("feifei");
+    std::thread first(Rename, std::ref(dog));
+    std::thread second(Print, std::ref(dog));
 
     std::cout << "main, rename and print now execute concurrently...\n";
     first.join();   // pauses until first finishes
