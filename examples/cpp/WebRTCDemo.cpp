11a4f8d1ba2398c7c20364a388394f4b48045021
Yixing Lao <yixing.lao@gmail.com>
2021-01-27 09:49:13 -0800

    demo on two threads

--- examples/cpp/WebRTCDemo.cpp
+++ examples/cpp/WebRTCDemo.cpp
@@ -1,6 +1,50 @@
-#include "open3d/visualization/webrtc/WebRTCStreamer.h"
+// #include "open3d/visualization/webrtc/WebRTCStreamer.h"
+
+// int main() {
+//     WebRTCStreamer wss;
+//     wss.Run();
+// }
+
+#include <chrono>
+#include <iostream>
+#include <string>
+#include <thread>
+
+class Dog {
+public:
+    Dog(const std::string& name) : name_(name) {}
+    void PrintName() const { std::cout << "Dog: " << name_ << std::endl; }
+    std::string name_;
+};
+
+void Rename(Dog* dog) {
+    std::string new_name = "feifei";
+    while (true) {
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+        if (dog->name_ == "feifei") {
+            dog->name_ = "fafa";
+        } else {
+            dog->name_ = "feifei";
+        }
+    }
+}
+
+void Print(Dog* dog) {
+    while (true) {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        dog->PrintName();
+    }
+}
 
 int main() {
-    WebRTCStreamer wss;
-    wss.Run();
+    Dog d("feifei");
+    std::thread first(Rename, &d);
+    std::thread second(Print, &d);
+
+    std::cout << "main, rename and print now execute concurrently...\n";
+    first.join();   // pauses until first finishes
+    second.join();  // pauses until second finishes
+    std::cout << "rename and print completed.\n";
+
+    return 0;
 }
