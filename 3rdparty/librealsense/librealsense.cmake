cf21bd5189509f04c15f9329c6c479d14f6298ee
ssheorey <41028320+ssheorey@users.noreply.github.com>
2021-01-26 16:59:00 -0800

    Enable RSUSB backend only on Linux. (#2923)
    * Enable RSUSB backend only on Linux.
    Add Python unit test for RealSenseSensor
    
    * Modify error message in unit test

--- 3rdparty/librealsense/librealsense.cmake
+++ 3rdparty/librealsense/librealsense.cmake
@@ -7,7 +7,8 @@ ExternalProject_Add(
     GIT_TAG v2.40.0 # 18 Nov 2020
     UPDATE_COMMAND ""
     # Patch for libusb static build failure on Linux
-    PATCH_COMMAND  ${CMAKE_COMMAND} -E copy
+    PATCH_COMMAND git -C <SOURCE_DIR> reset --hard v2.40.0
+    COMMAND ${CMAKE_COMMAND} -E copy
     ${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/librealsense/libusb-CMakeLists.txt
     <SOURCE_DIR>/third-party/libusb/CMakeLists.txt
     # Patch for libstdc++ regex bug
@@ -25,7 +26,7 @@ ExternalProject_Add(
         -D BUILD_GRAPHICAL_EXAMPLES=OFF
         -D BUILD_PYTHON_BINDINGS=OFF
         -D BUILD_WITH_CUDA=${BUILD_CUDA_MODULE}
-        -D FORCE_RSUSB_BACKEND=ON      # https://github.com/IntelRealSense/librealsense/wiki/Release-Notes#release-2400
+        -D FORCE_RSUSB_BACKEND=$<IF:$<PLATFORM_ID:Linux>,ON,OFF>      # https://github.com/IntelRealSense/librealsense/wiki/Release-Notes#release-2400
         -D USE_EXTERNAL_USB=ON
         $<$<PLATFORM_ID:Darwin>:-DBUILD_WITH_OPENMP=OFF>
         $<$<PLATFORM_ID:Darwin>:-DHWM_OVER_XU=OFF>
@@ -39,9 +40,9 @@ set(LIBREALSENSE_LIB_DIR "${INSTALL_DIR}/lib")
 set(LIBREALSENSE_LIBRARIES realsense2 fw realsense-file usb) # The order is critical.
 if(MSVC)    # Rename debug libs to ${LIBREALSENSE_LIBRARIES}. rem (comment) is no-op
     ExternalProject_Add_Step(ext_librealsense rename_debug_libs
-        COMMAND $<IF:$<CONFIG:Debug>,rename,rem> realsense2d.lib realsense2.lib
-        COMMAND $<IF:$<CONFIG:Debug>,rename,rem> fwd.lib fw.lib
-        COMMAND $<IF:$<CONFIG:Debug>,rename,rem> realsense-filed.lib realsense-file.lib
+        COMMAND $<IF:$<CONFIG:Debug>,move,rem> /Y realsense2d.lib realsense2.lib
+        COMMAND $<IF:$<CONFIG:Debug>,move,rem> /Y fwd.lib fw.lib
+        COMMAND $<IF:$<CONFIG:Debug>,move,rem> /Y realsense-filed.lib realsense-file.lib
         WORKING_DIRECTORY "${LIBREALSENSE_LIB_DIR}"
         DEPENDEES install
     )
