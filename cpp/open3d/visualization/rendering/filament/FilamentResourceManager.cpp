efb5e216e529873802b0c8eed80a0012de74b8a2
Rene Sepulveda <errissa@gmail.com>
2021-02-02 09:36:57 -0500

    Set a better default color for ground plane lines

--- cpp/open3d/visualization/rendering/filament/FilamentResourceManager.cpp
+++ cpp/open3d/visualization/rendering/filament/FilamentResourceManager.cpp
@@ -878,7 +878,7 @@ void FilamentResourceManager::LoadDefaults() {
     const auto inf_path = resource_root + "/infiniteGroundPlane.filamat";
     auto inf_mat = LoadMaterialFromFile(inf_path, engine_);
     inf_mat->setDefaultParameter("baseColor", filament::RgbType::sRGB,
-                                 {0.0f, 0.0f, 0.0f});
+                                 {0.4f, 0.4f, 0.4f});
     materials_[kInfinitePlaneShader] = BoxResource(inf_mat, engine_);
 
     const auto line_path = resource_root + "/unlitLine.filamat";
