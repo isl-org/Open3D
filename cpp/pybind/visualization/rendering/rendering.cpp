6b75f88c5901c0c0140302fe57c3d02cd1634797
prewettg <prewettg@gmail.com>
2021-02-15 19:51:28 -0800

    Added python bindings for rendering::Camera (#3033)

--- cpp/pybind/visualization/rendering/rendering.cpp
+++ cpp/pybind/visualization/rendering/rendering.cpp
@@ -169,7 +169,28 @@ void pybind_rendering_classes(py::module &m) {
                  "image_width, image_height)")
             .def("look_at", &Camera::LookAt,
                  "Sets the position and orientation of the camera: "
-                 "look_at(center, eye, up)");
+                 "look_at(center, eye, up)")
+            .def("unproject", &Camera::Unproject,
+                 "unproject(x, y, z, view_width, view_height): takes the "
+                 "(x, y, z) location in the view, where x, y are the number of "
+                 "pixels from the upper left of the view, and z is the depth "
+                 "value. Returns the world coordinate (x', y', z').")
+            .def("get_near", &Camera::GetNear,
+                 "Returns the distance from the camera to the near plane")
+            .def("get_far", &Camera::GetFar,
+                 "Returns the distance from the camera to the far plane")
+            .def("get_field_of_view", &Camera::GetFieldOfView,
+                 "Returns the field of view of camera, in degrees. Only valid "
+                 "if it was passed to set_projection().")
+            .def("get_field_of_view_type", &Camera::GetFieldOfViewType,
+                 "Returns the field of view type. Only valid if it was passed "
+                 "to set_projection().")
+            .def("get_projection_matrix", &Camera::GetProjectionMatrix,
+                 "Returns the projection matrix of the camera")
+            .def("get_view_matrix", &Camera::GetViewMatrix,
+                 "Returns the view matrix of the camera")
+            .def("get_model_matrix", &Camera::GetModelMatrix,
+                 "Returns the model matrix of the camera");
 
     // ---- Gradient ----
     py::class_<Gradient, std::shared_ptr<Gradient>> gradient(
