957c49ae538f421ca9f4172ee8b73b3678a2baeb
Yixing Lao <yixing.lao@gmail.com>
2021-02-25 17:34:56 -0800

    no delay

--- cpp/open3d/visualization/webrtc_server/ImageCapturer.h
+++ cpp/open3d/visualization/webrtc_server/ImageCapturer.h
@@ -97,7 +97,7 @@ public:
             std::this_thread::sleep_for(std::chrono::milliseconds(5));
         } else {
             callback_->OnCaptureResult(GlobalBuffer::GetInstance().Read());
-            std::this_thread::sleep_for(std::chrono::milliseconds(10));
+            // std::this_thread::sleep_for(std::chrono::milliseconds(10));
         }
         // if (is_blank_) {
         //     frame_.AsRvalue() = lena_;
