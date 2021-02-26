1a894ddd934ff079fa6fd19aa3505e0bdd782f16
Yixing Lao <yixing.lao@gmail.com>
2021-02-26 01:19:39 -0800

    bind to local ip

--- cpp/open3d/visualization/webrtc_server/WebRTCServer.h
+++ cpp/open3d/visualization/webrtc_server/WebRTCServer.h
@@ -43,7 +43,7 @@ namespace webrtc_server {
 
 class WebRTCServer {
 public:
-    WebRTCServer(const std::string& http_address = "localhost:8888",
+    WebRTCServer(const std::string& http_address = "192.168.86.121:8888",
                  const std::string& web_root =
                          utility::filesystem::GetUnixHome() +
                          "/repo/Open3D/cpp/open3d/visualization/"
