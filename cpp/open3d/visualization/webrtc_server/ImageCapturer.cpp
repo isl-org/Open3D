d48b88ecac545a00a6e820793c84950cff1871a4
Yixing Lao <yixing.lao@gmail.com>
2021-02-25 18:28:37 -0800

    split imagecapturer in a separte file

---
+++ cpp/open3d/visualization/webrtc_server/ImageCapturer.cpp
@@ -0,0 +1,220 @@
+// ----------------------------------------------------------------------------
+// -                        Open3D: www.open3d.org                            -
+// ----------------------------------------------------------------------------
+// The MIT License (MIT)
+//
+// Copyright (c) 2021 www.open3d.org
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+// ----------------------------------------------------------------------------
+
+#include "open3d/visualization/webrtc_server/ImageCapturer.h"
+
+#include <api/video/i420_buffer.h>
+#include <libyuv/convert.h>
+#include <libyuv/video_common.h>
+#include <media/base/video_broadcaster.h>
+#include <media/base/video_common.h>
+#include <modules/desktop_capture/desktop_capture_options.h>
+#include <modules/desktop_capture/desktop_capturer.h>
+#include <rtc_base/logging.h>
+
+#include <chrono>
+#include <iostream>
+#include <memory>
+#include <thread>
+
+#include "open3d/core/Tensor.h"
+#include "open3d/t/io/ImageIO.h"
+#include "open3d/utility/Console.h"
+
+namespace open3d {
+namespace visualization {
+namespace webrtc_server {
+
+ImageReader::ImageReader() {
+    t::geometry::Image im;
+    t::io::ReadImage(
+            "/home/yixing/repo/Open3D/cpp/open3d/visualization/"
+            "webrtc_server/html/lena_color_640_480.jpg",
+            im);
+    // e.g. ImageReader initialized Tensor(shape={480, 640, 4}, dtype=UInt8)
+    frame_ =
+            core::Tensor::Zeros({im.GetRows(), im.GetCols(), 4}, im.GetDtype());
+    frame_.Slice(2, 0, 1) = im.AsTensor().Slice(2, 2, 3);
+    frame_.Slice(2, 1, 2) = im.AsTensor().Slice(2, 1, 2);
+    frame_.Slice(2, 2, 3) = im.AsTensor().Slice(2, 0, 1);
+}
+
+ImageReader::~ImageReader() {}
+
+void ImageReader::Start(Callback* callback) {
+    utility::LogInfo("ImageReader::Start");
+    callback_ = callback;
+}
+
+void ImageReader::CaptureFrame() {
+    if (init_frame_count_ < 12) {
+        init_frame_count_++;
+        callback_->OnCaptureResult(frame_);
+        std::this_thread::sleep_for(std::chrono::milliseconds(5));
+    } else {
+        callback_->OnCaptureResult(GlobalBuffer::GetInstance().Read());
+    }
+}
+
+ImageCapturer::ImageCapturer(const std::string& url_,
+                             const std::map<std::string, std::string>& opts)
+    : ImageCapturer(opts) {
+    capturer_ = std::unique_ptr<ImageReader>(new ImageReader());
+}
+
+ImageCapturer::~ImageCapturer() { this->Stop(); }
+
+ImageCapturer* ImageCapturer::Create(
+        const std::string& url,
+        const std::map<std::string, std::string>& opts) {
+    std::unique_ptr<ImageCapturer> capturer(new ImageCapturer(url, opts));
+    if (!capturer->Init()) {
+        RTC_LOG(LS_WARNING) << "Failed to create ImageCapturer";
+        return nullptr;
+    }
+    return capturer.release();
+}
+
+ImageCapturer::ImageCapturer(const std::map<std::string, std::string>& opts)
+    : width_(0), height_(0) {
+    if (opts.find("width") != opts.end()) {
+        width_ = std::stoi(opts.at("width"));
+    }
+    if (opts.find("height") != opts.end()) {
+        height_ = std::stoi(opts.at("height"));
+    }
+}
+bool ImageCapturer::Init() { return this->Start(); }
+
+void ImageCapturer::CaptureThread() {
+    RTC_LOG(INFO) << "ImageCapturer:Run start";
+    while (IsRunning()) {
+        capturer_->CaptureFrame();
+    }
+    RTC_LOG(INFO) << "ImageCapturer:Run exit";
+}
+
+bool ImageCapturer::Start() {
+    capturer_->Start(this);
+    is_running_ = true;
+    capture_thread_ = std::thread(&ImageCapturer::CaptureThread, this);
+    return true;
+}
+
+void ImageCapturer::Stop() {
+    is_running_ = false;
+    capture_thread_.join();
+}
+
+bool ImageCapturer::IsRunning() { return is_running_; }
+
+// An alias to call OnCaptureResult(). Called by external functions. This is
+// helpful when downcasting a webrtc::VideoTrackSourceInterfac pointer to an
+// ImageCapturer pointer, since we cannot mark OnCaptureResult as override.
+void ImageCapturer::OnFrame(const core::Tensor& frame) {
+    utility::LogInfo("ImageCapturer:OnFrame callback");
+    // e.g. ImageCapturer::OnFrame Tensor(shape={480, 640, 3}, dtype=UInt8)
+    utility::LogInfo("ImageCapturer::OnFrame Tensor(shape={}, dtype={})",
+                     frame.GetShape().ToString(), frame.GetDtype().ToString());
+
+    core::Tensor frame_bgra =
+            core::Tensor::Zeros({480, 640, 4}, core::Dtype::UInt8);
+    frame_bgra.Slice(2, 0, 1) = frame.Slice(2, 2, 3);
+    frame_bgra.Slice(2, 1, 2) = frame.Slice(2, 1, 2);
+    frame_bgra.Slice(2, 2, 3) = frame.Slice(2, 0, 1);
+    OnCaptureResult(frame_bgra);
+}
+
+// Overide webrtc::DesktopCapturer::Callback.
+// See: WindowCapturerX11::CaptureFrame
+// build/webrtc/src/ext_webrtc/src/modules/desktop_capture/linux/window_capturer_x11.cc
+void ImageCapturer::OnCaptureResult(const core::Tensor& frame) {
+    utility::LogInfo(
+            "ImageCapturer:OnCaptureResult callback <<<<<<<<< Good "
+            ">>>>>>>>>");
+    int height = (int)frame.GetShape(0);
+    int width = (int)frame.GetShape(1);
+
+    rtc::scoped_refptr<webrtc::I420Buffer> i420_buffer =
+            webrtc::I420Buffer::Create(width, height);
+
+    // frame->data()
+    const int conversion_result = libyuv::ConvertToI420(
+            static_cast<const uint8_t*>(frame.GetDataPtr()), 0,
+            i420_buffer->MutableDataY(), i420_buffer->StrideY(),
+            i420_buffer->MutableDataU(), i420_buffer->StrideU(),
+            i420_buffer->MutableDataV(), i420_buffer->StrideV(), 0, 0, width,
+            height, i420_buffer->width(), i420_buffer->height(),
+            libyuv::kRotate0, ::libyuv::FOURCC_ARGB);
+
+    if (conversion_result >= 0) {
+        webrtc::VideoFrame video_frame(i420_buffer,
+                                       webrtc::VideoRotation::kVideoRotation_0,
+                                       rtc::TimeMicros());
+        if ((height_ == 0) && (width_ == 0)) {
+            broadcaster_.OnFrame(video_frame);
+
+        } else {
+            int height = height_;
+            int width = width_;
+            if (height == 0) {
+                height = (video_frame.height() * width) / video_frame.width();
+            } else if (width == 0) {
+                width = (video_frame.width() * height) / video_frame.height();
+            }
+            int stride_y = width;
+            int stride_uv = (width + 1) / 2;
+            rtc::scoped_refptr<webrtc::I420Buffer> scaled_buffer =
+                    webrtc::I420Buffer::Create(width, height, stride_y,
+                                               stride_uv, stride_uv);
+            scaled_buffer->ScaleFrom(
+                    *video_frame.video_frame_buffer()->ToI420());
+            webrtc::VideoFrame frame = webrtc::VideoFrame(
+                    scaled_buffer, webrtc::kVideoRotation_0, rtc::TimeMicros());
+
+            broadcaster_.OnFrame(frame);
+        }
+    } else {
+        RTC_LOG(LS_ERROR) << "ImageCapturer:OnCaptureResult conversion error:"
+                          << conversion_result;
+    }
+}
+
+// Overide rtc::VideoSourceInterface<webrtc::VideoFrame>.
+void ImageCapturer::AddOrUpdateSink(
+        rtc::VideoSinkInterface<webrtc::VideoFrame>* sink,
+        const rtc::VideoSinkWants& wants) {
+    broadcaster_.AddOrUpdateSink(sink, wants);
+}
+
+void ImageCapturer::RemoveSink(
+        rtc::VideoSinkInterface<webrtc::VideoFrame>* sink) {
+    broadcaster_.RemoveSink(sink);
+}
+
+}  // namespace webrtc_server
+}  // namespace visualization
+}  // namespace open3d
--- cpp/open3d/visualization/webrtc_server/ImageCapturer.h
+++ cpp/open3d/visualization/webrtc_server/ImageCapturer.h
@@ -51,32 +51,6 @@ namespace webrtc_server {
 
 class ImageReader {
 public:
-    ImageReader() {
-        t::geometry::Image im;
-        t::io::ReadImage(
-                "/home/yixing/repo/Open3D/cpp/open3d/visualization/"
-                "webrtc_server/html/lena_color_640_480.jpg",
-                im);
-        frame_ = core::Tensor::Zeros({im.GetRows(), im.GetCols(), 4},
-                                     im.GetDtype());
-        // blank_ = core::Tensor::Zeros({im.GetRows(), im.GetCols(), 4},
-        //                              im.GetDtype());
-        // lena_ = core::Tensor::Zeros({im.GetRows(), im.GetCols(), 4},
-        //                             im.GetDtype());
-        // lena_.Slice(2, 0, 1) = im.AsTensor().Slice(2, 2, 3);
-        // lena_.Slice(2, 1, 2) = im.AsTensor().Slice(2, 1, 2);
-        // lena_.Slice(2, 2, 3) = im.AsTensor().Slice(2, 0, 1);
-
-        frame_.Slice(2, 0, 1) = im.AsTensor().Slice(2, 2, 3);
-        frame_.Slice(2, 1, 2) = im.AsTensor().Slice(2, 1, 2);
-        frame_.Slice(2, 2, 3) = im.AsTensor().Slice(2, 0, 1);
-        // e.g. ImageReader initialized Tensor(shape={480, 640, 4}, dtype=UInt8)
-        utility::LogInfo("ImageReader initialized Tensor(shape={}, dtype={})",
-                         frame_.GetShape().ToString(),
-                         frame_.GetDtype().ToString());
-    }
-    virtual ~ImageReader() {}
-
     class Callback {
     public:
         virtual void OnCaptureResult(const core::Tensor&) = 0;
@@ -85,209 +59,60 @@ public:
         virtual ~Callback() {}
     };
 
-    void Start(Callback* callback) {
-        utility::LogInfo("ImageReader::Start");
-        callback_ = callback;
-    }
+    ImageReader();
+    virtual ~ImageReader();
 
-    void CaptureFrame() {
-        if (init_frame_count_ < 12) {
-            init_frame_count_++;
-            callback_->OnCaptureResult(frame_);
-            std::this_thread::sleep_for(std::chrono::milliseconds(5));
-        } else {
-            callback_->OnCaptureResult(GlobalBuffer::GetInstance().Read());
-            // std::this_thread::sleep_for(std::chrono::milliseconds(10));
-        }
-        // if (is_blank_) {
-        //     frame_.AsRvalue() = lena_;
-        //     is_blank_ = false;
-        // } else {
-        //     frame_.AsRvalue() = blank_;
-        //     is_blank_ = true;
-        // }
-        // callback_->OnCaptureResult(frame_);
-    }
+    void Start(Callback* callback);
 
-    // void SetFrame(const core::Tensor& im_rgb) {
-    //     // utility::LogInfo("SetFrame called");
-    //     // utility::LogInfo("frame_.GetShape() = {}",
-    //     //                  frame_.GetShape().ToString());
-    //     // im_rgb.AssertShape({frame_.GetShape(0), frame_.GetShape(1), 3});
-    //     // im_rgb.AssertDtype(frame_.GetDtype());
-    //     frame_.Slice(2, 0, 1) = im_rgb.Slice(2, 2, 3);
-    //     frame_.Slice(2, 1, 2) = im_rgb.Slice(2, 1, 2);
-    //     frame_.Slice(2, 2, 3) = im_rgb.Slice(2, 0, 1);
-    // }
+    void CaptureFrame();
 
     Callback* callback_ = nullptr;
 
 private:
     core::Tensor frame_;
-    // core::Tensor lena_;
-    // core::Tensor blank_;
     int init_frame_count_ = 0;
-    // bool is_blank_ = true;
 };
 
 class ImageCapturer : public rtc::VideoSourceInterface<webrtc::VideoFrame>,
                       public ImageReader::Callback {
 public:
     ImageCapturer(const std::string& url_,
-                  const std::map<std::string, std::string>& opts)
-        : ImageCapturer(opts) {
-        capturer_ = std::unique_ptr<ImageReader>(new ImageReader());
-    }
+                  const std::map<std::string, std::string>& opts);
+    virtual ~ImageCapturer();
 
     static ImageCapturer* Create(
             const std::string& url,
-            const std::map<std::string, std::string>& opts) {
-        std::unique_ptr<ImageCapturer> capturer(new ImageCapturer(url, opts));
-        if (!capturer->Init()) {
-            RTC_LOG(LS_WARNING) << "Failed to create ImageCapturer";
-            return nullptr;
-        }
-        return capturer.release();
-    }
+            const std::map<std::string, std::string>& opts);
 
-    ImageCapturer(const std::map<std::string, std::string>& opts)
-        : width_(0), height_(0) {
-        if (opts.find("width") != opts.end()) {
-            width_ = std::stoi(opts.at("width"));
-        }
-        if (opts.find("height") != opts.end()) {
-            height_ = std::stoi(opts.at("height"));
-        }
-        // if (height_ != 480) {
-        //     utility::LogError(
-        //             "TODO: flexible height. Unsupported hight for now: {}",
-        //             height_);
-        // }
-        // if (width_ != 640) {
-        //     utility::LogError(
-        //             "TODO: flexible width. Unsupported width for now: {}",
-        //             width_);
-        // }
-    }
-    bool Init() { return this->Start(); }
-    virtual ~ImageCapturer() { this->Stop(); }
+    ImageCapturer(const std::map<std::string, std::string>& opts);
 
-    void CaptureThread() {
-        RTC_LOG(INFO) << "ImageCapturer:Run start";
-        while (IsRunning()) {
-            capturer_->CaptureFrame();
-        }
-        RTC_LOG(INFO) << "ImageCapturer:Run exit";
-    }
+    bool Init();
 
-    bool Start() {
-        capturer_->Start(this);
-        is_running_ = true;
-        capture_thread_ = std::thread(&ImageCapturer::CaptureThread, this);
-        return true;
-    }
-    void Stop() {
-        is_running_ = false;
-        capture_thread_.join();
-    }
-    bool IsRunning() { return is_running_; }
+    void CaptureThread();
+
+    bool Start();
+
+    void Stop();
+
+    bool IsRunning();
 
     // An alias to call OnCaptureResult(). Called by external functions. This is
     // helpful when downcasting a webrtc::VideoTrackSourceInterfac pointer to an
     // ImageCapturer pointer, since we cannot mark OnCaptureResult as override.
-    void OnFrame(const core::Tensor& frame) {
-        utility::LogInfo("ImageCapturer:OnFrame callback");
-        // e.g. ImageCapturer::OnFrame Tensor(shape={480, 640, 3}, dtype=UInt8)
-        utility::LogInfo("ImageCapturer::OnFrame Tensor(shape={}, dtype={})",
-                         frame.GetShape().ToString(),
-                         frame.GetDtype().ToString());
-
-        core::Tensor frame_bgra =
-                core::Tensor::Zeros({480, 640, 4}, core::Dtype::UInt8);
-        // blank_ = core::Tensor::Zeros({im.GetRows(), im.GetCols(), 4},
-        //                              im.GetDtype());
-        // lena_ = core::Tensor::Zeros({im.GetRows(), im.GetCols(), 4},
-        //                             im.GetDtype());
-        // lena_.Slice(2, 0, 1) = im.AsTensor().Slice(2, 2, 3);
-        // lena_.Slice(2, 1, 2) = im.AsTensor().Slice(2, 1, 2);
-        // lena_.Slice(2, 2, 3) = im.AsTensor().Slice(2, 0, 1);
-
-        frame_bgra.Slice(2, 0, 1) = frame.Slice(2, 2, 3);
-        frame_bgra.Slice(2, 1, 2) = frame.Slice(2, 1, 2);
-        frame_bgra.Slice(2, 2, 3) = frame.Slice(2, 0, 1);
-        OnCaptureResult(frame_bgra);
-    }
+    void OnFrame(const core::Tensor& frame);
 
     // Overide webrtc::DesktopCapturer::Callback.
     // See: WindowCapturerX11::CaptureFrame
     // build/webrtc/src/ext_webrtc/src/modules/desktop_capture/linux/window_capturer_x11.cc
-    virtual void OnCaptureResult(const core::Tensor& frame) override {
-        utility::LogInfo(
-                "ImageCapturer:OnCaptureResult callback <<<<<<<<< Good "
-                ">>>>>>>>>");
-        int height = (int)frame.GetShape(0);
-        int width = (int)frame.GetShape(1);
-
-        rtc::scoped_refptr<webrtc::I420Buffer> i420_buffer =
-                webrtc::I420Buffer::Create(width, height);
-
-        // frame->data()
-        const int conversion_result = libyuv::ConvertToI420(
-                static_cast<const uint8_t*>(frame.GetDataPtr()), 0,
-                i420_buffer->MutableDataY(), i420_buffer->StrideY(),
-                i420_buffer->MutableDataU(), i420_buffer->StrideU(),
-                i420_buffer->MutableDataV(), i420_buffer->StrideV(), 0, 0,
-                width, height, i420_buffer->width(), i420_buffer->height(),
-                libyuv::kRotate0, ::libyuv::FOURCC_ARGB);
-
-        if (conversion_result >= 0) {
-            webrtc::VideoFrame video_frame(
-                    i420_buffer, webrtc::VideoRotation::kVideoRotation_0,
-                    rtc::TimeMicros());
-            if ((height_ == 0) && (width_ == 0)) {
-                broadcaster_.OnFrame(video_frame);
-
-            } else {
-                int height = height_;
-                int width = width_;
-                if (height == 0) {
-                    height = (video_frame.height() * width) /
-                             video_frame.width();
-                } else if (width == 0) {
-                    width = (video_frame.width() * height) /
-                            video_frame.height();
-                }
-                int stride_y = width;
-                int stride_uv = (width + 1) / 2;
-                rtc::scoped_refptr<webrtc::I420Buffer> scaled_buffer =
-                        webrtc::I420Buffer::Create(width, height, stride_y,
-                                                   stride_uv, stride_uv);
-                scaled_buffer->ScaleFrom(
-                        *video_frame.video_frame_buffer()->ToI420());
-                webrtc::VideoFrame frame = webrtc::VideoFrame(
-                        scaled_buffer, webrtc::kVideoRotation_0,
-                        rtc::TimeMicros());
-
-                broadcaster_.OnFrame(frame);
-            }
-        } else {
-            RTC_LOG(LS_ERROR)
-                    << "ImageCapturer:OnCaptureResult conversion error:"
-                    << conversion_result;
-        }
-    }
+    virtual void OnCaptureResult(const core::Tensor& frame) override;
 
     // Overide rtc::VideoSourceInterface<webrtc::VideoFrame>.
     virtual void AddOrUpdateSink(
             rtc::VideoSinkInterface<webrtc::VideoFrame>* sink,
-            const rtc::VideoSinkWants& wants) override {
-        broadcaster_.AddOrUpdateSink(sink, wants);
-    }
+            const rtc::VideoSinkWants& wants) override;
 
     virtual void RemoveSink(
-            rtc::VideoSinkInterface<webrtc::VideoFrame>* sink) override {
-        broadcaster_.RemoveSink(sink);
-    }
+            rtc::VideoSinkInterface<webrtc::VideoFrame>* sink) override;
 
 protected:
     std::thread capture_thread_;
