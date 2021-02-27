def2f300942f560f3f74e4934ebcba195bd78c33
Wei Dong <weidong@andrew.cmu.edu>
2021-02-27 12:09:49 -0500

    fix windows macro dispatch

--- cpp/open3d/t/geometry/kernel/PointCloudImpl.h
+++ cpp/open3d/t/geometry/kernel/PointCloudImpl.h
@@ -76,34 +76,33 @@ void UnprojectCPU
 #endif
 
     int64_t n = rows_strided * cols_strided;
-    DISPATCH_DTYPE_TO_TEMPLATE(depth.GetDtype(), [&]() {
+
 #if defined(BUILD_CUDA_MODULE) && defined(__CUDACC__)
-        core::kernel::CUDALauncher::LaunchGeneralKernel(
-                n, [=] OPEN3D_DEVICE(int64_t workload_idx) {
+    core::kernel::CUDALauncher launcher;
 #else
-    core::kernel::CPULauncher::LaunchGeneralKernel(
-            n, [&](int64_t workload_idx) {
+    core::kernel::CPULauncher launcher;
 #endif
-                    int64_t y = (workload_idx / cols_strided) * stride;
-                    int64_t x = (workload_idx % cols_strided) * stride;
 
-                    float d =
-                            *depth_indexer.GetDataPtrFromCoord<scalar_t>(x, y) /
-                            depth_scale;
-                    if (d > 0 && d < depth_max) {
-                        int idx = OPEN3D_ATOMIC_ADD(count_ptr, 1);
+    DISPATCH_DTYPE_TO_TEMPLATE(depth.GetDtype(), [&]() {
+        launcher.LaunchGeneralKernel(n, [=] OPEN3D_DEVICE(
+                                                int64_t workload_idx) {
+            int64_t y = (workload_idx / cols_strided) * stride;
+            int64_t x = (workload_idx % cols_strided) * stride;
 
-                        float x_c = 0, y_c = 0, z_c = 0;
-                        ti.Unproject(static_cast<float>(x),
-                                     static_cast<float>(y), d, &x_c, &y_c,
-                                     &z_c);
+            float d = *depth_indexer.GetDataPtrFromCoord<scalar_t>(x, y) /
+                      depth_scale;
+            if (d > 0 && d < depth_max) {
+                int idx = OPEN3D_ATOMIC_ADD(count_ptr, 1);
 
-                        float* vertex =
-                                point_indexer.GetDataPtrFromCoord<float>(idx);
-                        ti.RigidTransform(x_c, y_c, z_c, vertex + 0, vertex + 1,
-                                          vertex + 2);
-                    }
-                });
+                float x_c = 0, y_c = 0, z_c = 0;
+                ti.Unproject(static_cast<float>(x), static_cast<float>(y), d,
+                             &x_c, &y_c, &z_c);
+
+                float* vertex = point_indexer.GetDataPtrFromCoord<float>(idx);
+                ti.RigidTransform(x_c, y_c, z_c, vertex + 0, vertex + 1,
+                                  vertex + 2);
+            }
+        });
     });
 
 #if defined(BUILD_CUDA_MODULE) && defined(__CUDACC__)
@@ -112,7 +111,7 @@ void UnprojectCPU
     int total_pts_count = (*count_ptr).load();
 #endif
     points = points.Slice(0, 0, total_pts_count);
-}
+}  // namespace pointcloud
 }  // namespace pointcloud
 }  // namespace kernel
 }  // namespace geometry
