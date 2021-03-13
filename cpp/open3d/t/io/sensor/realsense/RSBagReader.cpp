23e714ebb79c971996da16b7a68cf20ecf3b1c73
Nobuo Tsukamoto <nextremer.nb.o@gmail.com>
2021-03-13 16:34:47 +0900

    Add comment for RSBagReader::DEFAULT_BUFFER_SIZE.

--- cpp/open3d/t/io/sensor/realsense/RSBagReader.cpp
+++ cpp/open3d/t/io/sensor/realsense/RSBagReader.cpp
@@ -42,7 +42,10 @@ namespace open3d {
 namespace t {
 namespace io {
 
-constexpr size_t RSBagReader::DEFAULT_BUFFER_SIZE;
+// If DEFAULT_BUFFER_SIZE is odr-uses, a definition is required.
+// For Fedora33, GCC10, CUDA11.2, fix undefined symble error in pybind-*-linux-gnu.so.
+// See: https://github.com/intel-isl/Open3D/issues/3141
+const size_t RSBagReader::DEFAULT_BUFFER_SIZE;
 
 RSBagReader::RSBagReader(size_t buffer_size)
     : frame_buffer_(buffer_size),
