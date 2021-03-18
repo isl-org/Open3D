9be64c4764195ccd21c3621862ceae30fa3708ec
Yixing Lao <yixing.lao@gmail.com>
2021-03-17 23:04:58 -0700

    CustomTrackSource.cpp compiles

--- cpp/open3d/visualization/webrtc_server/CustomTrackSource.h
+++ cpp/open3d/visualization/webrtc_server/CustomTrackSource.h
@@ -26,15 +26,62 @@
 
 #pragma once
 
+#include <absl/types/optional.h>
 #include <api/media_stream_interface.h>
+#include <api/notifier.h>
+#include <api/sequence_checker.h>
+#include <api/video/recordable_encoded_frame.h>
+#include <api/video/video_frame.h>
+#include <api/video/video_sink_interface.h>
+#include <api/video/video_source_interface.h>
+#include <media/base/media_channel.h>
 
 namespace open3d {
 namespace visualization {
 namespace webrtc_server {
 
-class CustomTrackSource : public webrtc::VideoTrackSourceInterface {};
+class CustomTrackSourceInterface : public webrtc::VideoTrackSourceInterface {};
+
+// VideoTrackSource is a convenience base class for implementations of
+// VideoTrackSourceInterface.
+class VideoTrackSource : public webrtc::Notifier<CustomTrackSourceInterface> {
+public:
+    explicit VideoTrackSource(bool remote);
+    void SetState(webrtc::MediaSourceInterface::SourceState new_state);
+
+    webrtc::MediaSourceInterface::SourceState state() const override {
+        return state_;
+    }
+    bool remote() const override { return remote_; }
+
+    bool is_screencast() const override { return false; }
+    absl::optional<bool> needs_denoising() const override {
+        return absl::nullopt;
+    }
+
+    bool GetStats(Stats* stats) override { return false; }
+
+    void AddOrUpdateSink(rtc::VideoSinkInterface<webrtc::VideoFrame>* sink,
+                         const rtc::VideoSinkWants& wants) override;
+    void RemoveSink(rtc::VideoSinkInterface<webrtc::VideoFrame>* sink) override;
+
+    bool SupportsEncodedOutput() const override { return false; }
+    void GenerateKeyFrame() override {}
+    void AddEncodedSink(rtc::VideoSinkInterface<webrtc::RecordableEncodedFrame>*
+                                sink) override {}
+    void RemoveEncodedSink(
+            rtc::VideoSinkInterface<webrtc::RecordableEncodedFrame>* sink)
+            override {}
+
+protected:
+    virtual rtc::VideoSourceInterface<webrtc::VideoFrame>* source() = 0;
+
+private:
+    webrtc::SequenceChecker worker_thread_checker_;
+    webrtc::MediaSourceInterface::SourceState state_;
+    const bool remote_;
+};
 
 }  // namespace webrtc_server
-
 }  // namespace visualization
 }  // namespace open3d
