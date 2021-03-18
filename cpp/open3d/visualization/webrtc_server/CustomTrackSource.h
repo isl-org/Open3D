a6c450a3dc9f9de537557360f7347dc89986958b
Yixing Lao <yixing.lao@gmail.com>
2021-03-17 23:12:09 -0700

    license

--- cpp/open3d/visualization/webrtc_server/CustomTrackSource.h
+++ cpp/open3d/visualization/webrtc_server/CustomTrackSource.h
@@ -24,6 +24,16 @@
 // IN THE SOFTWARE.
 // ----------------------------------------------------------------------------
 
+// Refrence: WebRTC src/pc/video_track_source.h
+//
+// Copyright 2016 The WebRTC project authors. All Rights Reserved.
+//
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file in the root of the source
+// tree. An additional intellectual property rights grant can be found
+// in the file PATENTS.  All contributing project authors may
+// be found in the AUTHORS file in the root of the source tree.
+
 #pragma once
 
 #include <absl/types/optional.h>
