a67dfa8b3d20bec4aa7a7fa7da46f5616f6e61b1
Yixing Lao <yixing.lao@gmail.com>
2021-04-07 17:49:29 -0700

    add webrtc_config.cmake

--- 3rdparty/webrtc/webrtc_build.cmake
+++ 3rdparty/webrtc/webrtc_build.cmake
@@ -14,58 +14,8 @@ else()
 endif()
 set(WEBRTC_NINJA_ROOT ${WEBRTC_ROOT}/src/out/${WEBRTC_BUILD})
 
-function(get_webrtc_args WEBRTC_ARGS)
-    set(WEBRTC_ARGS "")
-
-    # ABI selection
-    if(GLIBCXX_USE_CXX11_ABI)
-        set(WEBRTC_ARGS rtc_use_cxx11_abi=true\n${WEBRTC_ARGS})
-    else()
-        set(WEBRTC_ARGS rtc_use_cxx11_abi=false\n${WEBRTC_ARGS})
-    endif()
-
-    set(WEBRTC_ARGS rtc_include_tests=false\n${WEBRTC_ARGS})
-    set(WEBRTC_ARGS rtc_enable_protobuf=false\n${WEBRTC_ARGS})
-    set(WEBRTC_ARGS rtc_build_examples=false\n${WEBRTC_ARGS})
-    set(WEBRTC_ARGS rtc_build_tools=false\n${WEBRTC_ARGS})
-    set(WEBRTC_ARGS treat_warnings_as_errors=false\n${WEBRTC_ARGS})
-    set(WEBRTC_ARGS rtc_enable_libevent=false\n${WEBRTC_ARGS})
-    set(WEBRTC_ARGS rtc_build_libevent=false\n${WEBRTC_ARGS})
-    set(WEBRTC_ARGS use_sysroot=false\n${WEBRTC_ARGS})
-
-    # Disable screen capturing
-    set(WEBRTC_ARGS rtc_use_x11=false\n${WEBRTC_ARGS})
-    set(WEBRTC_ARGS rtc_use_pipewire=false\n${WEBRTC_ARGS})
-
-    # Don't use libstdc++ (Clang), use libc++ (GNU)
-    # https://stackoverflow.com/a/47384787/1255535
-    set(WEBRTC_ARGS use_custom_libcxx=false\n${WEBRTC_ARGS})
-    set(WEBRTC_ARGS use_custom_libcxx_for_host=false\n${WEBRTC_ARGS})
-
-    # Debug/Release
-    if(WEBRTC_IS_DEBUG)
-        set(WEBRTC_ARGS is_debug=true\n${WEBRTC_ARGS})
-    else()
-        set(WEBRTC_ARGS is_debug=false\n${WEBRTC_ARGS})
-    endif()
-
-    # H264 support
-    set(WEBRTC_ARGS is_chrome_branded=true\n${WEBRTC_ARGS})
-
-    # Sound support
-    set(WEBRTC_ARGS rtc_include_pulse_audio=false\n${WEBRTC_ARGS})
-    set(WEBRTC_ARGS rtc_include_internal_audio_device=false\n${WEBRTC_ARGS})
-
-    # Use clang for compilation
-    set(WEBRTC_ARGS is_clang=true\n${WEBRTC_ARGS})
-
-    # Use ccache if available, not recommended inside Docker
-    find_program(CCACHE_BIN "ccache")
-    if(CCACHE_BIN)
-        set(WEBRTC_ARGS cc_wrapper="ccache"\n${WEBRTC_ARGS})
-    endif()
-  set(WEBRTC_ARGS ${WEBRTC_ARGS} PARENT_SCOPE)
-endfunction()
+# Common configs for WebRTC
+include(${PROJECT_SOURCE_DIR}/3rdparty/webrtc/webrtc_config.cmake)
 
 # Creates args.gn for WebRTC build.
 if(NOT EXISTS ${CMAKE_BINARY_DIR}/args.gn)
