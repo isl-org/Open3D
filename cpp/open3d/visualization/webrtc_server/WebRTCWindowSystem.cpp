e3f2a3318ad9e9e03aa2b337215522f1ccd5bb53
Yixing Lao <yixing.lao@gmail.com>
2021-04-08 23:45:05 -0700

    move WebRTCWindowSystem to webrtc_server

--- cpp/open3d/visualization/webrtc_server/WebRTCWindowSystem.cpp
+++ cpp/open3d/visualization/webrtc_server/WebRTCWindowSystem.cpp
@@ -24,7 +24,7 @@
 // IN THE SOFTWARE.
 // ----------------------------------------------------------------------------
 
-#include "open3d/visualization/gui/WebRTCWindowSystem.h"
+#include "open3d/visualization/webrtc_server/WebRTCWindowSystem.h"
 
 #include <chrono>
 #include <sstream>
