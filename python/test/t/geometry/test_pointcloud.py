b0571e7c3ea39d3671b3a8c2ece0328ce732b25e
Rishabh Singh <reyanshsolis@gmail.com>
2021-04-16 02:25:14 +0530

    pybind for voxel_down_sample t::pointcloud

--- python/test/t/geometry/test_pointcloud.py
+++ python/test/t/geometry/test_pointcloud.py
@@ -149,3 +149,12 @@ def test_member_functions(device):
     pcd.rotate(rotation, center)
     assert pcd.point["points"].allclose(o3c.Tensor([[3, 3, 2]], dtype, device))
     assert pcd.point["normals"].allclose(o3c.Tensor([[2, 2, 1]], dtype, device))
+
+    # voxel_down_sample
+    pcd = o3d.t.geometry.PointCloud(device)
+    pcd.point["points"] = o3c.Tensor(
+        [[0.1, 0.3, 0.9], [0.9, 0.2, 0.4], [0.3, 0.6, 0.8], [0.2, 0.4, 0.2]],
+        dtype, device)
+
+    pcd_small_down = pcd.voxel_down_sample(1)
+    assert pcd.point["points"].allclose(o3c.Tensor([[0, 0, 0]], dtype, device))
