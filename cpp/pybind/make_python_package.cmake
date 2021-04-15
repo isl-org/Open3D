5629f5989f1779cb77ab5df5ae3e05f8f4e0eb7c
Yixing Lao <yixing.lao@gmail.com>
2021-04-14 23:26:12 -0700

    clean up caching

--- cpp/pybind/make_python_package.cmake
+++ cpp/pybind/make_python_package.cmake
@@ -77,9 +77,9 @@ if (BUNDLE_OPEN3D_ML)
     file(RENAME "${PYTHON_PACKAGE_DST_DIR}/open3d/ml3d" "${PYTHON_PACKAGE_DST_DIR}/open3d/_ml3d")
 endif()
 
-# Build Jupyter plugin with webpack. This step distills and merges all js
-# dependencies and include all static assets. The generated output is in
-# ${PYTHON_PACKAGE_DST_DIR}/open3d/static.
+# Build Jupyter plugin.
+# TODO: currently there is no way to disable Jupyter plugin. We need to do so
+# in setup.py.
 if (BUILD_JUPYTER_EXTENSION)
     if(UNIX AND NOT APPLE)
         message(STATUS "Jupyter support is enabled, building Jupyter plugin now.")
@@ -104,43 +104,6 @@ if (BUILD_JUPYTER_EXTENSION)
         message(FATAL_ERROR "yarn not found. You may install yarm globally by"
                             "npm install -g yarn.")
     endif()
-
-    file(REMOVE_RECURSE ${PYTHON_PACKAGE_DST_DIR}/open3d/static)
-
-    if (WIN32)
-        find_program(NPM "npm")
-        execute_process(
-            COMMAND cmd /c "${NPM}" install
-            RESULT_VARIABLE res_var
-            WORKING_DIRECTORY ${PYTHON_PACKAGE_DST_DIR}/js
-        )
-    else()
-        execute_process(
-            COMMAND npm install
-            RESULT_VARIABLE res_var
-            WORKING_DIRECTORY ${PYTHON_PACKAGE_DST_DIR}/js
-        )
-    endif()
-    if (NOT "${res_var}" STREQUAL "0")
-        message(FATAL_ERROR "`npm install` failed with: '${res_var}'")
-    endif()
-
-    # We cache ${PYTHON_PACKAGE_DST_DIR}/js/node_modules in
-    #          ${PYTHON_PACKAGE_SRC_DIR}/js/node_modules
-    # to speed up webpack build speed during development.
-    # During build, the following steps will happen:
-    # 1) The entire ${PYTHON_PACKAGE_DST_DIR} in the build directory is cleared.
-    # 2) ${PYTHON_PACKAGE_SRC_DIR}/js/node_modules is copied to
-    #    ${PYTHON_PACKAGE_DST_DIR}/js/node_modules, regadless whether
-    #    ${PYTHON_PACKAGE_SRC_DIR}/js/node_modules is empty or not.
-    # 3) `npm install` is run in ${PYTHON_PACKAGE_DST_DIR}/js, so
-    #    ${PYTHON_PACKAGE_DST_DIR}/js/node_modules must be filled after
-    #    `npm install`.
-    # 4) ${PYTHON_PACKAGE_DST_DIR}/js/node_modules is then copied back to
-    #    ${PYTHON_PACKAGE_SRC_DIR}/js/node_modules for caching.
-    file(REMOVE_RECURSE ${PYTHON_PACKAGE_SRC_DIR}/js/node_modules)
-    file(COPY ${PYTHON_PACKAGE_DST_DIR}/js/node_modules
-        DESTINATION ${PYTHON_PACKAGE_SRC_DIR}/js)
 endif()
 
 if (BUILD_GUI)
