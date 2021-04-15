23758428da92fb8b79deedef55a305c1d589cb1b
Yixing Lao <yixing.lao@gmail.com>
2021-04-14 23:28:20 -0700

    bring back ml and vis python files

--- cpp/pybind/make_python_package.cmake
+++ cpp/pybind/make_python_package.cmake
@@ -44,12 +44,12 @@ configure_file("${PYTHON_PACKAGE_SRC_DIR}/setup.py"
                "${PYTHON_PACKAGE_DST_DIR}/setup.py")
 configure_file("${PYTHON_PACKAGE_SRC_DIR}/open3d/__init__.py"
                "${PYTHON_PACKAGE_DST_DIR}/open3d/__init__.py")
-# configure_file("${PYTHON_PACKAGE_SRC_DIR}/open3d/visualization/__init__.py"
-#                "${PYTHON_PACKAGE_DST_DIR}/open3d/visualization/__init__.py")
-# configure_file("${PYTHON_PACKAGE_SRC_DIR}/open3d/visualization/gui/__init__.py"
-#                "${PYTHON_PACKAGE_DST_DIR}/open3d/visualization/gui/__init__.py")
-# configure_file("${PYTHON_PACKAGE_SRC_DIR}/open3d/visualization/rendering/__init__.py"
-#                "${PYTHON_PACKAGE_DST_DIR}/open3d/visualization/rendering/__init__.py")
+configure_file("${PYTHON_PACKAGE_SRC_DIR}/open3d/visualization/__init__.py"
+               "${PYTHON_PACKAGE_DST_DIR}/open3d/visualization/__init__.py")
+configure_file("${PYTHON_PACKAGE_SRC_DIR}/open3d/visualization/gui/__init__.py"
+               "${PYTHON_PACKAGE_DST_DIR}/open3d/visualization/gui/__init__.py")
+configure_file("${PYTHON_PACKAGE_SRC_DIR}/open3d/visualization/rendering/__init__.py"
+               "${PYTHON_PACKAGE_DST_DIR}/open3d/visualization/rendering/__init__.py")
 # configure_file("${PYTHON_PACKAGE_SRC_DIR}/open3d/web_visualizer.py"
 #                "${PYTHON_PACKAGE_DST_DIR}/open3d/web_visualizer.py")
 configure_file("${PYTHON_PACKAGE_SRC_DIR}/conda_meta/conda_build_config.yaml"
