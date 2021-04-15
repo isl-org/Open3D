bc03db75e1c07c8a38a572faa84ca7033b9410cd
Yixing Lao <yixing.lao@gmail.com>
2021-04-15 00:56:52 -0700

    order

--- python/setup.py
+++ python/setup.py
@@ -1,3 +1,29 @@
+# ----------------------------------------------------------------------------
+# -                        Open3D: www.open3d.org                            -
+# ----------------------------------------------------------------------------
+# The MIT License (MIT)
+#
+# Copyright (c) 2018 www.open3d.org
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+# ----------------------------------------------------------------------------
+
 from __future__ import print_function
 from setuptools import setup, find_packages
 import os
@@ -67,9 +93,6 @@ if '@BUNDLE_OPEN3D_ML@' == 'ON':
 setup_args = dict(
     name="@PYPI_PACKAGE_NAME@",
     version='@PROJECT_VERSION@',
-    description='Open3D: A Modern Library for 3D Data Processing.',
-    long_description=open('README.rst').read(),
-    long_description_content_type='text/x-rst',
     include_package_data=True,
     install_requires=install_requires,
     packages=find_packages(),
@@ -116,6 +139,9 @@ setup_args = dict(
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: Utilities",
     ],
+    description='Open3D: A Modern Library for 3D Data Processing.',
+    long_description=open('README.rst').read(),
+    long_description_content_type='text/x-rst',
 )
 
 setup(**setup_args)
