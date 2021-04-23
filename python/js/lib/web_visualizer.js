f7f161d3a6c88d681e9e87342f0fb85e2a277bea
Yixing Lao <yixing.lao@gmail.com>
2021-04-22 19:55:53 -0700

    reset to basic email widget

--- python/js/lib/web_visualizer.js
+++ python/js/lib/web_visualizer.js
@@ -22,96 +22,14 @@ var WebRtcStreamer = require("./webrtcstreamer");
 // differ from the defaults will be specified.
 var WebVisualizerModel = widgets.DOMWidgetModel.extend({
   defaults: _.extend(widgets.DOMWidgetModel.prototype.defaults(), {
-    _model_name: "WebVisualizerModel",
     _view_name: "WebVisualizerView",
-    _model_module: "open3d",
     _view_module: "open3d",
-    _model_module_version: "@PROJECT_VERSION_THREE_NUMBER@",
     _view_module_version: "@PROJECT_VERSION_THREE_NUMBER@",
-    value: "Hello World!",
   }),
 });
 
 // Custom View. Renders the widget model.
 var WebVisualizerView = widgets.DOMWidgetView.extend({
-  onGetMediaList: function (mediaList) {
-    console.log("!!!!onGetMediaList mediaList: ", mediaList);
-  },
-
-  /**
-   * https://stackoverflow.com/a/52347011/1255535
-   */
-  executePython: function (python_code) {
-    return new Promise((resolve, reject) => {
-      var callbacks = {
-        iopub: {
-          output: (data) => resolve(data.content.text.trim()),
-        },
-      };
-      Jupyter.notebook.kernel.execute(python_code, callbacks);
-    });
-  },
-
-  /**
-   * https://stackoverflow.com/a/736970/1255535
-   * parseUrl(url).hostname
-   * parseUrl(url).entryPoint
-   */
-  parseUrl: function (url) {
-    var l = document.createElement("a");
-    l.href = url;
-    return l;
-  },
-
-  logAndReturn: function (value) {
-    console.log("!!! logAndReturn: ", value);
-    return value;
-  },
-
-  /**
-   * Similar to WebRtcStreamer.remoteCall() but instead uses Jupyter's COMMS
-   * interface.
-   */
-  commsCall: function (url, data = {}) {
-    entryPoint = this.parseUrl(url).pathname;
-    queryString = this.parseUrl(url).search;
-    console.log("WebVisualizerView.commsCall with url: ", url, " data: ", data);
-    console.log("WebVisualizerView.commsCall with entryPoint: ", entryPoint);
-    console.log("WebVisualizerView.commsCall with queryString: ", queryString);
-    console.log(
-      'WebVisualizerView.commsCall with data["body"]: ',
-      data["body"]
-    );
-    var command_prefix =
-      "import open3d; print(open3d.visualization.webrtc_server.WebRTCServer.instance.call_http_request(";
-    // entry_point
-    var command_args = '"' + entryPoint + '"';
-    // query_string
-    if (queryString) {
-      command_args += ', "' + queryString + '"';
-    } else {
-      command_args += ', ""';
-    }
-    // data
-    var dataStr = data["body"];
-    command_args += ", '" + dataStr + "'";
-    var command_suffix = "))";
-    var command = command_prefix + command_args + command_suffix;
-    console.log("commsCall command: " + command);
-    return this.executePython(command)
-      .then((jsonStr) => JSON.parse(jsonStr))
-      .then((val) => this.logAndReturn(val))
-      .then(
-        (jsonObj) =>
-          new Response(
-            new Blob([JSON.stringify(jsonObj)], {
-              type: "application/json",
-            })
-          )
-      )
-      .then((val) => this.logAndReturn(val));
-  },
-
   sleep: function (time_ms) {
     return new Promise((resolve) => setTimeout(resolve, time_ms));
   },
@@ -140,20 +58,16 @@ var WebVisualizerView = widgets.DOMWidgetView.extend({
     return message;
   },
 
-  // Defines how the widget gets rendered into the DOM
+  callPythonWrapper: function (func, args = []) {
+    return callPython(func, args).then((result) => result);
+  },
+
   render: function () {
-    console.log("render");
+    this.new_pyjs_message = false;
+    this.email_input = document.createElement("p");
+    this.el.appendChild(this.email_input);
 
-    this.videoElt = document.createElement("video");
-    this.videoElt.id = "video_tag";
-    this.videoElt.muted = true;
-    this.videoElt.controls = false;
-    this.videoElt.playsinline = true;
-
-    // The `el` property is the DOM element associated with the view
-    this.el.appendChild(this.videoElt);
-
-    // Listens for py->js message.
+    // Listen for py->js message.
     this.model.on("change:pyjs_channel", this.on_pyjs_message, this);
 
     // Send js->py message for testing.
@@ -164,34 +78,13 @@ var WebVisualizerView = widgets.DOMWidgetView.extend({
     ]).then((result) => {
       console.log("callPython.then()", result);
     });
-
-    // TODO: remove this after switching to purely comms-based communication.
-    var http_server =
-      location.protocol + "//" + window.location.hostname + ":" + 8888;
-
-    // TODO: remove this since the media name should be given by Python
-    // directly. This is only used for developing the pipe.
-    WebRtcStreamer.remoteCall(http_server + "/api/getMediaList", true, {}, this)
-      .then((response) => response.json())
-      .then((jsonObj) => this.onGetMediaList(jsonObj));
-
-    // Create WebRTC stream
-    this.webRtcClient = new WebRtcStreamer(
-      this.videoElt,
-      location.protocol + "//" + window.location.hostname + ":" + 8888,
-      /*useComms(when supported)=*/ true,
-      /*webVisualizer=*/ this
-    );
-    this.webRtcClient.connect(this.model.get("window_uid"));
   },
 
   on_pyjs_message: function () {
-    console.log("py->js message received:", message);
+    var message = this.model.get("pyjs_channel");
+    message = "pyjs_message received: " + message;
+    this.email_input.innerText = this.email_input.innerText + "\n" + message;
     this.new_pyjs_message = true;
-  },
-
-  value_changed: function () {
-    this.el.textContent = this.model.get("window_uid");
   },
 });
 
