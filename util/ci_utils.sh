a8e151e29031776a1ba4980be0e65501a64262ec
Yixing Lao <yixing.lao@gmail.com>
2021-04-27 16:36:14 -0700

    remove webrtc=on in ci_utils, since it will be automatic

--- util/ci_utils.sh
+++ util/ci_utils.sh
@@ -227,7 +227,6 @@ build_all() {
         -DBUILD_UNIT_TESTS=ON
         -DBUILD_BENCHMARKS=ON
         -DBUILD_EXAMPLES=OFF
-        -DBUILD_WEBRTC=ON
     )
 
     echo
@@ -293,7 +292,6 @@ build_pip_conda_package() {
         "-DBUILD_UNIT_TESTS=OFF"
         "-DBUILD_BENCHMARKS=OFF"
         "-DBUNDLE_OPEN3D_ML=$BUNDLE_OPEN3D_ML"
-        "-DBUILD_WEBRTC=ON"
     )
     set -x # Echo commands on
     cmake -DBUILD_CUDA_MODULE=OFF "${cmakeOptions[@]}" ..
