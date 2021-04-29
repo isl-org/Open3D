3cf46b0c4485c36543b84cf24197b86c3a2d680d
Yixing Lao <yixing.lao@gmail.com>
2021-04-29 15:55:13 -0700

    clean up

--- cpp/open3d/visualization/webrtc_server/PeerConnectionManager.cpp
+++ cpp/open3d/visualization/webrtc_server/PeerConnectionManager.cpp
@@ -268,10 +268,6 @@ PeerConnectionManager::~PeerConnectionManager() {}
 const Json::Value PeerConnectionManager::GetMediaList() {
     Json::Value value(Json::arrayValue);
 
-    // Json::Value media;
-    // media["video"] = "image://Open3D";
-    // value.append(media);
-
     for (const std::string &window_uid : webrtc_server_->GetWindowUIDs()) {
         Json::Value media;
         media["video"] = window_uid;
