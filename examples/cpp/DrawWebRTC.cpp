f81c6cdc28dbc640b6d190ea10b6818ac1e2cbdb
Yixing Lao <yixing.lao@gmail.com>
2021-05-03 16:35:11 -0700

    use monkey

--- examples/cpp/DrawWebRTC.cpp
+++ examples/cpp/DrawWebRTC.cpp
@@ -95,37 +95,42 @@ void EmptyBox() {
 }
 
 void BoxWithOjects() {
-    const double pc_rad = 1.0;
-    const double r = 0.4;
-    auto sphere_unlit = geometry::TriangleMesh::CreateSphere(r);
-    sphere_unlit->Translate({0.0, 1.0, 0.0});
-    auto sphere_colored_unlit = geometry::TriangleMesh::CreateSphere(r);
-    sphere_colored_unlit->PaintUniformColor({1.0, 0.0, 0.0});
-    sphere_colored_unlit->Translate({2.0, 1.0, 0.0});
-    auto sphere_lit = geometry::TriangleMesh::CreateSphere(r);
-    sphere_lit->ComputeVertexNormals();
-    sphere_lit->Translate({4, 1, 0});
-    auto sphere_colored_lit = geometry::TriangleMesh::CreateSphere(r);
-    sphere_colored_lit->ComputeVertexNormals();
-    sphere_colored_lit->PaintUniformColor({0.0, 1.0, 0.0});
-    sphere_colored_lit->Translate({6, 1, 0});
-    auto big_bbox = std::make_shared<geometry::AxisAlignedBoundingBox>(
-            Eigen::Vector3d{-pc_rad, -3, -pc_rad},
-            Eigen::Vector3d{6.0 + r, 1.0 + r, pc_rad});
-    auto bbox = sphere_unlit->GetAxisAlignedBoundingBox();
-    auto sphere_bbox = std::make_shared<geometry::AxisAlignedBoundingBox>(
-            bbox.min_bound_, bbox.max_bound_);
-    sphere_bbox->color_ = {1.0, 0.5, 0.0};
-    auto lines = geometry::LineSet::CreateFromAxisAlignedBoundingBox(
-            sphere_lit->GetAxisAlignedBoundingBox());
-    auto lines_colored = geometry::LineSet::CreateFromAxisAlignedBoundingBox(
-            sphere_colored_lit->GetAxisAlignedBoundingBox());
-    lines_colored->PaintUniformColor({0.0, 0.0, 1.0});
+    std::shared_ptr<geometry::TriangleMesh> mesh = io::CreateMeshFromFile(
+            "../../../examples/test_data/monkey/monkey.obj");
+    AddDrawWindow({mesh}, "Open3D BoxWithOjects", 640, 480);
 
-    AddDrawWindow(
-            {sphere_unlit, sphere_colored_unlit, sphere_lit, sphere_colored_lit,
-             big_bbox, sphere_bbox, lines, lines_colored},
-            "Open3D BoxWithOjects", 640, 480);
+    // const double pc_rad = 1.0;
+    // const double r = 0.4;
+    // auto sphere_unlit = geometry::TriangleMesh::CreateSphere(r);
+    // sphere_unlit->Translate({0.0, 1.0, 0.0});
+    // auto sphere_colored_unlit = geometry::TriangleMesh::CreateSphere(r);
+    // sphere_colored_unlit->PaintUniformColor({1.0, 0.0, 0.0});
+    // sphere_colored_unlit->Translate({2.0, 1.0, 0.0});
+    // auto sphere_lit = geometry::TriangleMesh::CreateSphere(r);
+    // sphere_lit->ComputeVertexNormals();
+    // sphere_lit->Translate({4, 1, 0});
+    // auto sphere_colored_lit = geometry::TriangleMesh::CreateSphere(r);
+    // sphere_colored_lit->ComputeVertexNormals();
+    // sphere_colored_lit->PaintUniformColor({0.0, 1.0, 0.0});
+    // sphere_colored_lit->Translate({6, 1, 0});
+    // auto big_bbox = std::make_shared<geometry::AxisAlignedBoundingBox>(
+    //         Eigen::Vector3d{-pc_rad, -3, -pc_rad},
+    //         Eigen::Vector3d{6.0 + r, 1.0 + r, pc_rad});
+    // auto bbox = sphere_unlit->GetAxisAlignedBoundingBox();
+    // auto sphere_bbox = std::make_shared<geometry::AxisAlignedBoundingBox>(
+    //         bbox.min_bound_, bbox.max_bound_);
+    // sphere_bbox->color_ = {1.0, 0.5, 0.0};
+    // auto lines = geometry::LineSet::CreateFromAxisAlignedBoundingBox(
+    //         sphere_lit->GetAxisAlignedBoundingBox());
+    // auto lines_colored = geometry::LineSet::CreateFromAxisAlignedBoundingBox(
+    //         sphere_colored_lit->GetAxisAlignedBoundingBox());
+    // lines_colored->PaintUniformColor({0.0, 0.0, 1.0});
+
+    // AddDrawWindow(
+    //         {sphere_unlit, sphere_colored_unlit, sphere_lit,
+    //         sphere_colored_lit,
+    //          big_bbox, sphere_bbox, lines, lines_colored},
+    //         "Open3D BoxWithOjects", 640, 480);
 }
 
 int main(int argc, char **argv) {
