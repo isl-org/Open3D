9b85f65f1d8ea9fff4979ad3b4105c09d4ad2b9e
Yixing Lao <yixing.lao@gmail.com>
2021-05-04 00:33:30 -0700

    reanme

--- python/open3d/web_visualizer.py
+++ python/open3d/web_visualizer.py
@@ -118,9 +118,9 @@ class _AsyncEventLoop:
         self._run_queue = []
         self._return_vals = {}
         self._started = False
-        self.start()
+        self._start()
 
-    def start(self):
+    def _start(self):
         if not self._started:
             self._thread = threading.Thread(target=self._thread_main)
             self._thread.start()
@@ -157,25 +157,25 @@ class _AsyncEventLoop:
 _async_event_loop = _AsyncEventLoop()
 
 
-def web_draw(geometry=None,
-             title="Open3D",
-             width=640,
-             height=480,
-             actions=None,
-             lookat=None,
-             eye=None,
-             up=None,
-             field_of_view=60.0,
-             bg_color=(1.0, 1.0, 1.0, 1.0),
-             bg_image=None,
-             show_ui=None,
-             point_size=None,
-             animation_time_step=1.0,
-             animation_duration=None,
-             rpc_interface=False,
-             on_init=None,
-             on_animation_frame=None,
-             on_animation_tick=None):
+def draw(geometry=None,
+         title="Open3D",
+         width=640,
+         height=480,
+         actions=None,
+         lookat=None,
+         eye=None,
+         up=None,
+         field_of_view=60.0,
+         bg_color=(1.0, 1.0, 1.0, 1.0),
+         bg_image=None,
+         show_ui=None,
+         point_size=None,
+         animation_time_step=1.0,
+         animation_duration=None,
+         rpc_interface=False,
+         on_init=None,
+         on_animation_frame=None,
+         on_animation_tick=None):
 
     uid = _async_event_loop.run_sync(
         functools.partial(o3d.visualization.draw,
