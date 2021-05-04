6c0ad4363345542d1b3afc0b7c69fff3be5508cf
Yixing Lao <yixing.lao@gmail.com>
2021-05-03 23:02:48 -0700

    move async loop to jupyter

--- python/open3d/web_visualizer.py
+++ python/open3d/web_visualizer.py
@@ -96,66 +96,6 @@ class WebVisualizer(widgets.DOMWidget):
             self.pyjs_channel = json.dumps(self.result_map)
 
 
-class _AsyncEventLoop:
-
-    class _Task:
-        _g_next_id = 0
-
-        def __init__(self, f):
-            self.task_id = self._g_next_id
-            self.func = f
-            self._g_next_id += 1
-
-    # Do not call this directly, use instance instead
-    def __init__(self):
-        # TODO: find a better solution. Currently py::print requires GIL which
-        # causes deadlock when AsyncEventLoop is used. By calling
-        # reset_print_function(), all C++ prints will be directed to the
-        # terminal while python print will still remain in the cell.
-        o3d.utility.reset_print_function()
-        self._lock = threading.Lock()
-        self._run_queue = []
-        self._return_vals = {}
-        self._started = False
-
-    def start(self):
-        if not self._started:
-            self._thread = threading.Thread(target=self._thread_main)
-            self._thread.start()
-            self._started = True
-
-    def run_sync(self, f):
-        with self._lock:
-            task = self._Task(f)
-            self._run_queue.append(task)
-
-        while True:
-            with self._lock:
-                if task.task_id in self._return_vals:
-                    return self._return_vals[task.task_id]
-
-    def _thread_main(self):
-        app = gui.Application.instance
-        app.initialize()
-
-        done = False
-        while not done:
-            with self._lock:
-                for task in self._run_queue:
-                    retval = task.func()
-                    self._return_vals[task.task_id] = retval
-                self._run_queue.clear()
-
-            done = not app.run_one_tick()
-
-
-# The _AsyncEventLoop class shall only be used to create a singleton instance.
-# There are different ways to achieve this, here we use the module as a holder
-# for singleton variables, see: https://stackoverflow.com/a/31887/1255535.
-_global_jupyter_loop = _AsyncEventLoop()
-_global_jupyter_loop.start()
-
-
 def _jupyter_labextension_paths():
     """Called by Jupyter Lab Server to detect if it is a valid labextension and
     to install the widget
