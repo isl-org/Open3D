a4f1e471c4e9ca3e0440bfe96c02dbd8486047a7
Geoffrey Prewett <prewettg@gmail.com>
2021-05-04 21:13:03 -0700

    Removed entire file menu when in Jupyter

--- cpp/open3d/visualization/visualizer/O3DVisualizer.cpp
+++ cpp/open3d/visualization/visualizer/O3DVisualizer.cpp
@@ -1760,13 +1760,13 @@ O3DVisualizer::O3DVisualizer(const std::string &title, int width, int height)
     app_menu->AddItem("About", MENU_ABOUT);
     menu->AddMenu("Open3D", app_menu);
 #endif  // __APPLE__
-    auto file_menu = std::make_shared<Menu>();
-    file_menu->AddItem("Export Current Image...", MENU_EXPORT_RGB);
     if (Application::GetInstance().UsingNativeWindows()) {
+        auto file_menu = std::make_shared<Menu>();
+        file_menu->AddItem("Export Current Image...", MENU_EXPORT_RGB);
         file_menu->AddSeparator();
         file_menu->AddItem("Close Window", MENU_CLOSE, KeyName::KEY_W);
+        menu->AddMenu("File", file_menu);
     }
-    menu->AddMenu("File", file_menu);
 
     auto actions_menu = std::make_shared<Menu>();
     actions_menu->AddItem("Show Settings", MENU_SETTINGS);
