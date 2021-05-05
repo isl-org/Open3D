f1bccf61b841a2f5fc7a424b7fc8657229712029
Yixing Lao <yixing.lao@gmail.com>
2021-05-05 00:37:42 -0700

    simplify print

--- cpp/open3d/visualization/webrtc_server/WebRTCWindowSystem.cpp
+++ cpp/open3d/visualization/webrtc_server/WebRTCWindowSystem.cpp
@@ -83,7 +83,6 @@ WebRTCWindowSystem::WebRTCWindowSystem()
     // redraw_callback is called when the server wants to send a frame to
     // the client without other triggering events.
     auto redraw_callback = [this](const std::string &window_uid) -> void {
-        utility::LogInfo("##### To call redraw_callback for: {}", window_uid);
         this->PostRedrawEvent(gui::Application::GetInstance()
                                       .GetWindowByUID(window_uid)
                                       ->GetOSWindow());
