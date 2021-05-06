a83bd77d628cce6ab8468e77f45d010b32eecf5f
Rishabh Singh <reyanshsolis@gmail.com>
2021-05-07 03:41:21 +0530

    review change

--- cpp/open3d/t/pipelines/kernel/ComputeTransformCUDA.cu
+++ cpp/open3d/t/pipelines/kernel/ComputeTransformCUDA.cu
@@ -38,6 +38,8 @@ namespace t {
 namespace pipelines {
 namespace kernel {
 
+const int kThread1DUnit = 256;
+
 __global__ void ComputePosePointToPlaneCUDAKernel(
         const float *source_points_ptr,
         const float *target_points_ptr,
@@ -46,10 +48,9 @@ __global__ void ComputePosePointToPlaneCUDAKernel(
         const int64_t *correspondences_second,
         const int n,
         float *global_sum) {
-    const int THREAD_1D_UNIT = 256;
-    __shared__ float local_sum0[THREAD_1D_UNIT];
-    __shared__ float local_sum1[THREAD_1D_UNIT];
-    __shared__ float local_sum2[THREAD_1D_UNIT];
+    __shared__ float local_sum0[kThread1DUnit];
+    __shared__ float local_sum1[kThread1DUnit];
+    __shared__ float local_sum2[kThread1DUnit];
 
     const int tid = threadIdx.x;
 
@@ -82,9 +83,9 @@ __global__ void ComputePosePointToPlaneCUDAKernel(
     reduction[offset++] = r * r;
     reduction[offset++] = valid;
 
-    ReduceSum6x6LinearSystem<float, THREAD_1D_UNIT>(tid, valid, reduction,
-                                                    local_sum0, local_sum1,
-                                                    local_sum2, global_sum);
+    ReduceSum6x6LinearSystem<float, kThread1DUnit>(tid, valid, reduction,
+                                                   local_sum0, local_sum1,
+                                                   local_sum2, global_sum);
 }
 
 void ComputePosePointToPlaneCUDA(const float *source_points_ptr,
@@ -100,9 +101,8 @@ void ComputePosePointToPlaneCUDA(const float *source_points_ptr,
             core::Tensor::Zeros({29}, core::Dtype::Float32, device);
     float *global_sum_ptr = global_sum.GetDataPtr<float>();
 
-    const int THREAD_1D_UNIT = 256;
-    const dim3 blocks((n + THREAD_1D_UNIT - 1) / THREAD_1D_UNIT);
-    const dim3 threads(THREAD_1D_UNIT);
+    const dim3 blocks((n + kThread1DUnit - 1) / kThread1DUnit);
+    const dim3 threads(kThread1DUnit);
 
     ComputePosePointToPlaneCUDAKernel<<<blocks, threads>>>(
             source_points_ptr, target_points_ptr, target_normals_ptr,
