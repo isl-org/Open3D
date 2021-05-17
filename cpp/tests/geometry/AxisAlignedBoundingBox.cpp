1bdade5056466b7cc37dcc75574b981acb8f8809
Matt Jarvis <mattj23@gmail.com>
2021-05-17 13:23:30 -0400

    incorporated review comments

--- cpp/tests/geometry/AxisAlignedBoundingBox.cpp
+++ cpp/tests/geometry/AxisAlignedBoundingBox.cpp
@@ -3,7 +3,7 @@
 // ----------------------------------------------------------------------------
 // The MIT License (MIT)
 //
-// Copyright (c) 2020 www.open3d.org
+// Copyright (c) 2021 www.open3d.org
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -30,63 +30,57 @@
 using namespace open3d::geometry;
 using namespace ::testing;
 
-// Short names for fitting parametrized test cases into the 80 column limit
-using v_t = Eigen::Vector3d;
+using Eigen::Vector3d;
 
-using emp_t = std::tuple<v_t, v_t, bool>;
-using dbl_t = std::tuple<v_t, v_t, double>;
+using emp_t = std::tuple<Vector3d, Vector3d, bool>;
+using dbl_t = std::tuple<Vector3d, Vector3d, double>;
 
 namespace open3d {
 namespace tests {
 
 TEST(AxisAlignedBoundingBox, EmptyConstructor) {
-    // This test now formally specifies the expected behavior of the empty
-    // constructor for an AxisAlignedBoundingBox based on its existing behavior
-    // since pull request #1131 in August of 2019. The min and max bounds are
-    // set to 0,0,0, as well as the color. Anything which breaks the existing
-    // behavior will cause this test to fail.
     AxisAlignedBoundingBox box;
 
     EXPECT_TRUE(box.IsEmpty());
-    ExpectEQ(box.min_bound_, v_t{0, 0, 0});
-    ExpectEQ(box.max_bound_, v_t{0, 0, 0});
-    ExpectEQ(box.color_, v_t{0, 0, 0});
+    ExpectEQ(box.min_bound_, Vector3d{0, 0, 0});
+    ExpectEQ(box.max_bound_, Vector3d{0, 0, 0});
+    ExpectEQ(box.color_, Vector3d{0, 0, 0});
 }
 
 TEST(AxisAlignedBoundingBox, ConstructorBounds) {
     AxisAlignedBoundingBox box({1, 2, 3}, {4, 5, 6});
-    ExpectEQ(box.min_bound_, v_t{1, 2, 3});
-    ExpectEQ(box.max_bound_, v_t{4, 5, 6});
-    ExpectEQ(box.color_, v_t{0, 0, 0});
+    ExpectEQ(box.min_bound_, Vector3d{1, 2, 3});
+    ExpectEQ(box.max_bound_, Vector3d{4, 5, 6});
+    ExpectEQ(box.color_, Vector3d{0, 0, 0});
 }
 
 TEST(AxisAlignedBoundingBox, BoundAccessors) {
     AxisAlignedBoundingBox box({1, 2, 3}, {4, 5, 6});
-    ExpectEQ(box.GetMinBound(), v_t{1, 2, 3});
-    ExpectEQ(box.GetMaxBound(), v_t{4, 5, 6});
+    ExpectEQ(box.GetMinBound(), Vector3d{1, 2, 3});
+    ExpectEQ(box.GetMaxBound(), Vector3d{4, 5, 6});
 }
 
 TEST(AxisAlignedBoundingBox, GetCenter) {
     AxisAlignedBoundingBox box({1, 2, 3}, {5, 6, 7});
-    ExpectEQ(box.GetCenter(), v_t{3, 4, 5});
+    ExpectEQ(box.GetCenter(), Vector3d{3, 4, 5});
 }
 
 TEST(AxisAlignedBoundingBox, GetExtent) {
     AxisAlignedBoundingBox box({1, 2, 3}, {4, 6, 8});
-    ExpectEQ(box.GetExtent(), v_t{3, 4, 5});
+    ExpectEQ(box.GetExtent(), Vector3d{3, 4, 5});
 }
 
 TEST(AxisAlignedBoundingBox, GetHalfExtent) {
     AxisAlignedBoundingBox box({1, 2, 3}, {7, 10, 13});
-    ExpectEQ(box.GetHalfExtent(), v_t{3, 4, 5});
+    ExpectEQ(box.GetHalfExtent(), Vector3d{3, 4, 5});
 }
 
 TEST(AxisAlignedBoundingBox, Clear) {
     AxisAlignedBoundingBox box({1, 2, 3}, {7, 10, 13});
     box.Clear();
     EXPECT_TRUE(box.IsEmpty());
-    ExpectEQ(box.min_bound_, v_t{0, 0, 0});
-    ExpectEQ(box.max_bound_, v_t{0, 0, 0});
+    ExpectEQ(box.min_bound_, Vector3d{0, 0, 0});
+    ExpectEQ(box.max_bound_, Vector3d{0, 0, 0});
 }
 
 TEST(AxisAlignedBoundingBox, GetXPercentage) {
@@ -110,11 +104,13 @@ TEST(AxisAlignedBoundingBox, GetZPercentage) {
 TEST(AxisAlignedBoundingBox, GetBoxPoints) {
     AxisAlignedBoundingBox box({1, 2, 3}, {4, 5, 6});
     auto points = box.GetBoxPoints();
-    std::vector<v_t> expected{{1, 2, 3}, {1, 2, 6}, {1, 5, 6}, {1, 5, 3},
-                              {4, 2, 3}, {4, 2, 6}, {4, 5, 3}, {4, 5, 6}};
+    std::vector<Vector3d> expected{{1, 2, 3}, {1, 2, 6}, {1, 5, 6}, {1, 5, 3},
+                                   {4, 2, 3}, {4, 2, 6}, {4, 5, 3}, {4, 5, 6}};
 
     for (auto exp : expected) {
-        auto locate = [&exp](const v_t& v) { return (v - exp).norm() < 1e-6; };
+        auto locate = [&exp](const Vector3d& v) {
+            return (v - exp).norm() < 1e-6;
+        };
         auto found = std::find_if(points.begin(), points.end(), locate);
         EXPECT_NE(found, points.end());
     }
@@ -125,10 +121,10 @@ TEST(AxisAlignedBoundingBox, GetBoxPoints) {
 // Point Indices within Bounds
 TEST(AxisAlignedBoundingBox, PointIndiciesInBounds) {
     AxisAlignedBoundingBox box({1, 2, 3}, {4, 5, 6});
-    std::vector<v_t> points{{2, 3, 4},  {1, 2, 3}, {1, 2, 6},  {1, 5, 6},
-                            {1, 5, 3},  {4, 2, 3}, {4, 2, 6},  {4, 5, 3},
-                            {4, 5, 6},  {0, 3, 4}, {10, 3, 4}, {2, 0, 4},
-                            {2, 10, 4}, {2, 3, 0}, {2, 3, 10}};
+    std::vector<Vector3d> points{{2, 3, 4},  {1, 2, 3}, {1, 2, 6},  {1, 5, 6},
+                                 {1, 5, 3},  {4, 2, 3}, {4, 2, 6},  {4, 5, 3},
+                                 {4, 5, 6},  {0, 3, 4}, {10, 3, 4}, {2, 0, 4},
+                                 {2, 10, 4}, {2, 3, 0}, {2, 3, 10}};
     std::vector<size_t> expected{0, 1, 2, 3, 4, 5, 6, 7, 8};
     auto indices = box.GetPointIndicesWithinBoundingBox(points);
     std::sort(indices.begin(), indices.end());
@@ -140,111 +136,111 @@ TEST(AxisAlignedBoundingBox, PointIndiciesInBounds) {
 }
 
 TEST(AxisAlignedBoundingBox, CreateFromPoints) {
-    std::vector<v_t> points{{2, 3, 4}, {1, 2, 6}, {1, 5, 6}, {1, 5, 3},
-                            {4, 2, 3}, {4, 2, 6}, {4, 5, 3}};
+    std::vector<Vector3d> points{{2, 3, 4}, {1, 2, 6}, {1, 5, 6}, {1, 5, 3},
+                                 {4, 2, 3}, {4, 2, 6}, {4, 5, 3}};
     auto box = AxisAlignedBoundingBox::CreateFromPoints(points);
-    ExpectEQ(box.min_bound_, v_t{1, 2, 3});
-    ExpectEQ(box.max_bound_, v_t{4, 5, 6});
+    ExpectEQ(box.min_bound_, Vector3d{1, 2, 3});
+    ExpectEQ(box.max_bound_, Vector3d{4, 5, 6});
 }
 
 TEST(AxisAlignedBoundingBox, ScaleBox1) {
     AxisAlignedBoundingBox box({1, 2, 3}, {3, 4, 5});
     box.Scale(2.0, {2, 3, 4});
-    ExpectEQ(box.min_bound_, v_t{0, 1, 2});
-    ExpectEQ(box.max_bound_, v_t{4, 5, 6});
+    ExpectEQ(box.min_bound_, Vector3d{0, 1, 2});
+    ExpectEQ(box.max_bound_, Vector3d{4, 5, 6});
 }
 
 TEST(AxisAlignedBoundingBox, ScaleBox2) {
     AxisAlignedBoundingBox box({1, 2, 3}, {3, 4, 5});
     box.Scale(2.0, {1, 2, 3});
-    ExpectEQ(box.min_bound_, v_t{1, 2, 3});
-    ExpectEQ(box.max_bound_, v_t{5, 6, 7});
+    ExpectEQ(box.min_bound_, Vector3d{1, 2, 3});
+    ExpectEQ(box.max_bound_, Vector3d{5, 6, 7});
 }
 
 TEST(AxisAlignedBoundingBox, TranslateRelative) {
     AxisAlignedBoundingBox box({1, 2, 3}, {3, 4, 5});
     box.Translate({3, 4, 5}, true);
-    ExpectEQ(box.min_bound_, v_t{4, 6, 8});
-    ExpectEQ(box.max_bound_, v_t{6, 8, 10});
+    ExpectEQ(box.min_bound_, Vector3d{4, 6, 8});
+    ExpectEQ(box.max_bound_, Vector3d{6, 8, 10});
 }
 
 TEST(AxisAlignedBoundingBox, TranslateAbsolute) {
     AxisAlignedBoundingBox box({1, 2, 3}, {3, 4, 5});
     box.Translate({-10, -15, -20}, false);
-    ExpectEQ(box.min_bound_, v_t{-11, -16, -21});
-    ExpectEQ(box.max_bound_, v_t{-9, -14, -19});
+    ExpectEQ(box.min_bound_, Vector3d{-11, -16, -21});
+    ExpectEQ(box.max_bound_, Vector3d{-9, -14, -19});
 }
 
 TEST(AxisAlignedBoundingBox, PlusEqualsEmpty1) {
     AxisAlignedBoundingBox box({1, 2, 3}, {1, 4, 5});
     AxisAlignedBoundingBox box1({2, 2, 2}, {3, 3, 3});
     box += box1;
-    ExpectEQ(box.min_bound_, v_t{2, 2, 2});
-    ExpectEQ(box.max_bound_, v_t{3, 3, 3});
+    ExpectEQ(box.min_bound_, Vector3d{2, 2, 2});
+    ExpectEQ(box.max_bound_, Vector3d{3, 3, 3});
 }
 
 TEST(AxisAlignedBoundingBox, PlusEqualsEmpty2) {
     AxisAlignedBoundingBox box({1, 2, 3}, {3, 2, 5});
     AxisAlignedBoundingBox box1({2, 2, 2}, {3, 3, 3});
     box += box1;
-    ExpectEQ(box.min_bound_, v_t{2, 2, 2});
-    ExpectEQ(box.max_bound_, v_t{3, 3, 3});
+    ExpectEQ(box.min_bound_, Vector3d{2, 2, 2});
+    ExpectEQ(box.max_bound_, Vector3d{3, 3, 3});
 }
 
 TEST(AxisAlignedBoundingBox, PlusEqualsEmpty3) {
     AxisAlignedBoundingBox box({1, 2, 3}, {3, 4, 3});
     AxisAlignedBoundingBox box1({2, 2, 2}, {3, 3, 3});
     box += box1;
-    ExpectEQ(box.min_bound_, v_t{2, 2, 2});
-    ExpectEQ(box.max_bound_, v_t{3, 3, 3});
+    ExpectEQ(box.min_bound_, Vector3d{2, 2, 2});
+    ExpectEQ(box.max_bound_, Vector3d{3, 3, 3});
 }
 
 TEST(AxisAlignedBoundingBox, PlusEquals1) {
     AxisAlignedBoundingBox box({0, 0, 0}, {1, 1, 1});
     AxisAlignedBoundingBox box1({-1, 0.25, 0.25}, {0.5, 0.5, 0.5});
     box += box1;
-    ExpectEQ(box.min_bound_, v_t{-1, 0, 0});
-    ExpectEQ(box.max_bound_, v_t{1, 1, 1});
+    ExpectEQ(box.min_bound_, Vector3d{-1, 0, 0});
+    ExpectEQ(box.max_bound_, Vector3d{1, 1, 1});
 }
 
 TEST(AxisAlignedBoundingBox, PlusEquals2) {
     AxisAlignedBoundingBox box({0, 0, 0}, {1, 1, 1});
     AxisAlignedBoundingBox box1({0.25, -1, 0.25}, {0.5, 0.5, 0.5});
     box += box1;
-    ExpectEQ(box.min_bound_, v_t{0, -1, 0});
-    ExpectEQ(box.max_bound_, v_t{1, 1, 1});
+    ExpectEQ(box.min_bound_, Vector3d{0, -1, 0});
+    ExpectEQ(box.max_bound_, Vector3d{1, 1, 1});
 }
 
 TEST(AxisAlignedBoundingBox, PlusEquals3) {
     AxisAlignedBoundingBox box({0, 0, 0}, {1, 1, 1});
     AxisAlignedBoundingBox box1({0.25, 0.25, -1}, {0.5, 0.5, 0.5});
     box += box1;
-    ExpectEQ(box.min_bound_, v_t{0, 0, -1});
-    ExpectEQ(box.max_bound_, v_t{1, 1, 1});
+    ExpectEQ(box.min_bound_, Vector3d{0, 0, -1});
+    ExpectEQ(box.max_bound_, Vector3d{1, 1, 1});
 }
 
 TEST(AxisAlignedBoundingBox, PlusEquals4) {
     AxisAlignedBoundingBox box({0, 0, 0}, {1, 1, 1});
     AxisAlignedBoundingBox box1({0.25, 0.25, 0.25}, {2, 0.5, 0.5});
     box += box1;
-    ExpectEQ(box.min_bound_, v_t{0, 0, 0});
-    ExpectEQ(box.max_bound_, v_t{2, 1, 1});
+    ExpectEQ(box.min_bound_, Vector3d{0, 0, 0});
+    ExpectEQ(box.max_bound_, Vector3d{2, 1, 1});
 }
 
 TEST(AxisAlignedBoundingBox, PlusEquals5) {
     AxisAlignedBoundingBox box({0, 0, 0}, {1, 1, 1});
     AxisAlignedBoundingBox box1({0.25, 0.25, 0.25}, {0.5, 2, 0.5});
     box += box1;
-    ExpectEQ(box.min_bound_, v_t{0, 0, 0});
-    ExpectEQ(box.max_bound_, v_t{1, 2, 1});
+    ExpectEQ(box.min_bound_, Vector3d{0, 0, 0});
+    ExpectEQ(box.max_bound_, Vector3d{1, 2, 1});
 }
 
 TEST(AxisAlignedBoundingBox, PlusEquals6) {
     AxisAlignedBoundingBox box({0, 0, 0}, {1, 1, 1});
     AxisAlignedBoundingBox box1({0.25, 0.25, 0.25}, {0.5, 0.5, 2});
     box += box1;
-    ExpectEQ(box.min_bound_, v_t{0, 0, 0});
-    ExpectEQ(box.max_bound_, v_t{1, 1, 2});
+    ExpectEQ(box.min_bound_, Vector3d{0, 0, 0});
+    ExpectEQ(box.max_bound_, Vector3d{1, 1, 2});
 }
 
 // IsEmpty permutations
