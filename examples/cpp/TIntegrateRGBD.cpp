d83ccb6e710fd24087477b3af921e8807bbb6ea9
Wei Dong <weidong@andrew.cmu.edu>
2021-06-10 21:35:01 -0400

    minor

--- examples/cpp/TIntegrateRGBD.cpp
+++ examples/cpp/TIntegrateRGBD.cpp
@@ -134,8 +134,7 @@ int main(int argc, char* argv[]) {
     double time_total = 0;
     double time_int = 0;
     double time_raycasting = 0;
-    for (size_t i = 0; i < 10; ++i) {
-        // trajectory->parameters_.size(); ++i) {
+    for (size_t i = 0; i < trajectory->parameters_.size(); ++i) {
         utility::Timer timer;
         timer.Start();
 
@@ -235,15 +234,9 @@ int main(int argc, char* argv[]) {
                                     *pcd_legacy);
     }
 
-    open3d::t::io::WriteTSDFVoxelGrid("tsdf.json", voxel_grid);
+    if (utility::ProgramOptionExists(argc, argv, "--tsdf")) {
+        open3d::t::io::WriteTSDFVoxelGrid("tsdf.json", voxel_grid);
+    }
 
-    auto loaded_voxel_grid =
-            open3d::t::io::CreateTSDFVoxelGridFromFile("tsdf.json");
-
-    auto pcd = loaded_voxel_grid->ExtractSurfacePoints(
-            -1, 3.0f,
-            MaskCode::VertexMap | MaskCode::ColorMap | MaskCode::NormalMap);
-    auto pcd_legacy = std::make_shared<open3d::geometry::PointCloud>(
-            pcd.ToLegacyPointCloud());
-    open3d::io::WritePointCloud("new_pcd.ply", *pcd_legacy);
+    return 0;
 }
