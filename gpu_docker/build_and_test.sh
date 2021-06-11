be3a65f13d3ed77c0d82057942bc841d8f86338f
Yixing Lao <yixing.lao@gmail.com>
2021-06-10 17:22:10 -0700

    add gpu_docker folder

---
+++ gpu_docker/build_and_test.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+pushd /root/Open3D
+mkdir build
+pushd build
+
+# Configure
+cmake -DBUILD_SHARED_LIBS=OFF \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DBUILD_LIBREALSENSE=ON \
+      -DBUILD_CUDA_MODULE=ON \
+      -DBUILD_TENSORFLOW_OPS=ON \
+      -DBUILD_PYTORCH_OPS=ON \
+      -DBUILD_RPC_INTERFACE=OFF \
+      -DCMAKE_INSTALL_PREFIX=/root/open3d_install \
+      -DBUILD_UNIT_TESTS=ON \
+      -DBUILD_BENCHMARKS=ON \
+      -DBUILD_EXAMPLES=ON \
+      ..
+
+# Build
+make -j$(nproc)
+make -j$(nproc) install-pip-package
+
+CUDA_VISIBLE_DEVICES=0 ./bin/tests
+CUDA_VISIBLE_DEVICES=0 pytest ../python/test
+
+popd
+popd
