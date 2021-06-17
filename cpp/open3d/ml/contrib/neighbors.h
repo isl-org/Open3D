0b96552048348ff5ccaa7461e765107ba94083f8
Rishabh Singh <rishabh.17iitkgp@gmail.com>
2021-06-17 23:40:03 +0530

    revert changes on external licenses

--- cpp/open3d/ml/contrib/neighbors.h
+++ cpp/open3d/ml/contrib/neighbors.h
@@ -5,7 +5,7 @@
 //
 // MIT License
 //
-// Copyright (c) 2018 - 2021 HuguesTHOMAS
+// Copyright (c) 2019 HuguesTHOMAS
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
