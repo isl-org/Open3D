aebdb9a4396808edece4c24156e0231dc47a5edd
Rishabh Singh <rishabh.17iitkgp@gmail.com>
2021-06-18 06:14:47 +0530

    License update 2018-2021 (#3600)

--- cpp/open3d/ml/tensorflow/continuous_conv/ContinuousConvTransposeOps.cpp
+++ cpp/open3d/ml/tensorflow/continuous_conv/ContinuousConvTransposeOps.cpp
@@ -3,7 +3,7 @@
 // ----------------------------------------------------------------------------
 // The MIT License (MIT)
 //
-// Copyright (c) 2020 www.open3d.org
+// Copyright (c) 2018-2021 www.open3d.org
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -278,7 +278,7 @@ inp_neighbors_index:
 
 
 inp_neighbors_importance_sum:
-  1D tensor of the same length as 'inp_positions' or zero length if 
+  1D tensor of the same length as 'inp_positions' or zero length if
   neighbors_importance is empty. This is the sum of the values in
   'neighbors_importance' for each input point.
 
