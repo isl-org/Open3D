31a162d84fa3f5e3b95daae94b58d4a6410dd0ba
Yixing Lao <yixing.lao@gmail.com>
2021-06-25 03:42:21 -0700

    Console->Logger in pybind and unit test (#3642)

--- cpp/pybind/utility/logging.cpp
+++ cpp/pybind/utility/logging.cpp
@@ -25,13 +25,14 @@
 // ----------------------------------------------------------------------------
 
 #include "open3d/utility/Logging.h"
+
 #include "pybind/docstring.h"
 #include "pybind/open3d_pybind.h"
 
 namespace open3d {
 namespace utility {
 
-void pybind_console(py::module& m) {
+void pybind_logging(py::module& m) {
     py::enum_<VerbosityLevel> vl(m, "VerbosityLevel", py::arithmetic(),
                                  "VerbosityLevel");
     vl.value("Error", VerbosityLevel::Error)
