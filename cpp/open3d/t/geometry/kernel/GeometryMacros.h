09aa226f993180bf2ab7e8884e740db6e9c219bd
Rishabh Singh <rishabh.17iitkgp@gmail.com>
2021-06-26 16:21:39 +0530

    Registration Reduction6x6Impl template datatype support. (#3636)
    * template dtype support for Reduction6x6Impl
    
    Co-authored-by: Patrick Stotko <stotko@cs.uni-bonn.de>

--- cpp/open3d/t/geometry/kernel/GeometryMacros.h
+++ cpp/open3d/t/geometry/kernel/GeometryMacros.h
@@ -31,7 +31,30 @@
 #include "open3d/core/CUDAUtils.h"
 
 #if defined(__CUDACC__)
+
+#if defined(__CUDA_ARCH__)
+#if __CUDA_ARCH__ < 600
+__device__ double atomicAdd(double *address, double val) {
+    unsigned long long int *address_as_ull = (unsigned long long int *)address;
+    unsigned long long int old = *address_as_ull, assumed;
+
+    do {
+        assumed = old;
+        old = atomicCAS(
+                address_as_ull, assumed,
+                __double_as_longlong(val + __longlong_as_double(assumed)));
+
+        // Note: uses integer comparison to avoid hang in case of NaN (since NaN
+        // != NaN)
+    } while (assumed != old);
+
+    return __longlong_as_double(old);
+}
+#endif
+#endif
+
 #define OPEN3D_ATOMIC_ADD(X, Y) atomicAdd(X, Y)
+
 #define ISNAN(X) isnan(X)
 #else
 #define OPEN3D_ATOMIC_ADD(X, Y) (*X).fetch_add(Y)
