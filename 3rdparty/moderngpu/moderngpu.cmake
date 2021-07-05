db63969cf07bf49da3742354797117d93d993274
Junha Lee <junhakiwi@postech.ac.kr>
2021-07-05 23:27:11 +0900

    fix moderngpu.cmake to follow new format

--- 3rdparty/moderngpu/moderngpu.cmake
+++ 3rdparty/moderngpu/moderngpu.cmake
@@ -1,17 +1,14 @@
-# Build system for header-only moderngpu libraries.
-#
-
 include(ExternalProject)
-
 
 ExternalProject_Add(
     ext_moderngpu
     PREFIX moderngpu
-    GIT_REPOSITORY https://github.com/moderngpu/moderngpu.git
-    GIT_TAG 2b3985541c8e88a133769598c406c33ddde9d0a5
+    URL https://github.com/moderngpu/moderngpu/archive/2b3985541c8e88a133769598c406c33ddde9d0a5.zip
+    URL_HASH SHA256=191546af18cd5fb858ecb561316f3af67537ab16f610fc8f1a5febbffc27755a
+    DOWNLOAD_DIR "${OPEN3D_THIRD_PARTY_DOWNLOAD_DIR}/moderngpu"
+    UPDATE_COMMAND ""
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
-    UPDATE_COMMAND ""
     INSTALL_COMMAND ""
 )
 
@@ -20,3 +17,4 @@ message(STATUS "moderngpu source dir: ${SOURCE_DIR}")
 
 set(MODERNGPU_INCLUDE_DIRS "${SOURCE_DIR}/src/")
 message(STATUS "moderngpu include dir: ${MODERNGPU_INCLUDE_DIRS}")
+
