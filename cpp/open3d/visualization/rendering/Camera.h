0c3071e7d05f0c8e0e62872f8bb3dd4d9795c6c5
Rene Sepulveda <errissa@gmail.com>
2021-07-16 10:12:31 -0400

    Add method for computing Z in view space from Z depth buffer

--- cpp/open3d/visualization/rendering/Camera.h
+++ cpp/open3d/visualization/rendering/Camera.h
@@ -125,6 +125,7 @@ public:
     // is in the range [-1, 1] if the point is in view, or outside the range if
     // the point is out of view.
     virtual Eigen::Vector2f GetNDC(const Eigen::Vector3f& pt) const = 0;
+    virtual double GetViewZ(float z_buffer) const = 0;
 
     struct ProjectionInfo {
         bool is_ortho;
