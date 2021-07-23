1e23db1270a5d6b285f6ea7399fc52978709f6ad
Yixing Lao <yixing.lao@gmail.com>
2021-07-23 02:15:21 -0700

    no collision of .o

--- merge_lib.sh
+++ merge_lib.sh
@@ -7,4 +7,11 @@ pushd cuda_lib
     rm -rf libcublasLt_static && mkdir libcublasLt_static && ar x lib/libcublasLt_static.a && mv *.o libcublasLt_static && rm -rf *.o && ls -al libcublasLt_static/*.o | wc -l
     rm -rf liblapack_static   && mkdir liblapack_static   && ar x lib/liblapack_static.a   && mv *.o liblapack_static   && rm -rf *.o && ls -al liblapack_static/*.o   | wc -l
     rm -rf libculibos         && mkdir libculibos         && ar x lib/libculibos.a         && mv *.o libculibos         && rm -rf *.o && ls -al libculibos/*.o         | wc -l
+
+    cp libcublas_static/*.o   .
+    cp libcublasLt_static/*.o .
+    cp liblapack_static/*.o   .
+    cp libculibos/*.o         .
+    ls -al *.o | wc -l
+    rm -rf *.o
 popd
