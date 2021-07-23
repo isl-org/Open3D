f76880d9421c5a75bc8e8332edb3d1291bef04c0
Yixing Lao <yixing.lao@gmail.com>
2021-07-23 02:14:34 -0700

    counter

--- merge_lib.sh
+++ merge_lib.sh
@@ -3,8 +3,8 @@
 pushd cuda_lib
     rm -rf *.o
 
-    rm -rf libcublas_static   && mkdir libcublas_static   && ar x lib/libcublas_static.a   && mv *.o libcublas_static   && rm -rf *.o && ls -alh libcublas_static   | wc -l
-    rm -rf libcublasLt_static && mkdir libcublasLt_static && ar x lib/libcublasLt_static.a && mv *.o libcublasLt_static && rm -rf *.o && ls -alh libcublasLt_static | wc -l
-    rm -rf liblapack_static   && mkdir liblapack_static   && ar x lib/liblapack_static.a   && mv *.o liblapack_static   && rm -rf *.o && ls -alh liblapack_static   | wc -l
-    rm -rf libculibos         && mkdir libculibos         && ar x lib/libculibos.a         && mv *.o libculibos         && rm -rf *.o && ls -alh libculibos         | wc -l
+    rm -rf libcublas_static   && mkdir libcublas_static   && ar x lib/libcublas_static.a   && mv *.o libcublas_static   && rm -rf *.o && ls -al libcublas_static/*.o   | wc -l
+    rm -rf libcublasLt_static && mkdir libcublasLt_static && ar x lib/libcublasLt_static.a && mv *.o libcublasLt_static && rm -rf *.o && ls -al libcublasLt_static/*.o | wc -l
+    rm -rf liblapack_static   && mkdir liblapack_static   && ar x lib/liblapack_static.a   && mv *.o liblapack_static   && rm -rf *.o && ls -al liblapack_static/*.o   | wc -l
+    rm -rf libculibos         && mkdir libculibos         && ar x lib/libculibos.a         && mv *.o libculibos         && rm -rf *.o && ls -al libculibos/*.o         | wc -l
 popd
