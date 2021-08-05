17ed1beb5d592cd7b5be32f46d7579ac66dfb4cb
Yixing Lao <yixing.lao@gmail.com>
2021-08-04 20:31:26 -0700

    try poweroff

--- util/docker/open3d-gpu/scripts/auto-shutdown.sh
+++ util/docker/open3d-gpu/scripts/auto-shutdown.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-# https://cloud.google.com/community/tutorials/create-a-self-deleting-virtual-machine
-sleep 5h
-export NAME=$(curl -X GET http://metadata.google.internal/computeMetadata/v1/instance/name -H 'Metadata-Flavor: Google')
-export ZONE=$(curl -X GET http://metadata.google.internal/computeMetadata/v1/instance/zone -H 'Metadata-Flavor: Google')
-gcloud --quiet compute instances delete $NAME --zone=$ZONE
+# sleep 5h
+sleep 10m
+sudo poweroff
