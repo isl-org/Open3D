43bd3b65a536dcc4094575e38c84f5f665a3a4d2
Yixing Lao <yixing.lao@gmail.com>
2021-08-05 20:07:21 -0700

    mkdir temp ccache

--- util/docker/open3d-gpu/scripts/tar-ccache.sh
+++ util/docker/open3d-gpu/scripts/tar-ccache.sh
@@ -5,6 +5,10 @@ ccache -s
 CCACHE_DIR=$(ccache -p | grep cache_dir | grep -oE "[^ ]+$")
 echo "CCACHE_DIR: ${CCACHE_DIR}"
 
+# Remove this after ccache is working
+mkdir -p ${CCACHE_DIR}
+touch ${CCACHE_DIR}/hello.txt
+
 CCACHE_DIR_NAME=$(basename ${CCACHE_DIR})
 CCACHE_DIR_PARENT=$(dirname ${CCACHE_DIR})
 cd ${CCACHE_DIR_PARENT}
