75987dca6f92e9033aa3b655725448c151d1852f
Rene Sepulveda <errissa@gmail.com>
2021-08-12 09:12:35 -0400

    Add documentation to new method

--- cpp/open3d/visualization/rendering/Camera.h
+++ cpp/open3d/visualization/rendering/Camera.h
@@ -125,6 +125,9 @@ public:
     // is in the range [-1, 1] if the point is in view, or outside the range if
     // the point is out of view.
     virtual Eigen::Vector2f GetNDC(const Eigen::Vector3f& pt) const = 0;
+
+    /// Returns the view space depth (i.e., distance from camera) for the given
+    /// Z-buffer value
     virtual double GetViewZ(float z_buffer) const = 0;
 
     struct ProjectionInfo {
