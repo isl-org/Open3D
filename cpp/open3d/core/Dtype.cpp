fe48e171468e66d6b2de5f0c126bfde5329070e5
Nobuo Tsukamoto <nextremer.nb.o@gmail.com>
2021-08-30 22:02:08 +0900

    Avoiding the stringop-truncation warning.

--- cpp/open3d/core/Dtype.cpp
+++ cpp/open3d/core/Dtype.cpp
@@ -75,7 +75,8 @@ Dtype::Dtype(DtypeCode dtype_code, int64_t byte_size, const std::string &name)
     if (name.size() > max_name_len_ - 1) {
         utility::LogError("Name {} must be shorter.", name);
     } else {
-        std::strncpy(name_, name.c_str(), max_name_len_ - 1);
+        std::strncpy(name_, name.c_str(), max_name_len_);
+        name_[max_name_len_ - 1] = '\0';
     }
 }
 
