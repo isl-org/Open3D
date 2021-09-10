a14b6dade337cc2c372e59173b0842175cbd7921
Sanskar Agrawal <sanskaragrawal107@gmail.com>
2021-09-10 14:09:36 +0530

    fix headers

--- cpp/open3d/ml/contrib/TrilinearDevoxelize.cu
+++ cpp/open3d/ml/contrib/TrilinearDevoxelize.cu
@@ -1,3 +1,29 @@
+// ----------------------------------------------------------------------------
+// -                        Open3D: www.open3d.org                            -
+// ----------------------------------------------------------------------------
+// The MIT License (MIT)
+//
+// Copyright (c) 2018-2021 www.open3d.org
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+// ----------------------------------------------------------------------------
+
 #include "open3d/ml/contrib/TrilinearDevoxelize.cuh"
 
 namespace open3d {
@@ -95,18 +121,6 @@ __global__ void trilinear_devoxelize_kernel(int b,
     }
 }
 
-/*
-Function: trilinear devoxlization (backward)
-Args:
-b   : batch size
-c   : #channels
-n   : number of points
-r3  : voxel cube size = voxel resolution ** 3
-inds   : the voxel indices of point cube, IntTensor[b, 8, n]
-wgts   : weight for trilinear interpolation, FloatTensor[b, 8, n]
-grad_y : grad outputs, FloatTensor[b, c, n]
-grad_x : grad inputs, FloatTensor[b, c, r3]
-*/
 __global__ void trilinear_devoxelize_grad_kernel(
         int b,
         int c,
