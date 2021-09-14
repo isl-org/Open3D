73db0267b267b0656e1f8c3d737a5cc298a54e78
Yixing Lao <yixing.lao@gmail.com>
2021-09-14 01:34:50 -0700

    SizeVector implict conversion in Pybind

--- cpp/pybind/core/size_vector.cpp
+++ cpp/pybind/core/size_vector.cpp
@@ -39,15 +39,19 @@ void pybind_core_size_vector(py::module& m) {
             "A vector of integers for specifying shape, strides, etc.");
 
     // In Python, We want (3), (3,), [3] and [3,] to represent the same thing.
-    // The followings are all equivalent to core::SizeVector({3}).
+    // The followings are all equivalent to core::SizeVector({3}):
     // - o3d.core.SizeVector(3)     # int
     // - o3d.core.SizeVector((3))   # int, not tuple
     // - o3d.core.SizeVector((3,))  # tuple
     // - o3d.core.SizeVector([3])   # list
     // - o3d.core.SizeVector([3,])  # list
     //
-    // Note that o3d.core.SizeVector(3) creates a 1-D SizeVector of value 3, but
-    // core::SizeVector(3) creates a 3-D SizeVector with value 0.
+    // Difference between C++ and Python:
+    // - o3d.core.SizeVector(3) creates a 1-D SizeVector: {3}.
+    // - core::SizeVector(3) creates a 3-D SizeVector: {0, 0, 0}.
+    //
+    // The API difference is due to the NumPy convention which allows integer to
+    // represent a 1-element tuple, and the C++ constructor for vectors.
     sv.def(py::init([](int64_t i) { return SizeVector({i}); }));
     py::implicitly_convertible<py::int_, SizeVector>();
 
