a5f0027dd7401aa34612426717c9dc07b104b054
Patrick Stotko <stotko@cs.uni-bonn.de>
2021-09-15 14:12:39 +0200

    Add vectorized ParallelFor function and helper macros

--- cpp/tests/utility/Helper.cpp
+++ cpp/tests/utility/Helper.cpp
@@ -28,6 +28,10 @@
 
 #include "tests/Tests.h"
 
+#ifdef BUILD_ISPC_MODULE
+#include "Helper_ispc.h"
+#endif
+
 namespace open3d {
 namespace tests {
 
@@ -59,6 +63,26 @@ TEST(Helper, UniformRandIntGeneratorWithRandomSeed) {
             *it = new_rand_generator();
         EXPECT_FALSE(values == new_values);
     }
+}
+
+TEST(Helper, CHAR_BIT_constant) {
+#ifdef BUILD_ISPC_MODULE
+    int32_t value;
+    ispc::GetCharBit(&value);
+
+    EXPECT_EQ(value, CHAR_BIT);
+#endif
+}
+
+TEST(Helper, ENSURE_EXPORTED) {
+#ifdef BUILD_ISPC_MODULE
+    ispc::NotAutomaticallyExportedStruct s;
+    s.i = 1;
+    s.b = 255;
+
+    EXPECT_EQ(s.i, 1);
+    EXPECT_EQ(s.b, 255);
+#endif
 }
 
 }  // namespace tests
