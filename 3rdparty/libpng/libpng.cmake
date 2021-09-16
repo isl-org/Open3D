09355747fd39d43353138c12ceef302384ec4342
Yixing Lao <yixing.lao@gmail.com>
2021-09-16 14:37:32 -0700

    fix debug lib name for libpng

--- 3rdparty/libpng/libpng.cmake
+++ 3rdparty/libpng/libpng.cmake
@@ -22,11 +22,10 @@ ExternalProject_Add(
         -DZLIB_INCLUDE_DIR=${ZLIB_INCLUDE_DIRS}
         ${ExternalProject_CMAKE_ARGS_hidden}
     BUILD_BYPRODUCTS
-        <INSTALL_DIR>/${Open3D_INSTALL_LIB_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}${lib_name}${CMAKE_STATIC_LIBRARY_SUFFIX}
-        <INSTALL_DIR>/${Open3D_INSTALL_LIB_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}${lib_name}d${CMAKE_STATIC_LIBRARY_SUFFIX}
+        <INSTALL_DIR>/${Open3D_INSTALL_LIB_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}${lib_name}$<$<PLATFORM_ID:Windows>:$<$<CONFIG:Debug>:d>>${CMAKE_STATIC_LIBRARY_SUFFIX}
 )
 
 ExternalProject_Get_Property(ext_libpng INSTALL_DIR)
 set(LIBPNG_INCLUDE_DIRS ${INSTALL_DIR}/include/) # "/" is critical.
 set(LIBPNG_LIB_DIR ${INSTALL_DIR}/${Open3D_INSTALL_LIB_DIR})
-set(LIBPNG_LIBRARIES ${lib_name}$<$<CONFIG:Debug>:d>)
+set(LIBPNG_LIBRARIES ${lib_name}$<$<PLATFORM_ID:Windows>:$<$<CONFIG:Debug>:d>>)
