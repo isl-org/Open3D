3f66f0708b3a5c97242426352ed0c9904730badd
Yixing Lao <yixing.lao@gmail.com>
2021-10-04 10:46:08 -0700

    temporary fix for eigen download

--- 3rdparty/eigen/eigen.cmake
+++ 3rdparty/eigen/eigen.cmake
@@ -3,7 +3,9 @@ include(ExternalProject)
 ExternalProject_Add(
     ext_eigen
     PREFIX eigen
-    URL https://gitlab.com/libeigen/eigen/-/archive/3.4.0/eigen-3.4.0.tar.bz2
+    # Temporary fix: https://gitlab.com/libeigen/eigen/-/issues/2336
+    URL https://github.com/isl-org/open3d_downloads/releases/download/eigen-3.4.0/eigen-3.4.0.tar.bz2
+        https://gitlab.com/libeigen/eigen/-/archive/3.4.0/eigen-3.4.0.tar.bz2
     URL_HASH SHA256=b4c198460eba6f28d34894e3a5710998818515104d6e74e5cc331ce31e46e626
     DOWNLOAD_DIR "${OPEN3D_THIRD_PARTY_DOWNLOAD_DIR}/eigen"
     UPDATE_COMMAND ""
