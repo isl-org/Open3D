12e9ee861b9576648520e44b8856ce91b36da855
Benjamin Ummenhofer <benjaminum@gmail.com>
2021-10-10 09:23:08 +0200

    fix memleak in implicit conversion to Tensor in python (#4148)

--- cpp/pybind/core/tensor_type_caster.cpp
+++ cpp/pybind/core/tensor_type_caster.cpp
@@ -24,29 +24,34 @@
 // IN THE SOFTWARE.
 // ----------------------------------------------------------------------------
 
-#pragma once
+#include "pybind/core/tensor_type_caster.h"
 
-#include <pybind11/pybind11.h>
+#include "pybind/core/tensor_converter.h"
 
-namespace open3d {
-namespace core {
-class Tensor;
-}
-}  // namespace open3d
-
-// Define type caster allowing implicit conversion to Tensor from common types.
-// Needs to be included in each compilation unit.
 namespace pybind11 {
 namespace detail {
-template <>
-struct type_caster<open3d::core::Tensor>
-    : public type_caster_base<open3d::core::Tensor> {
-public:
-    bool load(handle src, bool convert);
 
-private:
-    std::unique_ptr<open3d::core::Tensor> holder_;
-};
+bool type_caster<open3d::core::Tensor>::load(handle src, bool convert) {
+    using base = type_caster_base<open3d::core::Tensor>;
+    if (this->base::load(src, convert)) {
+        return true;
+    }
+
+    if (convert) {
+        std::string class_name(py::str(src.get_type()));
+        if (class_name == "<class 'bool'>" || class_name == "<class 'int'>" ||
+            class_name == "<class 'float'>" || class_name == "<class 'list'>" ||
+            class_name == "<class 'tuple'>" ||
+            class_name == "<class 'numpy.ndarray'>") {
+            holder_ = std::make_unique<open3d::core::Tensor>(
+                    open3d::core::PyHandleToTensor(src));
+            value = holder_.get();
+            return true;
+        }
+    }
+
+    return false;
+}
 
 }  // namespace detail
 }  // namespace pybind11
