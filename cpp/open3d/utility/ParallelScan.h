94773fa6b3b6fd4d161e8dadd16345832bd5ba7d
Yixing Lao <yixing.lao@gmail.com>
2021-10-19 01:17:19 -0700

    [TEMP] disable parallelscan.h

--- cpp/open3d/utility/ParallelScan.h
+++ cpp/open3d/utility/ParallelScan.h
@@ -29,28 +29,25 @@
 #include <tbb/parallel_for.h>
 #include <tbb/parallel_scan.h>
 
-#if TBB_INTERFACE_VERSION >= 10000
+// #if TBB_INTERFACE_VERSION >= 10000
 
-// Check if the C++ standard library implements parallel algorithms
-// and use this over parallelstl to avoid conflicts.
-// Clang does not implement it so far, so checking for C++17 is not sufficient.
-#ifdef __cpp_lib_parallel_algorithm
-#include <execution>
-#include <numeric>
-#else
-#include <pstl/execution>
-#include <pstl/numeric>
+// // Check if the C++ standard library implements parallel algorithms
+// // and use this over parallelstl to avoid conflicts.
+// // Clang does not implement it so far, so checking for C++17 is not
+// sufficient. #ifdef __cpp_lib_parallel_algorithm #include <execution> #include
+// <numeric> #else #include <pstl/execution> #include <pstl/numeric>
 
-// parallelstl incorrectly assumes MSVC to unconditionally implement
-// parallel algorithms even if __cpp_lib_parallel_algorithm is not defined.
-// So manually include the header which pulls all "pstl::execution" definitions
-// into the "std" namespace.
-#if __PSTL_CPP17_EXECUTION_POLICIES_PRESENT
-#include <pstl/internal/glue_execution_defs.h>
-#endif
+// // parallelstl incorrectly assumes MSVC to unconditionally implement
+// // parallel algorithms even if __cpp_lib_parallel_algorithm is not defined.
+// // So manually include the header which pulls all "pstl::execution"
+// definitions
+// // into the "std" namespace.
+// #if __PSTL_CPP17_EXECUTION_POLICIES_PRESENT
+// #include <pstl/internal/glue_execution_defs.h>
+// #endif
 
-#endif
-#endif
+// #endif
+// #endif
 
 namespace open3d {
 namespace utility {
@@ -68,12 +65,12 @@ public:
 
     template <class Tag>
     void operator()(const tbb::blocked_range<size_t>& r, Tag) {
-        Tout temp = sum;
-        for (size_t i = r.begin(); i < r.end(); ++i) {
-            temp = temp + in[i];
-            if (Tag::is_final_scan()) out[i] = temp;
-        }
-        sum = temp;
+        // Tout temp = sum;
+        // for (size_t i = r.begin(); i < r.end(); ++i) {
+        //     temp = temp + in[i];
+        //     if (Tag::is_final_scan()) out[i] = temp;
+        // }
+        // sum = temp;
     }
     ScanSumBody(ScanSumBody& b, tbb::split) : sum(0), in(b.in), out(b.out) {}
     void reverse_join(ScanSumBody& a) { sum = a.sum + sum; }
@@ -83,14 +80,14 @@ public:
 
 template <class Tin, class Tout>
 void InclusivePrefixSum(const Tin* first, const Tin* last, Tout* out) {
-#if TBB_INTERFACE_VERSION >= 10000
-    // use parallelstl if we have TBB 2018 or later
-    std::inclusive_scan(std::execution::par_unseq, first, last, out);
-#else
-    ScanSumBody<Tin, Tout> body(out, first);
-    size_t n = std::distance(first, last);
-    tbb::parallel_scan(tbb::blocked_range<size_t>(0, n), body);
-#endif
+    // #if TBB_INTERFACE_VERSION >= 10000
+    //     // use parallelstl if we have TBB 2018 or later
+    //     std::inclusive_scan(std::execution::par_unseq, first, last, out);
+    // #else
+    //     ScanSumBody<Tin, Tout> body(out, first);
+    //     size_t n = std::distance(first, last);
+    //     tbb::parallel_scan(tbb::blocked_range<size_t>(0, n), body);
+    // #endif
 }
 
 }  // namespace utility
