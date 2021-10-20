c044451538886ae14518153d81aac9219767d0ed
Rene <errissa@gmail.com>
2021-10-19 19:23:32 -0500

    Add suppport for Material Properties to Tensor Geometries (#3858)
    * Add Material class for use with Tensor geometries
    
    * Add material methods to TriangleMesh
    
    * Complete implementation of Material class
    
    * Ignore CCLS language server cache files
    
    * Fix misspelling
    
    * Style fixes
    
    * Add Material.cpp to CMakeLists.txt
    
    * Make sure images added to the Material reside on CPU
    
    * Set default shader when setting default properties
    
    * Use empty string for invalid Material and fix typo
    
    * Add pybinds for Material
    
    * Add pybinds for Material
    
    * Move t-Material to rendering and rename legacy Material to MaterialRecord
    
    * Style fixes
    
    * Add DrawGeometry mix-in base class
    
    * Add methods for getting the full material property maps
    
    * Rename shader name to material name
    
    * Move material pybinds to correct directory
    
    * Add pybind for material property maps
    
    * Remove function call that has been moved
    
    * Fix compilation error in Debug builds
    
    * Style fix
    
    * Add pybind's for DrawableGeometry mix-in class
    
    * Add const version of Material getter
    
    * Add DrawableGeometry pybinds for LineSet and PointCloud
    
    * Modify AddGeometry to take any T Geometry
    
    * Support TGeometry TriangleMesh and fix color conversions
    
    * Add support for TGeometry TriangleMesh to O3D
    
    * Remove old commented out code
    
    * Make 'get' and 'has' methods const
    
    * Add method for converting from Material to MaterialRecord
    
    * User T Geometry material if there is a valid one
    
    * Use 'texture_uvs' vertex attribute when available
    
    * Add support for T Geometry LineSets
    
    * Fix pybinds for accessing Material maps directly
    
    * Style fix
    
    * Fix rendering issue with LineSets with per-line colors
    
    * Fix CI errors on Mac/Windows
    
    * Added example of using embedded TGeometry material
    
    * Correct name of MaterialRecord on Python side of pybinds
    
    * Make sure Image data is on CPU and make a copy
    
    * Clarify meaning of material name
    
    * Change name to make it more descriptive
    
    * Remove non-pythonic (and unnecessary) bindings
    
    * Fix Windows CI error
    
    * Move header file to avoid conflict with windows header files
    
    * Fix windows CI issue
    
    * Fix Windows CI issue
    
    * Style fixes
    
    * Use aligned allocator for VectorPropertyMap
    
    * Fix Mac CI issue
    
    * Use Tensor advanced indexing to duplicate vertices
    
    * Use Tensor slicing to build per-vertex filament buffer
    
    * Fix typo
    
    * Change property/map names to better match Filament naming
    
    * Support per triangle and per vertex attributes
    
    * Style fixes
    
    * Document AORoughnessMetal map which has no Filament equivalent
    
    * Use VectorPropertyMap to make sure pybind is compatible
    
    Co-authored-by: Yixing Lao <yixing.lao@gmail.com>

--- cpp/open3d/visualization/visualizer/O3DVisualizer.cpp
+++ cpp/open3d/visualization/visualizer/O3DVisualizer.cpp
@@ -38,6 +38,7 @@
 #include "open3d/geometry/VoxelGrid.h"
 #include "open3d/io/ImageIO.h"
 #include "open3d/io/rpc/ZMQReceiver.h"
+#include "open3d/t/geometry/LineSet.h"
 #include "open3d/t/geometry/PointCloud.h"
 #include "open3d/t/geometry/TriangleMesh.h"
 #include "open3d/utility/FileSystem.h"
@@ -822,7 +823,7 @@ struct O3DVisualizer::Impl {
                      std::shared_ptr<geometry::Geometry3D> geom,
                      std::shared_ptr<t::geometry::Geometry> tgeom,
                      std::shared_ptr<rendering::TriangleMeshModel> model,
-                     const rendering::Material *material,
+                     const rendering::MaterialRecord *material,
                      const std::string &group,
                      double time,
                      bool is_visible) {
@@ -832,15 +833,13 @@ struct O3DVisualizer::Impl {
         }
         bool is_default_color = false;
         bool no_shadows = false;
-        Material mat;
-        t::geometry::PointCloud *valid_tpcd = nullptr;
+        MaterialRecord mat;
 
         if (material) {
             mat = *material;
             is_default_color = false;
             auto t_cloud =
                     std::dynamic_pointer_cast<t::geometry::PointCloud>(tgeom);
-            valid_tpcd = t_cloud.get();
         } else if (model) {
             // Adding a triangle mesh model. Shader needs to be set to
             // defaultLit for O3D shader handling logic to work.
@@ -865,6 +864,8 @@ struct O3DVisualizer::Impl {
                     std::dynamic_pointer_cast<t::geometry::PointCloud>(tgeom);
             auto t_mesh =
                     std::dynamic_pointer_cast<t::geometry::TriangleMesh>(tgeom);
+            auto t_lines =
+                    std::dynamic_pointer_cast<t::geometry::LineSet>(tgeom);
 
             if (cloud) {
                 has_colors = !cloud->colors_.empty();
@@ -872,9 +873,11 @@ struct O3DVisualizer::Impl {
             } else if (t_cloud) {
                 has_colors = t_cloud->HasPointColors();
                 has_normals = t_cloud->HasPointNormals();
-                valid_tpcd = t_cloud.get();
             } else if (lines) {
                 has_colors = !lines->colors_.empty();
+                no_shadows = true;
+            } else if (t_lines) {
+                has_colors = t_lines->HasLineColors();
                 no_shadows = true;
             } else if (obb) {
                 has_colors = (obb->color_ != Eigen::Vector3d{0.0, 0.0, 0.0});
@@ -886,7 +889,7 @@ struct O3DVisualizer::Impl {
                 has_normals = !mesh->vertex_normals_.empty();
                 has_colors = true;  // always want base_color as white
             } else if (t_mesh) {
-                has_normals = !t_mesh->HasVertexNormals();
+                has_normals = t_mesh->HasVertexNormals();
                 has_colors = true;  // always want base_color as white
             } else if (voxel_grid) {
                 has_normals = false;
@@ -913,6 +916,15 @@ struct O3DVisualizer::Impl {
                 is_default_color = false;
             }
             mat.point_size = ConvertToScaledPixels(ui_state_.point_size);
+
+            // If T Geometry has a valid material convert it to MaterialRecord
+            if (t_mesh && t_mesh->HasMaterial()) {
+                t_mesh->GetMaterial().ToMaterialRecord(mat);
+            } else if (t_cloud && t_cloud->HasMaterial()) {
+                t_cloud->GetMaterial().ToMaterialRecord(mat);
+            } else if (t_lines && t_lines->HasMaterial()) {
+                t_lines->GetMaterial().ToMaterialRecord(mat);
+            }
 
             // Finally assign material properties if geometry is a triangle mesh
             auto tmesh =
@@ -978,15 +990,15 @@ struct O3DVisualizer::Impl {
         // Do we have a geometry, tgeometry or model?
         if (geom) {
             scene->AddGeometry(name, geom.get(), mat);
-        } else if (tgeom && valid_tpcd) {
-            scene->AddGeometry(name, valid_tpcd, mat);
+        } else if (tgeom) {
+            scene->AddGeometry(name, tgeom.get(), mat);
         } else if (model) {
             scene->AddModel(name, *model);
         } else {
             utility::LogWarning(
                     "No valid geometry specified to O3DVisualizer. Only "
-                    "supported "
-                    "geometries are Geometry3D and TGeometry PointClouds.");
+                    "supported geometries are Geometry3D and TGeometry "
+                    "PointClouds and TriangleMeshes.");
         }
 
         if (no_shadows) {
@@ -1234,7 +1246,7 @@ struct O3DVisualizer::Impl {
     }
 
     void OverrideMaterial(const std::string &name,
-                          const Material &original_material,
+                          const MaterialRecord &original_material,
                           O3DVisualizer::Shader shader) {
         bool is_lines = (original_material.shader == "unlitLine");
         auto scene = scene_->GetScene();
@@ -1244,7 +1256,7 @@ struct O3DVisualizer::Impl {
             (shader == Shader::UNLIT && is_lines)) {
             scene->ModifyGeometryMaterial(name, original_material);
         } else {
-            Material m = original_material;
+            MaterialRecord m = original_material;
             m.shader = GetShaderString(shader);
             scene->ModifyGeometryMaterial(name, m);
         }
@@ -1931,7 +1943,7 @@ void O3DVisualizer::SetShader(Shader shader) { impl_->SetShader(shader); }
 void O3DVisualizer::AddGeometry(
         const std::string &name,
         std::shared_ptr<geometry::Geometry3D> geom,
-        const rendering::Material *material /*=nullptr*/,
+        const rendering::MaterialRecord *material /*=nullptr*/,
         const std::string &group /*= ""*/,
         double time /*= 0.0*/,
         bool is_visible /*= true*/) {
@@ -1942,7 +1954,7 @@ void O3DVisualizer::AddGeometry(
 void O3DVisualizer::AddGeometry(
         const std::string &name,
         std::shared_ptr<t::geometry::Geometry> tgeom,
-        const rendering::Material *material /*=nullptr*/,
+        const rendering::MaterialRecord *material /*=nullptr*/,
         const std::string &group /*= ""*/,
         double time /*= 0.0*/,
         bool is_visible /*= true*/) {
@@ -1953,7 +1965,7 @@ void O3DVisualizer::AddGeometry(
 void O3DVisualizer::AddGeometry(
         const std::string &name,
         std::shared_ptr<rendering::TriangleMeshModel> model,
-        const rendering::Material *material /*=nullptr*/,
+        const rendering::MaterialRecord *material /*=nullptr*/,
         const std::string &group /*= ""*/,
         double time /*= 0.0*/,
         bool is_visible /*= true*/) {
