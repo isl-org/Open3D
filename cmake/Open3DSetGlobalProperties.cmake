8a254f1527f06432be464ad0cfb78df847bb5fac
Yixing Lao <yixing.lao@gmail.com>
2021-10-26 06:31:08 -0700

    simplify more

--- cmake/Open3DSetGlobalProperties.cmake
+++ cmake/Open3DSetGlobalProperties.cmake
@@ -3,7 +3,7 @@
 # Sets important project-related properties to <target>.
 function(open3d_set_global_properties target)
     # Tell CMake we want a compiler that supports C++14 features
-    target_compile_features(${target} PUBLIC cxx_std_14)
+    target_compile_features(${target} PUBLIC cxx_std_17)
 
     target_include_directories(${target} PUBLIC
         $<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/cpp>
