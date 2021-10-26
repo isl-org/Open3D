02144009ace637fd96cbe580445182b46dd1ca20
Yixing Lao <yixing.lao@gmail.com>
2021-10-26 06:19:34 -0700

    simplify

--- cmake/Open3DSetGlobalProperties.cmake
+++ cmake/Open3DSetGlobalProperties.cmake
@@ -5,24 +5,10 @@ function(open3d_set_global_properties target)
     # Tell CMake we want a compiler that supports C++14 features
     target_compile_features(${target} PUBLIC cxx_std_14)
 
-    # Colorize GCC/Clang terminal outputs
-    if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
-        target_compile_options(${target} PRIVATE $<$<COMPILE_LANGUAGE:CXX>:-fdiagnostics-color=always>)
-    elseif (CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
-        target_compile_options(${target} PRIVATE $<$<COMPILE_LANGUAGE:CXX>:-fcolor-diagnostics>)
-    endif()
-
     target_include_directories(${target} PUBLIC
         $<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/cpp>
         $<INSTALL_INTERFACE:${Open3D_INSTALL_INCLUDE_DIR}>
     )
-
-    # Required for static linking zeromq
-    target_compile_definitions(${target} PRIVATE ZMQ_STATIC)
-
-    if(NOT WITH_OPENMP)
-        target_compile_options(${target} PRIVATE "$<$<COMPILE_LANGUAGE:CXX>:-Wno-unknown-pragmas>")
-    endif()
 
     target_compile_definitions(${target} PRIVATE UNIX)
 
