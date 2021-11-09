bb3a72c4c3efb7bd2b8c7df3723333bb13a159b8
Yixing Lao <yixing.lao@gmail.com>
2021-11-09 02:12:09 -0800

    librealsense does not need to be in ci_utils.sh

--- util/ci_utils.sh
+++ util/ci_utils.sh
@@ -112,25 +112,6 @@ install_python_dependencies() {
     fi
 }
 
-install_librealsense2() {
-
-    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-        echo Installing librealsense
-        echo Reference: https://github.com/IntelRealSense/librealsense/blob/master/doc/distribution_linux.md
-        $SUDO apt-key adv --keyserver keys.gnupg.net --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE ||
-            $SUDO apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE
-
-        $SUDO apt-add-repository "deb http://realsense-hw-public.s3.amazonaws.com/Debian/apt-repo ${UBUNTU_VERSION} main" -u
-        $SUDO apt-get install --yes --no-install-recommends librealsense2-dkms librealsense2-udev-rules librealsense2-dev
-        $SUDO apt-get install --yes --no-install-recommends librealsense2-utils
-    elif [[ "$OSTYPE" == "darwin"* ]]; then
-        brew install librealsense
-    else
-        echo "Unsupported OS $OSTYPE"
-        exit 1
-    fi
-}
-
 build_all() {
 
     echo "Using cmake: $(command -v cmake)"
