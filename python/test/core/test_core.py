6ced90e39d13953926465fbf2af4613c309132d2
Shreyansh Darshan <shreyanshdarshan1@gmail.com>
2021-11-17 21:33:28 +0530

    Fix type in python unit test

--- python/test/core/test_core.py
+++ python/test/core/test_core.py
@@ -244,7 +244,7 @@ def test_arange(device):
 
 
 @pytest.mark.parametrize("device", list_devices())
-def test_arange(device):
+def test_flatten(device):
     src_t = o3c.Tensor([[[0, 1, 2, 3], [4, 5, 6, 7], [8, 9, 10, 11]],
                         [[12, 13, 14, 15], [16, 17, 18, 19], [20, 21, 22,
                                                               23]]]).to(device)
