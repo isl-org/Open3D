9188a1c4727765934ca2f0445b81c4e766152048
reyanshsolis <reyanshsolis@gmail.com>
2021-11-18 10:35:54 +0530

    working example

--- examples/cpp/DownloadFile.cpp
+++ examples/cpp/DownloadFile.cpp
@@ -1,63 +1,73 @@
-// ----------------------------------------------------------------------------
-// -                        Open3D: www.open3d.org                            -
-// ----------------------------------------------------------------------------
-// The MIT License (MIT)
-//
-// Copyright (c) 2018-2021 www.open3d.org
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-// IN THE SOFTWARE.
-// ----------------------------------------------------------------------------
-
+#define CURL_STATICLIB
 #include <curl/curl.h>
+#include <stdio.h>
+// #include <curl/types.h>
+#include <curl/easy.h>
+#include <stdlib.h>
+#include <string.h>
 
-#include <iostream>
-#include <string>
+#define false 0
 
-static size_t WriteCallback(void *contents,
-                            size_t size,
-                            size_t nmemb,
-                            void *userp) {
-    ((std::string *)userp)->append((char *)contents, size * nmemb);
-    return size * nmemb;
+size_t write_data(void *ptr, size_t size, size_t nmemb, FILE *stream) {
+    size_t written;
+    written = fwrite(ptr, size, nmemb, stream);
+    return written;
 }
 
 int main(void) {
-    std::cout << "testing 1";
     CURL *curl;
+    FILE *fp;
     CURLcode res;
-    std::string readBuffer;
+
+    const char url[] =
+            "https://github.com/reyanshsolis/rey_download/releases/download/"
+            "test_data/test_file.zip";
+    const char outfilename[FILENAME_MAX] = "test_file.zip";
+
+    curl_version_info_data *vinfo = curl_version_info(CURLVERSION_NOW);
+
+    if (vinfo->features & CURL_VERSION_SSL) {
+        printf("CURL: SSL enabled\n");
+    } else {
+        printf("CURL: SSL not enabled\n");
+    }
 
     curl = curl_easy_init();
     if (curl) {
-        std::cout << "testing 3";
+        fp = fopen(outfilename, "wb");
 
-        curl_easy_setopt(curl, CURLOPT_URL, "http://www.google.com");
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+        /* Setup the https:// verification options. Note we   */
+        /* do this on all requests as there may be a redirect */
+        /* from http to https and we still want to verify     */
+        curl_easy_setopt(curl, CURLOPT_URL, url);
+
+        // Follow redirection in link.
+        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+
+        // curl_easy_setopt(curl, CURLOPT_CAINFO, "./ca-bundle.crt");
+
+        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, false);
+        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, false);
+
+        // Write function callback.
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+
+        // curl_easy_setopt(curl, CURLOPT_NOSIGNAL, 1); //Prevent "longjmp
+        // causes uninitialized stack frame" bug curl_easy_setopt(curl,
+        // CURLOPT_ACCEPT_ENCODING, "deflate"); std::stringstream out;
+        // curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+        // curl_easy_setopt(curl, CURLOPT_WRITEDATA, &out);
+
+        // Performs the request. res gets the retun code.
         res = curl_easy_perform(curl);
         (void)res;
+
+        // Cleanup.
         curl_easy_cleanup(curl);
-
-        std::cout << readBuffer << std::endl;
+        // Close file.
+        fclose(fp);
     }
-
-    std::cout << "testing 2";
-
     return 0;
 }
