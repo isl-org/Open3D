7a2851e3658e16798922ebe996c7c2bfddd3d39f
Yixing Lao <yixing.lao@gmail.com>
2021-12-10 02:14:25 -0800

    simplify boring flags

--- 3rdparty/boringssl/boringssl.cmake
+++ 3rdparty/boringssl/boringssl.cmake
@@ -13,17 +13,13 @@ ExternalProject_Add(
     URL_HASH SHA256=54d51f4873d28d5e1eb6f99a37625d3d47b1ccd1f0a93453f3871f8b5c8ad207
     DOWNLOAD_DIR "${OPEN3D_THIRD_PARTY_DOWNLOAD_DIR}/boringssl"
     UPDATE_COMMAND ""
-    CMAKE_ARGS
-        -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
-        -DBUILD_SHARED_LIBS=OFF
-        ${ExternalProject_CMAKE_ARGS_hidden}
-        -DCMAKE_BUILD_TYPE=Debug
+    CMAKE_ARGS ${ExternalProject_CMAKE_ARGS_hidden}
     INSTALL_COMMAND ${CMAKE_COMMAND} -E copy_directory <SOURCE_DIR>/include <INSTALL_DIR>/include
     COMMAND ${CMAKE_COMMAND} -E copy <INSTALL_DIR>/src/ext_boringssl-build/ssl/libssl.a <INSTALL_DIR>/build/lib/libssl.a
     COMMAND ${CMAKE_COMMAND} -E copy <INSTALL_DIR>/src/ext_boringssl-build/crypto/libcrypto.a <INSTALL_DIR>/build/lib/libcrypto.a
 )
 
-#-DCMAKE_BUILD_TYPE=Release 
+#-DCMAKE_BUILD_TYPE=Release
 #-DOPENSSL_SMALL=1
 #-DBORINGSSL_DISPATCH_TEST=OFF
 # remove_definitions(-Werror=unused-result)
