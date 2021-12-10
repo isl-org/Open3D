faa51766b79c3705ee09b79c0fbfc52f96171251
Yixing Lao <yixing.lao@gmail.com>
2021-12-10 02:43:38 -0800

    only build ssl and crypto

--- 3rdparty/boringssl/boringssl.cmake
+++ 3rdparty/boringssl/boringssl.cmake
@@ -13,15 +13,11 @@ ExternalProject_Add(
     URL_HASH SHA256=54d51f4873d28d5e1eb6f99a37625d3d47b1ccd1f0a93453f3871f8b5c8ad207
     DOWNLOAD_DIR "${OPEN3D_THIRD_PARTY_DOWNLOAD_DIR}/boringssl"
     CMAKE_ARGS ${ExternalProject_CMAKE_ARGS_hidden}
+    BUILD_COMMAND ${CMAKE_COMMAND} --build . --config ${CMAKE_BUILD_TYPE} --target ssl crypto
     INSTALL_COMMAND ${CMAKE_COMMAND} -E copy_directory <SOURCE_DIR>/include <INSTALL_DIR>/include
     COMMAND ${CMAKE_COMMAND} -E copy <INSTALL_DIR>/src/ext_boringssl-build/ssl/libssl.a <INSTALL_DIR>/lib/libssl.a
     COMMAND ${CMAKE_COMMAND} -E copy <INSTALL_DIR>/src/ext_boringssl-build/crypto/libcrypto.a <INSTALL_DIR>/lib/libcrypto.a
 )
-
-#-DCMAKE_BUILD_TYPE=Release
-#-DOPENSSL_SMALL=1
-#-DBORINGSSL_DISPATCH_TEST=OFF
-# remove_definitions(-Werror=unused-result)
 
 set(BORINGSSL_BUILD_BYPRODUCTS "<INSTALL_DIR>/src/ext_boringssl-build/ssl/libssl.a"
                                "<INSTALL_DIR>/src/ext_boringssl-build/crypto/libcryp.a"
