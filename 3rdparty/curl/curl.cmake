8b05811a1eebd9a0ff5af8b61e69d1de0dc39c49
Yixing Lao <yixing.lao@gmail.com>
2021-12-12 22:46:57 -0800

    direct makefile does not work

--- 3rdparty/curl/curl.cmake
+++ 3rdparty/curl/curl.cmake
@@ -1,4 +1,6 @@
 include(ExternalProject)
+include(ProcessorCount)
+ProcessorCount(NPROC)
 
 if(NOT DEFINED OPENSSL_ROOT_DIR_FOR_CURL)
     message(FATAL_ERROR "OPENSSL_ROOT_DIR_FOR_CURL not set. "
@@ -12,15 +14,11 @@ if(NOT MSVC)
         URL https://github.com/curl/curl/releases/download/curl-7_79_1/curl-7.79.1.tar.gz
         URL_HASH SHA256=370b11201349816287fb0ccc995e420277fbfcaf76206e309b3f60f0eda090c2
         DOWNLOAD_DIR "${OPEN3D_THIRD_PARTY_DOWNLOAD_DIR}/curl"
-        CMAKE_ARGS
-            -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
-            -DBUILD_SHARED_LIBS=OFF
-            -DBUILD_CURL_EXE=OFF
-            -DBUILD_TESTING=OFF
-            -DCURL_DISABLE_LDAP=ON
-            -DCURL_DISABLE_LDAPS=ON
-            -DOPENSSL_ROOT_DIR=${OPENSSL_ROOT_DIR_FOR_CURL}
-            ${ExternalProject_CMAKE_ARGS_hidden}
+        UPDATE_COMMAND ""
+        BUILD_IN_SOURCE TRUE
+        CONFIGURE_COMMAND ./configure --prefix=<INSTALL_DIR> --with-openssl=${OPENSSL_ROOT_DIR_FOR_CURL}
+        BUILD_COMMAND make CC="${CMAKE_C_COMPILER}" -j${NPROC}
+        INSTALL_COMMAND make CC="${CMAKE_C_COMPILER}" install
         BUILD_BYPRODUCTS
             <INSTALL_DIR>/${Open3D_INSTALL_LIB_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}curl${CMAKE_STATIC_LIBRARY_SUFFIX}
     )
