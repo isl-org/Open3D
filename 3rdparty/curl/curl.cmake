a8220318c2b2ede9132e91e1c2144b4539f66595
Yixing Lao <yixing.lao@gmail.com>
2021-12-12 22:36:49 -0800

    separte codepath for msvc for curl

--- 3rdparty/curl/curl.cmake
+++ 3rdparty/curl/curl.cmake
@@ -5,32 +5,47 @@ if(NOT DEFINED OPENSSL_ROOT_DIR_FOR_CURL)
                         "Please include openssl.cmake before including this file.")
 endif()
 
-ExternalProject_Add(
-    ext_curl
-    PREFIX curl
-    URL https://github.com/curl/curl/releases/download/curl-7_79_1/curl-7.79.1.tar.gz
-    URL_HASH SHA256=370b11201349816287fb0ccc995e420277fbfcaf76206e309b3f60f0eda090c2
-    DOWNLOAD_DIR "${OPEN3D_THIRD_PARTY_DOWNLOAD_DIR}/curl"
-    CMAKE_ARGS
-        -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
-        -DBUILD_SHARED_LIBS=OFF
-        -DBUILD_CURL_EXE=OFF
-        -DBUILD_TESTING=OFF
-        -DCURL_DISABLE_LDAP=ON
-        -DCURL_DISABLE_LDAPS=ON
-        -DOPENSSL_ROOT_DIR=${OPENSSL_ROOT_DIR_FOR_CURL}
-        ${ExternalProject_CMAKE_ARGS_hidden}
-    BUILD_BYPRODUCTS
-        <INSTALL_DIR>/${Open3D_INSTALL_LIB_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}curl${CMAKE_STATIC_LIBRARY_SUFFIX}
-)
-
-add_dependencies(ext_curl ext_openssl)
-
-ExternalProject_Get_Property(ext_curl INSTALL_DIR)
-set(CURL_INCLUDE_DIRS ${INSTALL_DIR}/include/) # "/" is critical.
-set(CURL_LIB_DIR ${INSTALL_DIR}/${Open3D_INSTALL_LIB_DIR})
-if(MSVC)
-    set(CURL_LIBRARIES curl$<$<CONFIG:Debug>:d>)
+if(NOT MSVC)
+    ExternalProject_Add(
+        ext_curl
+        PREFIX curl
+        URL https://github.com/curl/curl/releases/download/curl-7_79_1/curl-7.79.1.tar.gz
+        URL_HASH SHA256=370b11201349816287fb0ccc995e420277fbfcaf76206e309b3f60f0eda090c2
+        DOWNLOAD_DIR "${OPEN3D_THIRD_PARTY_DOWNLOAD_DIR}/curl"
+        CMAKE_ARGS
+            -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
+            -DBUILD_SHARED_LIBS=OFF
+            -DBUILD_CURL_EXE=OFF
+            -DBUILD_TESTING=OFF
+            -DCURL_DISABLE_LDAP=ON
+            -DCURL_DISABLE_LDAPS=ON
+            -DOPENSSL_ROOT_DIR=${OPENSSL_ROOT_DIR_FOR_CURL}
+            ${ExternalProject_CMAKE_ARGS_hidden}
+        BUILD_BYPRODUCTS
+            <INSTALL_DIR>/${Open3D_INSTALL_LIB_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}curl${CMAKE_STATIC_LIBRARY_SUFFIX}
+    )
 else()
-    set(CURL_LIBRARIES curl)
+    ExternalProject_Add(
+        ext_curl
+        PREFIX curl
+        URL https://github.com/curl/curl/releases/download/curl-7_79_1/curl-7.79.1.tar.gz
+        URL_HASH SHA256=370b11201349816287fb0ccc995e420277fbfcaf76206e309b3f60f0eda090c2
+        DOWNLOAD_DIR "${OPEN3D_THIRD_PARTY_DOWNLOAD_DIR}/curl"
+        CMAKE_ARGS
+            -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
+            -DBUILD_SHARED_LIBS=OFF
+            -DBUILD_CURL_EXE=OFF
+            -DBUILD_TESTING=OFF
+            -DCURL_DISABLE_LDAP=ON
+            -DCURL_DISABLE_LDAPS=ON
+            -DOPENSSL_ROOT_DIR=${OPENSSL_ROOT_DIR_FOR_CURL}
+            ${ExternalProject_CMAKE_ARGS_hidden}
+        BUILD_BYPRODUCTS
+            <INSTALL_DIR>/${Open3D_INSTALL_LIB_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}curl_static${CMAKE_STATIC_LIBRARY_SUFFIX}
+    )
+    add_dependencies(ext_curl ext_openssl)
+    ExternalProject_Get_Property(ext_curl INSTALL_DIR)
+    set(CURL_INCLUDE_DIRS ${INSTALL_DIR}/include/) # "/" is critical.
+    set(CURL_LIB_DIR ${INSTALL_DIR}/${Open3D_INSTALL_LIB_DIR})
+    set(CURL_LIBRARIES curl_static$<$<CONFIG:Debug>:d>)
 endif()
