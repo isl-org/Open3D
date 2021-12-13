6468ea1cc099cd8221952330d3b9c3d7a67022dd
Yixing Lao <yixing.lao@gmail.com>
2021-12-12 16:43:52 -0800

    standarize library names

--- 3rdparty/boringssl/boringssl.cmake
+++ 3rdparty/boringssl/boringssl.cmake
@@ -1,5 +1,8 @@
 include(ExternalProject)
 
+set(libssl_name    ${CMAKE_STATIC_LIBRARY_PREFIX}ssl${CMAKE_STATIC_LIBRARY_SUFFIX})
+set(libcrypto_name ${CMAKE_STATIC_LIBRARY_PREFIX}crypto${CMAKE_STATIC_LIBRARY_SUFFIX})
+
 ExternalProject_Add(
     ext_boringssl
     PREFIX boringssl
@@ -8,13 +11,11 @@ ExternalProject_Add(
     DOWNLOAD_DIR "${OPEN3D_THIRD_PARTY_DOWNLOAD_DIR}/boringssl"
     CMAKE_ARGS ${ExternalProject_CMAKE_ARGS_hidden}
     BUILD_COMMAND ${CMAKE_COMMAND} --build . --config ${CMAKE_BUILD_TYPE} --target ssl crypto
-    INSTALL_COMMAND ${CMAKE_COMMAND} -E copy_directory <SOURCE_DIR>/include <INSTALL_DIR>/include
-    COMMAND ${CMAKE_COMMAND} -E copy <BINARY_DIR>/ssl/libssl.a <INSTALL_DIR>/lib/libssl.a
-    COMMAND ${CMAKE_COMMAND} -E copy <BINARY_DIR>/crypto/libcrypto.a <INSTALL_DIR>/lib/libcrypto.a
-)
-
-set(BORINGSSL_BUILD_BYPRODUCTS "<INSTALL_DIR>/src/ext_boringssl-build/ssl/libssl.a"
-                               "<INSTALL_DIR>/src/ext_boringssl-build/crypto/libcryp.a"
+    INSTALL_COMMAND ${CMAKE_COMMAND} -E copy_directory <SOURCE_DIR>/include        <INSTALL_DIR>/include
+    COMMAND         ${CMAKE_COMMAND} -E copy <BINARY_DIR>/ssl/${libssl_name}       <INSTALL_DIR>/${Open3D_INSTALL_LIB_DIR}/${libssl_name}
+    COMMAND         ${CMAKE_COMMAND} -E copy <BINARY_DIR>/crypto/${libcrypto_name} <INSTALL_DIR>/${Open3D_INSTALL_LIB_DIR}/${libcrypto_name}
+    BUILD_BYPRODUCTS <INSTALL_DIR>/${Open3D_INSTALL_LIB_DIR}/${libssl_name}
+                     <INSTALL_DIR>/${Open3D_INSTALL_LIB_DIR}/${libcrypto_name}
 )
 
 ExternalProject_Get_Property(ext_boringssl INSTALL_DIR)
