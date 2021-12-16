c90a392b64bfa463f7a534ebba27dae18c52d30c
Yixing Lao <yixing.lao@gmail.com>
2021-12-15 21:22:13 -0800

    use downloaded boringssl when webrtc is off

--- 3rdparty/openssl/openssl.cmake
+++ 3rdparty/openssl/openssl.cmake
@@ -60,30 +60,48 @@ else()
     # set(OPENSSL_ROOT_DIR_FOR_CURL ${INSTALL_DIR})
 
     # We use OpenSSL as it does not require golang to be installed.
-    include(ProcessorCount)
-    ProcessorCount(NPROC)
+    # include(ProcessorCount)
+    # ProcessorCount(NPROC)
+
+    # ExternalProject_Add(
+    #     ext_openssl
+    #     PREFIX openssl
+    #     URL https://github.com/openssl/openssl/archive/refs/tags/OpenSSL_1_1_1k.tar.gz
+    #     URL_HASH SHA256=b92f9d3d12043c02860e5e602e50a73ed21a69947bcc74d391f41148e9f6aa95
+    #     DOWNLOAD_DIR "${OPEN3D_THIRD_PARTY_DOWNLOAD_DIR}/openssl"
+    #     UPDATE_COMMAND ""
+    #     BUILD_IN_SOURCE TRUE
+    #     CONFIGURE_COMMAND ./config --prefix=<INSTALL_DIR> --openssldir=<INSTALL_DIR>
+    #     BUILD_COMMAND   make CC="${CMAKE_C_COMPILER}" -j${NPROC}
+    #     INSTALL_COMMAND make CC="${CMAKE_C_COMPILER}" install_sw
+    # )
+
+    # set(OPENSSL_BUILD_BYPRODUCTS "<INSTALL_DIR>/${Open3D_INSTALL_LIB_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}ssl${CMAKE_STATIC_LIBRARY_SUFFIX}"
+    #                              "<INSTALL_DIR>/${Open3D_INSTALL_LIB_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}crypto${CMAKE_STATIC_LIBRARY_SUFFIX}"
+    # )
+
+    # ExternalProject_Get_Property(ext_openssl INSTALL_DIR)
+    # set(OPENSSL_INCLUDE_DIRS ${INSTALL_DIR}/include/ ${INSTALL_DIR}/src/ext_openssl/) # "/" is critical.
+    # set(OPENSSL_LIB_DIR ${INSTALL_DIR}/${Open3D_INSTALL_LIB_DIR})
+    # set(OPENSSL_LIBRARIES ssl crypto)
+    # set(OPENSSL_ROOT_DIR_FOR_CURL ${INSTALL_DIR})
 
     ExternalProject_Add(
         ext_openssl
         PREFIX openssl
-        URL https://github.com/openssl/openssl/archive/refs/tags/OpenSSL_1_1_1k.tar.gz
-        URL_HASH SHA256=b92f9d3d12043c02860e5e602e50a73ed21a69947bcc74d391f41148e9f6aa95
+        URL https://github.com/isl-org/open3d_downloads/releases/download/boringssl-bin/boringssl_edfe413_linux_x86_64.tar.gz
         DOWNLOAD_DIR "${OPEN3D_THIRD_PARTY_DOWNLOAD_DIR}/openssl"
         UPDATE_COMMAND ""
-        BUILD_IN_SOURCE TRUE
-        CONFIGURE_COMMAND ./config --prefix=<INSTALL_DIR> --openssldir=<INSTALL_DIR>
-        BUILD_COMMAND   make CC="${CMAKE_C_COMPILER}" -j${NPROC}
-        INSTALL_COMMAND make CC="${CMAKE_C_COMPILER}" install_sw
+        CONFIGURE_COMMAND ""
+        BUILD_COMMAND ""
+        INSTALL_COMMAND ""
+        BUILD_BYPRODUCTS ""
     )
 
-    set(OPENSSL_BUILD_BYPRODUCTS "<INSTALL_DIR>/${Open3D_INSTALL_LIB_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}ssl${CMAKE_STATIC_LIBRARY_SUFFIX}"
-                                 "<INSTALL_DIR>/${Open3D_INSTALL_LIB_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}crypto${CMAKE_STATIC_LIBRARY_SUFFIX}"
-    )
-
-    ExternalProject_Get_Property(ext_openssl INSTALL_DIR)
-    set(OPENSSL_INCLUDE_DIRS ${INSTALL_DIR}/include/ ${INSTALL_DIR}/src/ext_openssl/) # "/" is critical.
-    set(OPENSSL_LIB_DIR ${INSTALL_DIR}/${Open3D_INSTALL_LIB_DIR})
+    ExternalProject_Get_Property(ext_openssl SOURCE_DIR)
+    set(OPENSSL_ROOT_DIR_FOR_CURL ${SOURCE_DIR})
+    set(OPENSSL_INCLUDE_DIRS ${OPENSSL_ROOT_DIR_FOR_CURL}/include/) # "/" is critical.
+    set(OPENSSL_LIB_DIR ${OPENSSL_ROOT_DIR_FOR_CURL}/lib)
     set(OPENSSL_LIBRARIES ssl crypto)
-    set(OPENSSL_ROOT_DIR_FOR_CURL ${INSTALL_DIR})
 endif()
 
