f8342f6e967b9c9f583eae90ae062683eac517ce
Yixing Lao <yixing.lao@gmail.com>
2021-12-17 03:03:40 -0800

    debug and release in one tar

--- 3rdparty/openssl/build_boringssl.ps1
+++ 3rdparty/openssl/build_boringssl.ps1
@@ -15,6 +15,7 @@ ${script_dir} = "${PSScriptRoot}"
 ${install_dir} = "${script_dir}\boringssl_install"
 ${boringssl_dir} = "${script_dir}\boringssl"
 ${boringssl_build_dir} = "${script_dir}\boringssl\build"
+${tar_name} = "boringssl_${boringssl_commit_short}_win_${env:PROCESSOR_ARCHITECTURE}.tar.gz".ToLower()
 
 function Remove-Dir($dir_path) {
     # https://stackoverflow.com/a/9012108/1255535
@@ -42,24 +43,19 @@ cmake -DCMAKE_OSX_DEPLOYMENT_TARGET=10.14 `
       -DCMAKE_C_VISIBILITY_PRESET=hidden `
       -DCMAKE_VISIBILITY_INLINES_HIDDEN=ON `
       ..
-
-# Release
-${tar_name} = "boringssl_${boringssl_commit_short}_win_release_${env:PROCESSOR_ARCHITECTURE}.tar.gz".ToLower()
 cmake --build . --config Release --parallel ${NPROC} --target ssl crypto
-cmake -E copy_directory ..\include                ${install_dir}\include
-cmake -E copy           ssl\Release\ssl.lib       ${install_dir}\lib\ssl.lib
-cmake -E copy           crypto\Release\crypto.lib ${install_dir}\lib\crypto.lib
-cd ${script_dir}
-tar -C ${install_dir} -czvf ${tar_name} include lib
-
-# Debug
-${tar_name} = "boringssl_${boringssl_commit_short}_win_debug_${env:PROCESSOR_ARCHITECTURE}.tar.gz".ToLower()
 cmake --build . --config Debug --parallel ${NPROC} --target ssl crypto
-cmake -E copy_directory ..\include                ${install_dir}\include
-cmake -E copy           ssl\Debug\ssl.lib       ${install_dir}\lib\ssl.lib
-cmake -E copy           crypto\Debug\crypto.lib ${install_dir}\lib\crypto.lib
+
+cmake -E copy_directory ..\include                ${install_dir}\Release\include
+cmake -E copy           ssl\Release\ssl.lib       ${install_dir}\Release\lib\ssl.lib
+cmake -E copy           crypto\Release\crypto.lib ${install_dir}\Release\lib\crypto.lib
+
+cmake -E copy_directory ..\include                ${install_dir}\Debug\include
+cmake -E copy           ssl\Debug\ssl.lib         ${install_dir}\Debug\lib\ssl.lib
+cmake -E copy           crypto\Debug\crypto.lib   ${install_dir}\Debug\lib\crypto.lib
+
 cd ${script_dir}
-tar -C ${install_dir} -czvf ${tar_name} include lib
+tar -C ${install_dir} -czvf ${tar_name} Release Debug
 
 Remove-Dir "${boringssl_dir}"
 Remove-Dir "${install_dir}"
