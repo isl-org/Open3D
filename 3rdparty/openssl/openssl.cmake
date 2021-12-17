2d3973cc609fc555d613c0d365e6684fd575c5a8
Yixing Lao <yixing.lao@gmail.com>
2021-12-17 00:42:20 -0800

    select correct binary for ARM (apple and linux)

--- 3rdparty/openssl/openssl.cmake
+++ 3rdparty/openssl/openssl.cmake
@@ -1,21 +1,24 @@
 include(ExternalProject)
 
-set(BORINGSSL_VER edfe413)
-if (APPLE)
-    set(BORINGSSL_URL
-        https://github.com/isl-org/open3d_downloads/releases/download/boringssl-bin/boringssl_${BORINGSSL_VER}_darwin_x86_64.tar.gz
-    )
-    set(BORINGSSL_SHA256 6d5d6372d9c448d2443eb52215993912ddc122aa6fa4f8ef6d643a4480e1854b)
+if(APPLE)
+    if(APPLE_AARCH64)
+        set(BORINGSSL_URL https://github.com/isl-org/open3d_downloads/releases/download/boringssl-bin/boringssl_edfe413_darwin_arm64.tar.gz)
+        set(BORINGSSL_SHA256 d0a574782cd1fa1ef4c02a57b96ea7aafef30a3a77b2163c02e9854ad8964d5e)
+    else()
+        set(BORINGSSL_URL https://github.com/isl-org/open3d_downloads/releases/download/boringssl-bin/boringssl_edfe413_darwin_x86_64.tar.gz)
+        set(BORINGSSL_SHA256 6d5d6372d9c448d2443eb52215993912ddc122aa6fa4f8ef6d643a4480e1854b)
+    endif()
 elseif (WIN32)
-    set(BORINGSSL_URL
-        https://github.com/isl-org/open3d_downloads/releases/download/boringssl-bin/boringssl_${BORINGSSL_VER}_win_amd64.tar.gz
-    )
+    set(BORINGSSL_URL https://github.com/isl-org/open3d_downloads/releases/download/boringssl-bin/boringssl_edfe413_win_amd64.tar.gz)
     set(BORINGSSL_SHA256 3baeffb18f2cb3ec9674cbe85415abcdc7a23bc9289aafc6e28eb8656f67136c)
-else()  # Linux
-    set(BORINGSSL_URL
-        https://github.com/isl-org/open3d_downloads/releases/download/boringssl-bin/boringssl_${BORINGSSL_VER}_linux_x86_64.tar.gz
-    )
-    set(BORINGSSL_SHA256 9ad9bab54d8872da9957535c15571d4976c14816f86675de3b15cf35ddb92056)
+else()
+    if (LINUX_AARCH64)
+        set(BORINGSSL_URL https://github.com/isl-org/open3d_downloads/releases/download/boringssl-bin/boringssl_edfe413_linux_aarch64.tar.gz)
+        set(BORINGSSL_SHA256 5f10d8d529094aa8b6d40f7caaead9652a8b4ce7dd2b48e186d1b519a66efd2c)
+    else()
+        set(BORINGSSL_URL https://github.com/isl-org/open3d_downloads/releases/download/boringssl-bin/boringssl_edfe413_linux_x86_64.tar.gz)
+        set(BORINGSSL_SHA256 9ad9bab54d8872da9957535c15571d4976c14816f86675de3b15cf35ddb92056)
+    endif()
 endif()
 
 ExternalProject_Add(
