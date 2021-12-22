1ac0c4a70f88979ceddc88fef7b6e5d6eb81b9d9
Wei Dong <thended@gmail.com>
2021-12-21 19:52:16 -0500

    apply pybind changes

--- cpp/pybind/pipelines/registration/robust_kernels.cpp
+++ cpp/pybind/pipelines/registration/robust_kernels.cpp
@@ -28,8 +28,7 @@
 // Copyright (c) 2020 Ignacio Vizzo, Cyrill Stachniss, University of Bonn.
 // ----------------------------------------------------------------------------
 
-#include <pybind11/attr.h>
-#include <pybind11/pybind11.h>
+#include "pybind/open3d_pybind.h"
 
 #include <memory>
 
