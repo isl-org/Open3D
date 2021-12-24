c3e73cd256ec769f94c6a6c1239f386aef35dc97
Rishabh Singh <rishabh.17iitkgp@gmail.com>
2021-12-24 06:25:50 +0530

    Data downloader, libcurl, and boringssl (#4396)

--- cpp/open3d/data/Downloader.h
+++ cpp/open3d/data/Downloader.h
@@ -24,36 +24,39 @@
 // IN THE SOFTWARE.
 // ----------------------------------------------------------------------------
 
-#include "open3d/data/Dataset.h"
+#pragma once
 
 #include <string>
-
-#include "open3d/utility/FileSystem.h"
-#include "open3d/utility/Logging.h"
 
 namespace open3d {
 namespace data {
 
-std::string LocateDataRoot() {
-    std::string data_root = "";
-    if (const char* env_p = std::getenv("OPEN3D_DATA_ROOT")) {
-        data_root = std::string(env_p);
-    }
-    if (data_root.empty()) {
-        data_root = utility::filesystem::GetHomeDirectory() + "/open3d_data";
-    }
-    return data_root;
-}
+/// \brief Computes SHA256 Hash for the given file.
+/// \param file_path Path to the file.
+std::string GetSHA256(const std::string& file_path);
 
-Dataset::Dataset(const std::string& data_root) {
-    if (data_root.empty()) {
-        data_root_ = LocateDataRoot();
-    } else {
-        data_root_ = data_root;
-    }
-}
-
-std::string Dataset::GetDataRoot() const { return data_root_; }
+/// \brief Function to download the file from URL.
+/// \param url URL for the file to be downloaded.
+/// \param output_file_path Custom directory to download the file. If
+/// directory does not exists, it will be created. If empty string is
+/// passed, the default data-root will be used.
+/// \param output_file_name Name of the downloaded file. If empty string is
+/// passed, the default file name will be used, extracted from the url.
+/// \param always_download If `false`, it will skip download if the file is
+/// present in the given location with given file name and expected SHA256SUM.
+/// It will trigger download if these conditions are not met. If `true`, it will
+/// always trigger download and over-write the file if present. Default: `true`.
+/// \param sha256 SHA256SUM HASH value to verify the file after download. If
+/// empty string is passed, the verification will be skipped. If
+/// `always_download` is set to `false`, then it `SHA256` is a required
+/// parameter.
+/// \param print_progress Display progress bar for download.
+bool DownloadFromURL(const std::string& url,
+                     const std::string& output_file_path = "",
+                     const std::string& output_file_name = "",
+                     const bool always_download = true,
+                     const std::string& sha256 = "",
+                     const bool print_progress = false);
 
 }  // namespace data
 }  // namespace open3d
