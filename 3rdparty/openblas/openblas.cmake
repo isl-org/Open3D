444784aaebed93e3df03755fd995056374ea97a5
Yixing Lao <yixing.lao@gmail.com>
2022-01-03 01:13:58 -0800

    use cmake to build openblas

--- 3rdparty/openblas/openblas.cmake
+++ 3rdparty/openblas/openblas.cmake
@@ -11,9 +11,24 @@ else()
     set(OPENBLAS_TARGET "NEHALEM")
 endif()
 
-set(OPENBLAS_INCLUDE_DIR "${OPENBLAS_INSTALL_PREFIX}/include/") # The "/"" is critical, see open3d_import_3rdparty_library.
+set(OPENBLAS_INCLUDE_DIR "${OPENBLAS_INSTALL_PREFIX}/include/openblas/") # The "/"" is critical, see open3d_import_3rdparty_library.
 set(OPENBLAS_LIB_DIR "${OPENBLAS_INSTALL_PREFIX}/lib")
 set(OPENBLAS_LIBRARIES openblas)  # Extends to libopenblas.a automatically.
+
+# ExternalProject_Add(
+#     ext_openblas
+#     PREFIX openblas
+#     URL https://github.com/xianyi/OpenBLAS/archive/refs/tags/v0.3.18.tar.gz
+#     URL_HASH SHA256=1632c1e8cca62d8bed064b37747e331a1796fc46f688626337362bf0d16aeadb
+#     DOWNLOAD_DIR "${OPEN3D_THIRD_PARTY_DOWNLOAD_DIR}/openblas"
+#     UPDATE_COMMAND ""
+#     CONFIGURE_COMMAND ""
+#     BUILD_COMMAND $(MAKE) CC="${CMAKE_C_COMPILER}" TARGET=${OPENBLAS_TARGET} NO_SHARED=1 LIBNAME=CUSTOM_LIB_NAME -j ${NPROC}
+#     BUILD_IN_SOURCE True
+#     INSTALL_COMMAND $(MAKE) install PREFIX=${OPENBLAS_INSTALL_PREFIX} NO_SHARED=1 LIBNAME=CUSTOM_LIB_NAME
+#     COMMAND ${CMAKE_COMMAND} -E rename ${OPENBLAS_LIB_DIR}/CUSTOM_LIB_NAME ${OPENBLAS_LIB_DIR}/libopenblas.a
+#     BUILD_BYPRODUCTS ${OPENBLAS_LIB_DIR}/libopenblas.a
+# )
 
 ExternalProject_Add(
     ext_openblas
@@ -21,12 +36,9 @@ ExternalProject_Add(
     URL https://github.com/xianyi/OpenBLAS/archive/refs/tags/v0.3.18.tar.gz
     URL_HASH SHA256=1632c1e8cca62d8bed064b37747e331a1796fc46f688626337362bf0d16aeadb
     DOWNLOAD_DIR "${OPEN3D_THIRD_PARTY_DOWNLOAD_DIR}/openblas"
-    UPDATE_COMMAND ""
-    CONFIGURE_COMMAND ""
-    BUILD_COMMAND $(MAKE) CC="${CMAKE_C_COMPILER}" TARGET=${OPENBLAS_TARGET} NO_SHARED=1 LIBNAME=CUSTOM_LIB_NAME -j ${NPROC}
-    BUILD_IN_SOURCE True
-    INSTALL_COMMAND $(MAKE) install PREFIX=${OPENBLAS_INSTALL_PREFIX} NO_SHARED=1 LIBNAME=CUSTOM_LIB_NAME
-    COMMAND ${CMAKE_COMMAND} -E rename ${OPENBLAS_LIB_DIR}/CUSTOM_LIB_NAME ${OPENBLAS_LIB_DIR}/libopenblas.a
+    CMAKE_ARGS
+        ${ExternalProject_CMAKE_ARGS}
+        -DCMAKE_INSTALL_PREFIX=${OPENBLAS_INSTALL_PREFIX}
     BUILD_BYPRODUCTS ${OPENBLAS_LIB_DIR}/libopenblas.a
 )
 
