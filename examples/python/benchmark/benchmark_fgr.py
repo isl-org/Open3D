4ba78dd230424daa5436bae0e6162e1ca3b779c9
Rishabh Singh <rishabh.17iitkgp@gmail.com>
2022-01-09 23:35:31 +0530

    fix benchmark_fgr.py (#4566)

--- examples/python/benchmark/benchmark_fgr.py
+++ examples/python/benchmark/benchmark_fgr.py
@@ -35,7 +35,6 @@ import numpy as np
 from file import *
 from visualization import *
 from downloader import *
-from fast_global_registration import *
 from trajectory_io import *
 
 do_visualization = False
@@ -47,6 +46,35 @@ def get_ply_path(dataset_name, id):
 
 def get_log_path(dataset_name):
     return "%s/fgr_%s.log" % (dataset_path, dataset_name)
+
+
+def execute_fast_global_registration(source_down, target_down, source_fpfh,
+                                     target_fpfh, voxel_size):
+    distance_threshold = voxel_size * 0.5
+    print(":: Apply fast global registration with distance threshold %.3f" \
+            % distance_threshold)
+    result = o3d.pipelines.registration.registration_fgr_based_on_feature_matching(
+        source_down, target_down, source_fpfh, target_fpfh,
+        o3d.pipelines.registration.FastGlobalRegistrationOption(
+            maximum_correspondence_distance=distance_threshold))
+    return result
+
+
+def preprocess_point_cloud(pcd, voxel_size):
+    print(":: Downsample with a voxel size %.3f." % voxel_size)
+    pcd_down = pcd.voxel_down_sample(voxel_size)
+
+    radius_normal = voxel_size * 2
+    print(":: Estimate normal with search radius %.3f." % radius_normal)
+    pcd_down.estimate_normals(
+        o3d.geometry.KDTreeSearchParamHybrid(radius=radius_normal, max_nn=30))
+
+    radius_feature = voxel_size * 5
+    print(":: Compute FPFH feature with search radius %.3f." % radius_feature)
+    pcd_fpfh = o3d.pipelines.registration.compute_fpfh_feature(
+        pcd_down,
+        o3d.geometry.KDTreeSearchParamHybrid(radius=radius_feature, max_nn=100))
+    return pcd_down, pcd_fpfh
 
 
 dataset_path = 'testdata'
