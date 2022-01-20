7814b190e86f4844b3f708a4eea0a3dcde2c023c
reyanshsolis <reyanshsolis@gmail.com>
2022-01-20 16:19:10 +0530

    dataset submodule added

---
+++ cpp/pybind/data/data.cpp
@@ -0,0 +1,76 @@
+// ----------------------------------------------------------------------------
+// -                        Open3D: www.open3d.org                            -
+// ----------------------------------------------------------------------------
+// The MIT License (MIT)
+//
+// Copyright (c) 2018-2021 www.open3d.org
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+// ----------------------------------------------------------------------------
+
+#include "pybind/data/data.h"
+
+#include "open3d/data/Dataset.h"
+#include "pybind/data/data_trampoline.h"
+#include "pybind/docstring.h"
+#include "pybind/open3d_pybind.h"
+
+namespace open3d {
+namespace data {
+
+void pybind_data_classes(py::module& m) {
+//     // open3d.data.Dataset
+//     py::class_<Dataset, PyDataset<Dataset>, std::shared_ptr<Dataset>> dataset(
+//             m, "Dataset", "The base dataset class.");
+//     dataset.def(py::init<const std::string&, const std::string&>(), "prefix"_a,
+//                 "data_root"_a = "")
+//             .def("get_data_root", &Dataset::GetDataRoot,
+//                  "Returns data root path.")
+//             .def("get_extract_dir", &Dataset::GetExtractDir,
+//                  "Returns data extract directory path.",
+//                  "relative_to_data_root"_a = false)
+//             .def("get_download_dir", &Dataset::GetDownloadDir,
+//                  "Returns data download directory path.",
+//                  "relative_to_data_root"_a = false)
+//             .def("get_help", &Dataset::GetHelp,
+//                  "Returns string of helpful documentation.");
+//     docstring::ClassMethodDocInject(m, "Dataset", "get_data_root");
+//     docstring::ClassMethodDocInject(m, "Dataset", "get_extract_dir");
+//     docstring::ClassMethodDocInject(m, "Dataset", "get_download_dir");
+//     docstring::ClassMethodDocInject(m, "Dataset", "get_help");
+
+//     // open3d.data.SimpleDataset
+//     py::class_<SimpleDataset, PySimpleDataset<SimpleDataset>,
+//                std::shared_ptr<SimpleDataset>, Dataset>
+//             simple_dataset(m, "SimpleDataset", "Simple dataset class.");
+//     simple_dataset.def(
+//             py::init<const std::string&, const std::vector<std::string>&,
+//                      const std::string&, const bool, const std::string&>(),
+//             "prefix"_a, "urls"_a, "md5"_a, "no_extract"_a = false,
+//             "data_root"_a = "");
+}
+
+void pybind_data(py::module& m) {
+    py::module m_submodule = m.def_submodule("data", "Data handling module.");
+    pybind_data_classes(m_submodule);
+    dataset::pybind_dataset(m_submodule);
+}
+
+}  // namespace data
+}  // namespace open3d
---
+++ cpp/pybind/data/dataset.cpp
@@ -0,0 +1,115 @@
+// ----------------------------------------------------------------------------
+// -                        Open3D: www.open3d.org                            -
+// ----------------------------------------------------------------------------
+// The MIT License (MIT)
+//
+// Copyright (c) 2018-2021 www.open3d.org
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+// ----------------------------------------------------------------------------
+
+#include "open3d/data/Dataset.h"
+
+#include <string>
+
+#include "pybind/data/data.h"
+#include "pybind/data/data_trampoline.h"
+#include "pybind/docstring.h"
+
+namespace open3d {
+namespace data {
+namespace dataset {
+
+void pybind_sample_pcd_fragments(py::module& m) {
+//     py::class_<SamplePCDFragments, PySimpleDataset<SamplePCDFragments>,
+//                std::shared_ptr<SamplePCDFragments>, SimpleDataset>
+//             sample_pcd_fragments(m, "SamplePCDFragments",
+//                                  "SamplePCDFragments dataset.");
+//     //     py::detail::bind_default_constructor<SamplePCDFragments>(
+//     //             sample_pcd_fragments);
+//     //     py::detail::bind_copy_functions<SamplePCDFragments>(sample_pcd_fragments);
+
+//     sample_pcd_fragments
+//             .def(py::init<const std::string&, const std::string&>(),
+//                  "prefix"_a = "SamplePCDFragments", "data_root"_a = "")
+//             .def_readwrite("path_to_fragments",
+//                            &SamplePCDFragments::path_to_fragments_,
+//                            "List of path to PCD point-cloud fragments. "
+//                            "path_to_fragments_[x] returns path to "
+//                            "`cloud_bin_x.pcd` where x is from 0 to 2.");
+//     docstring::ClassMethodDocInject(m, "SamplePCDFragments",
+//                                     "path_to_fragments");
+}
+
+void pybind_redwood_living_room_fragments(py::module& m) {
+//     py::class_<RedwoodLivingRoomFragments,
+//                PySimpleDataset<RedwoodLivingRoomFragments>,
+//                std::shared_ptr<RedwoodLivingRoomFragments>, SimpleDataset>
+//             redwood_living_room_fragments(
+//                     m, "RedwoodLivingRoomFragments",
+//                     "RedwoodLivingRoomFragments dataset.");
+//     //     py::detail::bind_default_constructor<RedwoodLivingRoomFragments>(
+//     //             redwood_living_room_fragments);
+//     //     py::detail::bind_copy_functions<RedwoodLivingRoomFragments>(
+//     //             redwood_living_room_fragments);
+
+//     redwood_living_room_fragments
+//             .def(py::init<const std::string&, const std::string&>(),
+//                  "prefix"_a = "RedwoodLivingRoomFragments", "data_root"_a = "")
+//             .def_readwrite("path_to_fragments",
+//                            &RedwoodLivingRoomFragments::path_to_fragments_,
+//                            "List of path to PCD point-cloud fragments. "
+//                            "path_to_fragments_[x] returns path to "
+//                            "`cloud_bin_x.pcd` where x is from 0 to 56.");
+//     docstring::ClassMethodDocInject(m, "RedwoodLivingRoomFragments",
+//                                     "path_to_fragments");
+}
+
+void pybind_redwood_office_fragments(py::module& m) {
+//     py::class_<RedwoodOfficeFragments, PySimpleDataset<RedwoodOfficeFragments>,
+//                std::shared_ptr<RedwoodOfficeFragments>, SimpleDataset>
+//             redwood_office_fragments(m, "RedwoodOfficeFragments",
+//                                      "RedwoodOfficeFragments dataset.");
+//     //     py::detail::bind_default_constructor<RedwoodOfficeFragments>(
+//     //             redwood_office_fragments);
+//     //     py::detail::bind_copy_functions<RedwoodOfficeFragments>(
+//     //             redwood_office_fragments);
+
+//     redwood_office_fragments
+//             .def(py::init<const std::string&, const std::string&>(),
+//                  "prefix"_a = "RedwoodOfficeFragments", "data_root"_a = "")
+//             .def_readwrite("path_to_fragments",
+//                            &RedwoodOfficeFragments::path_to_fragments_,
+//                            "List of path to PCD point-cloud fragments. "
+//                            "path_to_fragments_[x] returns path to "
+//                            "`cloud_bin_x.pcd` where x is from 0 to 51.");
+//     docstring::ClassMethodDocInject(m, "RedwoodOfficeFragments",
+//                                     "path_to_fragments");
+}
+
+void pybind_dataset(py::module& m) {
+    py::module m_dataset = m.def_submodule("dataset", "Dataset module.");
+    // pybind_sample_pcd_fragments(m_dataset);
+    // pybind_redwood_living_room_fragments(m_dataset);
+    // pybind_redwood_office_fragments(m_dataset);
+}
+
+}  // namespace dataset
+}  // namespace data
+}  // namespace open3d
