e9c4d16a796216fb31d732480543bb356d618db4
Yixing Lao <yixing.lao@gmail.com>
2022-01-21 00:01:00 -0800

    upgrade to 0.3.19

--- 3rdparty/openblas/openblas.cmake
+++ 3rdparty/openblas/openblas.cmake
@@ -15,8 +15,8 @@ set(OPENBLAS_LIBRARIES openblas)  # Extends to libopenblas.a automatically.
 ExternalProject_Add(
     ext_openblas
     PREFIX openblas
-    URL https://github.com/xianyi/OpenBLAS/archive/refs/tags/v0.3.18.tar.gz
-    URL_HASH SHA256=1632c1e8cca62d8bed064b37747e331a1796fc46f688626337362bf0d16aeadb
+    URL https://github.com/xianyi/OpenBLAS/releases/download/v0.3.19/OpenBLAS-0.3.19.tar.gz
+    URL_HASH SHA256=947f51bfe50c2a0749304fbe373e00e7637600b0a47b78a51382aeb30ca08562
     DOWNLOAD_DIR "${OPEN3D_THIRD_PARTY_DOWNLOAD_DIR}/openblas"
     CMAKE_ARGS
         ${ExternalProject_CMAKE_ARGS}
