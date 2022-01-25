b19b724bb925f0a797d4196432891d429e10830b
Shreyansh Darshan <shreyanshdarshan1@gmail.com>
2022-01-25 22:29:46 +0530

    Remove import in benchmark_ransac

--- examples/python/benchmark/benchmark_ransac.py
+++ examples/python/benchmark/benchmark_ransac.py
@@ -27,7 +27,6 @@
 import os
 import sys
 import numpy as np
-from visualization import *
 
 pyexample_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(pyexample_path)
