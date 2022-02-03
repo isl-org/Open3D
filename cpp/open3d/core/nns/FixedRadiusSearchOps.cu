078de31eb811a6a5e39c4f719749e4b17724cb21
Yixing Lao <yixing.lao@gmail.com>
2022-02-02 16:01:32 -0800

    comment out code

--- cpp/open3d/core/nns/FixedRadiusSearchOps.cu
+++ cpp/open3d/core/nns/FixedRadiusSearchOps.cu
@@ -26,9 +26,9 @@
 //
 
 #include "open3d/core/Tensor.h"
-#include "open3d/core/nns/FixedRadiusIndex.h"
-#include "open3d/core/nns/FixedRadiusSearchImpl.cuh"
-#include "open3d/core/nns/NeighborSearchAllocator.h"
+// #include "open3d/core/nns/FixedRadiusIndex.h"
+// #include "open3d/core/nns/FixedRadiusSearchImpl.cuh"
+// #include "open3d/core/nns/NeighborSearchAllocator.h"
 #include "open3d/core/nns/NeighborSearchCommon.h"
 
 namespace open3d {
@@ -42,36 +42,36 @@ void BuildSpatialHashTableCUDA(const Tensor& points,
                                const Tensor& hash_table_splits,
                                Tensor& hash_table_index,
                                Tensor& hash_table_cell_splits) {
-    const cudaStream_t stream = 0;
-    int texture_alignment = 512;
+    //     const cudaStream_t stream = 0;
+    //     int texture_alignment = 512;
 
-    void* temp_ptr = nullptr;
-    size_t temp_size = 0;
+    //     void* temp_ptr = nullptr;
+    //     size_t temp_size = 0;
 
-    open3d::core::nns::impl::BuildSpatialHashTableCUDA(
-            stream, temp_ptr, temp_size, texture_alignment,
-            points.GetShape()[0], points.GetDataPtr<T>(), T(radius),
-            points_row_splits.GetShape()[0],
-            points_row_splits.GetDataPtr<int64_t>(),
-            hash_table_splits.GetDataPtr<uint32_t>(),
-            hash_table_cell_splits.GetShape()[0],
-            hash_table_cell_splits.GetDataPtr<uint32_t>(),
-            hash_table_index.GetDataPtr<uint32_t>());
+    //     open3d::core::nns::impl::BuildSpatialHashTableCUDA(
+    //             stream, temp_ptr, temp_size, texture_alignment,
+    //             points.GetShape()[0], points.GetDataPtr<T>(), T(radius),
+    //             points_row_splits.GetShape()[0],
+    //             points_row_splits.GetDataPtr<int64_t>(),
+    //             hash_table_splits.GetDataPtr<uint32_t>(),
+    //             hash_table_cell_splits.GetShape()[0],
+    //             hash_table_cell_splits.GetDataPtr<uint32_t>(),
+    //             hash_table_index.GetDataPtr<uint32_t>());
 
-    Device device = points.GetDevice();
-    Tensor temp_tensor =
-            Tensor::Empty({int64_t(temp_size)}, Dtype::UInt8, device);
-    temp_ptr = temp_tensor.GetDataPtr();
+    //     Device device = points.GetDevice();
+    //     Tensor temp_tensor =
+    //             Tensor::Empty({int64_t(temp_size)}, Dtype::UInt8, device);
+    //     temp_ptr = temp_tensor.GetDataPtr();
 
-    open3d::core::nns::impl::BuildSpatialHashTableCUDA(
-            stream, temp_ptr, temp_size, texture_alignment,
-            points.GetShape()[0], points.GetDataPtr<T>(), T(radius),
-            points_row_splits.GetShape()[0],
-            points_row_splits.GetDataPtr<int64_t>(),
-            hash_table_splits.GetDataPtr<uint32_t>(),
-            hash_table_cell_splits.GetShape()[0],
-            hash_table_cell_splits.GetDataPtr<uint32_t>(),
-            hash_table_index.GetDataPtr<uint32_t>());
+    //     open3d::core::nns::impl::BuildSpatialHashTableCUDA(
+    //             stream, temp_ptr, temp_size, texture_alignment,
+    //             points.GetShape()[0], points.GetDataPtr<T>(), T(radius),
+    //             points_row_splits.GetShape()[0],
+    //             points_row_splits.GetDataPtr<int64_t>(),
+    //             hash_table_splits.GetDataPtr<uint32_t>(),
+    //             hash_table_cell_splits.GetShape()[0],
+    //             hash_table_cell_splits.GetDataPtr<uint32_t>(),
+    //             hash_table_index.GetDataPtr<uint32_t>());
 }
 
 template <class T>
@@ -90,88 +90,91 @@ void FixedRadiusSearchCUDA(const Tensor& points,
                            Tensor& neighbors_index,
                            Tensor& neighbors_row_splits,
                            Tensor& neighbors_distance) {
-    const cudaStream_t stream = 0;
-    int texture_alignment = 512;
+    //     const cudaStream_t stream = 0;
+    //     int texture_alignment = 512;
 
-    Device device = points.GetDevice();
-    Dtype dtype = points.GetDtype();
+    //     Device device = points.GetDevice();
+    //     Dtype dtype = points.GetDtype();
 
-    NeighborSearchAllocator<T> output_allocator(device);
-    void* temp_ptr = nullptr;
-    size_t temp_size = 0;
+    //     NeighborSearchAllocator<T> output_allocator(device);
+    //     void* temp_ptr = nullptr;
+    //     size_t temp_size = 0;
 
-    open3d::core::nns::impl::FixedRadiusSearchCUDA(
-            stream, temp_ptr, temp_size, texture_alignment,
-            neighbors_row_splits.GetDataPtr<int64_t>(), points.GetShape()[0],
-            points.GetDataPtr<T>(), queries.GetShape()[0],
-            queries.GetDataPtr<T>(), T(radius), points_row_splits.GetShape()[0],
-            points_row_splits.GetDataPtr<int64_t>(),
-            queries_row_splits.GetShape()[0],
-            queries_row_splits.GetDataPtr<int64_t>(),
-            hash_table_splits.GetDataPtr<uint32_t>(),
-            hash_table_cell_splits.GetShape()[0],
-            hash_table_cell_splits.GetDataPtr<uint32_t>(),
-            hash_table_index.GetDataPtr<uint32_t>(), metric, ignore_query_point,
-            return_distances, output_allocator);
+    //     open3d::core::nns::impl::FixedRadiusSearchCUDA(
+    //             stream, temp_ptr, temp_size, texture_alignment,
+    //             neighbors_row_splits.GetDataPtr<int64_t>(),
+    //             points.GetShape()[0], points.GetDataPtr<T>(),
+    //             queries.GetShape()[0], queries.GetDataPtr<T>(), T(radius),
+    //             points_row_splits.GetShape()[0],
+    //             points_row_splits.GetDataPtr<int64_t>(),
+    //             queries_row_splits.GetShape()[0],
+    //             queries_row_splits.GetDataPtr<int64_t>(),
+    //             hash_table_splits.GetDataPtr<uint32_t>(),
+    //             hash_table_cell_splits.GetShape()[0],
+    //             hash_table_cell_splits.GetDataPtr<uint32_t>(),
+    //             hash_table_index.GetDataPtr<uint32_t>(), metric,
+    //             ignore_query_point, return_distances, output_allocator);
 
-    Tensor temp_tensor =
-            Tensor::Empty({int64_t(temp_size)}, Dtype::UInt8, device);
-    temp_ptr = temp_tensor.GetDataPtr();
+    //     Tensor temp_tensor =
+    //             Tensor::Empty({int64_t(temp_size)}, Dtype::UInt8, device);
+    //     temp_ptr = temp_tensor.GetDataPtr();
 
-    open3d::core::nns::impl::FixedRadiusSearchCUDA(
-            stream, temp_ptr, temp_size, texture_alignment,
-            neighbors_row_splits.GetDataPtr<int64_t>(), points.GetShape()[0],
-            points.GetDataPtr<T>(), queries.GetShape()[0],
-            queries.GetDataPtr<T>(), T(radius), points_row_splits.GetShape()[0],
-            points_row_splits.GetDataPtr<int64_t>(),
-            queries_row_splits.GetShape()[0],
-            queries_row_splits.GetDataPtr<int64_t>(),
-            hash_table_splits.GetDataPtr<uint32_t>(),
-            hash_table_cell_splits.GetShape()[0],
-            hash_table_cell_splits.GetDataPtr<uint32_t>(),
-            hash_table_index.GetDataPtr<uint32_t>(), metric, ignore_query_point,
-            return_distances, output_allocator);
+    //     open3d::core::nns::impl::FixedRadiusSearchCUDA(
+    //             stream, temp_ptr, temp_size, texture_alignment,
+    //             neighbors_row_splits.GetDataPtr<int64_t>(),
+    //             points.GetShape()[0], points.GetDataPtr<T>(),
+    //             queries.GetShape()[0], queries.GetDataPtr<T>(), T(radius),
+    //             points_row_splits.GetShape()[0],
+    //             points_row_splits.GetDataPtr<int64_t>(),
+    //             queries_row_splits.GetShape()[0],
+    //             queries_row_splits.GetDataPtr<int64_t>(),
+    //             hash_table_splits.GetDataPtr<uint32_t>(),
+    //             hash_table_cell_splits.GetShape()[0],
+    //             hash_table_cell_splits.GetDataPtr<uint32_t>(),
+    //             hash_table_index.GetDataPtr<uint32_t>(), metric,
+    //             ignore_query_point, return_distances, output_allocator);
 
-    Tensor indices_unsorted = output_allocator.NeighborsIndex();
-    Tensor distances_unsorted = output_allocator.NeighborsDistance();
+    //     Tensor indices_unsorted = output_allocator.NeighborsIndex();
+    //     Tensor distances_unsorted = output_allocator.NeighborsDistance();
 
-    if (!sort) {
-        neighbors_index = indices_unsorted;
-        neighbors_distance = distances_unsorted;
-    } else {
-        // Sort indices & distances.
-        temp_ptr = nullptr;
-        temp_size = 0;
+    //     if (!sort) {
+    //         neighbors_index = indices_unsorted;
+    //         neighbors_distance = distances_unsorted;
+    //     } else {
+    //         // Sort indices & distances.
+    //         temp_ptr = nullptr;
+    //         temp_size = 0;
 
-        int64_t num_indices = indices_unsorted.GetShape()[0];
-        int64_t num_segments = neighbors_row_splits.GetShape()[0] - 1;
-        Tensor indices_sorted =
-                Tensor::Empty({num_indices}, Dtype::Int32, device);
-        Tensor distances_sorted = Tensor::Empty({num_indices}, dtype, device);
+    //         int64_t num_indices = indices_unsorted.GetShape()[0];
+    //         int64_t num_segments = neighbors_row_splits.GetShape()[0] - 1;
+    //         Tensor indices_sorted =
+    //                 Tensor::Empty({num_indices}, Dtype::Int32, device);
+    //         Tensor distances_sorted = Tensor::Empty({num_indices}, dtype,
+    //         device);
 
-        // Determine temp_size for sorting
-        open3d::core::nns::impl::SortPairs(
-                temp_ptr, temp_size, texture_alignment, num_indices,
-                num_segments, neighbors_row_splits.GetDataPtr<int64_t>(),
-                indices_unsorted.GetDataPtr<int32_t>(),
-                distances_unsorted.GetDataPtr<T>(),
-                indices_sorted.GetDataPtr<int32_t>(),
-                distances_sorted.GetDataPtr<T>());
+    //         // Determine temp_size for sorting
+    //         open3d::core::nns::impl::SortPairs(
+    //                 temp_ptr, temp_size, texture_alignment, num_indices,
+    //                 num_segments, neighbors_row_splits.GetDataPtr<int64_t>(),
+    //                 indices_unsorted.GetDataPtr<int32_t>(),
+    //                 distances_unsorted.GetDataPtr<T>(),
+    //                 indices_sorted.GetDataPtr<int32_t>(),
+    //                 distances_sorted.GetDataPtr<T>());
 
-        temp_tensor = Tensor::Empty({int64_t(temp_size)}, Dtype::UInt8, device);
-        temp_ptr = temp_tensor.GetDataPtr();
+    //         temp_tensor = Tensor::Empty({int64_t(temp_size)}, Dtype::UInt8,
+    //         device); temp_ptr = temp_tensor.GetDataPtr();
 
-        // Actually run the sorting.
-        open3d::core::nns::impl::SortPairs(
-                temp_ptr, temp_size, texture_alignment, num_indices,
-                num_segments, neighbors_row_splits.GetDataPtr<int64_t>(),
-                indices_unsorted.GetDataPtr<int32_t>(),
-                distances_unsorted.GetDataPtr<T>(),
-                indices_sorted.GetDataPtr<int32_t>(),
-                distances_sorted.GetDataPtr<T>());
-        neighbors_index = indices_sorted;
-        neighbors_distance = distances_sorted;
-    }
+    //         // Actually run the sorting.
+    //         open3d::core::nns::impl::SortPairs(
+    //                 temp_ptr, temp_size, texture_alignment, num_indices,
+    //                 num_segments, neighbors_row_splits.GetDataPtr<int64_t>(),
+    //                 indices_unsorted.GetDataPtr<int32_t>(),
+    //                 distances_unsorted.GetDataPtr<T>(),
+    //                 indices_sorted.GetDataPtr<int32_t>(),
+    //                 distances_sorted.GetDataPtr<T>());
+    //         neighbors_index = indices_sorted;
+    //         neighbors_distance = distances_sorted;
+    //     }
 }
 
 template <class T>
@@ -188,27 +191,28 @@ void HybridSearchCUDA(const Tensor& points,
                       Tensor& neighbors_index,
                       Tensor& neighbors_count,
                       Tensor& neighbors_distance) {
-    const cudaStream_t stream = 0;
+    //     const cudaStream_t stream = 0;
 
-    Device device = points.GetDevice();
+    //     Device device = points.GetDevice();
 
-    NeighborSearchAllocator<T> output_allocator(device);
+    //     NeighborSearchAllocator<T> output_allocator(device);
 
-    open3d::core::nns::impl::HybridSearchCUDA(
-            stream, points.GetShape()[0], points.GetDataPtr<T>(),
-            queries.GetShape()[0], queries.GetDataPtr<T>(), T(radius), max_knn,
-            points_row_splits.GetShape()[0],
-            points_row_splits.GetDataPtr<int64_t>(),
-            queries_row_splits.GetShape()[0],
-            queries_row_splits.GetDataPtr<int64_t>(),
-            hash_table_splits.GetDataPtr<uint32_t>(),
-            hash_table_cell_splits.GetShape()[0],
-            hash_table_cell_splits.GetDataPtr<uint32_t>(),
-            hash_table_index.GetDataPtr<uint32_t>(), metric, output_allocator);
+    //     open3d::core::nns::impl::HybridSearchCUDA(
+    //             stream, points.GetShape()[0], points.GetDataPtr<T>(),
+    //             queries.GetShape()[0], queries.GetDataPtr<T>(), T(radius),
+    //             max_knn, points_row_splits.GetShape()[0],
+    //             points_row_splits.GetDataPtr<int64_t>(),
+    //             queries_row_splits.GetShape()[0],
+    //             queries_row_splits.GetDataPtr<int64_t>(),
+    //             hash_table_splits.GetDataPtr<uint32_t>(),
+    //             hash_table_cell_splits.GetShape()[0],
+    //             hash_table_cell_splits.GetDataPtr<uint32_t>(),
+    //             hash_table_index.GetDataPtr<uint32_t>(), metric,
+    //             output_allocator);
 
-    neighbors_index = output_allocator.NeighborsIndex();
-    neighbors_distance = output_allocator.NeighborsDistance();
-    neighbors_count = output_allocator.NeighborsCount();
+    //     neighbors_index = output_allocator.NeighborsIndex();
+    //     neighbors_distance = output_allocator.NeighborsDistance();
+    //     neighbors_count = output_allocator.NeighborsCount();
 }
 
 #define INSTANTIATE_BUILD(T)                                                  \
