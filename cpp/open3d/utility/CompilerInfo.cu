629d282a560a6159a04b9ea62d7d9a7bc4d1b5cc
Yixing Lao <yixing.lao@gmail.com>
2022-02-04 00:30:27 -0800

    compiler info to print cxx standard and compilers

--- cpp/open3d/utility/CompilerInfo.cu
+++ cpp/open3d/utility/CompilerInfo.cu
@@ -24,48 +24,22 @@
 // IN THE SOFTWARE.
 // ----------------------------------------------------------------------------
 
-#include <gmock/gmock.h>
-#include <gtest/gtest.h>
+#include <fmt/format.h>
 
-#include <cstring>
+#include <memory>
 #include <string>
 
-#ifdef BUILD_CUDA_MODULE
-#include "open3d/core/CUDAUtils.h"
-#endif
+#include "open3d/utility/CompilerInfo.h"
 
-#include "open3d/Open3D.h"
-#include "tests/Tests.h"
+namespace open3d {
+namespace utility {
 
 #ifdef BUILD_CUDA_MODULE
-/// Returns true if --disable_p2p flag is used.
-bool ShallDisableP2P(int argc, char** argv) {
-    bool shall_disable_p2p = false;
-    for (int i = 1; i < argc; ++i) {
-        if (std::strcmp(argv[i], "--disable_p2p") == 0) {
-            shall_disable_p2p = true;
-            break;
-        }
-    }
-    return shall_disable_p2p;
+std::string CompilerInfo::CUDACompilerVersion() const {
+    return fmt::format("{}.{}.{}", __CUDACC_VER_MAJOR__, __CUDACC_VER_MINOR__,
+                       __CUDACC_VER_BUILD__);
 }
 #endif
 
-int main(int argc, char** argv) {
-    using namespace open3d;
-
-    utility::SetVerbosityLevel(utility::VerbosityLevel::Debug);
-    utility::CompilerInfo::GetInstance().Print();
-    utility::CPUInfo::GetInstance().Print();
-    utility::ISAInfo::GetInstance().Print();
-
-#ifdef BUILD_CUDA_MODULE
-    if (ShallDisableP2P(argc, argv)) {
-        core::CUDAState::GetInstance().ForceDisableP2PForTesting();
-        utility::LogInfo("P2P device transfer has been disabled.");
-    }
-#endif
-
-    testing::InitGoogleMock(&argc, argv);
-    return RUN_ALL_TESTS();
-}
+}  // namespace utility
+}  // namespace open3d
