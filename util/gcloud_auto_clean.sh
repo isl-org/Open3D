6896f59a50a1c28b82e27b25c27df4df57dc96a5
Yixing Lao <yixing.lao@gmail.com>
2022-02-19 22:29:36 -0800

    add startup script while creating vm

--- util/gcloud_auto_clean.sh
+++ util/gcloud_auto_clean.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-sleep 600s
+
+# https://cloud.google.com/community/tutorials/create-a-self-deleting-virtual-machine
+sleep 300s
 export NAME=$(curl -X GET http://metadata.google.internal/computeMetadata/v1/instance/name -H 'Metadata-Flavor: Google')
 export ZONE=$(curl -X GET http://metadata.google.internal/computeMetadata/v1/instance/zone -H 'Metadata-Flavor: Google')
 gcloud --quiet compute instances delete $NAME --zone=$ZONE
