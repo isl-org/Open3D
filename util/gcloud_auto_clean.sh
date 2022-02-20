4a824e70a67180b7c310e8d9755eb18fa1299ed7
Yixing Lao <yixing.lao@gmail.com>
2022-02-19 22:24:20 -0800

    add autoclean script

---
+++ util/gcloud_auto_clean.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+sleep 600s
+export NAME=$(curl -X GET http://metadata.google.internal/computeMetadata/v1/instance/name -H 'Metadata-Flavor: Google')
+export ZONE=$(curl -X GET http://metadata.google.internal/computeMetadata/v1/instance/zone -H 'Metadata-Flavor: Google')
+gcloud --quiet compute instances delete $NAME --zone=$ZONE
