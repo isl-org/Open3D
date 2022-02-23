4e4600e1d8480a984bc09c59c5c6d0f06afc13a6
reyanshsolis <reyanshsolis@gmail.com>
2022-02-23 23:12:37 +0530

    updated examples python benchmarks using datasets

--- examples/python/benchmark/benchmark_pre.py
+++ examples/python/benchmark/benchmark_pre.py
@@ -53,35 +53,13 @@ def preprocess_point_cloud(pcd, voxel_size):
     return pcd_down, pcd_fpfh
 
 
-def get_ply_path(dataset_name, id):
-    return "%s/%s/cloud_bin_%d.ply" % (dataset_path, dataset_name, id)
-
-
-def get_log_path(dataset_name):
-    return "%s/fgr_%s.log" % (dataset_path, dataset_name)
-
-
-dataset_path = os.path.join(os.path.dirname(pyexample_path), 'test_data',
-                            'benchmark_data')
-dataset_names = ['livingroom1', 'livingroom2', 'office1', 'office2']
-
 if __name__ == "__main__":
     # data preparation
-    get_redwood_dataset()
-    print("downloading data")
+    dataset = o3d.data.LivingRoomPointClouds()
+    n_ply_files = len(dataset.paths)
     voxel_size = 0.05
 
-    # do RANSAC based alignment
-    for dataset_name in dataset_names:
-        ply_file_names = get_file_list("%s/%s/" % (dataset_path, dataset_name),
-                                       ".ply")
-        n_ply_files = len(ply_file_names)
-
-        alignment = []
-        for s in range(n_ply_files):
-            source = o3d.io.read_point_cloud(get_ply_path(dataset_name, s))
-            source_down, source_fpfh = preprocess_point_cloud(
-                source, voxel_size)
-            f = open('store.pckl', 'wb')
-            pickle.dump([source_down, source_fpfh], f)
-            f.close()
+    alignment = []
+    for s in range(n_ply_files):
+        source = o3d.io.read_point_cloud(dataset.paths[s])
+        source_down, source_fpfh = preprocess_point_cloud(source, voxel_size)
