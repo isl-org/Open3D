ed0c29bccf4feee126987a7864eecb793a0c3624
Yixing Lao <yixing.lao@gmail.com>
2022-03-04 02:56:09 -0800

    run sycl demo fix

--- cpp/open3d/core/TensorFunction.cpp
+++ cpp/open3d/core/TensorFunction.cpp
@@ -35,20 +35,46 @@ namespace core {
 void RunSYCLDemo() {
     utility::LogInfo("Hello RunSYCLDemo()!");
 
-    constexpr int size = 16;
-    std::array<int, size> data;
+    // Creating buffer of 4 ints to be used inside the kernel code
+    cl::sycl::buffer<cl::sycl::cl_int, 1> Buffer(4);
 
-    sycl::queue Q;
+    // Creating SYCL queue
+    cl::sycl::queue Queue;
 
-    sycl::buffer B{data};
-    Q.submit([&](sycl::handler& h) {
-        sycl::accessor A{B, h};
-        h.parallel_for(size, [=](auto& idx) { A[idx] = idx; });
+    // Size of index space for kernel
+    cl::sycl::range<1> NumOfWorkItems{Buffer.size()};
+
+    // Submitting command group(work) to queue
+    Queue.submit([&](cl::sycl::handler& cgh) {
+        // Getting write only access to the buffer on a device
+        auto Accessor = Buffer.get_access<cl::sycl::access::mode::write>(cgh);
+        // Executing kernel
+        cgh.parallel_for<class FillBuffer>(
+                NumOfWorkItems, [=](cl::sycl::id<1> WIid) {
+                    // Fill buffer with indexes
+                    Accessor[WIid] = (cl::sycl::cl_int)WIid.get(0);
+                });
     });
 
-    sycl::host_accessor A{B};
-    for (int i = 0; i < size; i++) {
-        std::cout << "data[" << i << "] = " << A[i] << "\n";
+    // Getting read only access to the buffer on the host.
+    // Implicit barrier waiting for queue to complete the work.
+    const auto HostAccessor = Buffer.get_access<cl::sycl::access::mode::read>();
+
+    // Check the results
+    bool MismatchFound = false;
+    for (size_t I = 0; I < Buffer.size(); ++I) {
+        if (HostAccessor[I] != I) {
+            std::cout << "The result is incorrect for element: " << I
+                      << " , expected: " << I << " , got: " << HostAccessor[I]
+                      << std::endl;
+            MismatchFound = true;
+        }
+    }
+
+    if (!MismatchFound) {
+        std::cout << "The results are correct!" << std::endl;
+    } else {
+        std::cout << "The results are not correct!" << std::endl;
     }
 }
 
