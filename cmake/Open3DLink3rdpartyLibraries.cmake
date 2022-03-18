87618e36574100f31af172af30d96013fbabfb0a
Yixing Lao <yixing.lao@gmail.com>
2022-03-18 05:59:01 -0700

    fix double link

--- cmake/Open3DLink3rdpartyLibraries.cmake
+++ cmake/Open3DLink3rdpartyLibraries.cmake
@@ -24,7 +24,21 @@ function(open3d_link_3rdparty_libraries target)
             message(WARNING "Skipping non-existent header dependency ${dep}")
         endif()
     endforeach()
+
     # Link header dependencies privately.
     target_link_libraries(${target} PRIVATE ${Open3D_3RDPARTY_HEADER_TARGETS})
 
+    # Avoid duplicate linking SYCL when building BUILD_SHARED_LIBS.
+    # If target links Open3D::Open3D, and Open3D links Open3D::3rdparty_sycl
+    # target should not link Open3D::3rdparty_sycl again.
+    if (TARGET Open3D::Open3D AND BUILD_SHARED_LIBS AND BUILD_SYCL_MODULE)
+        get_target_property(open3d_link_libs Open3D::Open3D LINK_LIBRARIES)
+        get_target_property(target_link_libs ${target} LINK_LIBRARIES)
+        if ((Open3D::Open3D        IN_LIST target_link_libs) AND
+            (Open3D::3rdparty_sycl IN_LIST target_link_libs) AND
+            (Open3D::3rdparty_sycl IN_LIST open3d_link_libs))
+            list(REMOVE_ITEM target_link_libs Open3D::3rdparty_sycl)
+            set_property(TARGET ${target} PROPERTY LINK_LIBRARIES ${target_link_libs})
+        endif()
+    endif()
 endfunction()
