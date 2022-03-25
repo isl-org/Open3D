fb17a7cd59166da07d9b9dea95c33bc963442c91
Yixing Lao <yixing.lao@gmail.com>
2022-03-25 06:54:46 -0700

    rename

--- cmake/Open3DSYCLTargetSources.cmake
+++ cmake/Open3DSYCLTargetSources.cmake
@@ -1,4 +1,4 @@
-# open3d_target_sycl_sources(name ...)
+# open3d_sycl_target_sources(name ...)
 #
 # When BUILD_SYCL_MODULE=ON, set SYCL-specific compile flags for the listed
 # source files and call target_sources(). If BUILD_SYCL_MODULE=OFF, this
@@ -8,24 +8,24 @@
 # limited set of arguments. See the example usage below.
 #
 # Example usage:
-#   open3d_target_sycl_sources(core PRIVATE a.cpp b.cpp)
-#   open3d_target_sycl_sources(core PUBLIC a.cpp b.cpp)
-#   open3d_target_sycl_sources(core VERBOSE PRIVATE a.cpp b.cpp)
-#   open3d_target_sycl_sources(core VERBOSE PUBLIC a.cpp b.cpp)
-function(open3d_target_sycl_sources target)
+#   open3d_sycl_target_sources(core PRIVATE a.cpp b.cpp)
+#   open3d_sycl_target_sources(core PUBLIC a.cpp b.cpp)
+#   open3d_sycl_target_sources(core VERBOSE PRIVATE a.cpp b.cpp)
+#   open3d_sycl_target_sources(core VERBOSE PUBLIC a.cpp b.cpp)
+function(open3d_sycl_target_sources target)
     cmake_parse_arguments(arg "PUBLIC;PRIVATE;INTERFACE;VERBOSE" "" "" ${ARGN})
     if(arg_UNPARSED_ARGUMENTS)
         if(arg_PUBLIC)
             target_sources(${target} PUBLIC ${arg_UNPARSED_ARGUMENTS})
-            message(STATUS "open3d_target_sycl_sources(${target}): PUBLIC")
+            message(STATUS "open3d_sycl_target_sources(${target}): PUBLIC")
         elseif (arg_PRIVATE)
             target_sources(${target} PRIVATE ${arg_UNPARSED_ARGUMENTS})
-            message(STATUS "open3d_target_sycl_sources(${target}): PRIVATE")
+            message(STATUS "open3d_sycl_target_sources(${target}): PRIVATE")
         elseif (arg_INTERFACE)
             target_sources(${target} INTERFACE ${arg_UNPARSED_ARGUMENTS})
-            message(STATUS "open3d_target_sycl_sources(${target}): INTERFACE")
+            message(STATUS "open3d_sycl_target_sources(${target}): INTERFACE")
         else()
-            message(FATAL_ERROR "Invalid syntax: open3d_target_sycl_sources(${name} ${ARGN})")
+            message(FATAL_ERROR "Invalid syntax: open3d_sycl_target_sources(${name} ${ARGN})")
         endif()
 
         if(BUILD_SYCL_MODULE)
@@ -33,7 +33,7 @@ function(open3d_target_sycl_sources target)
                 set_source_files_properties(${sycl_file} PROPERTIES
                     COMPILE_OPTIONS -fsycl -fsycl-unnamed-lambda -fsycl-targets=spir64_x86_64)
                 if(arg_VERBOSE)
-                    message(STATUS "open3d_target_sycl_sources(${target}): marked ${sycl_file} as SYCL code")
+                    message(STATUS "open3d_sycl_target_sources(${target}): marked ${sycl_file} as SYCL code")
                 endif()
             endforeach()
         endif()
