8280227709133ff76edd6f581bd4fdd10cdb4150
Yixing Lao <yixing.lao@gmail.com>
2022-04-21 17:12:13 -0700

    move fixes

--- util/ci_utils.sh
+++ util/ci_utils.sh
@@ -58,11 +58,10 @@ install_python_dependencies() {
 
     # TODO: modify other locations to use requirements.txt
     python -m pip install -r "${OPEN3D_SOURCE_ROOT}/python/requirements.txt"
+    python -m pip install -r "${OPEN3D_SOURCE_ROOT}/python/requirements_build.txt"
     if [[ "with-jupyter" =~ ^($options)$ ]]; then
         python -m pip install -r "${OPEN3D_SOURCE_ROOT}/python/requirements_jupyter_build.txt"
     fi
-    python -m pip install -r "${OPEN3D_SOURCE_ROOT}/python/requirements_build.txt"
-
     if [[ "with-unit-test" =~ ^($options)$ ]]; then
         python -m pip install -r "${OPEN3D_SOURCE_ROOT}/python/requirements_test.txt"
     fi
