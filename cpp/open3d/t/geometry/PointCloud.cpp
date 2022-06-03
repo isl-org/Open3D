3b0ca5d509288aebebe3dcc05cf8efe461f1183c
Yixing Lao <yixing.lao@gmail.com>
2022-06-03 02:38:45 -0700

    add back kernel

--- cpp/open3d/t/geometry/PointCloud.cpp
+++ cpp/open3d/t/geometry/PointCloud.cpp
@@ -179,9 +179,11 @@ PointCloud PointCloud::Append(const PointCloud &other) const {
 PointCloud &PointCloud::Transform(const core::Tensor &transformation) {
     core::AssertTensorShape(transformation, {4, 4});
 
-    core::Tensor R = transformation.Slice(0, 0, 3).Slice(1, 0, 3);
-    core::Tensor t = transformation.Slice(0, 0, 3).Slice(1, 3, 4).Flatten();
-    GetPointPositions() = GetPointPositions().Matmul(R.T()).Add(t);
+    kernel::transform::TransformPoints(transformation, GetPointPositions());
+
+    // core::Tensor R = transformation.Slice(0, 0, 3).Slice(1, 0, 3);
+    // core::Tensor t = transformation.Slice(0, 0, 3).Slice(1, 3, 4).Flatten();
+    // GetPointPositions() = GetPointPositions().Matmul(R.T()).Add(t);
 
     return *this;
 }
