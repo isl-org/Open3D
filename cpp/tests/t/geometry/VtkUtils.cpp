948cb538bdcc63bcde91c97d3ec7071faef739ad
Benjamin Ummenhofer <benjamin.ummenhofer@intel.com>
2022-06-10 15:02:23 +0200

    add comparison of tensor content

--- cpp/tests/t/geometry/VtkUtils.cpp
+++ cpp/tests/t/geometry/VtkUtils.cpp
@@ -41,6 +41,23 @@ namespace tests {
 
 class VtkUtilsTest : public testing::TestWithParam<bool> {};
 
+void Compare2DTensorVtkDataArrayContent(core::Tensor& tensor,
+                                        vtkDataArray* data_array) {
+    using namespace t::geometry;
+    ASSERT_EQ(tensor.NumDims(), 2);
+
+    EXPECT_EQ(tensor.GetShape(0), data_array->GetNumberOfTuples());
+    EXPECT_EQ(tensor.GetShape(1), data_array->GetNumberOfComponents());
+    EXPECT_EQ(tensor.GetDtype().ByteSize(), data_array->GetDataTypeSize());
+    EXPECT_EQ(vtkutils::DtypeToVtkType(tensor.GetDtype()),
+              data_array->GetDataType());
+
+    const void* ptr_tensor = tensor.GetDataPtr();
+    const void* ptr_vtk = data_array->GetVoidPointer(0);
+    size_t nbytes = tensor.NumElements() * tensor.GetDtype().ByteSize();
+    EXPECT_EQ(memcmp(ptr_tensor, ptr_vtk, nbytes), 0);
+}
+
 TEST_P(VtkUtilsTest, PointCloudToVtkPolyData) {
     using namespace t::geometry;
     bool copy = GetParam();
@@ -60,8 +77,7 @@ TEST_P(VtkUtilsTest, PointCloudToVtkPolyData) {
     } else {
         EXPECT_EQ(ptr_tensor, ptr_vtk);
     }
-    EXPECT_EQ(tensor.GetShape(0), data_array->GetNumberOfTuples());
-    EXPECT_EQ(tensor.GetShape(1), data_array->GetNumberOfComponents());
+    Compare2DTensorVtkDataArrayContent(tensor, data_array);
 }
 
 TEST_P(VtkUtilsTest, TriangleMeshToVtkPolyData) {
@@ -83,8 +99,7 @@ TEST_P(VtkUtilsTest, TriangleMeshToVtkPolyData) {
     } else {
         EXPECT_EQ(ptr_tensor, ptr_vtk);
     }
-    EXPECT_EQ(tensor.GetShape(0), data_array->GetNumberOfTuples());
-    EXPECT_EQ(tensor.GetShape(1), data_array->GetNumberOfComponents());
+    Compare2DTensorVtkDataArrayContent(tensor, data_array);
 }
 
 TEST_P(VtkUtilsTest, VtkPolyDataToO3D) {
@@ -125,8 +140,12 @@ TEST_P(VtkUtilsTest, VtkPolyDataToO3D) {
             EXPECT_EQ(ptr_tensor, ptr_vtk);
             EXPECT_GE(data_array->GetReferenceCount(), 2);
         }
-        EXPECT_EQ(tensor.GetShape(0), data_array->GetNumberOfTuples());
-        EXPECT_EQ(tensor.GetShape(1), data_array->GetNumberOfComponents());
+        Compare2DTensorVtkDataArrayContent(tensor, data_array);
+    
+        // connectivity is always a copy, just check the content
+        auto tensor_connectivity = tmesh.GetTriangleIndices();
+        tensor_connectivity = tensor_connectivity.Reshape({tensor_connectivity.NumElements(), 1}).Contiguous();
+        Compare2DTensorVtkDataArrayContent(tensor_connectivity, polydata->GetPolys()->GetConnectivityArray());
     }
     if (!copy) {
         // Only tmesh holds a reference to the data array
