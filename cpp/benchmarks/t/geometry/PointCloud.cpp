272ce93d6dae241a4e076f401fb18b9c8fde81ea
yuecideng <dengyueci@qq.com>
2022-08-21 06:07:29 +0000

    add benchmark for crop

--- cpp/benchmarks/t/geometry/PointCloud.cpp
+++ cpp/benchmarks/t/geometry/PointCloud.cpp
@@ -292,12 +292,46 @@ void CropByAxisAlignedBox(benchmark::State& state, const core::Device& device) {
     }
 }
 
+void CropByOrientedBox(benchmark::State& state, const core::Device& device) {
+    t::geometry::PointCloud pcd;
+    t::io::ReadPointCloud(path, pcd, {"auto", false, false, false});
+
+    pcd = pcd.To(device);
+    t::geometry::OrientedBoundingBox box(
+            core::Tensor::Init<float>({0, 0, 0}, device),
+            core::Tensor::Eye(3, core::Float32, device),
+            core::Tensor::Init<float>({1, 1, 1}, device));
+
+    // Warm up.
+    pcd.Crop(box);
+
+    for (auto _ : state) {
+        pcd.Crop(box);
+    }
+}
+
 void LegacyCropByAxisAlignedBox(benchmark::State& state, const int no_use) {
     open3d::geometry::PointCloud pcd;
     open3d::io::ReadPointCloud(path, pcd, {"auto", false, false, false});
 
     open3d::geometry::AxisAlignedBoundingBox box(Eigen::Vector3d(0, 0, 0),
                                                  Eigen::Vector3d(1, 1, 1));
+
+    // Warm up.
+    pcd.Crop(box);
+
+    for (auto _ : state) {
+        pcd.Crop(box);
+    }
+}
+
+void LegacyCropByOrientedBox(benchmark::State& state, const int no_use) {
+    open3d::geometry::PointCloud pcd;
+    open3d::io::ReadPointCloud(path, pcd, {"auto", false, false, false});
+
+    open3d::geometry::OrientedBoundingBox box(Eigen::Vector3d(0, 0, 0),
+                                              Eigen::Matrix3d::Identity(),
+                                              Eigen::Vector3d(1, 1, 1));
 
     // Warm up.
     pcd.Crop(box);
@@ -527,17 +561,23 @@ BENCHMARK_CAPTURE(
         ->Unit(benchmark::kMillisecond);
 BENCHMARK_CAPTURE(CropByAxisAlignedBox, CPU, core::Device("CPU:0"))
         ->Unit(benchmark::kMillisecond);
+BENCHMARK_CAPTURE(CropByOrientedBox, CPU, core::Device("CPU:0"))
+        ->Unit(benchmark::kMillisecond);
 #ifdef BUILD_CUDA_MODULE
 BENCHMARK_CAPTURE(
         RemoveRadiusOutliers, CUDA[50 | 0.05], core::Device("CUDA:0"), 50, 0.03)
         ->Unit(benchmark::kMillisecond);
-BENCHMARK_CAPTURE(CropByAxisAlignedBox, CPU, core::Device("CUDA:0"))
+BENCHMARK_CAPTURE(CropByAxisAlignedBox, CUDA, core::Device("CUDA:0"))
+        ->Unit(benchmark::kMillisecond);
+BENCHMARK_CAPTURE(CropByOrientedBox, CUDA, core::Device("CUDA:0"))
         ->Unit(benchmark::kMillisecond);
 #endif
 
 BENCHMARK_CAPTURE(LegacyRemoveRadiusOutliers, Legacy[50 | 0.05], 50, 0.03)
         ->Unit(benchmark::kMillisecond);
 BENCHMARK_CAPTURE(LegacyCropByAxisAlignedBox, Legacy, 1)
+        ->Unit(benchmark::kMillisecond);
+BENCHMARK_CAPTURE(LegacyCropByOrientedBox, Legacy, 1)
         ->Unit(benchmark::kMillisecond);
 
 }  // namespace geometry
