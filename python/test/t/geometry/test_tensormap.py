20eb8ca15711a047d9db8ff496be890e764274a0
yuecideng <dengyueci@qq.com>
2022-08-22 03:58:16 +0000

    change TensoeMap pytest

--- python/test/t/geometry/test_tensormap.py
+++ python/test/t/geometry/test_tensormap.py
@@ -35,6 +35,10 @@ sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../..")
 from open3d_test import list_devices
 
 
+class WrongType():
+    pass
+
+
 @pytest.mark.parametrize("device", list_devices())
 def test_tensormap(device):
     dtype = o3c.float32
@@ -51,10 +55,10 @@ def test_tensormap(device):
     colors = o3c.Tensor.ones((0, 3), dtype, device)
     tm = o3d.t.geometry.TensorMap("positions")
     assert "positions" not in tm
-    tm["positions"] = points
+    tm.positions = points
     assert "positions" in tm
     assert "colors" not in tm
-    tm["colors"] = colors
+    tm.colors = colors
     assert "colors" in tm
 
     # Constructor with tm values.
@@ -67,8 +71,53 @@ def test_tensormap(device):
 
     # __delitem__ operator.
     with pytest.raises(RuntimeError) as excinfo:
-        del tm["positions"]
+        del tm['positions']
         assert 'cannot be deleted' in str(excinfo.value)
+
+    # Test setter.
+    tm = o3d.t.geometry.TensorMap("positions")
+
+    # Set attributes.
+    tm.positions = o3c.Tensor.ones((2, 3), dtype, device)
+    tm.colors = o3c.Tensor.ones((2, 3), dtype, device)
+
+    # Set attributes with numpy array.
+    tm.positions = np.ones((3, 3), np.float32)
+    tm.colors = np.ones((3, 3), np.float32)
+    assert len(tm.positions) == 3
+    assert len(tm.colors) == 3
+
+    # Set existing attributes with wrong type.
+    with pytest.raises(TypeError) as e:
+        tm.positions = WrongType()
+
+    # Set new attributes with wrong type.
+    with pytest.raises(TypeError) as e:
+        tm.normals = WrongType()
+
+    # Set primary key.
+    with pytest.raises(KeyError) as e:
+        tm.primary_key = o3c.Tensor.ones((2, 3), dtype, device)
+
+    # Test getter.
+    tm = o3d.t.geometry.TensorMap("positions")
+    assert isinstance(tm, o3d.t.geometry.TensorMap)
+
+    # Set attributes.
+    tm.positions = o3c.Tensor.ones((2, 3), dtype, device)
+    tm.colors = o3c.Tensor.ones((2, 3), dtype, device)
+
+    # Get existing attributes.
+    colors = tm.colors
+    assert len(colors) == 2
+
+    # Get unknown attributes.
+    with pytest.raises(KeyError) as e:
+        normals = tm.normals
+
+    # Get primary key.
+    primary_key = tm.primary_key
+    assert primary_key == "positions"
 
 
 @pytest.mark.parametrize("device", list_devices())
@@ -76,50 +125,50 @@ def test_tensormap_modify(device):
     # Assigning to the *elements* of an alias will change the value in the map.
     # This tests that the alias shares the same memory as the tensor in the map.
     tm = o3d.t.geometry.TensorMap("positions")
-    tm["a"] = o3c.Tensor([100], device=device)
-    a_alias = tm["a"]
+    tm.a = o3c.Tensor([100], device=device)
+    a_alias = tm.a
     a_alias[:] = o3c.Tensor([200], device=device)
     np.testing.assert_equal(a_alias.cpu().numpy(), [200])
-    np.testing.assert_equal(tm["a"].cpu().numpy(), [200])
+    np.testing.assert_equal(tm.a.cpu().numpy(), [200])
 
     tm = o3d.t.geometry.TensorMap("positions")
-    tm["a"] = o3c.Tensor([100], device=device)
-    a_alias = tm["a"]
-    tm["a"][:] = o3c.Tensor([200], device=device)
+    tm.a = o3c.Tensor([100], device=device)
+    a_alias = tm.a
+    tm.a[:] = o3c.Tensor([200], device=device)
     np.testing.assert_equal(a_alias.cpu().numpy(), [200])
-    np.testing.assert_equal(tm["a"].cpu().numpy(), [200])
+    np.testing.assert_equal(tm.a.cpu().numpy(), [200])
 
     # Assigning a new tensor to an alias should not change the tensor in the
     # map. The alias name simply points to a new tensor.
     tm = o3d.t.geometry.TensorMap("positions")
-    tm["a"] = o3c.Tensor([100], device=device)
-    a_alias = tm["a"]
+    tm.a = o3c.Tensor([100], device=device)
+    a_alias = tm.a
     a_alias = o3c.Tensor([200], device=device)
     np.testing.assert_equal(a_alias.cpu().numpy(), [200])
-    np.testing.assert_equal(tm["a"].cpu().numpy(), [100])
+    np.testing.assert_equal(tm.a.cpu().numpy(), [100])
 
     tm = o3d.t.geometry.TensorMap("positions")
-    tm["a"] = o3c.Tensor([100], device=device)
-    a_alias = tm["a"]
-    tm["a"] = o3c.Tensor([200], device=device)
+    tm.a = o3c.Tensor([100], device=device)
+    a_alias = tm.a
+    tm.a = o3c.Tensor([200], device=device)
     np.testing.assert_equal(a_alias.cpu().numpy(), [100])
-    np.testing.assert_equal(tm["a"].cpu().numpy(), [200])
+    np.testing.assert_equal(tm.a.cpu().numpy(), [200])
 
     # Pybind always returns a "shallow copy" of the tensor. This is a copy since
     # the new variable points to a different tensor object, and thus the id() is
     # different. The copy is shallow because the new tensor shares the same
     # memory as the tensor in the map.
     tm = o3d.t.geometry.TensorMap("positions")
-    tm["a"] = o3c.Tensor([100], device=device)
-    a_alias = tm["a"]
-    assert id(a_alias) != id(tm["a"])
+    tm.a = o3c.Tensor([100], device=device)
+    a_alias = tm.a
+    assert id(a_alias) != id(tm.a)
 
     # After deleting the key-value from the map, the alias shall still be alive.
     tm = o3d.t.geometry.TensorMap("positions")
-    tm["a"] = o3c.Tensor([100], device=device)
-    a_alias = tm["a"]
+    tm.a = o3c.Tensor([100], device=device)
+    a_alias = tm.a
     assert len(tm) == 1
-    del tm["a"]
+    del tm['a']
     assert len(tm) == 0
     np.testing.assert_equal(a_alias.cpu().numpy(), [100])
     a_alias[:] = 200
@@ -127,15 +176,15 @@ def test_tensormap_modify(device):
 
     # With this, swapping elements are supported.
     tm = o3d.t.geometry.TensorMap("positions")
-    tm["a"] = o3c.Tensor([100], device=device)
-    tm["b"] = o3c.Tensor([200], device=device)
-    a_alias = tm["a"]
-    b_alias = tm["b"]
-    tm["a"], tm["b"] = tm["b"], tm["a"]
+    tm.a = o3c.Tensor([100], device=device)
+    tm.b = o3c.Tensor([200], device=device)
+    a_alias = tm.a
+    b_alias = tm.b
+    tm.a, tm.b = tm.b, tm.a
     np.testing.assert_equal(a_alias.cpu().numpy(), [100])
     np.testing.assert_equal(b_alias.cpu().numpy(), [200])
-    np.testing.assert_equal(tm["a"].cpu().numpy(), [200])
-    np.testing.assert_equal(tm["b"].cpu().numpy(), [100])
+    np.testing.assert_equal(tm.a.cpu().numpy(), [200])
+    np.testing.assert_equal(tm.b.cpu().numpy(), [100])
 
 
 @pytest.mark.parametrize("device", list_devices())
