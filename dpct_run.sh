3141ab325d35728177c797a9647458c37d27d949
Yixing Lao <yixing.lao@gmail.com>
2022-09-15 15:39:46 -0700

    output to build

--- dpct_run.sh
+++ dpct_run.sh
@@ -4,7 +4,7 @@ open3d_root=/home/yixing/repo/Open3D
 
 dpct \
     --in-root ${open3d_root}/cpp \
-    --out-root ${open3d_root}/out \
+    --out-root ${open3d_root}/build/out \
     --extra-arg="-DBUILD_CUDA_MODULE" \
     --extra-arg="-I${open3d_root}/cpp" \
     --extra-arg="-I${open3d_root}/build/fmt/include" \
