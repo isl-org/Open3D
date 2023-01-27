e2e887cb0886271e28c3ae544d383df1f9d97cbb
Sameer Sheorey <sameer.sheorey@intel.com>
2023-01-27 13:44:01 -0800

    Update copyright year to 2023.

--- python/open3d/visualization/tensorboard_plugin/labellut.py
+++ python/open3d/visualization/tensorboard_plugin/labellut.py
@@ -3,7 +3,7 @@
 # ----------------------------------------------------------------------------
 # The MIT License (MIT)
 #
-# Copyright (c) 2018-2021 www.open3d.org
+# Copyright (c) 2018-2023 www.open3d.org
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
