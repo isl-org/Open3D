7f85127ceede9472e94b1ac0dcef7b0ab669ea48
Sameer Sheorey <sameer.sheorey@intel.com>
2023-06-29 18:33:18 -0700

    Update jupyter web_visualizer for ipywidgets 8
    Changed some status messages from Info to Debug

---
+++ python/js/amd-public-path.js
@@ -0,0 +1,8 @@
+// In an AMD module, we set the public path using the magic requirejs 'module' dependency
+// See https://github.com/requirejs/requirejs/wiki/Differences-between-the-simplified-CommonJS-wrapper-and-standard-AMD-define#module
+// Since 'module' is a requirejs magic module, we must include 'module' in the webpack externals configuration.
+var module = require('module');
+var url = new URL(module.uri, document.location)
+// Using lastIndexOf('/')+1 gives us the empty string if there is no '/', so pathname becomes '/'
+url.pathname = url.pathname.slice(0, url.pathname.lastIndexOf('/') + 1);
+__webpack_public_path__ = `${url.origin}${url.pathname}`;
