20878e26de3ca9b18d9058112fd66ab2e549336f
Benjamin Ummenhofer <benjaminum@gmail.com>
2023-10-31 18:10:23 +0100

    minor changes to list_intersections example

--- cpp/pybind/t/geometry/raycasting_scene.cpp
+++ cpp/pybind/t/geometry/raycasting_scene.cpp
@@ -190,43 +190,44 @@ Lists the intersections of the rays with the scene::
     # Create scene and add the monkey model.
     scene = o3d.t.geometry.RaycastingScene()
     d = o3d.data.MonkeyModel()
-    mesh = o3d.t.io.read_triangle_mesh(d.path)    
+    mesh = o3d.t.io.read_triangle_mesh(d.path)
     mesh_id = scene.add_triangles(mesh)
-    
-    # Create an offset grid of rays.
-    p_min = np.min(mesh.vertex['positions'].numpy() - 1, axis=0)
-    p_max = np.max(mesh.vertex['positions'].numpy() - 1, axis=0)
-    x = np.arange(p_min[0], p_max[0], .1)
-    y = np.arange(p_min[1], p_max[1], .1)
-    xv, yv = np.meshgrid(x, y) 
-    orig = np.vstack([xv.flatten(), yv.flatten(), np.tile(p_min[2], xv.size)]).T
-    dest = np.copy(orig)
-    dest[:, 2] = p_max[2] + 1
-    rays = np.hstack([orig, dest - orig]).astype('float32') 
+
+    # Create a grid of rays covering the bounding box
+    bb_min = mesh.vertex['positions'].min(dim=0).numpy()
+    bb_max = mesh.vertex['positions'].max(dim=0).numpy()
+    x,y = np.linspace(bb_min, bb_max, num=10)[:,:2].T
+    xv, yv = np.meshgrid(x,y)
+    orig = np.stack([xv, yv, np.full_like(xv, bb_min[2]-1)], axis=-1).reshape(-1,3)
+    dest = orig + np.full(orig.shape, (0,0,2+bb_max[2]-bb_min[2]),dtype=np.float32)
+    rays = np.concatenate([orig, dest-orig], axis=-1).astype(np.float32)
 
     # Compute the ray intersections.
     lx = scene.list_intersections(rays)
+    lx = {k:v.numpy() for k,v in lx.items()}
 
-    # Calculate intersection coordinates.
+    # Calculate intersection coordinates using the primitive uvs and the mesh
     v = mesh.vertex['positions'].numpy()
     t = mesh.triangle['indices'].numpy()
-    tidx = lx['primitive_ids'].numpy()
-    uv = lx['primitive_uvs'].numpy()
+    tidx = lx['primitive_ids']
+    uv = lx['primitive_uvs']
     w = 1 - np.sum(uv, axis=1)
     c = \
-      v[t[tidx, 1].flatten(), :] * uv[:, 0][:, None] + \
-      v[t[tidx, 2].flatten(), :] * uv[:, 1][:, None] + \
-      v[t[tidx, 0].flatten(), :] * w[:, None]
-      
-    # Visualize the intersections.
-    entry = o3d.geometry.PointCloud(points = o3d.utility.Vector3dVector(orig))
-    target = o3d.geometry.PointCloud(points = o3d.utility.Vector3dVector(dest))
-    correspondence = [(i, i) for i in range(rays.shape[0])]
-    traj = o3d.geometry.LineSet.create_from_point_cloud_correspondences(entry , target , correspondence)
-    traj.colors = o3d.utility.Vector3dVector(np.tile([1, 0, 0], [rays.shape[0], 1]))
-    x = o3d.geometry.PointCloud(points = o3d.utility.Vector3dVector(c))
-    o3d.visualization.draw([mesh, traj, x])
-    
+    v[t[tidx, 1].flatten(), :] * uv[:, 0][:, None] + \
+    v[t[tidx, 2].flatten(), :] * uv[:, 1][:, None] + \
+    v[t[tidx, 0].flatten(), :] * w[:, None]
+
+    # Calculate intersection coordinates using ray_ids
+    c = rays[lx['ray_ids']][:,:3] + rays[lx['ray_ids']][:,3:]*lx['t_hit'][...,None]
+                                    
+    # Visualize the rays and intersections.
+    lines = o3d.t.geometry.LineSet()
+    lines.point.positions = np.hstack([orig,dest]).reshape(-1,3)
+    lines.line.indices = np.arange(lines.point.positions.shape[0]).reshape(-1,2)
+    lines.line.colors = np.full((lines.line.indices.shape[0],3), (1,0,0))
+    x = o3d.t.geometry.PointCloud(positions=c)
+    o3d.visualization.draw([mesh, lines, x], point_size=8)
+
 
 Args:
     rays (open3d.core.Tensor): A tensor with >=2 dims, shape {.., 6}, and Dtype
