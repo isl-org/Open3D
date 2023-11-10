c6fc5e13d23006563fa19509bfd5d2d7418e15d2
Saurabh Khanduja <khandujasaurabh@gmail.com>
2023-11-10 22:10:23 +0100

    Fix code style and update CHANGELOG.md

--- docs/conf.py
+++ docs/conf.py
@@ -136,7 +136,6 @@ pygments_dark_style = "monokai"
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
 
-
 # -- Options for HTML output ----------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
