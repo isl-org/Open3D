isl-org/Open3D

commit 6bbf0ff36ea2729d3db2ce18b26d93048aa3bb9b

Correct bound check for axis aligned bounding box and use same formatting for printing cuda or cpu tensor (#6444)

cpp/open3d/core/Tensor.cpp
@@ -750,7 +750,7 @@ std::string Tensor::ToString(bool with_suffix,
     std::ostringstream rc;
     if (IsCUDA() || !IsContiguous()) {
         Tensor host_contiguous_tensor = Contiguous().To(Device("CPU:0"));
-        rc << host_contiguous_tensor.ToString(false, "");
+        rc << host_contiguous_tensor.ToString(with_suffix, indent);
     } else {
         if (shape_.NumElements() == 0) {
             rc << indent;

cpp/open3d/geometry/BoundingVolume.cpp
@@ -256,6 +256,22 @@ OrientedBoundingBox AxisAlignedBoundingBox::GetMinimalOrientedBoundingBox(
     return OrientedBoundingBox::CreateFromAxisAlignedBoundingBox(*this);
 }
 
+AxisAlignedBoundingBox::AxisAlignedBoundingBox(const Eigen::Vector3d& min_bound,
+                                               const Eigen::Vector3d& max_bound)
+    : Geometry3D(Geometry::GeometryType::AxisAlignedBoundingBox),
+      min_bound_(min_bound),
+      max_bound_(max_bound),
+      color_(1, 1, 1) {
+    if ((max_bound_.array() < min_bound_.array()).any()) {
+        open3d::utility::LogWarning(
+                "max_bound {} of bounding box is smaller than min_bound {} in "
+                "one or more axes. Fix input values to remove this warning.",
+                max_bound_, min_bound_);
+        max_bound_ = max_bound.cwiseMax(min_bound);
+        min_bound_ = max_bound.cwiseMin(min_bound);
+    }
+}
+
 AxisAlignedBoundingBox& AxisAlignedBoundingBox::Transform(
         const Eigen::Matrix4d& transformation) {
     utility::LogError(

cpp/open3d/geometry/BoundingVolume.h
@@ -171,11 +171,7 @@ public:
     /// \param min_bound Lower bounds of the bounding box for all axes.
     /// \param max_bound Upper bounds of the bounding box for all axes.
     AxisAlignedBoundingBox(const Eigen::Vector3d& min_bound,
-                           const Eigen::Vector3d& max_bound)
-        : Geometry3D(Geometry::GeometryType::AxisAlignedBoundingBox),
-          min_bound_(min_bound),
-          max_bound_(max_bound),
-          color_(1, 1, 1) {}
+                           const Eigen::Vector3d& max_bound);
     ~AxisAlignedBoundingBox() override {}
 
 public:

commit bd1de76645b37c5dacfb55e14be0cd49b3d14713

Fix regression introduced in #6444

cpp/open3d/core/Tensor.cpp
@@ -750,7 +750,7 @@ std::string Tensor::ToString(bool with_suffix,
     std::ostringstream rc;
     if (IsCUDA() || !IsContiguous()) {
         Tensor host_contiguous_tensor = Contiguous().To(Device("CPU:0"));
-        rc << host_contiguous_tensor.ToString(with_suffix, indent);
+        rc << host_contiguous_tensor.ToString(false, indent);
     } else {
         if (shape_.NumElements() == 0) {
             rc << indent;