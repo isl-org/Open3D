181a431ff3a5bbc300afca2f97ef8f23cff7aab8
Daniel Simon <DanSimon09@gmail.com>
2024-01-26 16:35:16 -0800

    Get maximum threads from TBB instead of OpenMP

--- cpp/open3d/utility/Parallel.cpp
+++ cpp/open3d/utility/Parallel.cpp
@@ -7,42 +7,14 @@
 
 #include "open3d/utility/Parallel.h"
 
-#ifdef _OPENMP
-#include <omp.h>
-#endif
+#include <tbb/task_arena.h>
 
-#include <cstdlib>
-#include <string>
-
-#include "open3d/utility/CPUInfo.h"
-#include "open3d/utility/Logging.h"
 
 namespace open3d {
 namespace utility {
 
-static std::string GetEnvVar(const std::string& name) {
-    if (const char* value = std::getenv(name.c_str())) {
-        return std::string(value);
-    } else {
-        return "";
-    }
-}
-
 int EstimateMaxThreads() {
-#ifdef _OPENMP
-    if (!GetEnvVar("OMP_NUM_THREADS").empty() ||
-        !GetEnvVar("OMP_DYNAMIC").empty()) {
-        // See the full list of OpenMP environment variables at:
-        // https://www.openmp.org/spec-html/5.0/openmpch6.html
-        return omp_get_max_threads();
-    } else {
-        // Returns the number of physical cores.
-        return utility::CPUInfo::GetInstance().NumCores();
-    }
-#else
-    (void)&GetEnvVar;  // Avoids compiler warning.
-    return 1;
-#endif
+    return tbb::this_task_arena::max_concurrency();
 }
 
 std::size_t& DefaultGrainSizeTBB() noexcept {
