a0d1fa9ab6b6900f49724935d71b915419c70a58
Rene Sepulveda <errissa@gmail.com>
2024-02-28 23:11:55 -0500

    Upgrade to GLFW 3.4

--- 3rdparty/glfw/glfw.cmake
+++ 3rdparty/glfw/glfw.cmake
@@ -5,8 +5,8 @@ set(GLFW_LIB_NAME glfw3)
 ExternalProject_Add(
     ext_glfw
     PREFIX glfw
-    URL https://github.com/glfw/glfw/archive/refs/tags/3.3.9.tar.gz
-    URL_HASH SHA256=a7e7faef424fcb5f83d8faecf9d697a338da7f7a906fc1afbc0e1879ef31bd53
+    URL https://github.com/glfw/glfw/archive/refs/tags/3.4.tar.gz
+    URL_HASH SHA256=c038d34200234d071fae9345bc455e4a8f2f544ab60150765d7704e08f3dac01
     DOWNLOAD_DIR "${OPEN3D_THIRD_PARTY_DOWNLOAD_DIR}/glfw"
     UPDATE_COMMAND ""
     CMAKE_ARGS
