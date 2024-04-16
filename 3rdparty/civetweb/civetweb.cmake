15624686c4fffdde9ad7b6b102a498f593436906
Ahmed Elsayed <ae.helal@eng.zu.edu.eg>
2024-04-16 18:52:48 +0200

    Update ispc binary path on Windows, update civetweb to v1.16 (#6577)

--- 3rdparty/civetweb/civetweb.cmake
+++ 3rdparty/civetweb/civetweb.cmake
@@ -3,8 +3,8 @@ include(ExternalProject)
 ExternalProject_Add(
     ext_civetweb
     PREFIX civetweb
-    URL https://github.com/civetweb/civetweb/archive/refs/tags/v1.15.tar.gz
-    URL_HASH SHA256=90a533422944ab327a4fbb9969f0845d0dba05354f9cacce3a5005fa59f593b9
+    URL https://github.com/civetweb/civetweb/archive/refs/tags/v1.16.tar.gz
+    URL_HASH SHA256=f0e471c1bf4e7804a6cfb41ea9d13e7d623b2bcc7bc1e2a4dd54951a24d60285
     DOWNLOAD_DIR "${OPEN3D_THIRD_PARTY_DOWNLOAD_DIR}/civetweb"
     UPDATE_COMMAND ""
     CMAKE_ARGS
