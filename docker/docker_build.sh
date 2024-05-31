cd841a81e361f757865ecc16de76f265b37422e0
Murillo Rojas, Luis <luis.murillo.rojas@intel.com>
2024-05-31 14:15:37 -0700

    Build Embree with SYCL support and OneAPI 2024.1.0

--- docker/docker_build.sh
+++ docker/docker_build.sh
@@ -76,7 +76,7 @@ HOST_OPEN3D_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")"/.. >/dev/null 2>&1 && pw
 
 # Shared variables
 CCACHE_VERSION=4.3
-CMAKE_VERSION=cmake-3.24.4-linux-x86_64
+CMAKE_VERSION=cmake-3.29.2-linux-x86_64
 CMAKE_VERSION_AARCH64=cmake-3.24.4-linux-aarch64
 CUDA_VERSION=11.7.1-cudnn8
 CUDA_VERSION_LATEST=11.8.0-cudnn8
@@ -437,7 +437,7 @@ sycl-shared_export_env() {
 
     # https://hub.docker.com/r/intel/oneapi-basekit
     # https://github.com/intel/oneapi-containers/blob/main/images/docker/basekit/Dockerfile.ubuntu-20.04
-    export BASE_IMAGE=intel/oneapi-basekit:2022.2-devel-ubuntu20.04
+    export BASE_IMAGE=intel/oneapi-basekit:2024.1.0-devel-ubuntu20.04
     export DEVELOPER_BUILD=ON
     export CCACHE_TAR_NAME=open3d-ci-sycl
     export PYTHON_VERSION=3.8
@@ -453,8 +453,8 @@ sycl-static_export_env() {
     export DOCKER_TAG=open3d-ci:sycl-static
 
     # https://hub.docker.com/r/intel/oneapi-basekit
-    # https://github.com/intel/oneapi-containers/blob/main/images/docker/basekit/Dockerfile.ubuntu-20.04
-    export BASE_IMAGE=intel/oneapi-basekit:2022.2-devel-ubuntu20.04
+    # https://github.com/intel/oneapi-containers/blob/main/images/docker/basekit/Dockerfile.ubuntu-18.04
+    export BASE_IMAGE=intel/oneapi-basekit:2024.1.0-devel-ubuntu20.04
     export DEVELOPER_BUILD=ON
     export CCACHE_TAR_NAME=open3d-ci-sycl
     export PYTHON_VERSION=3.8
