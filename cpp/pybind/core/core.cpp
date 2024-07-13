902fd6a22fe241ef3605a1af63bcd014a53e1289
Tim Ohliger <ohliger.tim@gmail.com>
2024-07-13 17:40:02 +0200

    Split declarations/definitions for more o3d.core.* components.
    The o3d.core.(blob,kernel,linalg,scalar,sycl) files are split now.

--- cpp/pybind/core/core.cpp
+++ cpp/pybind/core/core.cpp
@@ -21,18 +21,16 @@ void pybind_core_declarations(py::module& m) {
 
     // opn3d::core namespace.
     pybind_cuda_utils_declarations(m_core);
-    pybind_sycl_utils(m_core);
-    pybind_core_blob(m_core);
+    pybind_core_blob_declarations(m_core);
     pybind_core_dtype(m_core);
     pybind_core_device_declarations(m_core);
     pybind_core_size_vector(m_core);
     pybind_core_tensor(m_core);
     pybind_core_tensor_function(m_core);
-    pybind_core_linalg(m_core);
-    pybind_core_kernel(m_core);
+    pybind_core_kernel_declarations(m_core);
     pybind_core_hashmap(m_core);
     pybind_core_hashset(m_core);
-    pybind_core_scalar(m_core);
+    pybind_core_scalar_declarations(m_core);
 
     // opn3d::core::nns namespace.
     py::module m_nns = m_core.def_submodule("nns");
@@ -42,7 +40,11 @@ void pybind_core_declarations(py::module& m) {
 void pybind_core_definitions(py::module& m) {
     auto m_core = static_cast<py::module>(m.attr("core"));
     pybind_cuda_utils_definitions(m_core);
+    pybind_sycl_utils_definitions(m_core);
     pybind_core_device_definitions(m_core);
+    pybind_core_linalg_definitions(m_core);
+    pybind_core_kernel_definitions(m_core);
+    pybind_core_scalar_definitions(m_core);
 }
 
 }  // namespace core
