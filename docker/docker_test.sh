e86fcb38ddb5cb5e3d77c53cab234ddd687fbcfd
Sameer Sheorey <41028320+ssheorey@users.noreply.github.com>
2024-08-08 23:47:42 -0700

    Update min OS to Ubuntu 20.04 (#6902)
    * Update min OS to Ubuntu 20.04
    * Switch latest Ubuntu from noble to jammy: No CUDNN docker image for noble.
    * Update CUDA version selection, add support for Hopper
    * Remove CPU rendering support for Ubuntu 18.04 (old mesa SW)
    * Autodetect CUDA version to install the correct PyTorch in CI
    * CUDA latest 11.8 to support PyTorch 2.0
    Fix CUDAARCHS in Windows CI, since no GPU present.
    ARM Linux: Fix for "cannot allocate memory in TLS blc
    * Fix for CUDA ARCH case with no GPU
    * new docker version fix
    * remove docker docs for Ubutu 18.04

--- docker/docker_test.sh
+++ docker/docker_test.sh
@@ -47,12 +47,12 @@ OPTION:
     sycl-static                : SYCL (oneAPI) with static lib
 
     # ML CIs (Dockerfile.ci)
-    2-bionic                   : CUDA CI, 2-bionic, developer mode
-    3-ml-shared-bionic-release : CUDA CI, 3-ml-shared-bionic, release mode
-    3-ml-shared-bionic         : CUDA CI, 3-ml-shared-bionic, developer mode
-    4-shared-bionic            : CUDA CI, 4-shared-bionic, developer mode
-    4-shared-bionic-release    : CUDA CI, 4-shared-bionic, release mode
-    5-ml-focal                 : CUDA CI, 5-ml-focal, developer mode
+    2-focal                   : CUDA CI, 2-focal, developer mode
+    3-ml-shared-focal-release : CUDA CI, 3-ml-shared-focal, release mode
+    3-ml-shared-focal         : CUDA CI, 3-ml-shared-focal, developer mode
+    4-shared-focal            : CUDA CI, 4-shared-focal, developer mode
+    4-shared-focal-release    : CUDA CI, 4-shared-focal, release mode
+    5-ml-jammy                : CUDA CI, 5-ml-jammy, developer mode
 "
 
 HOST_OPEN3D_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")"/.. >/dev/null 2>&1 && pwd)"
@@ -331,33 +331,33 @@ sycl-static)
     ;;
 
 # ML CIs
-2-bionic)
-    2-bionic_export_env
+2-focal)
+    2-focal_export_env
     ci_print_env
     cpp_python_linking_uninstall_test
     ;;
-3-ml-shared-bionic)
-    3-ml-shared-bionic_export_env
+3-ml-shared-focal)
+    3-ml-shared-focal_export_env
     ci_print_env
     cpp_python_linking_uninstall_test
     ;;
-3-ml-shared-bionic-release)
-    3-ml-shared-bionic-release_export_env
+3-ml-shared-focal-release)
+    3-ml-shared-focal-release_export_env
     ci_print_env
     cpp_python_linking_uninstall_test
     ;;
-4-shared-bionic)
-    4-shared-bionic_export_env
+4-shared-focal)
+    4-shared-focal_export_env
     ci_print_env
     cpp_python_linking_uninstall_test
     ;;
-4-shared-bionic-release)
-    4-shared-bionic-release_export_env
+4-shared-focal-release)
+    4-shared-focal-release_export_env
     ci_print_env
     cpp_python_linking_uninstall_test
     ;;
-5-ml-focal)
-    5-ml-focal_export_env
+5-ml-jammy)
+    5-ml-jammy_export_env
     ci_print_env
     cpp_python_linking_uninstall_test
     ;;
