3eb5962b6e90f5cf455b3695c60a06e0f32bff66
Sameer Sheorey <sameer.sheorey@intel.com>
2024-08-19 17:23:01 -0700

    revert fetchcontent_makeavailable -> fetchcontent_populate for qhull fetchcontent to avoid Windows build errors

--- 3rdparty/qhull/qhull.cmake
+++ 3rdparty/qhull/qhull.cmake
@@ -8,11 +8,7 @@ FetchContent_Declare(
     URL_HASH
     SHA256=8774e9a12c70b0180b95d6b0b563c5aa4bea8d5960c15e18ae3b6d2521d64f8b
     DOWNLOAD_DIR "${OPEN3D_THIRD_PARTY_DOWNLOAD_DIR}/qhull"
-    UPDATE_COMMAND ""
-    CONFIGURE_COMMAND ""
-    BUILD_COMMAND ""
-    INSTALL_COMMAND ""
 )
 
-FetchContent_MakeAvailable(ext_qhull)
+FetchContent_Populate(ext_qhull)
 FetchContent_GetProperties(ext_qhull SOURCE_DIR QHULL_SOURCE_DIR)
