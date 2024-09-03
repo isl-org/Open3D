53915ba7dce735cc3686a1f50791658aec865f6f
Sharon Berezalsky <sharon@buildots.com>
2024-09-03 16:43:14 +0300

    Expose the static functions to the python API

--- cpp/pybind/t/geometry/boundingvolume.cpp
+++ cpp/pybind/t/geometry/boundingvolume.cpp
@@ -355,6 +355,15 @@ that could be computed for example with O'Rourke's algorithm
 (cf. http://cs.smith.edu/~jorourke/Papers/MinVolBox.pdf, https://www.geometrictools.com/Documentation/MinimumVolumeBox.pdf)
 This is a wrapper for a CPU implementation.)",
                    "points"_a, "robust"_a = false);
+    obb.def_static("create_from_points_minimal", &OrientedBoundingBox::CreateFromPointsMinimal,
+                   R"(Creates the oriented bounding box with the smallest volume.
+The algorithm makes use of the fact that at least one edge of
+the convex hull must be collinear with an edge of the minimum
+bounding box: for each triangle in the convex hull, calculate
+the minimal axis aligned box in the frame of that triangle.
+at the end, return the box with the smallest volume.
+This is a wrapper for a CPU implementation.)",
+                   "points"_a, "robust"_a = false);
 
     docstring::ClassMethodDocInject(
             m, "OrientedBoundingBox", "set_center",
@@ -411,6 +420,15 @@ This is a wrapper for a CPU implementation.)",
               "If set to true uses a more robust method which works in "
               "degenerate cases but introduces noise to the points "
               "coordinates."}});
+    docstring::ClassMethodDocInject(
+        m, "OrientedBoundingBox", "create_from_points_minimal",
+        {{"points",
+          "A list of points with data type of float32 or float64 (N x 3 "
+          "tensor, where N must be larger than 3)."},
+         {"robust",
+          "If set to true uses a more robust method which works in "
+          "degenerate cases but introduces noise to the points "
+          "coordinates."}});
 }
 
 }  // namespace geometry
