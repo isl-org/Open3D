3325a55273e5708693a17458f580bae110653176
Daniel Simon <dansimon09@gmail.com>
2024-10-11 20:28:32 -0700

    Extracted minizip from zlib so that an external zlib could be used in its place. (The removes the patch to zlib required for it to use integrated minizip)

--- 3rdparty/find_dependencies.cmake
+++ 3rdparty/find_dependencies.cmake
@@ -853,6 +853,25 @@ if (BUILD_LIBREALSENSE)
     endif()
 endif()
 
+if(USE_SYSTEM_ZLIB)
+    open3d_find_package_3rdparty_library(3rdparty_zlib
+        PACKAGE ZLIB
+        TARGETS ZLIB::ZLIB
+    )
+    list(APPEND Open3D_3RDPARTY_PRIVATE_TARGETS_FROM_SYSTEM Open3D::3rdparty_zlib)
+endif()
+if(NOT USE_SYSTEM_ZLIB)
+    include(${Open3D_3RDPARTY_DIR}/zlib/zlib.cmake)
+    open3d_import_3rdparty_library(3rdparty_zlib
+        HIDDEN
+        INCLUDE_DIRS ${ZLIB_INCLUDE_DIRS}
+        LIB_DIR      ${ZLIB_LIB_DIR}
+        LIBRARIES    ${ZLIB_LIBRARIES}
+        DEPENDS      ext_zlib
+    )
+    list(APPEND Open3D_3RDPARTY_PRIVATE_TARGETS_FROM_CUSTOM Open3D::3rdparty_zlib)
+endif()
+
 # Curl
 # - Curl should be linked before PNG, otherwise it will have undefined symbols.
 # - openssl.cmake needs to be included before curl.cmake, for the
@@ -912,6 +931,25 @@ if(NOT USE_SYSTEM_CURL)
 endif()
 list(APPEND Open3D_3RDPARTY_PRIVATE_TARGETS_FROM_CUSTOM Open3D::3rdparty_curl)
 
+if(USE_SYSTEM_MINIZIP)
+    open3d_find_package_3rdparty_library(3rdparty_zlib
+        PACKAGE minizip
+        TARGETS MINIZIP::minizip
+    )
+    list(APPEND Open3D_3RDPARTY_PRIVATE_TARGETS_FROM_SYSTEM Open3D::3rdparty_minizip)
+endif()
+if(NOT USE_SYSTEM_MINIZIP)
+    include(${Open3D_3RDPARTY_DIR}/zlib/minizip.cmake)
+    open3d_import_3rdparty_library(3rdparty_minizip
+        HIDDEN
+        INCLUDE_DIRS ${MINIZIP_INCLUDE_DIRS}
+        LIB_DIR      ${MINIZIP_LIB_DIR}
+        LIBRARIES    ${MINIZIP_LIBRARIES}
+        DEPENDS      ext_minizip
+    )
+    list(APPEND Open3D_3RDPARTY_PRIVATE_TARGETS_FROM_CUSTOM Open3D::3rdparty_minizip)
+endif()
+
 # PNG
 if(USE_SYSTEM_PNG)
     # ZLIB::ZLIB is automatically included by the PNG package.
@@ -925,15 +963,6 @@ if(USE_SYSTEM_PNG)
     endif()
 endif()
 if(NOT USE_SYSTEM_PNG)
-    include(${Open3D_3RDPARTY_DIR}/zlib/zlib.cmake)
-    open3d_import_3rdparty_library(3rdparty_zlib
-        HIDDEN
-        INCLUDE_DIRS ${ZLIB_INCLUDE_DIRS}
-        LIB_DIR      ${ZLIB_LIB_DIR}
-        LIBRARIES    ${ZLIB_LIBRARIES}
-        DEPENDS      ext_zlib
-    )
-
     include(${Open3D_3RDPARTY_DIR}/libpng/libpng.cmake)
     open3d_import_3rdparty_library(3rdparty_png
         INCLUDE_DIRS ${LIBPNG_INCLUDE_DIRS}
