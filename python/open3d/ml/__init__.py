3c53f39f0215cf7b1c67934229ce2962639d73b7
Kai Song <1136780339@qq.com>
2024-11-22 10:40:31 +0800

    fix bug that cannot call torch/tf under open3d.ml

--- python/open3d/ml/__init__.py
+++ python/open3d/ml/__init__.py
@@ -7,6 +7,7 @@
 
 import os as _os
 import open3d as _open3d
+from open3d import _build_config
 if _open3d.__DEVICE_API__ == 'cuda':
     from open3d.cuda.pybind.ml import *
 else:
@@ -16,3 +17,7 @@ from . import configs
 from . import datasets
 from . import vis
 from . import utils
+if _build_config["BUILD_TENSORFLOW_OPS"]:
+    from . import tf
+if _build_config["BUILD_PYTORCH_OPS"]:
+    from . import torch
