41e299e214c02c48e22f57d98a35e2ca1975e2ba
Tim Ohliger <ohliger.tim@gmail.com>
2024-12-19 10:01:09 +0100

    Removed cmake update from docs ci

--- util/ci_utils.sh
+++ util/ci_utils.sh
@@ -340,11 +340,7 @@ test_cpp_example() {
 install_docs_dependencies() {
     echo
     echo Install ubuntu dependencies
-    echo Update cmake needed in Ubuntu 20.04
-    sudo apt-key adv --fetch-keys https://apt.kitware.com/keys/kitware-archive-latest.asc
-    sudo apt-add-repository --yes 'deb https://apt.kitware.com/ubuntu/ focal main'
     ./util/install_deps_ubuntu.sh assume-yes
-    sudo apt-get install --yes cmake
     sudo apt-get install --yes libxml2-dev libxslt-dev python3-dev
     sudo apt-get install --yes doxygen
     sudo apt-get install --yes texlive
