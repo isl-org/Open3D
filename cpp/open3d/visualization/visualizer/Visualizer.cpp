1e7b17438687a0b0c1e5a7187321ac7044afe275
Sameer Sheorey <41028320+ssheorey@users.noreply.github.com>
2025-01-07 15:20:18 -0800

    Build fixes for v0.19 (#7128)
    * Prevent libomp linking when libomp5-dev is installed, due to cmake adding the libc++ dir as a linker search path. In sycl mode, libiomp is the default OMP runtime.
    Do not install clang to prevent libomp-dev being installed.
    DEVELOPER_BUILD option for SYCL CI
    ccache gz -> xz
    Dockerfile for building docs.
    Use cxx11 abi pytorch to allow building both torch and tf docs at once.
    * Use glfw null platform auto selection
    * Upgrade glew to v2.2
    * Fix GLFW platform selection when re-initialized. GLFW remembers last null platform.
    * macOS script typos

--- cpp/open3d/visualization/visualizer/Visualizer.cpp
+++ cpp/open3d/visualization/visualizer/Visualizer.cpp
@@ -35,6 +35,14 @@ private:
         glfwInitHint(GLFW_COCOA_CHDIR_RESOURCES, GLFW_FALSE);
 #endif
         init_status_ = glfwInit();
+        if (init_status_ != GLFW_TRUE) {
+            glfwInitHint(GLFW_PLATFORM, GLFW_PLATFORM_NULL);
+            init_status_ = glfwInit();
+        }
+        if (init_status_ == GLFW_TRUE) init_status_ = glfwGetPlatform();
+        if (init_status_ == GLFW_PLATFORM_NULL) {
+            utility::LogWarning("GLFW initialized for headless rendering.");
+        }
     }
 
     GLFWContext(const GLFWContext &) = delete;
@@ -42,13 +50,14 @@ private:
 
 public:
     ~GLFWContext() {
-        if (init_status_ == GLFW_TRUE) {
+        if (init_status_ != GLFW_FALSE) {
             glfwTerminate();
+            init_status_ = GLFW_FALSE;
             utility::LogDebug("GLFW destruct.");
         }
     }
 
-    /// \brief Get the glfwInit status.
+    /// \brief Get the glfwInit status / GLFW_PLATFORM initialized.
     inline int InitStatus() const { return init_status_; }
 
     /// \brief Get a shared instance of the GLFW context.
@@ -109,7 +118,7 @@ bool Visualizer::CreateVisualizerWindow(
     utility::LogDebug("[Visualizer] Creating window.");
     glfwSetErrorCallback(GLFWContext::GLFWErrorCallback);
     glfw_context_ = GLFWContext::GetInstance();
-    if (glfw_context_->InitStatus() != GLFW_TRUE) {
+    if (glfw_context_->InitStatus() == GLFW_FALSE) {
         utility::LogWarning("Failed to initialize GLFW");
         return false;
     }
@@ -117,11 +126,15 @@ bool Visualizer::CreateVisualizerWindow(
     glfwWindowHint(GLFW_SAMPLES, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-#ifndef HEADLESS_RENDERING
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-#endif
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_VISIBLE, visible ? 1 : 0);
+    int visible_hint = visible ? 1 : 0;
+    if (glfw_context_->InitStatus() == GLFW_PLATFORM_NULL) {
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_FALSE);
+        visible_hint = 0;  // NULL platform does not support visible window
+    } else {
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    }
+    glfwWindowHint(GLFW_VISIBLE, visible_hint);
 
     window_ = glfwCreateWindow(width, height, window_name_.c_str(), NULL, NULL);
     if (!window_) {
@@ -279,9 +292,9 @@ void Visualizer::Run() {
             if (animation_callback_func_in_loop_(this)) {
                 UpdateGeometry();
             }
-            // Set render flag as dirty anyways, because when we use callback
-            // functions, we assume something has been changed in the callback
-            // and the redraw event should be triggered.
+            // Set render flag as dirty anyways, because when we use
+            // callback functions, we assume something has been changed in
+            // the callback and the redraw event should be triggered.
             UpdateRender();
         }
     }
