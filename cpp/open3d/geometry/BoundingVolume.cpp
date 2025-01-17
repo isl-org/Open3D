afc1802556fbdf7cf618f7bbac411e02f6ce365f
Quentin Leboutet <quentin.leboutet@tum.de>
2025-01-17 22:36:19 +0100

    Solved the ordering issue.

--- cpp/open3d/geometry/BoundingVolume.cpp
+++ cpp/open3d/geometry/BoundingVolume.cpp
@@ -252,6 +252,65 @@ OrientedBoundingBox OrientedBoundingBox::CreateFromPointsMinimal(
         return OrientedBoundingBox();
     }
 
+    auto mapOBBToClosestIdentity = [&](OrientedBoundingBox& obb) {
+        Eigen::Matrix3d& R = obb.R_;
+        Eigen::Vector3d& extent = obb.extent_;
+        Eigen::Vector3d col[3] = {R.col(0), R.col(1), R.col(2)};
+        Eigen::Vector3d ext = extent;
+        double best_score = -1e9;
+        Eigen::Matrix3d best_R;
+        Eigen::Vector3d best_extent;
+        // Hard-coded permutations of indices [0,1,2]
+        static const std::array<std::array<int, 3>, 6> permutations = {
+                {{{0, 1, 2}},
+                 {{0, 2, 1}},
+                 {{1, 0, 2}},
+                 {{1, 2, 0}},
+                 {{2, 0, 1}},
+                 {{2, 1, 0}}}};
+
+        // Evaluate all 6 permutations × 8 sign flips = 48 candidates
+        for (const auto& p : permutations) {
+            for (int sign_bits = 0; sign_bits < 8; ++sign_bits) {
+                // Derive the sign of each axis from bits (0 => -1, 1 => +1)
+                // s0 is bit0, s1 is bit1, s2 is bit2 of sign_bits
+                const int s0 = (sign_bits & 1) ? 1 : -1;
+                const int s1 = (sign_bits & 2) ? 1 : -1;
+                const int s2 = (sign_bits & 4) ? 1 : -1;
+
+                // Construct candidate columns
+                Eigen::Vector3d c0 = s0 * col[p[0]];
+                Eigen::Vector3d c1 = s1 * col[p[1]];
+                Eigen::Vector3d c2 = s2 * col[p[2]];
+
+                // Score: how close are we to the identity?
+                // Since e_x = (1,0,0), e_y = (0,1,0), e_z = (0,0,1),
+                // we can skip dot products & do c0(0)+c1(1)+c2(2).
+                double score = c0(0) + c1(1) + c2(2);
+
+                // If this orientation is better, update the best.
+                if (score > best_score) {
+                    best_score = score;
+                    best_R.col(0) = c0;
+                    best_R.col(1) = c1;
+                    best_R.col(2) = c2;
+
+                    // Re-permute extents: if the axis p[0] in old frame
+                    // now goes to new X, etc.
+                    best_extent(0) = ext(p[0]);
+                    best_extent(1) = ext(p[1]);
+                    best_extent(2) = ext(p[2]);
+                }
+            }
+        }
+
+        // Update the OBB with the best orientation found
+        obb.R_ = best_R;
+        obb.extent_ = best_extent;
+
+        // Note: obb.center_ and obb.color_ remain unchanged.
+    };
+
     // --------------------------------------------------------------------
     // 1) Precompute vertex adjacency data, face normals, and edge data
     // --------------------------------------------------------------------
@@ -1324,7 +1383,7 @@ OrientedBoundingBox OrientedBoundingBox::CreateFromPointsMinimal(
     if (minOBB.R_.col(0).cross(minOBB.R_.col(1)).dot(minOBB.R_.col(2)) < 0.0) {
         minOBB.R_.col(2) = -minOBB.R_.col(2);
     }
-
+    mapOBBToClosestIdentity(minOBB);
     return minOBB;
 }
 
