89e7f3df3f44f695890a428b1af80ea0f375b3f3
Tim Ohliger <ohliger.tim@gmail.com>
2025-01-26 22:41:29 +0100

    Added missing "" to Device __repr__

--- cpp/pybind/core/device.cpp
+++ cpp/pybind/core/device.cpp
@@ -48,7 +48,7 @@ void pybind_core_device_definitions(py::module &m) {
                 utility::LogError("Unknown device type");
                 return d.ToString();
         }
-        return fmt::format("Device({}, {})", device_type, d.GetID());
+        return fmt::format("Device(\"{}\", {})", device_type, d.GetID());
     });
     device.def("__str__", &Device::ToString);
     device.def("get_type", &Device::GetType);
