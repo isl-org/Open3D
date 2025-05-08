cac697581a073341a0df9cfc53285ec76e3acaea
Johnny <johnnync13@gmail.com>
2025-05-08 14:37:37 +0200

    revert

--- util/ci_utils.sh
+++ util/ci_utils.sh
@@ -27,11 +27,11 @@ BUILD_SYCL_MODULE=${BUILD_SYCL_MODULE:-OFF}
 # CUDA: see docker/docker_build.sh
 # ML
 TENSORFLOW_VER="2.16.2"
-TORCH_VER="2.7.0"
+TORCH_VER="2.2.2"
 TORCH_REPO_URL="https://download.pytorch.org/whl/torch/"
 TORCH_CXX11_URL="https://download.pytorch.org/whl/"
 # Python
-PIP_VER="25.1.1"
+PIP_VER="24.3.1"
 PROTOBUF_VER="4.24.0"
 
 OPEN3D_INSTALL_DIR=~/open3d_install
