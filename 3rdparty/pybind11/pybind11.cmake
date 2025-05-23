094b7e5a090d287d9a826e26e5f23456f65820f9
Tim Ohliger <ohliger.tim@gmail.com>
2025-05-23 11:21:33 +0200

    Upgrade pybind11 to v3.0.0rc1

--- 3rdparty/pybind11/pybind11.cmake
+++ 3rdparty/pybind11/pybind11.cmake
@@ -3,8 +3,8 @@ include(FetchContent)
 FetchContent_Declare(
     ext_pybind11
     PREFIX pybind11
-    GIT_REPOSITORY https://github.com/pybind/pybind11.git
-    GIT_TAG d28904f12e906dc43f72b549a6b26bceb9c2caff
+    URL https://github.com/pybind/pybind11/archive/refs/tags/v3.0.0rc1.tar.gz
+    URL_HASH SHA256=385a60c19e40458fd63509ccdb21c3f3317a59944d98dab093b53dbcf2aaac80
     DOWNLOAD_DIR "${OPEN3D_THIRD_PARTY_DOWNLOAD_DIR}/pybind11"
 )
 
