8c6ad8ce97f4fb7cca2a9438b51640aa7ffd2b83
johnnynunez <johnnynuca14@gmail.com>
2025-06-10 10:45:22 +0200

    fix: update eigen dependency to version 3.4 and adjust URL hash

--- 3rdparty/eigen/eigen.cmake
+++ 3rdparty/eigen/eigen.cmake
@@ -3,8 +3,8 @@ include(ExternalProject)
 ExternalProject_Add(
     ext_eigen
     PREFIX eigen
-    URL https://gitlab.com/libeigen/eigen/-/archive/3.3/eigen-3.3.tar.gz
-    URL_HASH SHA256=1bc843b08d0015d5cc0d403662ee182366cb19b0859fab8872585342c671ec12
+    URL https://gitlab.com/libeigen/eigen/-/archive/3.4/eigen-3.4.tar.gz
+    URL_HASH SHA256=8b9be3abc1bfd89cb8917aa10ea4b68ca6a4bfadcb0be8e8ef9c79114b92871b
     DOWNLOAD_DIR "${OPEN3D_THIRD_PARTY_DOWNLOAD_DIR}/eigen"
     UPDATE_COMMAND ""
     CONFIGURE_COMMAND ""
