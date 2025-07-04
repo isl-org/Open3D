b2faf724763df2940f71c84bcdb1e95d30337f47
johnnynunez <johnnynuca14@gmail.com>
2025-07-04 23:25:21 +0200

    WEBRTC COMPATIBLE NOT YET with build aarch64 in this framework

--- util/ci_utils.sh
+++ util/ci_utils.sh
@@ -140,7 +140,14 @@ build_pip_package() {
     echo "Building Open3D wheel"
     options="$(echo "$@" | tr ' ' '|')"
 
-    BUILD_FILAMENT_FROM_SOURCE=OFF
+    AARCH="$(uname -m)"
+    if [[ "$AARCH" == "aarch64" ]]; then
+        echo "Building for aarch64 architecture"
+        BUILD_FILAMENT_FROM_SOURCE=ON
+    else
+        echo "Building for x86_64 architecture"
+        BUILD_FILAMENT_FROM_SOURCE=OFF
+    fi
     set +u
     if [[ -f "${OPEN3D_ML_ROOT}/set_open3d_ml_root.sh" ]] &&
         [[ "$BUILD_TENSORFLOW_OPS" == "ON" || "$BUILD_PYTORCH_OPS" == "ON" ]]; then
@@ -165,9 +172,11 @@ build_pip_package() {
     if [[ "build_jupyter" =~ ^($options)$ ]]; then
         echo "Building Jupyter extension in Python wheel."
         BUILD_JUPYTER_EXTENSION=ON
+        BUILD_WEBRTC_FROM_SOURCE=ON
     else
         echo "Jupyter extension disabled in Python wheel."
         BUILD_JUPYTER_EXTENSION=OFF
+        BUILD_WEBRTC_FROM_SOURCE=OFF
     fi
     set -u
 
@@ -184,6 +193,7 @@ build_pip_package() {
         "-DBUILD_PYTORCH_OPS=$BUILD_PYTORCH_OPS"
         "-DBUILD_FILAMENT_FROM_SOURCE=$BUILD_FILAMENT_FROM_SOURCE"
         "-DBUILD_JUPYTER_EXTENSION=$BUILD_JUPYTER_EXTENSION"
+        "-DBUILD_WEBRTC=$BUILD_WEBRTC_FROM_SOURCE"
         "-DCMAKE_INSTALL_PREFIX=$OPEN3D_INSTALL_DIR"
         "-DCMAKE_BUILD_TYPE=Release"
         "-DBUILD_UNIT_TESTS=OFF"
