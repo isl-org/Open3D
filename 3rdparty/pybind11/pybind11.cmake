6c4934eca134f2ed8dcdb191a5e83982ef81aefd
Tim Ohliger <ohliger.tim@gmail.com>
2025-07-16 11:12:46 +0200

    Use pybind11 v3.0.0 final release

--- 3rdparty/pybind11/pybind11.cmake
+++ 3rdparty/pybind11/pybind11.cmake
@@ -3,8 +3,8 @@ include(FetchContent)
 FetchContent_Declare(
     ext_pybind11
     PREFIX pybind11
-    URL https://github.com/pybind/pybind11/archive/refs/tags/v3.0.0rc4.tar.gz
-    URL_HASH SHA256=c6d5d337697d7390d268ded56235fdfb925614be4139c2fb4671a7c2e8828c4b
+    URL https://github.com/pybind/pybind11/archive/refs/tags/v3.0.0.tar.gz
+    URL_HASH SHA256=453b1a3e2b266c3ae9da872411cadb6d693ac18063bd73226d96cfb7015a200c
     DOWNLOAD_DIR "${OPEN3D_THIRD_PARTY_DOWNLOAD_DIR}/pybind11"
 )
 
