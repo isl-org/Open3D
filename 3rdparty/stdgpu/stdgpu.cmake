4ed1328c0793b7279e8df30b88f7379d2ef56fee
johnnynunez <johnnynuca14@gmail.com>
2025-12-16 15:15:37 +0100

    fix

--- 3rdparty/stdgpu/stdgpu.cmake
+++ 3rdparty/stdgpu/stdgpu.cmake
@@ -4,12 +4,22 @@
 
 include(ExternalProject)
 
+find_package(Git QUIET REQUIRED)
+
 # In CUDA 13.0+, Thrust moved from include/thrust/ to include/cccl/thrust/
 # Set the appropriate include directory based on CUDA version
 if(CUDAToolkit_VERSION VERSION_GREATER_EQUAL "13.0")
     set(THRUST_INCLUDE_DIR_PATH "${CUDAToolkit_LIBRARY_ROOT}/include/cccl")
+    # In CUDA 13.0+, Thrust renamed is_proxy_reference to is_wrapped_reference
+    # Initialize git repository and apply patch for Thrust API compatibility
+    set(STDGPU_PATCH_COMMAND
+        COMMAND ${CMAKE_COMMAND} -E chdir <SOURCE_DIR> ${GIT_EXECUTABLE} init
+        COMMAND ${CMAKE_COMMAND} -E chdir <SOURCE_DIR> ${GIT_EXECUTABLE} add -A
+        COMMAND ${CMAKE_COMMAND} -E chdir <SOURCE_DIR> ${GIT_EXECUTABLE} apply --ignore-space-change --ignore-whitespace ${CMAKE_CURRENT_LIST_DIR}/fix-thrust-is-proxy-reference.patch
+    )
 else()
     set(THRUST_INCLUDE_DIR_PATH "${CUDAToolkit_LIBRARY_ROOT}/include")
+    set(STDGPU_PATCH_COMMAND "")
 endif()
 
 ExternalProject_Add(
@@ -19,6 +29,7 @@ ExternalProject_Add(
     URL_HASH SHA256=85b38dc3807ac24b5afe8d06a674dbff8e66579dfd67cbb33189783b0a84a0aa
     DOWNLOAD_DIR "${OPEN3D_THIRD_PARTY_DOWNLOAD_DIR}/stdgpu"
     UPDATE_COMMAND ""
+    PATCH_COMMAND ${STDGPU_PATCH_COMMAND}
     CMAKE_ARGS
         -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
         -DCUDAToolkit_ROOT=${CUDAToolkit_LIBRARY_ROOT}
