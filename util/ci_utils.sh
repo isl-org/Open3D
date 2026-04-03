490051f3652601458930860e0e1d51a03780926e
johnnynunez <johnnynuca14@gmail.com>
2026-04-03 18:40:19 +0200

    revert 12.6

--- util/ci_utils.sh
+++ util/ci_utils.sh
@@ -28,7 +28,7 @@ BUILD_SYCL_MODULE=${BUILD_SYCL_MODULE:-OFF}
 # CUDA: see docker/docker_build.sh
 # ML
 TENSORFLOW_VER="2.20.0"
-TORCH_VER="2.12"
+TORCH_VER="2.10"
 TORCH_REPO_URL="https://download.pytorch.org/whl/torch/"
 TORCH_NIGHTLY_INDEX_URL="https://download.pytorch.org/whl/nightly"
 # Python
