4824f519b2ceb38524d251449038f6341fc32e4e
johnnynunez <johnnynuca14@gmail.com>
2026-04-03 18:39:16 +0200

    revert 12.6

--- docker/docker_build.sh
+++ docker/docker_build.sh
@@ -83,8 +83,8 @@ HOST_OPEN3D_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")"/.. >/dev/null 2>&1 && pw
 AARCH="$(uname -m)"
 # do cmake pending on the architecture
 CMAKE_VERSION=cmake-3.31.10-linux-${AARCH}
-CUDA_VERSION=13.2.0-cudnn
-CUDA_VERSION_LATEST=13.2.0-cudnn
+CUDA_VERSION=12.6.0-cudnn
+CUDA_VERSION_LATEST=12.6.0-cudnn
 
 print_usage_and_exit_docker_build() {
     echo "$__usage_docker_build"
