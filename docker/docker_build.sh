58985ea75710b26593c778f59a2cb9973d5d4338
johnnynunez <johnnynuca14@gmail.com>
2026-05-04 09:00:38 +0200

    downgrade to 13.2.1 (waiting for cuda 13.2.2)

--- docker/docker_build.sh
+++ docker/docker_build.sh
@@ -82,8 +82,8 @@ HOST_OPEN3D_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")"/.. >/dev/null 2>&1 && pw
 AARCH="$(uname -m)"
 # do cmake pending on the architecture
 CMAKE_VERSION=cmake-3.31.8-linux-${AARCH}
-CUDA_VERSION=13.2.2-cudnn
-CUDA_VERSION_LATEST=13.2.2-cudnn
+CUDA_VERSION=13.2.1-cudnn
+CUDA_VERSION_LATEST=13.2.1-cudnn
 
 print_usage_and_exit_docker_build() {
     echo "$__usage_docker_build"
