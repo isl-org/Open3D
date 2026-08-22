6a74f7e706381a89e893ea01bbb1e9148d19ae8f
Sameer Sheorey <sameer.sheorey@intel.com>
2026-08-21 23:42:46 -0700

    Close TensorBoard writer in PyTorch test

--- python/test/visualization/test_tensorboard_plugin.py
+++ python/test/visualization/test_tensorboard_plugin.py
@@ -322,6 +322,7 @@ def test_pytorch_summary(geometry_data, tmp_path):
     # in the same Python process, since it's usually buffered by GFile / Python
     # / OS and written to disk in increments of the filesystem blocksize.
     # Complete write is guaranteed after Python has exited.
+    writer.close()
     summary._async_data_writer.close()
     shutil.rmtree(logdir)
 
