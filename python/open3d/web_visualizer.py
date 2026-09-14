cdf1d4d0be96cdb6e7334d8413d907896d063644
Sameer Sheorey <sameer.sheorey@intel.com>
2026-09-13 22:24:32 -0700

    web_visualizer.draw() now forwards args to draw()

--- python/open3d/web_visualizer.py
+++ python/open3d/web_visualizer.py
@@ -104,48 +104,14 @@ class WebVisualizer(ipywidgets.DOMWidget):
             self.pyjs_channel = json.dumps(self.result_map)
 
 
-def draw(geometry=None,
-         title="Open3D",
-         width=640,
-         height=480,
-         actions=None,
-         lookat=None,
-         eye=None,
-         up=None,
-         field_of_view=60.0,
-         bg_color=(1.0, 1.0, 1.0, 1.0),
-         bg_image=None,
-         show_ui=None,
-         point_size=None,
-         animation_time_step=1.0,
-         animation_duration=None,
-         rpc_interface=False,
-         on_init=None,
-         on_animation_frame=None,
-         on_animation_tick=None):
-    """Draw in Jupyter Cell"""
+def draw(*args, **kwargs):
+    """Draw in Jupyter Cell.
 
+    See :func:`open3d.visualization.draw` for the available arguments.
+    """
+
+    kwargs["non_blocking_and_return_uid"] = True
     window_uid = async_event_loop.run_sync(
-        functools.partial(o3d.visualization.draw,
-                          geometry=geometry,
-                          title=title,
-                          width=width,
-                          height=height,
-                          actions=actions,
-                          lookat=lookat,
-                          eye=eye,
-                          up=up,
-                          field_of_view=field_of_view,
-                          bg_color=bg_color,
-                          bg_image=bg_image,
-                          show_ui=show_ui,
-                          point_size=point_size,
-                          animation_time_step=animation_time_step,
-                          animation_duration=animation_duration,
-                          rpc_interface=rpc_interface,
-                          on_init=on_init,
-                          on_animation_frame=on_animation_frame,
-                          on_animation_tick=on_animation_tick,
-                          non_blocking_and_return_uid=True))
+        functools.partial(o3d.visualization.draw, *args, **kwargs))
     visualizer = WebVisualizer(window_uid=window_uid)
     visualizer.show()
